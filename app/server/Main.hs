24dd23fa7bc47ec585b334aa9adc24bcb2064ae3
Johannes Lund <johannes.lund@iohk.io>
2019-03-04 20:00:59 +0100

    try out cmdargs

--- app/server/Main.hs
+++ app/server/Main.hs
@@ -1,8 +1,43 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
 import Cardano.Wallet
     ( sayHello )
+import Data.Data
 import Prelude
+import System.Console.CmdArgs
+
+data Network = Mainnet | Testnet
+    deriving (Data, Typeable, Show, Eq)
+
+-- | Record for storing CLI parameters.
+--
+-- Naming conventions according to
+-- https://hackage.haskell.org/package/cmdargs-0.4
+data WalletServer = WalletServer
+    { network
+        :: Network
+    , nodePort
+        :: Int
+    , walletServerPort
+        :: Int
+    } deriving (Show, Data, Typeable)
+
+walletServer :: WalletServer
+walletServer = WalletServer
+    { network = enum
+        [ Mainnet &= help "mainnet"
+        , Testnet &= help "testnet"
+        ]
+    , nodePort =
+         8080 &= help "port used for node-wallet communication"
+    , walletServerPort =
+         8090 &= help "port used for serving the wallet API"
+    } &= summary "Cardano Wallet vX.X.X"
+
 
 main :: IO ()
-main = sayHello
+main = do
+    sayHello
+    print =<< cmdArgs walletServer
