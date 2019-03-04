bce066524880a70685f95d42a9c509e12a06b8f1
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-03-04 11:08:06 +1000

    Add stack/nix integration
    Relates to #4

---
+++ nix/stack-shell.nix
@@ -0,0 +1,20 @@
+# This is the derivation used by "stack --nix".
+# It provides the system dependencies required for a stack build.
+{ system ? builtins.currentSystem
+, config ? {}
+, walletPackages ? import ./.. { inherit config system; }
+, pkgs ? walletPackages.pkgs
+}:
+with pkgs;
+
+haskell.lib.buildStackProject {
+  name = "cardano-wallet-stack-env";
+  ghc = haskell.compiler.ghc863;
+
+  buildInputs = [
+    zlib gmp ncurses lzma openssl git
+  ] ++ (lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [ Cocoa CoreServices libcxx libiconv ]));
+
+  phases = ["nobuildPhase"];
+  nobuildPhase = "mkdir -p $out";
+}
