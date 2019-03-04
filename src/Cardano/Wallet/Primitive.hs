77a7f58d4a5c4746e60003e7c53b03b0d9198eeb
KtorZ <matthias.benkort@gmail.com>
2019-03-04 15:54:47 +0100

    remove temporary 'Cardano.Wallet' placeholder file

--- src/Cardano/Wallet/Primitive.hs
+++ src/Cardano/Wallet/Primitive.hs
@@ -4,6 +4,13 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
 
+{-
+    This module contains the core primitive of a Wallet. This is roughly a
+    Haskell translation of the 'Formal Specification for a Cardano Wallet'.
+
+    It doesn't contain any particular business-logic code, but define a few
+    primitive operations on Wallet core types as well.
+-}
 module Cardano.Wallet.Primitive
     (
     -- * Block
@@ -23,6 +30,10 @@ module Cardano.Wallet.Primitive
 
     -- * UTxO
     , UTxO (..)
+    , excluding
+    , isSubsetOf
+    , restrictedBy
+    , restrictedTo
     , Dom(..)
 
     -- * Generic
@@ -47,6 +58,7 @@ import GHC.TypeLits
     ( Symbol )
 
 import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
 
 
 -- * Block
@@ -125,40 +137,84 @@ instance NFData Address
 -- | Coins are stored as Lovelace (reminder: 1 Lovelace = 1e6 ADA)
 newtype Coin = Coin
     { getCoin :: Word64
-    } deriving (Show, Ord, Eq, Generic)
+    } deriving stock (Show, Ord, Eq, Generic)
+      deriving newtype (Enum, Num, Real, Integral)
 
 instance NFData Coin
 
+instance Bounded Coin where
+    minBound = Coin 0
+    maxBound = Coin 45000000000000000
+
 instance Semigroup Coin where
-  (Coin a) <> (Coin b) = Coin (a + b)
+    (Coin a) <> (Coin b) =
+        invariant
+            ( mconcat
+                [ "Cardano.Wallet.Primitive.Coin (<>), sum out of bounds: "
+                , show a
+                , " + "
+                , show b
+                ]
+            )
+            (Coin (a + b))
+            (<= maxBound)
 
 instance Monoid Coin where
-  mempty  = Coin 0
-  mconcat = foldr (<>) mempty
+    mempty  = minBound
+    mconcat = foldr (<>) mempty
 
 
 -- * UTxO
 
 newtype UTxO = UTxO { getUTxO :: Map TxIn TxOut }
-  deriving stock (Show, Generic, Eq, Ord)
-  deriving newtype (Semigroup, Monoid)
+    deriving stock (Show, Generic, Eq, Ord)
+    deriving newtype (Semigroup, Monoid)
 
 instance NFData UTxO
 
 instance Dom UTxO where
-  type DomElem UTxO = TxIn
-  dom (UTxO utxo) = Map.keysSet utxo
+    type DomElem UTxO = TxIn
+    dom (UTxO utxo) = Map.keysSet utxo
+
+-- ins⋪ u
+excluding :: UTxO -> Set TxIn ->  UTxO
+excluding (UTxO utxo) =
+    UTxO . Map.withoutKeys utxo
+
+-- a ⊆ b
+isSubsetOf :: UTxO -> UTxO -> Bool
+isSubsetOf (UTxO a) (UTxO b) =
+    a `Map.isSubmapOf` b
+
+-- ins⊲ u
+restrictedBy :: UTxO -> Set TxIn -> UTxO
+restrictedBy (UTxO utxo) =
+    UTxO . Map.restrictKeys utxo
+
+-- u ⊳ outs
+restrictedTo :: UTxO -> Set TxOut ->  UTxO
+restrictedTo (UTxO utxo) outs =
+    UTxO $ Map.filter (`Set.member` outs) utxo
 
 
 class Dom a where
-  type DomElem a :: *
-  dom :: a -> Set (DomElem a)
+    type DomElem a :: *
+    dom :: a -> Set (DomElem a)
 
 
--- * Generic Wrappers
+-- * Helpers
 
 newtype Hash (tag :: Symbol) = Hash
     { getHash :: ByteString
     } deriving (Show, Generic, Eq, Ord)
 
 instance NFData (Hash tag)
+
+
+invariant
+    :: String
+    -> a
+    -> (a -> Bool)
+    -> a
+invariant msg a predicate =
+    if predicate a then a else error msg
