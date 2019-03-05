bc9da3bcd194bc42c39e13eefd6d0cf7660ab73e
Pawel Jakubas <pawel.jakubas@iohk.io>
2019-03-05 10:05:48 +0100

    [3] Adding bracket
    [3] Adding ticking function test and downloading block logic
    
    [3] Block syncer working ok plus additional tests
    
    [3] Cleaning the code
    
    [3] cabal fix
    
    [3] Complete refactoring to the review
    
    [3] Final refactoring
    
    [3] cabal fix
    
    [3] hlint and weeding
    
    [3] add killing thread at the end of test
    
    [3] Replace IORef with MVar

---
+++ src/Cardano/Wallet/BlockSyncer.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- |
+-- Copyright: © 2018-2019 IOHK
+-- License: MIT
+--
+-- This module contains the ticking function that is responsible for invoking block acquisition functionality and executing it in periodic fashion.
+--
+-- Known limitations: ticking function makes sure action is not executed on already consumed block, but does not check and handle block gaps (aka catching up)
+
+module Cardano.Wallet.BlockSyncer
+  (
+    BlockHeadersConsumed(..)
+  , tickingFunction
+  ) where
+
+
+import Prelude
+
+import Cardano.Wallet.Primitive
+    ( Block (..), BlockHeader )
+import Control.Concurrent
+    ( threadDelay )
+import Data.Time.Units
+    ( Second, toMicroseconds )
+
+import qualified Data.List as L
+
+
+newtype BlockHeadersConsumed = BlockHeadersConsumed [BlockHeader] deriving (Show, Eq)
+
+storingLimit :: Int
+storingLimit = 2160
+
+tickingFunction
+    :: IO [Block]
+    -- ^ a way to get a new block
+    -> (Block -> IO ())
+    -- ^ action taken on a new block
+    -> Second
+    -- ^ tick time
+    -> BlockHeadersConsumed
+    -> IO ()
+tickingFunction getNextBlocks action tickTime = go
+    where
+      go
+          :: BlockHeadersConsumed
+          -> IO ()
+      go (BlockHeadersConsumed headersConsumed) = do
+          blocksDownloaded <- getNextBlocks
+          let blocksToProcess =
+                  filter (checkIfAlreadyConsumed headersConsumed) blocksDownloaded
+          mapM_ action blocksToProcess
+          threadDelay $ (fromIntegral . toMicroseconds) tickTime
+          go $ BlockHeadersConsumed
+              $ take storingLimit
+              $ map header blocksToProcess ++ headersConsumed
+
+      checkIfAlreadyConsumed
+          :: [BlockHeader]
+          -> Block
+          -> Bool
+      checkIfAlreadyConsumed consumedHeaders (Block theHeader _) =
+          theHeader `L.notElem` consumedHeaders
---
+++ test/unit/Cardano/Wallet/BlockSyncerSpec.hs
@@ -0,0 +1,177 @@
+{-# LANGUAGE DataKinds #-}
+
+module Cardano.Wallet.BlockSyncerSpec
+    ( spec
+    ) where
+
+
+import Prelude
+
+import Cardano.Wallet.BlockSyncer
+    ( BlockHeadersConsumed (..), tickingFunction )
+import Cardano.Wallet.Primitive
+    ( Block (..), BlockHeader (..), Hash (..) )
+import Control.Concurrent
+    ( ThreadId, forkIO, killThread, threadDelay )
+import Control.Concurrent.MVar
+    ( MVar, newEmptyMVar, putMVar, takeMVar )
+import Control.Monad
+    ( forM_ )
+import Control.Monad.IO.Class
+    ( liftIO )
+import Data.ByteString
+    ( ByteString, pack )
+import Data.Time.Units
+    ( Second, fromMicroseconds )
+import Test.Hspec
+    ( Arg, Spec, SpecWith, describe, it )
+import Test.Hspec.Expectations
+    ( shouldBe )
+import Test.QuickCheck
+    ( generate, vector )
+import Test.QuickCheck.Gen
+    ( Gen, choose, vectorOf )
+
+import qualified Data.List as L
+import qualified Data.Set as Set
+
+spec :: Spec
+spec = do
+    describe "Block syncer downloads blocks properly" $ do
+        let tests =
+                [
+                    ("check ticking function when blocks are sent in exactly-once fashion"
+                    , ExactlyOnce)
+                ,
+                    ("check ticking function when blocks are sent in at-least-once fashion"
+                    , AtLeastOnce)
+                ]
+        forM_ ((L.take 10 . L.cycle) tests) $ \pair ->
+            uncurry tickingFunctionTest pair
+        where
+            tickingFunctionTest
+                :: String
+                -> DeliveryMode
+                -> SpecWith (Arg (IO ()))
+            tickingFunctionTest testDescr deliveryMode = it testDescr $ do
+                (chunkSizesToTest, testTime, tickTime) <- generateQuantities
+                consecutiveBlocks <- liftIO $ mkConsecutiveTestBlocks (sum chunkSizesToTest)
+                (consumerData, threadId) <- injectToTickingFunction chunkSizesToTest tickTime consecutiveBlocks deliveryMode
+                threadDelay testTime
+                (BlocksConsumed obtainedData) <- takeMVar consumerData
+                killThread threadId
+                obtainedData `shouldBe` ((map fst . reverse) consecutiveBlocks)
+
+            generateQuantities :: IO ([Int], Int, Second)
+            generateQuantities = do
+                chunkSizesToTest <- generateBlockChunkSizes
+                tickingFunctionTime <- generate $ choose (1,3)
+                putStrLn $
+                    "        block chunks injected :"
+                    ++ show chunkSizesToTest
+                    ++ " ticking time: "
+                    ++ show tickingFunctionTime
+                    ++ " seconds"
+                let testTime = (L.length chunkSizesToTest + 1)*(fromIntegral tickingFunctionTime)*1000*1000
+                let tickTime = (fromMicroseconds tickingFunctionTime*1000*1000)
+                return (chunkSizesToTest, testTime, tickTime)
+
+            injectToTickingFunction
+                :: [Int]
+                -> Second
+                -> [(Hash "BlockHeader", Block)]
+                -> DeliveryMode
+                -> IO (MVar BlocksConsumed, ThreadId)
+            injectToTickingFunction chunkSizesToTest tickTime consecutiveBlocks deliveryMode = do
+                consumerData <- newEmptyMVar
+                putMVar consumerData $ BlocksConsumed []
+                producerData <- newEmptyMVar
+                putMVar producerData $ BlocksToInject (chunkSizesToTest,consecutiveBlocks)
+                blockToIOaction <- writeToIORefAction consumerData consecutiveBlocks
+                let blockDelivery = pushNextBlocks producerData deliveryMode
+                threadId <- forkIO $ tickingFunction blockDelivery blockToIOaction tickTime (BlockHeadersConsumed [])
+                return (consumerData, threadId)
+
+mkConsecutiveTestBlocks
+    :: Int
+    -- ^ number of consecutive blocks to create
+    -> IO [((Hash "BlockHeader"),Block)]
+    -- ^ returns block paired with generated hashes starting from the oldest
+mkConsecutiveTestBlocks blockNum =
+    loop blockNum []
+    where
+        loop
+            :: Int
+            -> [((Hash "BlockHeader"),Block)]
+            -> IO [((Hash "BlockHeader"),Block)]
+        loop blockNumToGo res = do
+            let bytelistGenerator = pack <$> vector 10 :: Gen ByteString
+            if blockNumToGo <= 0 then
+                return $ reverse res
+            else
+               case res of
+                   [] -> do
+                       lastBlockHeader <- Hash <$> generate bytelistGenerator
+                       theBlockHeader <- Hash <$> generate bytelistGenerator
+                       let theEpoch = 0
+                       let theSlot = 1
+                       let theBlock = Block (BlockHeader theEpoch theSlot lastBlockHeader) Set.empty
+                       loop (blockNumToGo - 1) [(theBlockHeader, theBlock)]
+                   (lastBlockHeader, Block (BlockHeader lastEpoch lastSlot _) _ ):_ -> do
+                       let theBlock = Block (BlockHeader lastEpoch (lastSlot + 1) lastBlockHeader) Set.empty
+                       theBlockHeader <- Hash <$> generate bytelistGenerator
+                       loop (blockNumToGo - 1) $ (theBlockHeader, theBlock):res
+
+
+newtype BlocksConsumed = BlocksConsumed [(Hash "BlockHeader")] deriving (Show, Eq)
+
+newtype BlocksToInject = BlocksToInject ([Int],[((Hash "BlockHeader"),Block)]) deriving (Show, Eq)
+
+
+data DeliveryMode = ExactlyOnce | AtLeastOnce
+
+pushNextBlocks
+    :: MVar BlocksToInject
+    -> DeliveryMode
+    -> IO [Block]
+pushNextBlocks ref mode = do
+    (BlocksToInject (blocksToTake, blocksRemaining)) <- takeMVar ref
+    if L.null blocksRemaining then return [] else
+        (do case blocksToTake of
+                num : rest -> do
+                    let (bOut, bStay) = L.splitAt num blocksRemaining
+                    putMVar ref $ BlocksToInject (rest, bStay)
+                    case mode of
+                        ExactlyOnce ->
+                            return $ map snd bOut
+                        AtLeastOnce -> do
+                            additionalBlocks <- generate $ choose (1,3) :: IO Int
+                            return $ map snd (bOut ++ take additionalBlocks bStay)
+                [] -> return [])
+
+
+writeToIORefAction
+    :: MVar BlocksConsumed
+    -> [((Hash "BlockHeader"),Block)]
+    -> IO (Block -> IO ())
+writeToIORefAction ref blocks = return $
+    \block -> do
+        (BlocksConsumed headerHashesConsumed) <- takeMVar ref
+        case (map fst . filter (\(_,b) -> b == block) ) blocks of
+            [blockHeader] -> do
+                putMVar ref $ BlocksConsumed (blockHeader : headerHashesConsumed)
+            _ ->
+                return ()
+
+
+generateBlockChunkSizes :: IO [Int]
+generateBlockChunkSizes = do
+    numberOfTicks <- generate $ choose (1,15)
+    generate $ generateBlockChunks numberOfTicks
+        where
+            generateBlockChunks
+                :: Int
+                -> Gen [Int]
+            generateBlockChunks numberOfTicks = do
+                let chunkSizeGen = choose (0,15)
+                vectorOf numberOfTicks chunkSizeGen
