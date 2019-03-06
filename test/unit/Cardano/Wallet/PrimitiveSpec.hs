b7a3a385ed76a00f6bb01de6b2a1f5cb9f2c2f25
Jonathan Knowles <jonathan.knowles@iohk.io>
2019-03-06 09:14:23 +0000

    Wrap code in `PrimitiveSpec` to comply with code width rule.

--- test/unit/Cardano/Wallet/PrimitiveSpec.hs
+++ test/unit/Cardano/Wallet/PrimitiveSpec.hs
@@ -45,15 +45,24 @@ import qualified Data.Set as Set
 spec :: Spec
 spec = do
     describe "Lemma 2.1 - Properties of UTxO operations" $ do
-        it "2.1.1) ins⊲ u ⊆ u" (checkCoverage prop_2_1_1)
-        it "2.1.2) ins⋪ u ⊆ u" (checkCoverage prop_2_1_2)
-        it "2.1.3) u ⊳ outs ⊆ u" (checkCoverage prop_2_1_3)
-        it "2.1.4) ins⊲ (u ⋃ v) = (ins⊲ u) ⋃ (ins⊲ v)" (checkCoverage prop_2_1_4)
-        it "2.1.5) ins⋪ (u ⋃ v) = (ins⋪ u) ⋃ (ins⋪ v)" (checkCoverage prop_2_1_5)
-        it "2.1.6) (dom u ⋂ ins) ⊲ u = ins⊲ u" (checkCoverage prop_2_1_6)
-        it "2.1.7) (dom u ⋂ ins) ⋪ u = ins⋪ u" (checkCoverage prop_2_1_7)
-        it "2.1.8) (dom u ⋃ ins) ⋪ (u ⋃ v) = (ins ⋃ dom u) ⋪ v" $ checkCoverage prop_2_1_8
-        it "2.1.9) ins⋪ u = (dom u \\ ins)⊲ u" (checkCoverage prop_2_1_9)
+        it "2.1.1) ins⊲ u ⊆ u" $
+            checkCoverage prop_2_1_1
+        it "2.1.2) ins⋪ u ⊆ u" $
+            checkCoverage prop_2_1_2
+        it "2.1.3) u ⊳ outs ⊆ u" $
+            checkCoverage prop_2_1_3
+        it "2.1.4) ins⊲ (u ⋃ v) = (ins⊲ u) ⋃ (ins⊲ v)" $
+            checkCoverage prop_2_1_4
+        it "2.1.5) ins⋪ (u ⋃ v) = (ins⋪ u) ⋃ (ins⋪ v)" $
+            checkCoverage prop_2_1_5
+        it "2.1.6) (dom u ⋂ ins) ⊲ u = ins⊲ u" $
+            checkCoverage prop_2_1_6
+        it "2.1.7) (dom u ⋂ ins) ⋪ u = ins⋪ u" $
+            checkCoverage prop_2_1_7
+        it "2.1.8) (dom u ⋃ ins) ⋪ (u ⋃ v) = (ins ⋃ dom u) ⋪ v" $
+            checkCoverage prop_2_1_8
+        it "2.1.9) ins⋪ u = (dom u \\ ins)⊲ u" $
+            checkCoverage prop_2_1_9
 
 {-------------------------------------------------------------------------------
        Wallet Specification - Lemma 2.1 - Properties of UTxO operations
@@ -77,7 +86,8 @@ prop_2_1_3 :: (Set TxOut, UTxO) -> Property
 prop_2_1_3 (outs, u) =
     cover 50 cond "u ⋂ outs ≠ ∅" (property prop)
   where
-    cond = not $ Set.null $ Set.fromList (Map.elems (getUTxO u)) `Set.intersection` outs
+    cond = not $ Set.null $
+        Set.fromList (Map.elems (getUTxO u)) `Set.intersection` outs
     prop = (u `restrictedTo` outs) `isSubsetOf` u
 
 prop_2_1_4 :: (Set TxIn, UTxO, UTxO) -> Property
@@ -105,14 +115,20 @@ prop_2_1_6 (ins, u) =
     cover 50 cond "dom u ⋂ ins ≠ ∅" (property prop)
   where
     cond = not $ Set.null $ dom u `Set.intersection` ins
-    prop = (u `restrictedBy` (dom u `Set.intersection` ins)) === (u `restrictedBy` ins)
+    prop =
+        (u `restrictedBy` (dom u `Set.intersection` ins))
+            ===
+        (u `restrictedBy` ins)
 
 prop_2_1_7 :: (Set TxIn, UTxO) -> Property
 prop_2_1_7 (ins, u) =
     cover 50 cond "dom u ⋂ ins ≠ ∅" (property prop)
   where
     cond = not $ Set.null $ dom u `Set.intersection` ins
-    prop = (u `excluding` (dom u `Set.intersection` ins)) === (u `excluding` ins)
+    prop =
+        (u `excluding` (dom u `Set.intersection` ins))
+            ===
+        (u `excluding` ins)
 
 prop_2_1_8 :: (Set TxIn, UTxO, UTxO) -> Property
 prop_2_1_8 (ins, u, v) =
