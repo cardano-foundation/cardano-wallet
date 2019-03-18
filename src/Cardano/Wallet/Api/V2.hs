9b21df26324fec6d56794fb6df4ce00c2677953c
Jonathan Knowles <jonathan.knowles@iohk.io>
2019-03-18 04:04:48 +0000

    Change imports to qualified.

--- src/Cardano/Wallet/Api/V2.hs
+++ src/Cardano/Wallet/Api/V2.hs
@@ -3,12 +3,13 @@
 
 module Cardano.Wallet.Api.V2 where
 
-import Cardano.Wallet.Api.V2.Types.Wallet
-import Cardano.Wallet.Api.V2.Types.WalletId
 import Data.Proxy
     ( Proxy (..) )
 import Servant.API
     ( (:>), (:<|>), Capture, Delete, Get, JSON )
+
+import qualified Cardano.Wallet.Api.V2.Types.Wallet as T
+import qualified Cardano.Wallet.Api.V2.Types.WalletId as T
 
 type Api = DeleteWallet :<|> GetWallet :<|> ListWallets
 
@@ -17,14 +18,14 @@ api = Proxy
 
 type DeleteWallet = "wallets"
     :> "deleteWallet"
-    :> Capture "walletId" WalletId
+    :> Capture "walletId" T.WalletId
     :> Delete '[] ()
 
 type GetWallet = "wallets"
     :> "getWallet"
-    :> Capture "walletId" WalletId
-    :> Get '[JSON] Wallet
+    :> Capture "walletId" T.WalletId
+    :> Get '[JSON] T.Wallet
 
 type ListWallets = "wallets"
     :> "listWallets"
-    :> Get '[JSON] [Wallet]
+    :> Get '[JSON] [T.Wallet]
