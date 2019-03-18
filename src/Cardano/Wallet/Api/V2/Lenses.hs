d9d1adcd3c6976bdccd9e4c0d59c44281a6dca81
Jonathan Knowles <jonathan.knowles@iohk.io>
2019-03-18 06:49:35 +0000

    Satisfy import rules in module `Api.V2.Lenses`.

--- src/Cardano/Wallet/Api/V2/Lenses.hs
+++ src/Cardano/Wallet/Api/V2/Lenses.hs
@@ -5,21 +5,21 @@
 
 module Cardano.Wallet.Api.V2.Lenses where
 
-import Cardano.Wallet.Api.V2.Types.Amount
-import Cardano.Wallet.Api.V2.Types.Wallet
-import Cardano.Wallet.Api.V2.Types.WalletBalance
-import Cardano.Wallet.Api.V2.Types.WalletDelegation
-import Cardano.Wallet.Api.V2.Types.WalletId
-import Cardano.Wallet.Api.V2.Types.WalletPassphraseInfo
-import Cardano.Wallet.Api.V2.Types.WalletState
+import qualified Cardano.Wallet.Api.V2.Types.Amount as T
+import qualified Cardano.Wallet.Api.V2.Types.Wallet as T
+import qualified Cardano.Wallet.Api.V2.Types.WalletBalance as T
+import qualified Cardano.Wallet.Api.V2.Types.WalletDelegation as T
+import qualified Cardano.Wallet.Api.V2.Types.WalletId as T
+import qualified Cardano.Wallet.Api.V2.Types.WalletPassphraseInfo as T
+import qualified Cardano.Wallet.Api.V2.Types.WalletState as T
 
 import Control.Lens
     ( makeFieldsNoPrefix )
 
-makeFieldsNoPrefix ''Amount
-makeFieldsNoPrefix ''Wallet
-makeFieldsNoPrefix ''WalletBalance
-makeFieldsNoPrefix ''WalletDelegation
-makeFieldsNoPrefix ''WalletId
-makeFieldsNoPrefix ''WalletPassphraseInfo
-makeFieldsNoPrefix ''WalletState
+makeFieldsNoPrefix ''T.Amount
+makeFieldsNoPrefix ''T.Wallet
+makeFieldsNoPrefix ''T.WalletBalance
+makeFieldsNoPrefix ''T.WalletDelegation
+makeFieldsNoPrefix ''T.WalletId
+makeFieldsNoPrefix ''T.WalletPassphraseInfo
+makeFieldsNoPrefix ''T.WalletState
