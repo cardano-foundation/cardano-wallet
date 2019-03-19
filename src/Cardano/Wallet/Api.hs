c801f31972faffa32adfd1b54969e5a2c4150608
Jonathan Knowles <jonathan.knowles@iohk.io>
2019-03-19 08:24:19 +0000

    Use `NoContent` for `DeleteWallet` endpoint.

--- src/Cardano/Wallet/Api.hs
+++ src/Cardano/Wallet/Api.hs
@@ -6,7 +6,7 @@ module Cardano.Wallet.Api where
 import Data.Proxy
     ( Proxy (..) )
 import Servant.API
-    ( (:<|>), (:>), Capture, Delete, Get, JSON )
+    ( (:<|>), (:>), Capture, Delete, Get, JSON, NoContent )
 
 import qualified Cardano.Wallet.Api.Types.Wallet as T
 import qualified Cardano.Wallet.Api.Types.WalletId as T
@@ -18,7 +18,7 @@ api = Proxy
 
 type DeleteWallet = "wallets"
     :> Capture "walletId" T.WalletId
-    :> Delete '[] ()
+    :> Delete '[JSON] NoContent
 
 type GetWallet = "wallets"
     :> Capture "walletId" T.WalletId
