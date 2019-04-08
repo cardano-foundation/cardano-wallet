0ec9a442a9a566f3b142d28e95dadd01b18430bb
Johannes Lund <johannes.lund@iohk.io>
2019-04-08 12:22:44 +0200

    re-add SignTag and tx hashing

--- src/Cardano/Wallet/Primitive/Signing.hs
+++ src/Cardano/Wallet/Primitive/Signing.hs
@@ -1,15 +1,21 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeApplications #-}
 module Cardano.Wallet.Primitive.Signing where
 
 import Prelude
 
 import Cardano.Wallet.Binary
-    ( TxWitness (..), toByteString )
+    ( TxWitness (..), encodeTx, toByteString )
 import Cardano.Wallet.Primitive.Types
     ( Address, Hash (..), Tx (..), TxIn, TxOut, protocolMagic )
 import Control.Monad
     ( forM )
+import Crypto.Hash
+    ( Blake2b_256, hash )
+import Data.ByteArray
+    ( convert )
 import Data.ByteString
     ( ByteString )
 import GHC.Generics
@@ -61,13 +67,19 @@ mkStdTx seqState rootPrv pass ownedIns outs = do
 
     let ins = (fmap fst ownedIns)
         tx = Tx ins outs
+        txSigData = hashTx tx
 
-    txWitness <- forM ownedIns (\(_in, addr) -> mkWitness <$> keyFrom addr)
+    txWitness <- forM ownedIns (\(_in, addr) -> mkWitness txSigData <$> keyFrom addr)
 
     return (tx, txWitness)
 
   where
-    txSigData = Hash "tx"
+    hashTx :: Tx -> Hash "tx"
+    hashTx tx = Hash
+        $ convert
+        $ (hash @ByteString @Blake2b_256)
+        $ toByteString
+        $ encodeTx tx
 
     keyFrom :: Address -> (Maybe (Key 'AddressK XPrv))
     keyFrom addr = do
@@ -81,11 +93,11 @@ mkStdTx seqState rootPrv pass ownedIns outs = do
 
         return $ deriveAddressPrivateKey pass accountPrv ExternalChain index
 
-    mkWitness :: Key 'AddressK XPrv -> TxWitness
-    mkWitness ss =
+    mkWitness :: Hash "tx" -> Key 'AddressK XPrv -> TxWitness
+    mkWitness txSigData xPrv =
         PublicKeyWitness
-            (encode $ publicKey ss)
-            (Hash $ signRaw signTag ss txSigData)
+            (encode $ publicKey xPrv)
+            (Hash $ signRaw (Just SignTx) xPrv txSigData)
 
     encode :: (Key level XPub) -> ByteString
     encode (Key k) = CC.unXPub k
@@ -109,11 +121,43 @@ mkStdTx seqState rootPrv pass ownedIns outs = do
 --
 -- The wallet only cares about the 'SignTx' tag. In 'cardano-sl' there was
 -- a whole @SignTag@ data-type
-signTag :: ByteString
-signTag = signTxTag <> network
+data SignTag
+    = SignForTestingOnly  -- ^ Anything (to be used for testing only)
+    | SignTx              -- ^ Tx:               @TxSigData@
+    | SignRedeemTx        -- ^ Redeem tx:        @TxSigData@
+    | SignVssCert         -- ^ Vss certificate:  @(VssPublicKey, EpochIndex)@
+    | SignUSProposal      -- ^ Update proposal:  @UpdateProposalToSign@
+    | SignCommitment      -- ^ Commitment:       @(EpochIndex, Commitment)@
+    | SignUSVote          -- ^ US proposal vote: @(UpId, Bool)@
+    | SignMainBlock       -- ^ Main block:       @MainToSign@
+    | SignMainBlockLight
+    | SignMainBlockHeavy
+    | SignProxySK         -- ^ Proxy key:        @ProxySecretKey@
+    deriving (Eq, Ord, Show, Generic)
+
+-- TODO: it would be nice if we couldn't use 'SignTag' with wrong
+-- types. Maybe something with GADTs and data families?
+
+
+
+-- | Get magic bytes corresponding to a 'SignTag'. Guaranteed to be different
+-- (and begin with a different byte) for different tags.
+signTag :: SignTag -> ByteString
+signTag = \case
+    SignForTestingOnly -> "\x00"
+    SignTx             -> "\x01" <> network
+    SignRedeemTx       -> "\x02" <> network
+    SignVssCert        -> "\x03" <> network
+    SignUSProposal     -> "\x04" <> network
+    SignCommitment     -> "\x05" <> network
+    SignUSVote         -> "\x06" <> network
+    SignMainBlock      -> "\x07" <> network
+    SignMainBlockLight -> "\x08" <> network
+    SignMainBlockHeavy -> "\x09" <> network
+    SignProxySK        -> "\x0a" <> network
   where
-    signTxTag = "\x01"
-    network = toByteString $ CBOR.encodeInt32 protocolMagic
+    network = toByteString . CBOR.encodeInt32 $ pm
+    pm = protocolMagic
 
 -- Signatures
 
@@ -122,13 +166,25 @@ signTag = signTxTag <> network
 newtype Signature a = Signature CC.XSignature
     deriving (Eq, Ord, Show, Generic)
 
+
+--
+--
+--
+--
+--
+
+
 -- | Sign a bytestring.
 signRaw
-    :: ByteString
+    :: Maybe SignTag   -- ^ See docs for 'SignTag'. Unlike in 'sign', we
+                       -- allow no tag to be provided just in case you need
+                       -- to sign /exactly/ the bytestring you provided
     -> Key level XPrv
     -> Hash "tx"
     -> ByteString -- Previously Raw
-signRaw tag (Key k) (Hash x) = CC.unXSignature $ CC.sign emptyPassphrase k (tag <> x)
+signRaw mbTag (Key k) (Hash x) = CC.unXSignature $ CC.sign emptyPassphrase k (tag <> x)
   where
+    tag = maybe mempty signTag mbTag
+
     emptyPassphrase :: ByteString
     emptyPassphrase = mempty
