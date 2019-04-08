e0d0e08b85f4eeb20b12cfa14257428ff8bc9e6b
Johannes Lund <johannes.lund@iohk.io>
2019-04-08 17:52:24 +0200

    Scaffold tx creation in WalletLayer

--- src/Cardano/Wallet.hs
+++ src/Cardano/Wallet.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RankNTypes #-}
@@ -16,9 +17,11 @@
 
 module Cardano.Wallet
     (
-    -- * Types
+    -- * Interface
       WalletLayer (..)
     , NewWallet(..)
+
+    -- * Errors
     , ErrNoSuchWallet(..)
     , ErrWalletAlreadyExists(..)
 
@@ -31,6 +34,10 @@ module Cardano.Wallet
 
 import Prelude
 
+import Cardano.Wallet.Binary
+    ( encodeSignedTx, toByteString )
+import Cardano.Wallet.CoinSelection
+    ( CoinSelection (..), CoinSelectionError (..), CoinSelectionOptions )
 import Cardano.Wallet.DB
     ( DBLayer
     , ErrNoSuchWallet (..)
@@ -41,7 +48,10 @@ import Cardano.Wallet.Network
     ( NetworkLayer (..), drain, listen )
 import Cardano.Wallet.Primitive.AddressDerivation
     ( ChangeChain (..)
+    , Depth (RootK)
+    , Key
     , Passphrase
+    , XPrv
     , deriveAccountPrivateKey
     , digest
     , generateKeyFromSeed
@@ -50,10 +60,14 @@ import Cardano.Wallet.Primitive.AddressDerivation
 import Cardano.Wallet.Primitive.AddressDiscovery
     ( AddressPoolGap, SeqState (..), mkAddressPool )
 import Cardano.Wallet.Primitive.Model
-    ( Wallet, applyBlocks, initWallet )
+    ( Wallet, applyBlocks, availableUTxO, getState, initWallet )
 import Cardano.Wallet.Primitive.Types
     ( Block (..)
+    , SignedTx (..)
     , SlotId (..)
+    , Tx (..)
+    , TxOut (..)
+    , TxWitness
     , WalletDelegation (..)
     , WalletId (..)
     , WalletMetadata (..)
@@ -70,16 +84,19 @@ import Control.Monad.Fail
 import Control.Monad.IO.Class
     ( liftIO )
 import Control.Monad.Trans.Except
-    ( ExceptT, runExceptT )
+    ( ExceptT, runExceptT, throwE, withExceptT )
 import Control.Monad.Trans.Maybe
     ( MaybeT (..), maybeToExceptT )
 import Data.Functor
     ( ($>) )
+import Data.List.NonEmpty
+    ( NonEmpty )
 import Data.Time.Clock
     ( getCurrentTime )
 import GHC.Generics
     ( Generic )
 
+import qualified Cardano.Wallet.CoinSelection.Random as CoinSelection
 import qualified Cardano.Wallet.DB as DB
 
 {-------------------------------------------------------------------------------
@@ -89,17 +106,47 @@ import qualified Cardano.Wallet.DB as DB
 data WalletLayer s = WalletLayer
     { createWallet
         :: NewWallet
-        -> ExceptT (ErrWalletAlreadyExists "createWallet") IO WalletId
+        -> ExceptT ErrWalletAlreadyExists IO WalletId
         -- ^ Initialise and store a new wallet, returning its ID.
+
     , readWallet
         :: WalletId
-        -> ExceptT (ErrNoSuchWallet "readWallet") IO (Wallet s, WalletMetadata)
+        -> ExceptT ErrNoSuchWallet IO (Wallet s, WalletMetadata)
         -- ^ Retrieve the wallet state for the wallet with the given ID.
+
+    , createUnsignedTx
+        :: WalletId
+        -> CoinSelectionOptions
+        -> NonEmpty TxOut
+        -> ExceptT ErrCreateUnsignedTx IO CoinSelection
+        -- ^ Prepare a transaction and automatically select inputs from the
+        -- wallet to cover the requested outputs. Note that this only run the
+        -- coin selection for the given outputs. In order to construct (and
+        -- sign) an actual transaction, have a look at 'signTx'.
+
+    , signTx
+        :: WalletId
+        -> Key 'RootK XPrv
+        -> Passphrase "encryption"
+        -> CoinSelection
+        -> ExceptT ErrSignTx IO (Tx, [TxWitness])
+        -- ^ Produce witnesses and construct a transaction from a given
+        -- selection. Requires the encryption passphrase in order to decrypt
+        -- the root private key. Note that this doesn't broadcast the
+        -- transaction to the network. In order to do so, have a look at
+        -- 'submitTx'.
+
+    , submitTx
+        :: (Tx, [TxWitness])
+        -> ExceptT ErrSubmitTx IO ()
+        -- ^ Broadcast a (signed) transaction to the network.
+
     , watchWallet
         :: forall a. WalletId
         -> IO a
         -- ^ Consume blocks from the node as they arrive, and apply them to the
         -- wallet. This function never returns, but may throw an exception.
+
     , processWallet
         :: (SlotId -> IO ())
         -> WalletId
@@ -122,6 +169,19 @@ data NewWallet = NewWallet
         :: !AddressPoolGap
     } deriving (Show, Generic)
 
+-- | Errors occuring when creating an unsigned transaction
+data ErrCreateUnsignedTx
+    = ErrCreateUnsignedTxNoSuchWallet ErrNoSuchWallet
+    | ErrCreateUnsignedTxCoinSelection CoinSelectionError
+
+-- | Errors occuring when signing a transaction
+data ErrSignTx
+    = ErrSignTxNoSuchWallet ErrNoSuchWallet
+    | ErrSignTx
+
+-- | Errors occuring when submitting a signed transaction to the network
+data ErrSubmitTx = forall a. NetworkError a
+
 {-------------------------------------------------------------------------------
                                  Construction
 -------------------------------------------------------------------------------}
@@ -133,6 +193,11 @@ mkWalletLayer
     -> NetworkLayer IO e0 e1
     -> WalletLayer SeqState
 mkWalletLayer db network = WalletLayer
+
+    {---------------------------------------------------------------------------
+                                       Wallets
+    ---------------------------------------------------------------------------}
+
     { createWallet = \w -> do
         let rootXPrv =
                 generateKeyFromSeed (seed w, secondFactor w) (passphrase w)
@@ -157,19 +222,50 @@ mkWalletLayer db network = WalletLayer
                 }
         DB.createWallet db (PrimaryKey wid) checkpoint metadata $> wid
 
-    , readWallet = \wid -> maybeToExceptT (ErrNoSuchWallet wid) $ do
-        cp <- MaybeT $ DB.readCheckpoint db (PrimaryKey wid)
-        meta <- MaybeT $ DB.readWalletMeta db (PrimaryKey wid)
-        return (cp, meta)
+    , readWallet = _readWallet
+
+    {---------------------------------------------------------------------------
+                                    Transactions
+    ---------------------------------------------------------------------------}
+
+    , createUnsignedTx = \wid opts recipients -> do
+        (w, _) <- withExceptT ErrCreateUnsignedTxNoSuchWallet
+            (_readWallet wid)
+        let utxo = availableUTxO w
+        withExceptT ErrCreateUnsignedTxCoinSelection $
+            CoinSelection.random opts utxo recipients
+
+    , submitTx = \(tx, witnesses) -> do
+        let signed = SignedTx $ toByteString $ encodeSignedTx (tx, witnesses)
+        withExceptT NetworkError $ postTx network signed
+
+    , signTx = \wid rootXPrv password (CoinSelection ins outs chgs) -> do
+        (w, _) <- withExceptT ErrSignTxNoSuchWallet $ _readWallet wid
+        maybe
+            (throwE ErrSignTx)
+            return
+            (mkStdTx (getState w) rootXPrv password ins outs chgs)
+
+    {---------------------------------------------------------------------------
+                                  W.I.P. / Debts
+    ---------------------------------------------------------------------------}
 
     , watchWallet =
         liftIO . listen network . onNextblocks
 
     , processWallet = \logInfo wid -> drain network $ \slot blocks -> do
-            logInfo slot
-            onNextblocks wid blocks
+        logInfo slot
+        onNextblocks wid blocks
     }
   where
+    _readWallet
+        :: WalletId
+        -> ExceptT ErrNoSuchWallet IO (Wallet SeqState, WalletMetadata)
+    _readWallet wid = maybeToExceptT (ErrNoSuchWallet wid) $ do
+        cp <- MaybeT $ DB.readCheckpoint db (PrimaryKey wid)
+        meta <- MaybeT $ DB.readWalletMeta db (PrimaryKey wid)
+        return (cp, meta)
+
     onNextblocks :: WalletId -> [Block] -> IO ()
     onNextblocks wid blocks = do
         (txs, cp') <- DB.readCheckpoint db (PrimaryKey wid) >>= \case
@@ -192,6 +288,8 @@ mkWalletLayer db network = WalletLayer
         -- history
         unsafeRunExceptT $ DB.putCheckpoint db (PrimaryKey wid) cp'
         unsafeRunExceptT $ DB.putTxHistory db (PrimaryKey wid) txs
+
+    mkStdTx = error "TODO: mkStdTx not implemented yet"
 
 {-------------------------------------------------------------------------------
                                  Helpers
