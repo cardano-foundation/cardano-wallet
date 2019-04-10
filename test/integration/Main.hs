8d4965e4536f546ba626e832caf3a110e1ad37fd
Piotr Stachyra <piotr.stachyra@iohk.io>
2019-04-10 08:00:18 +0200

    Create a wallet via API endpoint - integration test
    Create a wallet via API endpoint - scenario

--- test/integration/Main.hs
+++ test/integration/Main.hs
@@ -14,28 +14,18 @@ import Control.Concurrent.Async
     ( async, cancel, link )
 import Control.Monad
     ( void )
-import Data.Aeson
-    ( Value )
 import Data.Time
     ( addUTCTime, defaultTimeLocale, formatTime, getCurrentTime )
 import Network.HTTP.Client
     ( defaultManagerSettings, newManager )
-import Network.HTTP.Types.Status
-    ( status200, status404, status405 )
 import Test.Hspec
-    ( SpecWith, afterAll, beforeAll, describe, hspec, it, shouldBe )
+    ( afterAll, beforeAll, describe, hspec )
 import Test.Integration.Framework.DSL
-    ( Context (..)
-    , Headers (..)
-    , Payload (..)
-    , expectResponseCode
-    , json
-    , request
-    )
+    ( Context (..) )
 
 import qualified Cardano.Wallet.Network.HttpBridgeSpec as HttpBridge
 import qualified Cardano.WalletSpec as Wallet
-import qualified Data.Text as T
+import qualified Test.Integration.Scenario.Wallets as Wallets
 
 main :: IO ()
 main = do
@@ -44,10 +34,8 @@ main = do
         describe "Cardano.Wallet.Network.HttpBridge" HttpBridge.spec
 
         beforeAll startCluster $ afterAll killCluster $ do
-            describe "Integration test framework" dummySpec
+            describe "Wallets API endpoint tests" Wallets.spec
 
-        beforeAll dummySetup $ do
-            describe "Test response codes" respCodesSpec
   where
     -- Run a local cluster of cardano-sl nodes, a cardano-http-bridge on top and
     -- a cardano wallet server connected to the bridge.
@@ -66,6 +54,7 @@ main = do
         link cluster
         let baseURL = "http://localhost:1337/"
         manager <- newManager defaultManagerSettings
+        threadDelay 6000000
         return $ Context cluster (baseURL, manager)
 
     killCluster :: Context -> IO ()
@@ -88,58 +77,8 @@ main = do
 
     cardanoWalletLauncher serverPort bridgePort network = Command
         "cardano-wallet-launcher"
-        [ "--wallet-server-port", serverPort
+        [ "--network", network
+        , "--wallet-server-port", serverPort
         , "--http-bridge-port", bridgePort
-        , "--network", network
-        ] (threadDelay 6000000)
+        ] (pure ())
         Inherit
-
-
--- Exercise the request functions, which just fail at the moment.
-dummySpec :: SpecWith Context
-dummySpec = do
-    it "dummy spec" $ \(Context _ (url, _))  -> do
-        url `shouldBe` "http://localhost:1337/"
-
--- Temporary test setup for testing response codes
-dummySetup :: IO Context
-dummySetup = do
-    cluster <- async (return ())
-    let baseURL = T.pack ("http://httpbin.org")
-    manager <- newManager defaultManagerSettings
-    return $ Context cluster (baseURL, manager)
-
--- Exercise response codes
-respCodesSpec :: SpecWith Context
-respCodesSpec = do
-    it "GET; Response code 200" $ \ctx -> do
-        response <- request @Value ctx ("GET", "/get?my=arg") Default Empty
-        expectResponseCode @IO status200 response
-
-    it "GET; Response code 404" $ \ctx -> do
-        response <- request @Value ctx ("GET", "/get/nothing") Default Empty
-        expectResponseCode @IO status404 response
-
-    it "POST; Response code 200" $ \ctx -> do
-        let headers = Headers [("dummy", "header")]
-        let payload = Json [json| {
-                "addressPoolGap": 70,
-                "assuranceLevel": "strict",
-                "name": "Wallet EOS"
-                } |]
-        response <- request @Value ctx ("POST", "/post") headers payload
-        expectResponseCode @IO status200 response
-
-    it "POST; Response code 200" $ \ctx -> do
-        let headers = Headers [("dummy", "header")]
-        let payloadInvalid = NonJson "{\
-                        \\"addressPoolGap: 70,\
-                        \\"assuranceLevel\": strict,\
-                        \\"name\": \"Wallet EOS\"\
-                        \}"
-        response <- request @Value ctx ("POST", "/post") headers payloadInvalid
-        expectResponseCode @IO status200 response
-
-    it "POST; Response code 405" $ \ctx -> do
-        response <- request @Value ctx ("POST", "/get") None Empty
-        expectResponseCode @IO status405 response
