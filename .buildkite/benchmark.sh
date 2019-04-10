9d456f2683bc570969b37d228017c8959bbd8b6b
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-04-10 11:42:32 +1000

    Buildkite: Separate build and benchmark steps

--- .buildkite/benchmark.sh
+++ .buildkite/benchmark.sh
@@ -10,6 +10,10 @@ if [ -z "$netname" ]; then
    exit 1
 fi
 
+echo "--- Build code and benchmarks"
+stack build --bench --no-run-benchmarks
+
+echo "+++ Run benchmarks"
 stack bench cardano-wallet:restore --interleaved-output --ba "$netname +RTS -N2 -qg -A1m -I0 -T -M1G -h -RTS"
 
 hp2pretty restore.hp
