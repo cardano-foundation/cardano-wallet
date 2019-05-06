b053b1135e011e5b7fe912b589a0c64de19eaef5
KtorZ <matthias.benkort@gmail.com>
2019-05-06 15:55:21 +0200

    run the wallet server in the same thread as the test scenario
    Before, we ran this in a separate process which, as a major downside, has been escaping any
    form of coverage measurement. Having it ran in another _thread_ solves this problem

---
+++ lib/http-bridge/test/integration/Cardano/LauncherSpec.hs
@@ -0,0 +1,36 @@
+module Cardano.LauncherSpec
+    ( spec
+    ) where
+
+import Prelude
+
+import Cardano.Launcher
+    ( Command (..), StdStream (..), launch )
+import Control.Concurrent
+    ( threadDelay )
+import Control.Concurrent.Async
+    ( async, cancel, race, wait )
+import Control.Monad
+    ( void )
+import Test.Hspec
+    ( Spec, describe, expectationFailure, it )
+
+spec :: Spec
+spec = describe "cardano-wallet-launcher" $ do
+    it "Can start launcher against testnet" $ do
+        let cardanoWalletLauncher = Command
+                "cardano-wallet-launcher"
+                [ "--wallet-server-port", "1337"
+                , "--http-bridge-port", "8080"
+                ] (return ())
+                Inherit
+        handle <- async $ void $ launch [cardanoWalletLauncher]
+        let fiveSeconds = 5000000
+        winner <- race (threadDelay fiveSeconds) (wait handle)
+        case winner of
+            Left _ ->
+                cancel handle
+            Right _ ->
+                expectationFailure
+                    "cardano-wallet-launcher isn't supposed to terminate. \
+                    \Something went wrong."
