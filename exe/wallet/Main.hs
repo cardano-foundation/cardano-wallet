e575e62c8a3c375febea6461055d68ec3a9b08ee
Ante Kegalj <akegalj@gmail.com>
2019-05-09 14:05:48 +0200

    Fix CLI example comment

--- exe/wallet/Main.hs
+++ exe/wallet/Main.hs
@@ -146,7 +146,7 @@ Options:
   --payment <PAYMENT>         address to send to and amount to send separated by @: '<amount>@<address>'
 
 Examples:
-  cardano-wallet transaction create --wallet-id 2512a00e9653fe49a44a5886202e24d77eeb998f --payment 22@2cWKMJemoBam7gg1y5K2aFDhAm5L8fVc96NfxgcGhdLMFTsToNAU9t5HVdBBQKy4iDswL # Create archive.tar from files foo and bar.
+  cardano-wallet transaction create --wallet-id 2512a00e9653fe49a44a5886202e24d77eeb998f --payment 22@2cWKMJemoBam7gg1y5K2aFDhAm5L8fVc96NfxgcGhdLMFTsToNAU9t5HVdBBQKy4iDswL # Create a transaction and send 22 lovelace from wallet-id to specified addres
 
 |]
 
