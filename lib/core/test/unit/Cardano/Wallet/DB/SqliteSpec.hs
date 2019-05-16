ef1a2cba1f7a4acf50993e82ef9a16678b148555
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-05-16 20:25:52 +1000

    tests: Add simple Sqlite DB layer test

--- lib/core/test/unit/Cardano/Wallet/DB/SqliteSpec.hs
+++ lib/core/test/unit/Cardano/Wallet/DB/SqliteSpec.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Cardano.Wallet.DB.SqliteSpec
     ( spec
@@ -8,35 +11,56 @@ module Cardano.Wallet.DB.SqliteSpec
 
 import Prelude
 
+import Cardano.Wallet
+    ( unsafeRunExceptT )
+import Cardano.Wallet.DB
+    ( DBLayer (..), PrimaryKey (..) )
 import Cardano.Wallet.DB.Sqlite
-    ( migrateAll )
-import Conduit
-    ( MonadUnliftIO, ResourceT, runResourceT )
-import Control.Monad.Logger
-    ( LoggingT, runStderrLoggingT )
-import Control.Monad.Reader
-    ( ReaderT )
-import Data.Text
-    ( Text )
-import Database.Persist.Sqlite
-    ( SqlBackend, runMigration, runSqlConn, withSqliteConn )
+    ( newDBLayer )
+import Cardano.Wallet.Primitive.Types
+    ( WalletDelegation (..)
+    , WalletId (..)
+    , WalletMetadata (..)
+    , WalletName (..)
+    , WalletPassphraseInfo (..)
+    , WalletState (..)
+    )
+import Crypto.Hash
+    ( hash )
+import Data.ByteString
+    ( ByteString )
+import Data.Time.Clock
+    ( getCurrentTime )
 import Test.Hspec
     ( Spec, describe, it, shouldReturn )
 
-runSqlite'
-    :: (MonadUnliftIO m)
-    => Text
-    -> ReaderT SqlBackend (LoggingT (ResourceT m)) a
-    -> m a
-runSqlite' connstr =
-    runResourceT . runStderrLoggingT . withSqliteConn connstr . runSqlConn
-
-testMigrate :: IO ()
-testMigrate = runSqlite' ":memory:" $ do
-    runMigration migrateAll
-
 spec :: Spec
 spec = do
-    describe "Generated SQL schema" $ do
-        it "looks like SQL" $ do
-            testMigrate `shouldReturn` ()
+    describe "Wallet table" $ do
+        it "create and list works" $ do
+            db <- newDBLayer Nothing
+            now <- getCurrentTime
+            let wid = PrimaryKey (WalletId (hash ("test" :: ByteString)))
+                md = WalletMetadata
+                    { name = WalletName "test wallet"
+                    , passphraseInfo = Just $ WalletPassphraseInfo now
+                    , status = Ready
+                    , delegation = NotDelegating
+                    }
+            unsafeRunExceptT (createWallet db wid undefined md) `shouldReturn` ()
+            listWallets db `shouldReturn` [wid]
+
+        it "create and get meta works" $ do
+            db <- newDBLayer Nothing
+            now <- getCurrentTime
+            let wid = PrimaryKey (WalletId (hash ("test" :: ByteString)))
+                md = WalletMetadata
+                    { name = WalletName "test wallet"
+                    , passphraseInfo = Just $ WalletPassphraseInfo now
+                    , status = Ready
+                    , delegation = NotDelegating
+                    }
+            unsafeRunExceptT (createWallet db wid undefined md) `shouldReturn` ()
+            readWalletMeta db wid `shouldReturn` Just md
+
+deriving instance Show (PrimaryKey WalletId)
