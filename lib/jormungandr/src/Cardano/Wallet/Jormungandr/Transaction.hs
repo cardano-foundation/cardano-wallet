1e7dd9a6bf407314d85c418f9cbe3096e73d3459
Johannes Lund <johannes.lund@iohk.io>
2019-06-15 22:10:14 +0200

    Implement jörmungandr mkStdTx

--- lib/jormungandr/src/Cardano/Wallet/Jormungandr/Transaction.hs
+++ lib/jormungandr/src/Cardano/Wallet/Jormungandr/Transaction.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Cardano.Wallet.Jormungandr.Transaction
     ( newTransactionLayer
@@ -7,13 +9,59 @@ module Cardano.Wallet.Jormungandr.Transaction
 
 import Prelude
 
+import Cardano.Wallet.Jormungandr.Compatibility
+import Cardano.Wallet.Primitive.AddressDerivation
+    ( Depth (AddressK), Key, Passphrase (..), XPrv, getKey )
+import Cardano.Wallet.Primitive.Types
+    ( Hash (..), Tx (..), TxId (..), TxOut (..), TxWitness (..) )
 import Cardano.Wallet.Transaction
-    ( TransactionLayer (..) )
+    ( ErrMkStdTx (..), TransactionLayer (..) )
+import Control.Monad
+    ( forM )
+import Data.ByteString
+    ( ByteString )
 
+import qualified Cardano.Crypto.Wallet as CC
 
 -- | Construct a 'TransactionLayer' compatible with Shelley and 'Jörmungandr'
-newTransactionLayer :: TransactionLayer t
-newTransactionLayer = TransactionLayer
-    { mkStdTx = error "TODO: See http-bridge as starting point"
+newTransactionLayer
+    :: forall n . Hash "Block0Hash"
+    -> TransactionLayer (Jormungandr n)
+newTransactionLayer block0Hash = TransactionLayer
+    { mkStdTx = \keyFrom inps outs -> do
+        let ins = (fmap fst inps)
+        let tx = Tx ins outs
+        let witData = witnessUtxoData block0Hash (txId @(Jormungandr n) tx)
+        txWitnesses <- forM inps $ \(_in, TxOut addr _c) -> mkWitness witData
+            <$> withEither (ErrKeyNotFoundForAddress addr) (keyFrom addr)
+        return (tx, txWitnesses)
     , estimateSize = error "TODO: See http-bridge as starting point"
     }
+  where
+    withEither :: e -> Maybe a -> Either e a
+    withEither e = maybe (Left e) Right
+
+
+newtype WitnessData = WitnessData ByteString
+
+witnessUtxoData :: Hash "Block0Hash" -> Hash "Tx" -> WitnessData
+witnessUtxoData (Hash block0) (Hash tx) = WitnessData (block0 <> tx)
+
+mkWitness
+    :: WitnessData
+    -> (Key 'AddressK XPrv, Passphrase "encryption")
+    -> TxWitness
+mkWitness (WitnessData dat) (xPrv, pwd) =
+    PublicKeyWitness dummyXPub $ sign dat (xPrv, pwd)
+  where
+    -- We can't easily modify the TxWitness type. This is a temporary solution
+    -- before we can decide on better abstractions. We might want to have
+    -- different witness types for Jormungandr and http-bridge.
+    dummyXPub = error "The witness xPub should not be used for the new scheme."
+
+sign
+    :: ByteString
+    -> (Key 'AddressK XPrv, Passphrase "encryption")
+    -> Hash "signature"
+sign contents (key, (Passphrase pwd)) =
+    Hash . CC.unXSignature $ CC.sign pwd (getKey key) contents
