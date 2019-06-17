096d91223204eb69e84239a3e73a512e45d82bcd
Jonathan Knowles <jonathan.knowles@iohk.io>
2019-06-17 09:00:18 +0000

    Write version string to the log when starting the wallet server.

--- exe/wallet/Main.hs
+++ exe/wallet/Main.hs
@@ -415,6 +415,9 @@ execHttpBridge args _ = do
     minLogSeverity <- getOptionValue <$>
         args `parseArg` longOption "min-log-severity"
     tracer <- initTracer minLogSeverity "serve"
+    logInfo tracer $ "Wallet backend server starting. "
+        <> "Version "
+        <> T.pack (showVersion version)
     walletListen <- parseWalletListen args
     (bridgePort :: Int)
         <- args `parseArg` longOption "bridge-port"
