1ff49463afb4c283b810faee883ee4f5b96a02c5
Pawel Jakubas <pawel.jakubas@iohk.io>
2019-06-17 12:54:52 +0200

    add bech32 encoding to properly represent addresses later in genesis.yaml

--- lib/jormungandr/test/integration/Cardano/Faucet.hs
+++ lib/jormungandr/test/integration/Cardano/Faucet.hs
@@ -19,13 +19,18 @@ import Test.Integration.Faucet
 initFaucet :: NetworkLayer t IO -> IO Faucet
 initFaucet _nl = undefined
 
+
 {--
+-- code used for mnemonics generation, deriving the corresponding first address
+-- and calculating encoding to bench32
+
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Cardano.Faucet
     ( generateMnemonicsAndAdresses
+    , generateTriples
     ) where
 
 import Prelude
@@ -50,16 +55,24 @@ import Cardano.Wallet.Primitive.Mnemonic
     , mnemonicToText
     )
 import Cardano.Wallet.Primitive.Types
-    ( Address )
+    ( Address (..) )
+import Codec.Binary.Bech32
+    ( HumanReadablePart
+    , dataPartFromBytes
+    , encodeLenient
+    , humanReadablePartFromText
+    )
 import Control.Monad
     ( replicateM )
+import Data.ByteString
+    ( ByteString )
 import Data.Text
     ( Text )
 
-generateMnemonicsAndAdresses :: Int -> IO [([Text], Address)]
+generateMnemonicsAndAdresses :: Int -> IO [([Text], ByteString)]
 generateMnemonicsAndAdresses pairNum = do
     mnemonics <- replicateM pairNum genMnemonic
-    pure $ zip (mnemonicToText @15 <$> mnemonics) (firstAddress <$> mnemonics)
+    pure $ zip (mnemonicToText @15 <$> mnemonics) (getAddress . firstAddress <$> mnemonics)
       where
           genMnemonic :: IO (Mnemonic 15)
           genMnemonic = entropyToMnemonic <$> genEntropy
@@ -76,37 +89,63 @@ generateMnemonicsAndAdresses pairNum = do
                       deriveAddressPrivateKey pwd accXPrv ExternalChain minBound
               in
                   keyToAddress @(Jormungandr 'Testnet) (publicKey addrXPrv)
+
+generateTriples :: Int -> IO [([Text], ByteString, Text)]
+generateTriples tripleNum = do
+    pairs <- generateMnemonicsAndAdresses tripleNum
+    let bech32outs = map encodeToBech32 (map snd pairs)
+    pure $ zipWith (\(m,bs) b -> (m,bs,b)) pairs bech32outs
+    where
+        unsafeHumanReadablePart :: Text -> HumanReadablePart
+        unsafeHumanReadablePart = either errUnsafe id . humanReadablePartFromText
+            where
+                errUnsafe _ =
+                    error "Invalid bech32 human-readable part"
+        encodeToBech32 :: ByteString -> Text
+        encodeToBech32 bs =
+            let hrp = unsafeHumanReadablePart "ta"
+            in encodeLenient hrp (dataPartFromBytes bs)
 --}
+
 {--
-λ generateMnemonicsAndAdresses 10
-    [
-(["valid","agree","faith","tower","present","minute","valid","vague","engine","believe","catch","giggle","lumber","hip","unaware"],
-Address {getAddress = "\131Yy\228Z\198:9\180\140\RS\2\45L\159O;\ESC:\171\NUL J\200D\ETB\162\170\225\DC1+\214\248\184Q\200\a\133Y\ESCO)\149$\194U\221\245\200\&2\141\USX\218<TPeFQ6\ACK\180\&7\233j"}),
+ghci> generateTriples 10
+[(["tattoo","potato","foil","mutual","slab","path","forward","pencil","suit","marble","hill","meat","close","garden","bird"],
+"\131\ENQK\186?\203{_$\145\134\ESCn+\139\240\163\249%|\223/\223A\202Z\247\a.w\199\SI:",
+"ta1svz5hw3leda47fy3scdku2ut7z3ljftumuha7sw2ttmswtnhcu8n5nm0ynp"),
 
-(["trim","three","adult","wolf","brick","enter","sight","shiver","census","lamp","holiday","anger","life","pipe","venture"],
-Address {getAddress = "\131\242\175\EOT\177\241@\179\232Y\137\ff\176?\"\196\146\166:E\206\235\167\216K\247'P\f\226z2_q#m\223\SOH*\bW\164\CAN\SI\tN]g\217,\246\166\161$\"\231\254\230\ETB\214\202jZ\161"}),
+(["maximum","close","arrest","cheese","sleep","choice","fame","grape","enjoy","monkey","endorse","armed","urge","satoshi","scorpion"],
+"\131\147\147\196i\217\199\156^\"~z\242\228\"\136*\142.\188!l\221o\158T\224`\242\&1[\196\DC4",
+"ta1swfe83rfm8rech3z0ea09epz3q4gut4uy9kd6mu72nsxpu33t0zpg795k3y"),
 
-(["penalty","differ","gauge","tray","work","post","finger","loan","onion","monkey","question","another","benefit","together","theme"],
-Address {getAddress = "\131\192|=\196\191\244L\239\STXg\240L\DC2\143\176\US\193\169](E\197\201\186\195\&7\209\224^PfsV\222\rF\198##\132BP\215\155\181\130\ESC\239\173\128\n\249\&6\233\153\211\188zA7(7g|"}),
+(["glide","mule","shift","empower","jacket","desert","smoke","blossom","joke","crew","athlete","fabric","ozone","thought","private"],
+"\131\206\155\217\224f}\246\230\174y^\139\ACK\177\154\152=!\157\184\192&\v\175\204\239\215\RS\204\254\195\219",
+"ta1s08fhk0qve7lde4w090gkp43n2vr6gvahrqzvza0enhaw8kvlmpakd7swne"),
 
-(["oak","forget","perfect","panther","ladder","foam","valley","attitude","trade","alter","unaware","brain","body","hub","guilt"],
-Address {getAddress = "\131\203\203\217\176ko\178=v\150\NUL\233.}L\211E\136G\238vV\FS\SUBx\STX\135\241\154\162\212k\214\210.\198rGQ\STX\221\243a\238N\232F<A\f\240\&5\129\169\154\182\SYN\240=w\233\186R\143"}),
+(["appear","silent","loan","inch","plastic","maze","trouble","give","defy","detect","remember","issue","solution","increase","time"],
+"\131\STX\NUL^\t\129\243\223Q={0\155\193\&0\250_Z|\167\244I\ETX\166]\189\165\188\147u\131\218=",
+"ta1svpqqhsfs8ea75fa0vcfhsfslf045l9873ys8fjahkjmeym4s0dr69g8lec"),
 
-(["fire","bright","good","shine","trust","sight","home","camp","grid","enemy","face","polar","add","address","can"],
-Address {getAddress = "\131JB<J\167\148<xB\216/NjvpK.j\164\159\201\218\&62\SOH\194\242\188\162\233)s\253\DC1\232\&8GW\201e\231N\215\152\252\241\196\230\149\133\202C\DC4S\146\213\219T\135\179\152\v\136\&3"}),
+(["actress","kiwi","night","mimic","minor","local","mask","myself","remind","gesture","spirit","daring","rack","input","zoo"],
+"\131\140\164,\SI\STX\170\195\SOH\145\ENQ\206\164\172\186n\194G\228\133\147\251\ENQX3%(\230 ~\209\188%",
+"ta1swx2gtq0q24vxqv3qh82ft96dmpy0ey9j0as2kpny55wvgr76x7z2u8kvzf"),
 
-(["burger","raise","news","catch","face","crane","innocent","easy","leader","peanut","market","link","anchor","kangaroo","invite"],
-Address {getAddress = "\131\248+\RS\GS\DC4?\146$\254\252\234>\184\239\&1\138\238\167\ESC\DC4\SUB_@\ETX\217v\CANu\244{\232v\140Om\205\DC2\163\&1\157o\241\237\240h2\207#\159x\140&\"\216\SI~D\DC4\228\252\138\NAK\134\195"}),
+(["identify","black","engage","oil","better","keen","elephant","cross","citizen","law","soon","quiz","biology","march","tape"],
+"\131\175\217\146d[\t\133\238>\174s[\168W\193\177\141W\173\246B\255\177r\145\137\191[Q\230\176\187",
+"ta1swhanynytvyctm374ee4h2zhcxcc64ad7ep0lvtjjxym7k63u6ctkfjeg2n"),
 
-(["saddle","dune","wealth","later","tide","enrich","alley","flee","average","buddy","youth","clinic","manage","cycle","episode"],
-Address {getAddress = "\131\241\231\234\DC3x\227\143\222\210\208\STX\153,\r\158)\225Z\211j\229\200\253\190\250\206\133BJ\138\&8\171\197\227\STXe\DC1\DLE\153\150W\CAN\173i\158\DC2\198\DC3e\148\145\128\130\230\156\166\150\163\167Z\152\238;\219"}),
+(["uphold","century","misery","dwarf","arctic","merge","traffic","harbor","fine","latin","state","remain","pink","wrap","actual"],
+"\131\FS|T#\209X\a\236>\143S\DC1\169Ok/\181y\176g\182\212\DEL\167p\v\ETX\SOH\195c\t\160",
+"ta1svw8c4pr69vq0mp73af3r220dvhm27dsv7mdgla8wq9sxqwrvvy6qhpq5c8"),
 
-(["balance","lawsuit","castle","credit","curious","buyer","derive","design","electric","drive","into","hold","subject","leopard","rent"],
-Address {getAddress = "\131\236\247\152\242jAC@\ENQ\150\172/\184\nn\250\"\195]\ACK6\DEL\b\v\136\165\187\193\132\226\161,\147\230z}N\DC1\133\"t\171Y\178\201\159\r\\v\b\DC4\139\STX\215\211B\146,Z-+H\DC1\160"}),
+(["sudden","repeat","brother","arena","jaguar","polar","bounce","board","review","aware","jaguar","expand","leopard","mobile","gravity"],
+"\131#c\211\176a\143T\204 \168\&3y\193fGM\209\DC2\FSgM\250\131I~1\236\197\SUB\148V\209",
+"ta1sv3k85asvx84fnpq4qehnstxgaxazysuvaxl4q6f0cc7e3g6j3tdzp2vjac"),
 
-(["concert","measure","deal","vital","fortune","flag","doll","hobby","tragic","carpet","drastic","genre","glue","board","envelope"],
-Address {getAddress = "\131\164\194\198\177\173\243aF\251g*\238\239/\SOf\NUL\184\183&\bj*\230\DC3\223\SI\STX\188e\152\188{~\191\&7\"M\187\RS\177\138\220\219\DC1\b}~6U\173\179\165\185\231\ne\129x\f.6\239\DC3"}),
+(["bird","wave","record","proof","final","sponsor","parrot","cement","resist","dust","note","bean","spice","stage","prevent"],
+"\131\252\229\169j*u.n\159\184\CAN\214\203C2><\a&\191\140\202\150M\176\255\175\DLEFxZ!",
+"ta1s07wt2t29f6jum5lhqvddj6rxglrcpexh7xv49jdkrl67yzx0pdzzc3wnmv"),
 
-(["gadget","valve","bounce","fever","online","hat","elder","fat","glue","tower","oppose","coil","fringe","race","senior"],
-Address {getAddress = "\131\DC4\SOH\139j\195G\235\DC4\199 \232a\230\RS\157\225\140=\180G\185\239\ESC\206\247L\204\143+\158\196\137\240\187\194(y,\182,\200A\241\234_\158W.\ENQ\RS\134\135\224n\250\\\164\199\216\148\232t\SI\f"})]
+(["where","circle","abstract","estate","bless","state","stool","future","beauty","episode","patient","rhythm","scare","project","trigger"],
+"\131=K\138f\DC3\188x\154\144\173\DEL\241T\166\161\141Y\195\176\156\134\237;\146\173\232\250\&2\215\ACK@\254",
+"ta1sv75hznxzw783x5s44llz49x5xx4nsasnjrw6wuj4h505vkhqeq0uq77d56")]
 --}
