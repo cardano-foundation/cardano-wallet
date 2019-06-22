4d233dafa573ca2a482b9d8c70cc47d32ff17305
KtorZ <matthias.benkort@gmail.com>
2019-06-22 02:15:37 +0200

    fix witness creation: use txId from each input!

--- lib/jormungandr/src/Cardano/Wallet/Jormungandr/Transaction.hs
+++ lib/jormungandr/src/Cardano/Wallet/Jormungandr/Transaction.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
 
 module Cardano.Wallet.Jormungandr.Transaction
     ( newTransactionLayer
@@ -16,19 +15,13 @@ import Cardano.Wallet.Jormungandr.Primitive.Types
 import Cardano.Wallet.Primitive.AddressDerivation
     ( Depth (AddressK), Key, Passphrase (..), XPrv, getKey )
 import Cardano.Wallet.Primitive.Types
-    ( Hash (..)
-    , txId
-    , TxOut (..)
-    , TxWitness (..)
-    )
+    ( Hash (..), TxIn (..), TxOut (..), TxWitness (..) )
 import Cardano.Wallet.Transaction
     ( ErrMkStdTx (..), TransactionLayer (..) )
 import Control.Arrow
     ( second )
 import Control.Monad
     ( forM )
-import Data.ByteString
-    ( ByteString )
 import Data.Either.Combinators
     ( maybeToRight )
 import Data.Quantity
@@ -38,13 +31,12 @@ import qualified Cardano.Crypto.Wallet as CC
 
 -- | Construct a 'TransactionLayer' compatible with Shelley and 'Jörmungandr'
 newTransactionLayer
-    :: forall n . Hash "Block0Hash"
+    :: Hash "Genesis"
     -> TransactionLayer (Jormungandr n)
-newTransactionLayer block0Hash = TransactionLayer
+newTransactionLayer (Hash block0) = TransactionLayer
     { mkStdTx = \keyFrom inps outs -> do
         let tx = Tx (fmap (second coin) inps) outs
-        let witData = witnessUtxoData block0Hash (txId @(Jormungandr n) tx)
-        txWitnesses <- forM inps $ \(_in, TxOut addr _c) -> sign witData
+        txWitnesses <- forM inps $ \(txin, TxOut addr _) -> sign txin
             <$> maybeToRight (ErrKeyNotFoundForAddress addr) (keyFrom addr)
         return (tx, txWitnesses)
 
@@ -52,14 +44,9 @@ newTransactionLayer block0Hash = TransactionLayer
     , estimateSize = \_ -> Quantity 0
     }
   where
-    witnessUtxoData :: Hash "Block0Hash" -> Hash "Tx" -> WitnessData
-    witnessUtxoData (Hash block0) (Hash tx) = WitnessData (block0 <> tx)
-
     sign
-        :: WitnessData
+        :: TxIn
         -> (Key 'AddressK XPrv, Passphrase "encryption")
         -> TxWitness
-    sign (WitnessData contents) (key, (Passphrase pwd)) =
-        TxWitness . CC.unXSignature $ CC.sign pwd (getKey key) contents
-
-newtype WitnessData = WitnessData ByteString
+    sign (TxIn (Hash tx) _) (key, (Passphrase pwd)) =
+        TxWitness . CC.unXSignature $ CC.sign pwd (getKey key) (block0 <> tx)
