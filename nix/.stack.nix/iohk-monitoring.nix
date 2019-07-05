6b72b42289bd1cb11f2dba49ad0a65919d6a3b78
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-07-05 11:22:53 +1000

    Bump iohk-monitoring-framework to latest master

--- nix/.stack.nix/iohk-monitoring.nix
+++ nix/.stack.nix/iohk-monitoring.nix
@@ -14,7 +14,7 @@
       };
     package = {
       specVersion = "1.10";
-      identifier = { name = "iohk-monitoring"; version = "0.1.9.0"; };
+      identifier = { name = "iohk-monitoring"; version = "0.1.10.0"; };
       license = "MIT";
       copyright = "2018 IOHK";
       maintainer = "";
@@ -78,6 +78,7 @@
         "example-simple" = {
           depends = [
             (hsPkgs.base)
+            (hsPkgs.aeson)
             (hsPkgs.iohk-monitoring)
             (hsPkgs.async)
             (hsPkgs.bytestring)
@@ -154,8 +155,8 @@
     } // {
     src = (pkgs.lib).mkDefault (pkgs.fetchgit {
       url = "https://github.com/input-output-hk/iohk-monitoring-framework";
-      rev = "e2dc8b2160bb2ccd1ef9817a6498cda06ad36580";
-      sha256 = "1mp9fqva9lckbpx2fgakfzsyw85x3mmcb0mpszxyp55sbj3vyw1y";
+      rev = "bd31cd2f3922010ddb76bb869f29c4e63bb8001b";
+      sha256 = "1dfk505qbpk6p3gcpxa31wmg98qvx9hlrxlf0khaj7hizf3b8b60";
       });
     postUnpack = "sourceRoot+=/iohk-monitoring; echo source root reset to \$sourceRoot";
     }
