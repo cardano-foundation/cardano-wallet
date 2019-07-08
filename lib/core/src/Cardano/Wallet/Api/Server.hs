1666cb84b1f4160da22f3c98e81c7af6c7aa4e5b
Pawel Jakubas <pawel.jakubas@iohk.io>
2019-07-08 21:10:26 +0200

    correct msg from error

--- lib/core/src/Cardano/Wallet/Api/Server.hs
+++ lib/core/src/Cardano/Wallet/Api/Server.hs
@@ -548,9 +548,9 @@ instance Buildable e => LiftHandler (ErrEstimateTxFee e) where
         ErrEstimateTxFeeCoinSelection e -> handler e
         ErrEstimateTxFeeBackend _ ->
             apiError err403 CreatedInvalidTransaction $ mconcat
-                [ "I can't process this payment because it contains either at least"
-                , " one payment output of value 0 (Byron) or a transaction has more"
-                , " than 255 inputs or outputs (Jormungandr)."
+                [ "I can't estimate transaction fee because either"
+                , " transaction contains at least one payment output of value 0 (Byron)"
+                , " or a transaction has more than 255 inputs or outputs (Jormungandr)."
                 ]
 
 instance LiftHandler ErrSignTx where
