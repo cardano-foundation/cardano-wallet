d910de7aa82b8fb2a116880489195f21e111c30c
Pawel Jakubas <pawel.jakubas@iohk.io>
2019-08-01 11:04:01 +0200

    add RndState with isOurs instance

--- lib/core/src/Cardano/Wallet/Primitive/AddressDiscovery/Random.hs
+++ lib/core/src/Cardano/Wallet/Primitive/AddressDiscovery/Random.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 -- |
@@ -15,11 +17,14 @@ module Cardano.Wallet.Primitive.AddressDiscovery.Random
     (
     -- ** State
       RndState (..)
-    , mkRndState
     ) where
 
 import Prelude
 
+import Cardano.Wallet.Primitive.AddressDerivation
+    ( Depth (..), Key, XPrv, publicKey )
+import Cardano.Wallet.Primitive.AddressDerivation.Random
+    ( decodeAddressDerivationPath )
 import Cardano.Wallet.Primitive.AddressDiscovery
     ( CompareDiscovery (..)
     , GenChange (..)
@@ -27,16 +32,34 @@ import Cardano.Wallet.Primitive.AddressDiscovery
     , IsOwned (..)
     , KnownAddresses (..)
     )
+import Cardano.Wallet.Primitive.Types
+    ( Address (..) )
+import Control.DeepSeq
+    ( NFData )
 import Data.ByteString
     ( ByteString )
+import Data.ByteString.Base58
+    ( bitcoinAlphabet, decodeBase58 )
+import Data.Maybe
+    ( fromJust )
+import GHC.Generics
+    ( Generic )
 
-newtype RndState = RndState ByteString
+import qualified Codec.CBOR.Decoding as CBOR
+import qualified Codec.CBOR.Read as CBOR
+import qualified Data.ByteString.Lazy as BL
 
-mkRndState :: RndState
-mkRndState = error "AddressDiscovery.Random unimplemented"
+newtype RndState = RndState { getRndState :: Key 'RootK XPrv }
+    deriving (Generic)
+
+instance NFData RndState
 
 instance IsOurs RndState where
-    isOurs _ s = (False, s)
+    isOurs (Address addr) (RndState s) = do
+        let payload = unsafeDeserialiseFromBytes decodeAddressPayload $ b58decode addr
+        case deserialise (decodeAddressDerivationPath $ publicKey s) payload of
+            Right (Just _) -> (True, RndState s)
+            _ -> (False, RndState s)
 
 instance IsOwned RndState where
     isOwned _ _ _ = Nothing
@@ -54,3 +77,28 @@ instance CompareDiscovery RndState where
 
 instance KnownAddresses RndState where
     knownAddresses _ = []
+
+
+-- | Extract the HD payload part of an legacy scheme Address, which has already
+-- been base-58 decoded.
+decodeAddressPayload :: CBOR.Decoder s ByteString
+decodeAddressPayload = do
+    _ <- CBOR.decodeListLenCanonicalOf 2
+    _ <- CBOR.decodeTag
+    bytes <- CBOR.decodeBytes
+    _ <- CBOR.decodeWord32 -- CRC
+    return bytes
+
+-- | Decode a bitcoin base-58 address to a LBS, without error handling.
+b58decode :: ByteString -> BL.ByteString
+b58decode = BL.fromStrict . fromJust . decodeBase58 bitcoinAlphabet
+
+-- | CBOR deserialise without error handling - handy for prototypes or testing.
+unsafeDeserialiseFromBytes :: (forall s. CBOR.Decoder s a) -> BL.ByteString -> a
+unsafeDeserialiseFromBytes decoder bytes =
+    either (\e -> error $ "unsafeDeserialiseFromBytes: " <> show e) snd $
+        CBOR.deserialiseFromBytes decoder bytes
+
+-- | CBOR deserialise a strict bytestring
+deserialise :: (forall s. CBOR.Decoder s a) -> ByteString -> Either CBOR.DeserialiseFailure a
+deserialise dec = fmap snd . CBOR.deserialiseFromBytes dec . BL.fromStrict
