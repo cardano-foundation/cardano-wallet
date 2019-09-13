01c8ff574d262c6351f96a1398081563cb9566c6
KtorZ <matthias.benkort@gmail.com>
2019-09-13 14:49:03 +0200

    use qualified imports for 'Control.Retry'

--- lib/core/src/Cardano/Wallet/Network.hs
+++ lib/core/src/Cardano/Wallet/Network.hs
@@ -30,6 +30,7 @@ import Control.Exception
 import Control.Monad.Trans.Except
     ( ExceptT, runExceptT )
 import Control.Retry
+    ( RetryPolicyM, constantDelay, limitRetriesByCumulativeDelay, retrying )
 import Data.ByteString
     ( ByteString )
 import Data.Quantity
