cardano-foundation/cardano-wallet

commit 7208a54a018415108dc359b7e20cb024bef1af63

nix: Add wrapping of wallet executables to provide runtime deps

nix/default.nix
@@ -56,6 +56,23 @@ let
           unit.build-tools = [ jormungandr ];
         };
 
+
+        packages.cardano-wallet.components.exes.cardano-wallet-jormungandr = {
+          build-tools = [ pkgs.makeWrapper];
+          postInstall = ''
+            wrapProgram $out/bin/cardano-wallet-jormungandr \
+              --prefix PATH : ${jormungandr}/bin
+          '';
+        };
+
+        packages.cardano-wallet.components.exes.cardano-wallet-http-bridge = {
+          build-tools = [ pkgs.makeWrapper];
+          postInstall = ''
+            wrapProgram $out/bin/cardano-wallet-http-bridge \
+              --prefix PATH : ${cardano-http-bridge}/bin
+          '';
+        };
+
         packages.cardano-wallet-http-bridge.components.benchmarks.restore = {
           build-tools = [ pkgs.makeWrapper ];
           postInstall = ''
@@ -65,6 +82,10 @@ let
               --prefix PATH : ${cardano-http-bridge}/bin
           '';
         };
+
+        # Workaround for Haskell.nix issue
+        packages.cardano-wallet.components.all.postInstall = pkgs.lib.mkForce "";
+        packages.cardano-wallet-jormungandr.components.all.postInstall = pkgs.lib.mkForce "";
         packages.cardano-wallet-http-bridge.components.all.postInstall = pkgs.lib.mkForce "";
       }
 

commit 6f2d5ad2fa3c659ef8ffbe02a534c56c487e6710

nix: Fix windows cross build
I broke the cross build in 7208a54a018415108dc359b7e20cb024bef1af63.

This fixes it, and adds a necessary DLL to the packages.

nix/default.nix
@@ -24,6 +24,13 @@ let
   # Grab the compiler name from stack-to-nix output.
   compiler = (stack-pkgs.extras {}).compiler.nix-name;
 
+  # Use a postInstall wrapping script if this is not a windows
+  # build. Otherwise, copy DLL dependencies.
+  wrapForPosix = postInstall: if pkgs.stdenv.hostPlatform.isWindows
+    then ''
+      cp -v ${pkgs.libffi}/bin/libffi-6.dll $out/bin
+    '' else postInstall;
+
   pkgSet = haskell.mkStackPkgSet {
     inherit stack-pkgs;
     modules = [
@@ -59,7 +66,7 @@ let
 
         packages.cardano-wallet.components.exes.cardano-wallet-jormungandr = {
           build-tools = [ pkgs.makeWrapper];
-          postInstall = ''
+          postInstall = wrapForPosix ''
             wrapProgram $out/bin/cardano-wallet-jormungandr \
               --prefix PATH : ${jormungandr}/bin
           '';
@@ -67,7 +74,7 @@ let
 
         packages.cardano-wallet.components.exes.cardano-wallet-http-bridge = {
           build-tools = [ pkgs.makeWrapper];
-          postInstall = ''
+          postInstall = wrapForPosix ''
             wrapProgram $out/bin/cardano-wallet-http-bridge \
               --prefix PATH : ${cardano-http-bridge}/bin
           '';