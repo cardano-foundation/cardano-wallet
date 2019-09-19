c58599e2aafef1fda4fa8cc17b031711071d1629
Johannes Lund <johannes.lund@iohk.io>
2019-09-19 21:52:53 +0200

    [WIP] Break up NetworkLayer to parameterize by block

--- lib/core/src/Cardano/Wallet/Network.hs
+++ lib/core/src/Cardano/Wallet/Network.hs
@@ -35,8 +35,8 @@ import Data.Text
 import GHC.Generics
     ( Generic )
 
-data NetworkLayer t m = NetworkLayer
-    { nextBlocks :: BlockHeader -> ExceptT ErrGetBlock m [Block (Tx t)]
+data Restorer block m = NetworkLayer
+    { nextBlocks :: BlockHeader -> ExceptT ErrGetBlock m [block]
         -- ^ Fetches a contiguous sequence of blocks from the node, starting
         -- from the first block available with a slot greater than the given
         -- block header.
@@ -53,10 +53,10 @@ data NetworkLayer t m = NetworkLayer
         :: ExceptT ErrNetworkTip m BlockHeader
         -- ^ Get the current network tip from the chain producer
 
-    , postTx
-        :: (Tx t, [TxWitness]) -> ExceptT ErrPostTx m ()
-        -- ^ Broadcast a transaction to the chain producer
     }
+
+-- | Broadcast a transaction to the chain producer
+type TxSubmitter tx m = (tx, [TxWitness]) -> ExceptT ErrPostTx m ()
 
 -- | Network is unavailable
 data ErrNetworkUnavailable
@@ -97,7 +97,7 @@ instance Exception ErrPostTx
 -- | Wait until 'networkTip networkLayer' succeeds according to a given
 -- retry policy. Throws an exception otherwise.
 waitForConnection
-    :: NetworkLayer t IO
+    :: Restorer b IO
     -> RetryPolicyM IO
     -> IO ()
 waitForConnection nw policy = do
