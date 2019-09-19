1434f39a550ed107641d6e880146e2a8c2ea31d7
Piotr Stachyra <piotr.stachyra@iohk.io>
2019-09-19 16:07:05 +0200

    nix/jormungandr.nix: Update procedure for updating version

--- nix/jormungandr.nix
+++ nix/jormungandr.nix
@@ -6,9 +6,10 @@
 #
 # To change the version:
 # 1. Adjust the "version" and/or "rev" variables below.
-# 2. Then change a single digit in *both* sha256 and cargoSha256.
-#    (e.g. change a 4 to 5). It's important that you change them to
-#    something different than before.
+# 2. Then invert the first digit in *both* sha256 and
+#    cargoSha256. That is, change 0 to 1 and 1 to 0. It's important
+#    that you change them to something different than before,
+#    otherwise you may get the previous version from your local cache.
 # 3. Run "nix-build -A jormungandr.src" (or let CI do it for you).
 #    It will say that the hash is wrong. Update sha256 to the value it got.
 # 4. Run "nix-build -A jormungandr". After some time downloading
@@ -28,8 +29,11 @@
 let
   release = rec {
     version = "0.3.3";
+    # Git revision of input-output-hk/jormungandr repo.
     rev = "v${version}";
+    # Hash of git repo and all of its submodules.
     sha256 = "1fw3cl2rxnw9mww1b1z96x2iapwbpdgyp4ra19dhvfzmlvaiml5j";
+    # Hash of all Cargo dependencies.
     cargoSha256 = "1ilp9ffaz3njv38mnqics4b5d7wh52mj4rwi71h5c0wzx4ww3zal";
   };
 
