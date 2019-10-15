ed8f9d5133c4308803c787972630837a22400311
Piotr Stachyra <piotr.stachyra@iohk.io>
2019-10-15 13:20:54 +0200

    Test against 406 and swagger update

--- lib/core-integration/src/Test/Integration/Scenario/API/Network.hs
+++ lib/core-integration/src/Test/Integration/Scenario/API/Network.hs
@@ -29,7 +29,7 @@ import Test.Integration.Framework.DSL
     , verify
     )
 import Test.Integration.Framework.TestData
-    ( errMsg405 )
+    ( errMsg405, errMsg406 )
 
 import qualified Network.HTTP.Types.Status as HTTP
 
@@ -48,3 +48,32 @@ spec = do
             r <- request @ApiNetworkInformation ctx endpoint Default Empty
             expectResponseCode @IO HTTP.status405 r
             expectErrorMessage errMsg405 r
+
+    describe "NETWORK - HTTP headers" $ do
+        let matrix =
+                  [ ( "No HTTP headers -> 200", None
+                    , [ expectResponseCode @IO HTTP.status200 ] )
+                  , ( "Accept: text/plain -> 406"
+                    , Headers
+                          [ ("Content-Type", "application/json")
+                          , ("Accept", "text/plain") ]
+                    , [ expectResponseCode @IO HTTP.status406
+                      , expectErrorMessage errMsg406 ]
+                    )
+                  , ( "No Accept -> 200"
+                    , Headers [ ("Content-Type", "application/json") ]
+                    , [ expectResponseCode @IO HTTP.status200 ]
+                    )
+                  , ( "No Content-Type -> 200"
+                    , Headers [ ("Accept", "application/json") ]
+                    , [ expectResponseCode @IO HTTP.status200 ]
+                    )
+                  , ( "Content-Type: text/plain -> 200"
+                    , Headers [ ("Content-Type", "text/plain") ]
+                    , [ expectResponseCode @IO HTTP.status200 ]
+                    )
+                  ]
+        forM_ matrix $ \(title, headers, expectations) -> it title $ \ctx -> do
+            let ep = ("GET", "v2/network/information")
+            r <- request @ApiNetworkInformation ctx ep headers Empty
+            verify r expectations
