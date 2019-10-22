82a90688e45de84ffde503e0d890c0a753e820a5
Johannes Lund <johannes.lund@iohk.io>
2019-10-22 17:23:25 +0200

    Actually start `monitorStakePools` on startup!

--- lib/jormungandr/src/Cardano/Wallet/Jormungandr.hs
+++ lib/jormungandr/src/Cardano/Wallet/Jormungandr.hs
@@ -23,6 +23,9 @@
 
 module Cardano.Wallet.Jormungandr
     ( serveWallet
+
+      -- * Utilities
+    , toSPBlock
     ) where
 
 import Prelude
@@ -34,7 +37,7 @@ import Cardano.CLI
 import Cardano.Launcher
     ( ProcessHasExited (..), installSignalHandlers )
 import Cardano.Pool.Metrics
-    ( StakePoolLayer, newStakePoolLayer )
+    ( StakePoolLayer, monitorStakePools, newStakePoolLayer )
 import Cardano.Wallet.Api
     ( ApiLayer )
 import Cardano.Wallet.Api.Server
@@ -78,15 +81,23 @@ import Cardano.Wallet.Primitive.AddressDiscovery.Sequential
 import Cardano.Wallet.Primitive.Model
     ( BlockchainParameters (..) )
 import Cardano.Wallet.Primitive.Types
-    ( Block, Hash (..) )
+    ( Block, BlockHeader (..), Hash (..) )
 import Cardano.Wallet.Transaction
     ( TransactionLayer )
+import Control.Concurrent
+    ( forkIO )
 import Control.Concurrent.Async
     ( race_ )
 import Control.DeepSeq
     ( NFData )
+import Control.Monad
+    ( void )
 import Data.Function
     ( (&) )
+import Data.Maybe
+    ( fromMaybe )
+import Data.Quantity
+    ( Quantity (..) )
 import Data.Text
     ( Text )
 import Data.Text.Class
@@ -100,6 +111,7 @@ import System.Exit
 
 import qualified Cardano.BM.Configuration.Model as CM
 import qualified Cardano.Pool.DB.Sqlite as Pool
+import qualified Cardano.Pool.Metrics as Pool
 import qualified Cardano.Wallet.Api.Server as Server
 import qualified Cardano.Wallet.DB.Sqlite as Sqlite
 import qualified Cardano.Wallet.Jormungandr.Binary as J
@@ -144,6 +156,11 @@ serveWallet (cfg, tr) databaseDir hostPref listen lj beforeMainLoop = do
             let path = fmap Pool.defaultFilePath databaseDir
             Pool.withDBLayer cfg tr path $
                 \splDB -> do
+                    -- TODO: I believe @KTorZ wants us to use @WorkerRegistry@
+                    -- here, instead of @forkIO@
+                    void $ forkIO $
+                        monitorStakePools (toSPBlock <$> nl) splDB tr
+
                     spl <- newStakePoolLayer splDB nl tr
                     startServer tr nPort bp rndApi seqApi spl
         Left e -> handleNetworkStartupError e
@@ -272,3 +289,21 @@ serveWallet (cfg, tr) databaseDir hostPref listen lj beforeMainLoop = do
             failWith tr $ mempty
                 <> "Cannot listen on the given port. "
                 <> "The operation is not permitted."
+
+--------------------------------------------------------------------------------
+-- Exported Utilities
+--------------------------------------------------------------------------------
+
+-- | Covert a raw block to one that the "Cardano.Pool.Metrics" module accepts.
+toSPBlock :: J.Block -> Pool.Block
+toSPBlock b = Pool.Block
+     (convertHeader header)
+     (fromMaybe (error "block has no producer") $ J.producedBy header)
+   where
+     header = J.header b
+     convertHeader :: J.BlockHeader -> BlockHeader
+     convertHeader h = BlockHeader
+         (J.slot h)
+         (Quantity $ fromIntegral $ J.chainLength h)
+         (J.headerHash h)
+         (J.parentHeaderHash h)
