153be4dcb472ff472736fb5ac2a6ad7345d5acd0
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-10-24 15:44:19 +1000

    nix: Add cross builds to Hercules-CI

--- nix/ci.nix
+++ nix/ci.nix
@@ -2,18 +2,35 @@
 # https://hercules-ci.com/github/input-output-hk/cardano-wallet
 # https://docs.hercules-ci.com/hercules-ci/getting-started/minimal-repository/
 
-builtins.mapAttrs (system: _:
-  let
-    walletPkgs = import ../default.nix { inherit system; };
-  in
+let
+  inherit (import ./nixpkgs-haskell.nix {}) lib;
+
+  walletJobs = walletPkgs:
     walletPkgs.pkgs.recurseIntoAttrs {
       inherit (walletPkgs)
         cardano-wallet-jormungandr
         tests
         benchmarks;
-    }
-) {
-  x86_64-linux = {};
-  # Uncomment to test build on macOS too
-  # x86_64-darwin = {};
-}
+    };
+
+  nativeBuilds = builtins.mapAttrs (system: _:
+    walletJobs (import ../default.nix { inherit system; })
+  ) {
+    x86_64-linux = {};
+    # Uncomment to test build on macOS too
+    # x86_64-darwin = {};
+  };
+
+  crossSystems = with lib.systems.examples;
+    [ mingwW64 ];
+
+  crossBuilds = lib.listToAttrs (map (crossSystem: {
+    name = crossSystem.config;
+    value = walletJobs (import ../default.nix {
+      system = "x86_64-linux";
+      inherit crossSystem;
+    });
+  }) crossSystems);
+
+in
+  nativeBuilds // crossBuilds
