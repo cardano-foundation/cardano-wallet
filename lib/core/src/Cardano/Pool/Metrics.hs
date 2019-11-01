f2e092b87d96f0a1d784ef94a6a57094755a884d
KtorZ <matthias.benkort@gmail.com>
2019-11-01 16:12:18 +0100

    review performance calculation to include multiple past epochs

--- lib/core/src/Cardano/Pool/Metrics.hs
+++ lib/core/src/Cardano/Pool/Metrics.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -16,6 +17,7 @@
 module Cardano.Pool.Metrics
     ( -- * Types
       Block (..)
+    , StakePool (..)
 
     -- * Listing stake-pools from the DB
     , StakePoolLayer (..)
@@ -28,6 +30,7 @@ module Cardano.Pool.Metrics
       -- * Combining Metrics
     , ErrMetricsInconsistency (..)
     , combineMetrics
+    , calculatePerformance
     )
     where
 
@@ -46,9 +49,9 @@ import Cardano.Wallet.Network
     , staticBlockchainParameters
     )
 import Cardano.Wallet.Primitive.Types
-    ( BlockHeader (..), PoolId (..), SlotId (..) )
+    ( BlockHeader (..), EpochNo (..), PoolId (..), SlotId (..) )
 import Control.Monad
-    ( forM_, when )
+    ( forM, forM_, when )
 import Control.Monad.IO.Class
     ( liftIO )
 import Control.Monad.Trans.Class
@@ -58,11 +61,11 @@ import Control.Monad.Trans.Except
 import Data.Generics.Internal.VL.Lens
     ( view, (^.) )
 import Data.List
-    ( sortOn )
+    ( foldl', sortOn )
 import Data.List.NonEmpty
     ( NonEmpty )
 import Data.Map.Merge.Strict
-    ( mergeA, traverseMissing, zipWithMatched )
+    ( dropMissing, traverseMissing, zipWithMatched )
 import Data.Map.Strict
     ( Map )
 import Data.Ord
@@ -77,94 +80,29 @@ import Fmt
     ( pretty )
 import GHC.Generics
     ( Generic )
-import Numeric.Natural
-    ( Natural )
 
+import qualified Data.Map.Merge.Strict as Map
 import qualified Data.Map.Strict as Map
+
+--------------------------------------------------------------------------------
+-- Types
+--------------------------------------------------------------------------------
 
 data Block = Block
     { header :: BlockHeader
     , producer :: PoolId
     } deriving (Eq, Show, Generic)
 
+data StakePool = StakePool
+    { poolId :: PoolId
+    , stake :: Quantity "lovelace" Word64
+    , production :: Quantity "block" Word64
+    , apparentPerformance :: Double
+    } deriving (Generic)
+
 --------------------------------------------------------------------------------
--- StakePoolLayer
+-- Stake Pool Monitoring
 --------------------------------------------------------------------------------
-
--- | @StakePoolLayer@ is a thin layer ontop of the DB. It is /one/ value that
--- can easily be passed to the API-server, where it can be used in a simple way.
-newtype StakePoolLayer m = StakePoolLayer
-      { listStakePools
-          :: ExceptT ErrListStakePools m
-             [(PoolId, (Quantity "lovelace" Word64, Quantity "block" Natural, Double))]
-      }
-
-data ErrListStakePools
-     = ErrMetricsIsUnsynced (Quantity "percent" Percentage)
-     | ErrListStakePoolsMetricsInconsistency ErrMetricsInconsistency
-     | ErrListStakePoolsErrNetworkTip ErrNetworkTip
-
-newStakePoolLayer
-     :: DBLayer IO
-     -> NetworkLayer IO t block
-     -> Trace IO Text
-     -> IO (StakePoolLayer IO)
-newStakePoolLayer db nl tr = do
-     return $ StakePoolLayer
-        { listStakePools = do
-            nodeTip <- withExceptT ErrListStakePoolsErrNetworkTip
-                $ networkTip nl
-            let nodeEpoch = nodeTip ^. #slotId . #epochNumber
-
-            distr <- liftIO $ Map.fromList <$>
-                readStakeDistribution db nodeEpoch
-            prod <- liftIO $ count <$>
-                readPoolProduction db nodeEpoch
-
-            when (Map.null distr || Map.null prod) $ do
-                computeProgress nodeTip >>= throwE . ErrMetricsIsUnsynced
-
-            case combineMetrics distr prod of
-                Right x -> return
-                    $ sortOn (\(_,(_,_,perf)) -> Down perf)
-                    $ Map.toList
-                    $ calculatePerformance x
-                Left e ->
-                    throwE $ ErrListStakePoolsMetricsInconsistency e
-        }
-  where
-    count = Map.map (Quantity . fromIntegral . length)
-
-    poolProductionTip = readPoolProductionCursor db 1 >>= \case
-        [x] -> return $ Just x
-        _ -> return Nothing
-
-    computeProgress
-        :: BlockHeader -- ^ The node tip, which respresents 100%.
-        -> ExceptT e IO (Quantity "percent" Percentage)
-    computeProgress nodeTip = liftIO $ do
-        mDbTip <- poolProductionTip
-        Quantity <$> case mDbTip of
-            Nothing -> return minBound
-            Just dbTip -> do
-                liftIO $ logDebug tr $ mconcat
-                    [ "The node tip is:\n"
-                    , pretty nodeTip
-                    , ",\nbut the last pool production stored in the db"
-                    , " is from:\n"
-                    , pretty dbTip
-                    ]
-                return $ progress dbTip nodeTip
-
-    progress :: BlockHeader -> BlockHeader -> Percentage
-    progress tip target =
-        let
-            s0 = getQuantity $ tip ^. #blockHeight
-            s1 = getQuantity $ target ^. #blockHeight
-        in toEnum $ round $ 100 * (toD s0) / (toD s1)
-      where
-        toD :: Integral i => i -> Double
-        toD = fromIntegral
 
 -- | 'monitorStakePools' follows the chain and puts pool productions and stake
 -- distributions to a 'DBLayer', such that the data in the database is always
@@ -244,63 +182,183 @@ data ErrMonitorStakePools
     | ErrMonitorStakePoolsWrongTip
     deriving (Show, Eq)
 
-calculatePerformance
-    :: Map PoolId (Quantity "lovelace" Word64, Quantity "block" Natural)
-    -> Map PoolId (Quantity "lovelace" Word64, Quantity "block" Natural, Double)
-calculatePerformance m = (flip Map.map) m $ \(Quantity stake, Quantity blocks) ->
-    let
-        s = fromIntegral stake
-        p = fromIntegral blocks
-        perf =
-            -- TODO: totalBlocks == 0 should be impossible?
-            if s == 0 || p == 0 || totalBlocks == 0
-            then 0
-            else (p / totalBlocks) * (totalStake / s)
-    in
-        (Quantity stake, Quantity blocks, perf)
-  where
-    totalStake = fromIntegral $
-        Map.foldl' (+) 0
-        $ Map.map (\(Quantity x, _) -> x) m
-    totalBlocks = fromIntegral $
-        Map.foldl' (+) 0
-        $ Map.map (\(_, Quantity x) -> x) m
+--------------------------------------------------------------------------------
+-- StakePoolLayer
+--------------------------------------------------------------------------------
 
--- | Combines two different sources of data into one:
+-- | @StakePoolLayer@ is a thin layer ontop of the DB. It is /one/ value that
+-- can easily be passed to the API-server, where it can be used in a simple way.
+newtype StakePoolLayer m = StakePoolLayer
+      { listStakePools
+          :: ExceptT ErrListStakePools m [StakePool]
+      }
+
+data ErrListStakePools
+     = ErrMetricsIsUnsynced (Quantity "percent" Percentage)
+     | ErrListStakePoolsMetricsInconsistency ErrMetricsInconsistency
+     | ErrListStakePoolsErrNetworkTip ErrNetworkTip
+
+newStakePoolLayer
+     :: DBLayer IO
+     -> NetworkLayer IO t block
+     -> Trace IO Text
+     -> IO (StakePoolLayer IO)
+newStakePoolLayer db nl tr = do
+     return $ StakePoolLayer
+        { listStakePools = do
+            nodeTip <- withExceptT ErrListStakePoolsErrNetworkTip
+                $ networkTip nl
+            let nodeEpoch = nodeTip ^. #slotId . #epochNumber
+
+            distr <- liftIO $ Map.fromList <$>
+                readStakeDistribution db nodeEpoch
+
+            prod <- liftIO $ count <$>
+                readPoolProduction db nodeEpoch
+
+            when (Map.null distr || Map.null prod) $ do
+                computeProgress nodeTip >>= throwE . ErrMetricsIsUnsynced
+
+            perfs <- liftIO $
+                readPoolsPerformances db nodeEpoch
+
+            case combineMetrics distr prod perfs of
+                Right x -> return
+                    $ sortOn (Down . apparentPerformance)
+                    $ map (uncurry mkStakePool)
+                    $ Map.toList x
+                Left e ->
+                    throwE $ ErrListStakePoolsMetricsInconsistency e
+        }
+  where
+    poolProductionTip = readPoolProductionCursor db 1 >>= \case
+        [x] -> return $ Just x
+        _ -> return Nothing
+
+    mkStakePool
+        :: PoolId
+        -> ( Quantity "lovelace" Word64
+           , Quantity "block" Word64
+           , Double
+           )
+        -> StakePool
+    mkStakePool poolId (stake, production, apparentPerformance) =
+        StakePool{poolId,stake,production,apparentPerformance}
+
+    computeProgress
+        :: BlockHeader -- ^ The node tip, which respresents 100%.
+        -> ExceptT e IO (Quantity "percent" Percentage)
+    computeProgress nodeTip = liftIO $ do
+        mDbTip <- poolProductionTip
+        Quantity <$> case mDbTip of
+            Nothing -> return minBound
+            Just dbTip -> do
+                liftIO $ logDebug tr $ mconcat
+                    [ "The node tip is:\n"
+                    , pretty nodeTip
+                    , ",\nbut the last pool production stored in the db"
+                    , " is from:\n"
+                    , pretty dbTip
+                    ]
+                return $ progress dbTip nodeTip
+
+    progress :: BlockHeader -> BlockHeader -> Percentage
+    progress tip target =
+        let
+            s0 = getQuantity $ tip ^. #blockHeight
+            s1 = getQuantity $ target ^. #blockHeight
+        in toEnum $ round $ 100 * (toD s0) / (toD s1)
+      where
+        toD :: Integral i => i -> Double
+        toD = fromIntegral
+
+readPoolsPerformances
+    :: DBLayer IO
+    -> EpochNo
+    -> IO (Map PoolId Double)
+readPoolsPerformances db (EpochNo epochNo) = do
+    let range = [max 0 (fromIntegral epochNo - 14) .. fromIntegral epochNo]
+    fmap avg $ forM range $ \ep -> calculatePerformance
+        <$> liftIO (Map.fromList <$> readStakeDistribution db ep)
+        <*> liftIO (count <$> readPoolProduction db ep)
+  where
+    -- | We compute performances on a range of epochs, because we don't know
+    -- whether we have enough details
+    avg :: [Map PoolId Double] -> Map PoolId Double
+    avg performances = Map.map (/ len) $ Map.unionsWith (+) performances
+      where
+        len = fromIntegral (length performances)
+
+-- | Calculate pool performance over the given data.
+calculatePerformance
+    :: Map PoolId (Quantity "lovelace" Word64)
+    -> Map PoolId (Quantity "block" Word64)
+    -> Map PoolId Double
+calculatePerformance mStake mProd =
+    let
+        stakeButNotProd = traverseMissing $ \_ _ -> 0
+        prodButNoStake  = dropMissing
+        stakeAndProd sTotal nTotal = zipWithMatched $ \_ s n ->
+            if (nTotal == 0 || s == Quantity 0) then
+                0
+            else
+                (double n / nTotal) * (sTotal / double s)
+    in
+        Map.merge
+            stakeButNotProd
+            prodButNoStake
+            (stakeAndProd (sumQ mStake) (sumQ mProd))
+            mStake
+            mProd
+  where
+    double :: Integral a => Quantity any a -> Double
+    double (Quantity a) = fromIntegral a
+
+    sumQ :: Integral a => Map k (Quantity any a) -> Double
+    sumQ = fromIntegral . foldl' (\y (Quantity x) -> (y + x)) 0 . Map.elems
+
+-- | Combines three different sources of data into one:
 --
 -- 1. A stake-distribution map
 -- 2. A pool-production map
+-- 3. A pool-performance map
 --
 -- If a pool has produced a block without existing in the stake-distribution,
 -- i.e it exists in (2) but not (1), this function will return
--- @Left ErrMetricsInconsistencyBlockProducerNotInStakeDistr@.
+-- @Left ErrMetricsInconsistency@.
 --
 -- If a pool is in (1) but not (2), it simply means it has produced 0 blocks so
 -- far.
+--
+-- Similarly, if we do have metrics about a pool in (3), but this pool is
+-- unknown from (1) & (2), this function also returns
+-- @Left ErrMetricsInconsistency@.
+--
+-- If a pool is in (1+2) but not in (3), it simply means it has produced 0
+-- blocks so far.
 combineMetrics
     :: Map PoolId (Quantity "lovelace" Word64)
-    -> Map PoolId (Quantity "block" Natural)
+    -> Map PoolId (Quantity "block" Word64)
+    -> Map PoolId Double
     -> Either
         ErrMetricsInconsistency
-        (Map PoolId (Quantity "lovelace" Word64, Quantity "block" Natural))
-combineMetrics =
-    mergeA
-        stakeButNoActivity
-        activityButNoStake
-        stakeAndActivity
+        ( Map PoolId
+            ( Quantity "lovelace" Word64
+            , Quantity "block" Word64
+            , Double
+            )
+        )
+combineMetrics mStake mProd mPerf = do
+    let errMissingLeft = ErrProducerNotInDistribution
+    mActivity <- zipWithRightDefault (,) errMissingLeft (Quantity 0) mStake mProd
+    zipWithRightDefault unzipZip3 errMissingLeft 0 mActivity mPerf
   where
-    stakeButNoActivity = traverseMissing $ \_k stake ->
-        pure (stake, Quantity 0)
-
-    activityButNoStake = traverseMissing $ \pool count ->
-        Left $ ErrProducerNotInDistribution pool count
-
-    stakeAndActivity =
-        zipWithMatched (\_k stake productions -> (stake, productions))
+    unzipZip3 :: (a,b) -> c -> (a,b,c)
+    unzipZip3 (a,b) c = (a,b,c)
 
 -- | Possible errors returned by 'combineMetrics'.
-data ErrMetricsInconsistency
-    = ErrProducerNotInDistribution PoolId (Quantity "block" Natural)
+newtype ErrMetricsInconsistency
+    = ErrProducerNotInDistribution PoolId
         -- ^ Somehow, we tried to combine invalid metrics together and passed
         -- a passed a block production that doesn't match the producers found in
         -- the stake activity.
@@ -310,3 +368,39 @@ data ErrMetricsInconsistency
         -- the distribution but not in the production! (In which case, we'll
         -- assign it a production of '0').
     deriving (Show, Eq)
+
+-- | Combine two maps with the given zipping function. It defaults when elements
+-- of the first map (left) are not present in the second (right), but returns an
+-- error when elements of the second (right) map are not present in the first
+-- (left).
+--
+-- Example:
+--
+-- @
+-- let m1 = Map.fromList [(1, 'a'), (2, 'b'), (3, 'c')]
+-- let m2 = Map.fromList [(2, True)]
+-- @
+--
+-- >>> zipWithRightDefault (,) ErrMissing False m1 m2
+-- Right (Map.fromList [(1, ('a', False)), (2, ('b', True)), (3, ('c', False))])
+--
+-- >>> zipWithRightDefault (,) ErrMissing False m2 m1
+-- Left (ErrMissing 1)
+zipWithRightDefault
+    :: Ord k
+    => (l -> r -> a)
+    -> (k -> errMissingLeft)
+    -> r
+    -> Map k l
+    -> Map k r
+    -> Either errMissingLeft (Map k a)
+zipWithRightDefault combine onMissing rZero =
+    Map.mergeA leftButNotRight rightButNotLeft bothPresent
+  where
+    leftButNotRight = traverseMissing $ \_k l -> pure (combine l rZero)
+    rightButNotLeft = traverseMissing $ \k _r -> Left (onMissing k)
+    bothPresent     = zipWithMatched  $ \_k l r -> (combine l r)
+
+-- | Count elements inside a 'Map'
+count :: Map k [a] -> Map k (Quantity any Word64)
+count = Map.map (Quantity . fromIntegral . length)
--- lib/core/test/unit/Cardano/Pool/MetricsSpec.hs
+++ lib/core/test/unit/Cardano/Pool/MetricsSpec.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 module Cardano.Pool.MetricsSpec (spec) where
 
 import Prelude
@@ -20,56 +21,73 @@ import Cardano.Wallet.Primitive.Types
     , flatSlot
     , fromFlatSlot
     )
+import Data.Map.Strict
+    ( Map )
 import Data.Quantity
     ( Quantity (..) )
 import Data.Word
     ( Word32, Word64 )
-import Numeric.Natural
-    ( Natural )
 import Test.Hspec
     ( Spec, describe, it, shouldBe )
 import Test.QuickCheck
     ( Arbitrary (..)
-    , InfiniteList (..)
+    , Property
     , checkCoverage
     , cover
+    , elements
     , property
-    , withMaxSuccess
+    , vectorOf
     , (===)
     )
 import Test.QuickCheck.Arbitrary.Generic
     ( genericArbitrary, genericShrink )
 
-import qualified Data.ByteString as BS
-import qualified Data.Map as Map
+import qualified Data.ByteString.Char8 as B8
+import qualified Data.Map.Strict as Map
 
 spec :: Spec
 spec = do
-    describe "Counting how many blocks each pool produced" $ do
-        describe "combineMetrics" $ do
-            it "pools with no entry for productions are included" $
-                property $ \stakeDistr -> do
-                    combineMetrics stakeDistr Map.empty
-                    `shouldBe`
-                    (Right $ Map.map (, Quantity 0) stakeDistr)
+    describe "combineMetrics" $ do
+        it "pools with no entry for productions are included" $
+            property $ \stakeDistr -> do
+                combineMetrics stakeDistr Map.empty Map.empty
+                `shouldBe`
+                (Right $ Map.map (, Quantity 0, 0) stakeDistr)
 
-            it "it fails if a block-producer is not in the stake distr" $ do
-                checkCoverage
-                . property
-                . withMaxSuccess 1000
-                $ \stakeDistr poolProd ->
-                    let
-                        aPoolWithoutStakeProduced =
-                            not . Map.null $ Map.difference poolProd stakeDistr
-                    in cover 20 aPoolWithoutStakeProduced
-                        "A pool without stake produced" $
-                       cover 1 (not aPoolWithoutStakeProduced)
-                        "Successfully combined the maps" $
-                        case combineMetrics stakeDistr poolProd of
-                            Left _ ->
-                                aPoolWithoutStakeProduced === True
-                            Right x ->
-                                Map.map fst x === stakeDistr
+        it "it fails if a block-producer is not in the stake distr"
+            $ checkCoverage
+            $ property prop_combineIsLeftBiased
+
+{-------------------------------------------------------------------------------
+                                Properties
+-------------------------------------------------------------------------------}
+
+-- | it fails if a block-producer or performance is not in the stake distr
+prop_combineIsLeftBiased
+    :: Map PoolId (Quantity "lovelace" Word64)
+    -> Map PoolId (Quantity "block" Word64)
+    -> Map PoolId Double
+    -> Property
+prop_combineIsLeftBiased mStake mProd mPerf =
+    let
+        shouldLeft = or
+            [ not . Map.null $ Map.difference mProd mStake
+            , not . Map.null $ Map.difference mPerf mStake
+            ]
+    in
+    cover 20 shouldLeft "A pool without stake produced"
+    $ cover 1 (not shouldLeft) "Successfully combined the maps"
+    $ case combineMetrics mStake mProd mPerf of
+        Left _ ->
+            shouldLeft === True
+        Right x ->
+            Map.map (\(a,_,_) -> a) x === mStake
+{-# HLINT ignore prop_combineIsLeftBiased "Use ||" #-}
+
+
+{-------------------------------------------------------------------------------
+                                 Arbitrary
+-------------------------------------------------------------------------------}
 
 instance Arbitrary BlockHeader where
     arbitrary = BlockHeader
@@ -88,30 +106,20 @@ epochLength :: EpochLength
 epochLength = EpochLength 50
 
 instance Arbitrary (Hash tag) where
-    arbitrary = do
-        InfiniteList bytes _ <- arbitrary
-        return $ Hash $ BS.pack $ take 32 bytes
-    shrink x = [zeros | x /= zeros]
-      where
-        zeros = Hash $ BS.pack $ replicate 32 0
+    shrink _  = []
+    arbitrary = Hash . getPoolId <$> arbitrary
 
 instance Arbitrary Block where
      arbitrary = genericArbitrary
      shrink = genericShrink
 
 instance Arbitrary (Quantity "block" Word32) where
-     arbitrary = Quantity . fromIntegral <$> (arbitrary @Word)
+     arbitrary = Quantity . fromIntegral <$> (arbitrary @Word32)
 
-instance Arbitrary (Quantity "lovelace" Word64) where
-     arbitrary = Quantity . fromIntegral <$> (arbitrary @Word64)
-
-instance Arbitrary (Quantity "block" Natural) where
+instance Arbitrary (Quantity any Word64) where
      arbitrary = Quantity . fromIntegral <$> (arbitrary @Word64)
 
 instance Arbitrary PoolId where
-    arbitrary = do
-        InfiniteList bytes _ <- arbitrary
-        return $ PoolId $ BS.pack $ take 32 bytes
-    shrink x = [zeros | x /= zeros]
-      where
-        zeros = PoolId $ BS.pack $ replicate 32 0
+    shrink _  = []
+    arbitrary =
+        PoolId . B8.pack <$> vectorOf 8 (elements (['a'..'f'] ++ ['0'..'9']))
