ad3cd33cd2e0522c96829b7c0584e75a86965236
Piotr Stachyra <piotr.stachyra@iohk.io>
2019-11-06 15:44:47 +0100

    indentation fixes

--- lib/jormungandr/test/integration/Test/Integration/Jormungandr/Scenario/API/StakePools.hs
+++ lib/jormungandr/test/integration/Test/Integration/Jormungandr/Scenario/API/StakePools.hs
@@ -109,14 +109,16 @@ spec = do
             expectErrorMessage errMsg405 r
 
     it "STAKE_POOLS_JOIN_01 - Can join a stakepool" $ \ctx -> do
-        (_, p:_) <- eventually' $ unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
+        (_, p:_) <- eventually' $
+            unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
         w <- emptyWallet ctx
         r <- joinStakePool ctx p (w, "Secure Passphrase")
         expectResponseCode HTTP.status501 r
 
     it "STAKE_POOLS_JOIN_02 - I cannot join another until I quit\
         \ or maybe I will just re-join another?" $ \ctx -> do
-        (_, p1:p2:_) <- eventually' $ unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
+        (_, p1:p2:_) <- eventually' $
+            unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
         w <- emptyWallet ctx
         r1 <- joinStakePool ctx p1 (w, "Secure Passprase")
         expectResponseCode HTTP.status501 r1
@@ -125,7 +127,8 @@ spec = do
         expectResponseCode HTTP.status501 r2
 
     it "STAKE_POOLS_JOIN_02 - I definitely can quit and join another" $ \ctx -> do
-        (_, p1:p2:_) <- eventually' $ unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
+        (_, p1:p2:_) <- eventually' $
+            unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
         w <- emptyWallet ctx
         r1 <- joinStakePool ctx p1 (w, "Secure Passprase")
         expectResponseCode HTTP.status501 r1
@@ -137,18 +140,21 @@ spec = do
         expectResponseCode HTTP.status501 r2
 
     it "STAKE_POOLS_JOIN_03 - Passphrase must be correct to join" $ \ctx -> do
-        (_, p:_) <- eventually' $ unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
+        (_, p:_) <- eventually' $
+            unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
         w <- emptyWallet ctx
         r <- joinStakePool ctx p (w, "Incorrect Passphrase")
         expectResponseCode HTTP.status501 r
 
-    describe "STAKE_POOLS_JOIN/QUIT_03 - Passphrase must have appropriate length" $ do
+    describe "STAKE_POOLS_JOIN/QUIT_03 -\
+        \ Passphrase must have appropriate length" $ do
+
         let pMax = passphraseMaxLength
         let pMin = passphraseMinLength
-        let tooShort = "passphrase is too short: expected at\
-                \ least 10 characters"
-        let tooLong = "passphrase is too long: expected at\
-                \ most 255 characters"
+        let tooShort =
+            "passphrase is too short: expected at least 10 characters"
+        let tooLong =
+            "passphrase is too long: expected at most 255 characters"
         let tests =
                 [ (tooLong, replicate (pMax + 1) '1')
                 , (tooShort, replicate (pMin - 1) '1')
@@ -161,20 +167,24 @@ spec = do
                 expectResponseCode HTTP.status400 r
                 expectErrorMessage expec r
 
-        forM_ tests $ \(expec, passphrase) -> it ("Join: " ++ expec) $ \ctx -> do
-            verifyIt ctx joinStakePool passphrase expec
+        forM_ tests $ \(expec, passphrase) ->
+            it ("Join: " ++ expec) $ \ctx -> do
+                verifyIt ctx joinStakePool passphrase expec
 
-        forM_ tests $ \(expec, passphrase) -> it ("Quit: " ++ expec) $ \ctx -> do
-            verifyIt ctx quitStakePool passphrase expec
+        forM_ tests $ \(expec, passphrase) ->
+            it ("Quit: " ++ expec) $ \ctx -> do
+                verifyIt ctx quitStakePool passphrase expec
 
     it "STAKE_POOLS_JOIN_04 - Byron wallet cannot join stake pool" $ \ctx -> do
-        (_, p:_) <- eventually' $ unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
+        (_, p:_) <- eventually' $
+            unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
         w <- emptyByronWallet ctx
         r <- joinStakePool ctx p (w, "Secure Passprase")
         expectResponseCode HTTP.status501 r
 
     it "STAKE_POOLS_QUIT_01 - Can quit stake pool" $ \ctx -> do
-        (_, p:_) <- eventually' $ unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
+        (_, p:_) <- eventually' $
+            unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
         w <- emptyWallet ctx
         r <- joinStakePool ctx p (w, "Secure Passprase")
         expectResponseCode HTTP.status501 r
@@ -183,14 +193,16 @@ spec = do
         expectResponseCode HTTP.status501 rq
 
     it "STAKE_POOLS_QUIT_01 - Quiting before even joining" $ \ctx -> do
-        (_, p:_) <- eventually' $ unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
+        (_, p:_) <- eventually' $
+            unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
         w <- emptyWallet ctx
 
         rq <- quitStakePool ctx p (w, "Secure Passprase")
         expectResponseCode HTTP.status501 rq
 
     it "STAKE_POOLS_QUIT_03 - Passphrase must be correct to quit" $ \ctx -> do
-        (_, p:_) <- eventually' $ unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
+        (_, p:_) <- eventually' $
+            unsafeRequest @[ApiStakePool] ctx listStakePoolsEp Empty
         w <- emptyWallet ctx
         joinStakePool ctx p (w, "Secure Passprase")
             >>= (expectResponseCode HTTP.status501)
