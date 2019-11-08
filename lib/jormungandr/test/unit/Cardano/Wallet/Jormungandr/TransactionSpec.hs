2e7fa8b4b4f65201a3d80c25a28625c26f7ab52d
KtorZ <matthias.benkort@gmail.com>
2019-11-08 11:22:59 +0100

    re-generate golden tests using jcli@0.7.0-rc5

--- lib/jormungandr/test/unit/Cardano/Wallet/Jormungandr/TransactionSpec.hs
+++ lib/jormungandr/test/unit/Cardano/Wallet/Jormungandr/TransactionSpec.hs
@@ -148,7 +148,7 @@ mkStdTxSpec = do
         --
         -- echo $XPRV0 \
         --   | jcli key from-bytes --type ed25519Extended \
-        --   | jcli transaction make-witness $(jcli transaction id --staging tx) \
+        --   | jcli transaction make-witness $(jcli transaction data-for-witness --staging tx) \
         --      --genesis-block-hash $BLOCK0 --type utxo > wit0.bin
         --
         -- cat tx \
@@ -160,12 +160,12 @@ mkStdTxSpec = do
             [ TxOut addr1 (Coin 14)
             , TxOut addr2 (Coin 9986)
             ]
-            "00bf020102000000000000002710666984dec4bc0ff1888be97bfe0694a96b35c5\
-            \8d025405ead51d5cc72a3019f403b2ddcfbf631000216a6e0410e64c20e6af91bb\
-            \b2826276ad6bfda777e766b24b000000000000000e0302ada8baca6d3a889a893d\
-            \a29aa8a291460d7ec95b36404bf094c1a72f2775a60000000000002702012c36bb\
-            \92280d2f627e85bf4ce363214f3a20f35944867ea74f38559ac6f34906fdbc9c33\
-            \84cb86e49ea8c2aacb066d75db18171deaed7323665aef25505e5701"
+            "00c002000102000000000000002710666984dec4bc0ff1888be97bfe0694a96b35\
+            \c58d025405ead51d5cc72a3019f403b2ddcfbf631000216a6e0410e64c20e6af91\
+            \bbb2826276ad6bfda777e766b24b000000000000000e0302ada8baca6d3a889a89\
+            \3da29aa8a291460d7ec95b36404bf094c1a72f2775a60000000000002702012c36\
+            \bb92280d2f627e85bf4ce363214f3a20f35944867ea74f38559ac6f34906fdbc9c\
+            \3384cb86e49ea8c2aacb066d75db18171deaed7323665aef25505e5701"
 
         -- jcli transaction new \
         --   | jcli transaction add-input $TXIN0 0 10000 \
@@ -178,12 +178,12 @@ mkStdTxSpec = do
         --
         -- echo $XPRV0 \
         --   | jcli key from-bytes --type ed25519Extended \
-        --   | jcli transaction make-witness $(jcli transaction id --staging tx) \
+        --   | jcli transaction make-witness $(jcli transaction data-for-witness --staging tx) \
         --      --genesis-block-hash $BLOCK0 --type utxo > wit0.bin
         --
         -- echo $XPRV1 \
         --   | jcli key from-bytes --type ed25519Extended \
-        --   | jcli transaction make-witness $(jcli transaction id --staging tx) \
+        --   | jcli transaction make-witness $(jcli transaction data-for-witness --staging tx) \
         --      --genesis-block-hash $BLOCK0 --type utxo > wit1.bin
         --
         -- cat tx \
@@ -199,17 +199,17 @@ mkStdTxSpec = do
             , TxOut addr1 (Coin 42)
             , TxOut addr2 (Coin 1337)
             ]
-            "0152020203000000000000002710666984dec4bc0ff1888be97bfe0694a96b35c5\
-            \8d025405ead51d5cc72a3019f401000000003b9ac9ff1323856bc91c49e928f6f3\
-            \0f4e8d665d53eb4ab6028bd0ac971809d514c92db1032d155d07184387ba6ff883\
-            \6014ba0b626b1808e67e75dfa370c2dc5f581715fe000000000001869f03b2ddcf\
-            \bf631000216a6e0410e64c20e6af91bbb2826276ad6bfda777e766b24b00000000\
-            \0000002a0302ada8baca6d3a889a893da29aa8a291460d7ec95b36404bf094c1a7\
-            \2f2775a6000000000000053901815cc9a2ad72e0bc5dc76f6152c38761fc294325\
-            \29c55b48973b540c63eb8e5597d8b59de5adc25c2e4e33212ef326e044e6266c3d\
-            \8e5e65d110abd63d39460501d28ca6334756ddd335eaada4a7491373b4d7206978\
-            \080f5ac10474c7f6decbbc35f1620817b15bf1594981c034b7f6a15d0a24ec743d\
-            \332f1774eb8733a5d40c"
+            "015302000203000000000000002710666984dec4bc0ff1888be97bfe0694a96b35\
+            \c58d025405ead51d5cc72a3019f401000000003b9ac9ff1323856bc91c49e928f6\
+            \f30f4e8d665d53eb4ab6028bd0ac971809d514c92db1032d155d07184387ba6ff8\
+            \836014ba0b626b1808e67e75dfa370c2dc5f581715fe000000000001869f03b2dd\
+            \cfbf631000216a6e0410e64c20e6af91bbb2826276ad6bfda777e766b24b000000\
+            \000000002a0302ada8baca6d3a889a893da29aa8a291460d7ec95b36404bf094c1\
+            \a72f2775a6000000000000053901815cc9a2ad72e0bc5dc76f6152c38761fc2943\
+            \2529c55b48973b540c63eb8e5597d8b59de5adc25c2e4e33212ef326e044e6266c\
+            \3d8e5e65d110abd63d39460501d28ca6334756ddd335eaada4a7491373b4d72069\
+            \78080f5ac10474c7f6decbbc35f1620817b15bf1594981c034b7f6a15d0a24ec74\
+            \3d332f1774eb8733a5d40c"
 
     describe "mkStdTx (legacy) 'Mainnet" $ do
         let tl = newTransactionLayer @'Mainnet block0
@@ -229,22 +229,56 @@ mkStdTxSpec = do
                 , (addrRnd1, (xprvRnd1, pwdRnd1))
                 ]
 
-        -- See 'Mainnet description
+        -- jcli transaction new \
+        --   | jcli transaction add-input $TXIN0 0 10000 \
+        --   | jcli transaction add-output $ADDR1 14 \
+        --   | jcli transaction finalize $ADDR2 \
+        --   > tx
+        --
+        -- echo $XPRVRND0 \
+        --   | jcli transaction make-witness $(jcli transaction data-for-witness --staging tx) \
+        --      --genesis-block-hash $BLOCK0 --type legacy-utxo > wit0.bin
+        --
+        -- cat tx \
+        --   | jcli transaction add-witness wit0.bin \
+        --   | jcli transaction seal \
+        --   | jcli transaction to-message
         goldenTestStdTx tl keystore
             [ (TxIn txin0 0, TxOut addrRnd0 (Coin 10000)) ]
             [ TxOut addr1 (Coin 14)
             , TxOut addr2 (Coin 9986)
             ]
-            "00ff020102000000000000002710666984dec4bc0ff1888be97bfe0694a96b35c5\
-            \8d025405ead51d5cc72a3019f403b2ddcfbf631000216a6e0410e64c20e6af91bb\
-            \b2826276ad6bfda777e766b24b000000000000000e0302ada8baca6d3a889a893d\
-            \a29aa8a291460d7ec95b36404bf094c1a72f2775a6000000000000270200260e3f\
-            \0867d341caf86f9b7bd9689f1b21f04dad05711ea3594cc441f159a723537824ae\
-            \e5526bd50c6cc32113911bd591e3a3601326513ed88151ce8dbdfa488754e00ab7\
-            \e4f1e8d7534cd654b82b3eab841bb387113e46f9902519a94f9296b83a5fb99793\
-            \4c1cba5ccefc42e55b59c70752b356a77307f1a32e686a42970c"
+            "010002000102000000000000002710666984dec4bc0ff1888be97bfe0694a96b35\
+            \c58d025405ead51d5cc72a3019f403b2ddcfbf631000216a6e0410e64c20e6af91\
+            \bbb2826276ad6bfda777e766b24b000000000000000e0302ada8baca6d3a889a89\
+            \3da29aa8a291460d7ec95b36404bf094c1a72f2775a6000000000000270200260e\
+            \3f0867d341caf86f9b7bd9689f1b21f04dad05711ea3594cc441f159a723537824\
+            \aee5526bd50c6cc32113911bd591e3a3601326513ed88151ce8dbdfa488754e00a\
+            \b7e4f1e8d7534cd654b82b3eab841bb387113e46f9902519a94f9296b83a5fb997\
+            \934c1cba5ccefc42e55b59c70752b356a77307f1a32e686a42970c"
 
-        -- See 'Mainnet description
+        -- jcli transaction new \
+        --   | jcli transaction add-input $TXIN0 0 10000 \
+        --   | jcli transaction add-input $TXIN1 1 999999999 \
+        --   | jcli transaction add-output $ADDR0 99999 \
+        --   | jcli transaction add-output $ADDR1 42 \
+        --   | jcli transaction add-output $ADDR2 1337 \
+        --   | jcli transaction finalize \
+        --   > tx
+        --
+        -- echo $XPRVRND0 \
+        --   | jcli transaction make-witness $(jcli transaction data-for-witness --staging tx) \
+        --      --genesis-block-hash $BLOCK0 --type legacy-utxo > wit0.bin
+        --
+        -- echo $XPRVRND1 \
+        --   | jcli transaction make-witness $(jcli transaction data-for-witness --staging tx) \
+        --      --genesis-block-hash $BLOCK0 --type legacy-utxo > wit1.bin
+        --
+        -- cat tx \
+        --   | jcli transaction add-witness wit0.bin \
+        --   | jcli transaction add-witness wit1.bin \
+        --   | jcli transaction seal \
+        --   | jcli transaction to-message
         goldenTestStdTx tl keystore
             [ (TxIn txin0 0, TxOut addrRnd0 (Coin 10000))
             , (TxIn txin1 1, TxOut addrRnd1 (Coin 999999999))
@@ -253,21 +287,21 @@ mkStdTxSpec = do
             , TxOut addr1 (Coin 42)
             , TxOut addr2 (Coin 1337)
             ]
-            "01d2020203000000000000002710666984dec4bc0ff1888be97bfe0694a96b35c5\
-            \8d025405ead51d5cc72a3019f401000000003b9ac9ff1323856bc91c49e928f6f3\
-            \0f4e8d665d53eb4ab6028bd0ac971809d514c92db1032d155d07184387ba6ff883\
-            \6014ba0b626b1808e67e75dfa370c2dc5f581715fe000000000001869f03b2ddcf\
-            \bf631000216a6e0410e64c20e6af91bbb2826276ad6bfda777e766b24b00000000\
-            \0000002a0302ada8baca6d3a889a893da29aa8a291460d7ec95b36404bf094c1a7\
-            \2f2775a6000000000000053900260e3f0867d341caf86f9b7bd9689f1b21f04dad\
-            \05711ea3594cc441f159a723537824aee5526bd50c6cc32113911bd591e3a36013\
-            \26513ed88151ce8dbdfa4817c80704772729b5c0b2583abe0a3e675e2227879a74\
-            \52882ba9733baa03bfb9ec2ff75e086fdf8f8fd89c2c3ef9b9c4052e7096dd9fc1\
-            \2fc22541394a0b3d0d000205b66108c02e4d6dbc4c70adaa27d22ea2adeffe0de0\
-            \09c099ac230b520917d2c23ec4bf897f4fd236c676988b62877259f81bd9f08d43\
-            \55d5fe6b96cda06db052067dafcc5b525efe2bd9d03d21c356aa1b82887126a218\
-            \5231c749a117d70641583fe739d6b59fab049028ad68cbe935910251eb4aacb803\
-            \5353e609e400"
+            "01d302000203000000000000002710666984dec4bc0ff1888be97bfe0694a96b35\
+            \c58d025405ead51d5cc72a3019f401000000003b9ac9ff1323856bc91c49e928f6\
+            \f30f4e8d665d53eb4ab6028bd0ac971809d514c92db1032d155d07184387ba6ff8\
+            \836014ba0b626b1808e67e75dfa370c2dc5f581715fe000000000001869f03b2dd\
+            \cfbf631000216a6e0410e64c20e6af91bbb2826276ad6bfda777e766b24b000000\
+            \000000002a0302ada8baca6d3a889a893da29aa8a291460d7ec95b36404bf094c1\
+            \a72f2775a6000000000000053900260e3f0867d341caf86f9b7bd9689f1b21f04d\
+            \ad05711ea3594cc441f159a723537824aee5526bd50c6cc32113911bd591e3a360\
+            \1326513ed88151ce8dbdfa4817c80704772729b5c0b2583abe0a3e675e2227879a\
+            \7452882ba9733baa03bfb9ec2ff75e086fdf8f8fd89c2c3ef9b9c4052e7096dd9f\
+            \c12fc22541394a0b3d0d000205b66108c02e4d6dbc4c70adaa27d22ea2adeffe0d\
+            \e009c099ac230b520917d2c23ec4bf897f4fd236c676988b62877259f81bd9f08d\
+            \4355d5fe6b96cda06db052067dafcc5b525efe2bd9d03d21c356aa1b82887126a2\
+            \185231c749a117d70641583fe739d6b59fab049028ad68cbe935910251eb4aacb8\
+            \035353e609e400"
 
     describe "mkStdTx 'Testnet" $ do
         let tl = newTransactionLayer @'Testnet block0
@@ -292,12 +326,12 @@ mkStdTxSpec = do
             [ TxOut addr1 (Coin 14)
             , TxOut addr2 (Coin 9986)
             ]
-            "00bf020102000000000000002710666984dec4bc0ff1888be97bfe0694a96b35c5\
-            \8d025405ead51d5cc72a3019f483b2ddcfbf631000216a6e0410e64c20e6af91bb\
-            \b2826276ad6bfda777e766b24b000000000000000e8302ada8baca6d3a889a893d\
-            \a29aa8a291460d7ec95b36404bf094c1a72f2775a6000000000000270201c2c564\
-            \f812120d6c6efdaeb1781b5db14d0f109d749db39083a65d28c123eb1fc7b7762a\
-            \dd66f34c185212b4a4133b9fdc857487cc4497fc356ed01e91726c0d"
+            "00c002000102000000000000002710666984dec4bc0ff1888be97bfe0694a96b35\
+            \c58d025405ead51d5cc72a3019f483b2ddcfbf631000216a6e0410e64c20e6af91\
+            \bbb2826276ad6bfda777e766b24b000000000000000e8302ada8baca6d3a889a89\
+            \3da29aa8a291460d7ec95b36404bf094c1a72f2775a6000000000000270201c2c5\
+            \64f812120d6c6efdaeb1781b5db14d0f109d749db39083a65d28c123eb1fc7b776\
+            \2add66f34c185212b4a4133b9fdc857487cc4497fc356ed01e91726c0d"
 
         -- See 'Mainnet description
         goldenTestStdTx tl keystore
@@ -308,17 +342,17 @@ mkStdTxSpec = do
             , TxOut addr1 (Coin 42)
             , TxOut addr2 (Coin 1337)
             ]
-            "0152020203000000000000002710666984dec4bc0ff1888be97bfe0694a96b35c5\
-            \8d025405ead51d5cc72a3019f401000000003b9ac9ff1323856bc91c49e928f6f3\
-            \0f4e8d665d53eb4ab6028bd0ac971809d514c92db1832d155d07184387ba6ff883\
-            \6014ba0b626b1808e67e75dfa370c2dc5f581715fe000000000001869f83b2ddcf\
-            \bf631000216a6e0410e64c20e6af91bbb2826276ad6bfda777e766b24b00000000\
-            \0000002a8302ada8baca6d3a889a893da29aa8a291460d7ec95b36404bf094c1a7\
-            \2f2775a60000000000000539015e70794cd622240a1482f7aae5cd2abd4054e106\
-            \f0d590e37f0a4555a58ebcedbae7ba8de6bf952d480311c7f325e99f84b1b312b0\
-            \255545d62c97b00a4b16040137bd006254850d77ac2fcb273d6b9b6076133a3660\
-            \d6bd5c813d6f3f454f4935b009eabbac1ffd768a07e37773e5b568e8c76c57e53d\
-            \a6b26d3da9f466b78200"
+            "015302000203000000000000002710666984dec4bc0ff1888be97bfe0694a96b35\
+            \c58d025405ead51d5cc72a3019f401000000003b9ac9ff1323856bc91c49e928f6\
+            \f30f4e8d665d53eb4ab6028bd0ac971809d514c92db1832d155d07184387ba6ff8\
+            \836014ba0b626b1808e67e75dfa370c2dc5f581715fe000000000001869f83b2dd\
+            \cfbf631000216a6e0410e64c20e6af91bbb2826276ad6bfda777e766b24b000000\
+            \000000002a8302ada8baca6d3a889a893da29aa8a291460d7ec95b36404bf094c1\
+            \a72f2775a60000000000000539015e70794cd622240a1482f7aae5cd2abd4054e1\
+            \06f0d590e37f0a4555a58ebcedbae7ba8de6bf952d480311c7f325e99f84b1b312\
+            \b0255545d62c97b00a4b16040137bd006254850d77ac2fcb273d6b9b6076133a36\
+            \60d6bd5c813d6f3f454f4935b009eabbac1ffd768a07e37773e5b568e8c76c57e5\
+            \3da6b26d3da9f466b78200"
 
     describe "mkStdTx (legacy) 'Testnet" $ do
         let tl = newTransactionLayer @'Testnet block0
@@ -345,14 +379,14 @@ mkStdTxSpec = do
             [ TxOut addr1 (Coin 14)
             , TxOut addr2 (Coin 9986)
             ]
-            "00ff020102000000000000002710666984dec4bc0ff1888be97bfe0694a96b35c5\
-            \8d025405ead51d5cc72a3019f483b2ddcfbf631000216a6e0410e64c20e6af91bb\
-            \b2826276ad6bfda777e766b24b000000000000000e8302ada8baca6d3a889a893d\
-            \a29aa8a291460d7ec95b36404bf094c1a72f2775a6000000000000270200260e3f\
-            \0867d341caf86f9b7bd9689f1b21f04dad05711ea3594cc441f159a723537824ae\
-            \e5526bd50c6cc32113911bd591e3a3601326513ed88151ce8dbdfa486fb2c7b4bf\
-            \45910e1a07f26209654a00541d918a49f0339bb820da724f165df1c647ccfc6e5c\
-            \de86dd31e154b7cc6beef06d561359a2d102f8b3de15b5990007"
+            "010002000102000000000000002710666984dec4bc0ff1888be97bfe0694a96b35\
+            \c58d025405ead51d5cc72a3019f483b2ddcfbf631000216a6e0410e64c20e6af91\
+            \bbb2826276ad6bfda777e766b24b000000000000000e8302ada8baca6d3a889a89\
+            \3da29aa8a291460d7ec95b36404bf094c1a72f2775a6000000000000270200260e\
+            \3f0867d341caf86f9b7bd9689f1b21f04dad05711ea3594cc441f159a723537824\
+            \aee5526bd50c6cc32113911bd591e3a3601326513ed88151ce8dbdfa486fb2c7b4\
+            \bf45910e1a07f26209654a00541d918a49f0339bb820da724f165df1c647ccfc6e\
+            \5cde86dd31e154b7cc6beef06d561359a2d102f8b3de15b5990007"
 
         -- See 'Mainnet description
         goldenTestStdTx tl keystore
@@ -363,21 +397,21 @@ mkStdTxSpec = do
             , TxOut addr1 (Coin 42)
             , TxOut addr2 (Coin 1337)
             ]
-            "01d2020203000000000000002710666984dec4bc0ff1888be97bfe0694a96b35c5\
-            \8d025405ead51d5cc72a3019f401000000003b9ac9ff1323856bc91c49e928f6f3\
-            \0f4e8d665d53eb4ab6028bd0ac971809d514c92db1832d155d07184387ba6ff883\
-            \6014ba0b626b1808e67e75dfa370c2dc5f581715fe000000000001869f83b2ddcf\
-            \bf631000216a6e0410e64c20e6af91bbb2826276ad6bfda777e766b24b00000000\
-            \0000002a8302ada8baca6d3a889a893da29aa8a291460d7ec95b36404bf094c1a7\
-            \2f2775a6000000000000053900260e3f0867d341caf86f9b7bd9689f1b21f04dad\
-            \05711ea3594cc441f159a723537824aee5526bd50c6cc32113911bd591e3a36013\
-            \26513ed88151ce8dbdfa48b1c5041340c009641039e1dcdf436ecc2e7f219d3dd3\
-            \aa93b5841662b45e04e8232050f3c42cb64ca45219deb841ecef026488fbf0d653\
-            \2589c4e50fd7a11d00000205b66108c02e4d6dbc4c70adaa27d22ea2adeffe0de0\
-            \09c099ac230b520917d2c23ec4bf897f4fd236c676988b62877259f81bd9f08d43\
-            \55d5fe6b96cda06db22f7d78f0ff0ec3a7c7fbc3da777a6aea6aeddd8ffef0beb8\
-            \0afd3ba2dd9972585ae74b5876cca3301cee381daf5f9e10b5f8da2f2b9eabba26\
-            \bc5c0f01b803"
+            "01d302000203000000000000002710666984dec4bc0ff1888be97bfe0694a96b35\
+            \c58d025405ead51d5cc72a3019f401000000003b9ac9ff1323856bc91c49e928f6\
+            \f30f4e8d665d53eb4ab6028bd0ac971809d514c92db1832d155d07184387ba6ff8\
+            \836014ba0b626b1808e67e75dfa370c2dc5f581715fe000000000001869f83b2dd\
+            \cfbf631000216a6e0410e64c20e6af91bbb2826276ad6bfda777e766b24b000000\
+            \000000002a8302ada8baca6d3a889a893da29aa8a291460d7ec95b36404bf094c1\
+            \a72f2775a6000000000000053900260e3f0867d341caf86f9b7bd9689f1b21f04d\
+            \ad05711ea3594cc441f159a723537824aee5526bd50c6cc32113911bd591e3a360\
+            \1326513ed88151ce8dbdfa48b1c5041340c009641039e1dcdf436ecc2e7f219d3d\
+            \d3aa93b5841662b45e04e8232050f3c42cb64ca45219deb841ecef026488fbf0d6\
+            \532589c4e50fd7a11d00000205b66108c02e4d6dbc4c70adaa27d22ea2adeffe0d\
+            \e009c099ac230b520917d2c23ec4bf897f4fd236c676988b62877259f81bd9f08d\
+            \4355d5fe6b96cda06db22f7d78f0ff0ec3a7c7fbc3da777a6aea6aeddd8ffef0be\
+            \b80afd3ba2dd9972585ae74b5876cca3301cee381daf5f9e10b5f8da2f2b9eabba\
+            \26bc5c0f01b803"
 
     describe "mkStdTx unknown input" $ do
         unknownInputTest (Proxy @'Mainnet) block0
