4325914db60c658a03c52f154e915083a9d44044
Jonathan Knowles <jonathan.knowles@iohk.io>
2019-11-11 09:06:29 +0000

    Add decoding unit tests for example account state JSON objects.

--- lib/jormungandr/test/unit/Cardano/Wallet/Jormungandr/ApiSpec.hs
+++ lib/jormungandr/test/unit/Cardano/Wallet/Jormungandr/ApiSpec.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
@@ -11,21 +12,106 @@ module Cardano.Wallet.Jormungandr.ApiSpec
 import Prelude
 
 import Cardano.Wallet.Jormungandr.Api.Types
-    ( ApiStakeDistribution (..), ApiT (..), StakeApiResponse (..) )
+    ( ApiAccountDelegationInfo (..)
+    , ApiAccountState (..)
+    , ApiStakeDistribution (..)
+    , ApiT (..)
+    , StakeApiResponse (..)
+    )
 import Cardano.Wallet.Primitive.Types
     ( PoolId (..) )
 import Data.Aeson
     ( eitherDecode )
+import Data.Aeson.QQ
+    ( aesonQQ )
 import Data.Quantity
     ( Quantity (..) )
+import Data.Text
+    ( Text )
+import Data.Text.Class
+    ( FromText (..), ToText (..) )
 import Test.Hspec
     ( Spec, describe, it, shouldBe )
 
+import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy as BL
 
 spec :: Spec
 spec = do
     describe "Jormungandr Api" $ do
+
+        describe "Example account state objects are properly decoded" $ do
+
+            let testBalance = 1000
+            let testTransactionCount = 1
+            let testPoolRatio1 = 1
+            let testPoolRatio2 = 10
+
+            it "With 0 stake pools" $ do
+
+                let testAccountState = Aeson.encode [aesonQQ|
+                        { "value": #{testBalance}
+                        , "counter": #{testTransactionCount}
+                        , "delegation": {"pools": []}
+                        }|]
+                eitherDecode testAccountState `shouldBe` Right ApiAccountState
+                    { currentBalance =
+                        ApiT (Quantity testBalance)
+                    , totalTransactionCount =
+                        ApiT (Quantity testTransactionCount)
+                    , delegationInfo = ApiAccountDelegationInfo
+                        { stakePools = [] }
+                    }
+
+            it "With 1 stake pool" $ do
+
+                let testAccountState = Aeson.encode [aesonQQ|
+                        { "value": #{testBalance}
+                        , "counter": #{testTransactionCount}
+                        , "delegation":
+                            { "pools":
+                                [ [#{toText testPoolId1}, #{testPoolRatio1}] ]
+                            }
+                        }|]
+                eitherDecode testAccountState `shouldBe` Right ApiAccountState
+                    { currentBalance =
+                        ApiT (Quantity testBalance)
+                    , totalTransactionCount =
+                        ApiT (Quantity testTransactionCount)
+                    , delegationInfo = ApiAccountDelegationInfo
+                        { stakePools =
+                            [ ( ApiT testPoolId1
+                              , ApiT (Quantity testPoolRatio1))
+                            ]
+                        }
+                    }
+
+            it "With n stake pools" $ do
+
+                let testAccountState = Aeson.encode [aesonQQ|
+                        { "value": #{testBalance}
+                        , "counter": #{testTransactionCount}
+                        , "delegation":
+                            { "pools":
+                                [ [#{toText testPoolId1}, #{testPoolRatio1}]
+                                , [#{toText testPoolId2}, #{testPoolRatio2}]
+                                ]
+                            }
+                        }|]
+                eitherDecode testAccountState `shouldBe` Right ApiAccountState
+                    { currentBalance =
+                        ApiT (Quantity testBalance)
+                    , totalTransactionCount =
+                        ApiT (Quantity testTransactionCount)
+                    , delegationInfo = ApiAccountDelegationInfo
+                        { stakePools =
+                            [ ( ApiT testPoolId1
+                              , ApiT (Quantity testPoolRatio1))
+                            , ( ApiT testPoolId2
+                              , ApiT (Quantity testPoolRatio2))
+                            ]
+                        }
+                    }
 
         it "example stake endpoint response is properly decoded" $ do
             let exampleStake = "{\"epoch\": 252054,\"stake\": {\"dangling\":0,\"\
@@ -111,3 +197,20 @@ spec = do
             return ()
   where
     decodeJSON = eitherDecode :: BL.ByteString -> Either String StakeApiResponse
+
+{-------------------------------------------------------------------------------
+                                  Test data
+-------------------------------------------------------------------------------}
+
+testPoolId1 :: PoolId
+testPoolId1 = mkTestPoolId
+    "c780f14f9782770014d8bcd514b1bc664653d15f73a7158254730c6e1aa9f356"
+
+testPoolId2 :: PoolId
+testPoolId2 = mkTestPoolId
+    "653f9aa1e6c0374528517a37f51d356466cb1b415dcb8d4100772879f41f087c"
+
+mkTestPoolId :: Text -> PoolId
+mkTestPoolId = either (error textError) id . fromText
+  where
+    textError = "Unable to construct stake pool ID."
