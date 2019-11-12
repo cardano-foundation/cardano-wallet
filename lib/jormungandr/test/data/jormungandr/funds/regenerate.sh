08ab2171859676a801060300349c64ef108caec7
KtorZ <matthias.benkort@gmail.com>
2019-11-12 17:43:31 +0100

    re-generate delegation certs and integration setup to match 0.7.0

--- lib/jormungandr/test/data/jormungandr/funds/regenerate.sh
+++ lib/jormungandr/test/data/jormungandr/funds/regenerate.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 # First argument is the index for the account. (e.g. "1")
 # Second argument is the folder-name for the stake-pool (e.g. "a")
 delegate () {
-  jcli certificate new stake-delegation $(cat ../stake_pools/$2/pool_id) $(cat account$1.pub) > stake_delegation$1.cert
+  jcli certificate new stake-delegation $(cat account$1.pub) $(cat ../stake_pools/$2/pool_id) > stake_delegation$1.cert
   cat stake_delegation$1.cert | jcli certificate sign -k account$1.prv > stake_delegation$1.signedcert
 }
 
