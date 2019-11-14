f705eaa5c0be1148c12711ddc4d23dd58f3d68e9
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-11-14 22:02:49 +1000

    Bring back DaedalusIPC integration test

--- lib/jormungandr/test/integration/Test/Integration/Jormungandr/Scenario/CLI/Server.hs
+++ lib/jormungandr/test/integration/Test/Integration/Jormungandr/Scenario/CLI/Server.hs
@@ -37,7 +37,7 @@ import System.Process
     , withCreateProcess
     )
 import Test.Hspec
-    ( SpecWith, describe, it, pendingWith, runIO )
+    ( SpecWith, describe, it, runIO )
 import Test.Hspec.Expectations.Lifted
     ( shouldBe, shouldContain, shouldReturn )
 import Test.Integration.Framework.DSL
@@ -86,14 +86,12 @@ spec = do
         let filepath = "test/integration/js/mock-daedalus.js"
 
         it "Should reply with the port --random" $ \ctx -> do
-            pendingWith "Seems to cause some sort of race condition with --coverage"
             let scriptArgs = defaultArgs (ctx ^. typed @(Port "node"))
                     ++ ["--random-port"]
             (_, _, _, ph) <- createProcess (proc filepath scriptArgs)
             waitForProcess ph `shouldReturn` ExitSuccess
 
         it "Should reply with the port --random" $ \ctx -> do
-            pendingWith "Seems to cause some sort of race condition with --coverage"
             walletPort <- findPort
             let scriptArgs = defaultArgs (ctx ^. typed @(Port "node"))
                     ++ ["--port", show walletPort]
