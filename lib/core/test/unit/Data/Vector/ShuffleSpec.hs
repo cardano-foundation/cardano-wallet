7c95cec010651a4a5cad5e4c29c2d1451b8cdec0
KtorZ <matthias.benkort@gmail.com>
2019-12-03 15:38:29 +0100

    implement 'mkSeed' to generate a seed from a text
    This would be pretty useful to bridge use any entity (e.g. WalletId) with a
    ToText instance as a seed for such generator

--- lib/core/test/unit/Data/Vector/ShuffleSpec.hs
+++ lib/core/test/unit/Data/Vector/ShuffleSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Data.Vector.ShuffleSpec
     ( spec
     ) where
@@ -5,16 +7,27 @@ module Data.Vector.ShuffleSpec
 import Prelude
 
 import Data.Vector.Shuffle
-    ( shuffle )
+    ( mkSeed, shuffle, shuffleWith )
 import Test.Hspec
     ( Spec, describe, it )
 import Test.QuickCheck
-    ( Confidence (..), NonEmptyList (..), Property, checkCoverageWith, cover )
+    ( Confidence (..)
+    , NonEmptyList (..)
+    , Positive (..)
+    , PrintableString (..)
+    , Property
+    , arbitrary
+    , checkCoverageWith
+    , cover
+    , label
+    , vectorOf
+    , (==>)
+    )
 import Test.QuickCheck.Monadic
-    ( monadicIO, run )
+    ( assert, monadicIO, monitor, pick, run )
 
 import qualified Data.List as L
-
+import qualified Data.Text as T
 
 spec :: Spec
 spec = do
@@ -25,6 +38,13 @@ spec = do
             (checkCoverageWith lowerConfidence prop_shuffleNotDeterministic)
         it "sort (shuffled xs) == sort xs"
             (checkCoverageWith lowerConfidence prop_shufflePreserveElements)
+
+    describe "shuffleWith / mkSeed" $ do
+        it "shuffling with the same seed is deterministic"
+            (checkCoverageWith lowerConfidence prop_shuffleWithDeterministic)
+        it "different seed means different shuffles"
+            (checkCoverageWith lowerConfidence prop_shuffleDifferentSeed)
+
   where
     lowerConfidence :: Confidence
     lowerConfidence = Confidence (10^(6 :: Integer)) 0.75
@@ -54,3 +74,43 @@ prop_shufflePreserveElements
 prop_shufflePreserveElements xs = monadicIO $ run $ do
     xs' <- shuffle xs
     return $ cover 90 (not $ null xs) "non-empty" (L.sort xs == L.sort xs')
+
+-- ∀(g :: RandomGen).
+-- ∀(es :: [a]).
+--
+-- shuffleWith g es == shuffleWith g es
+prop_shuffleWithDeterministic
+    :: PrintableString
+    -> NonEmptyList Int
+    -> Property
+prop_shuffleWithDeterministic (PrintableString seed) (NonEmpty xs) =
+    monadicIO $ do
+        ys0 <- run $ shuffleWith (mkSeed $ T.pack seed) xs
+        ys1 <- run $ shuffleWith (mkSeed $ T.pack seed) xs
+        monitor $ cover 90 (length xs > 1) "non singleton"
+        assert (ys0 == ys1)
+
+-- ∀(x0 : Text, x1 : Text). g0 = mkSeed x0, g1 = mkSeed x1
+-- ∃(Δ: Int).
+-- ∀(es :: [a]).
+--
+-- g0 ≠g1, length es > Δ⇒ shuffleWith g0 es ≠shuffleWith g1 es
+prop_shuffleDifferentSeed
+    :: (PrintableString, PrintableString)
+    -> Positive Int
+    -> Property
+prop_shuffleDifferentSeed (x0, x1) (Positive len) = do
+    x0 /= x1 ==> monadicIO $ do
+        let g0 = mkSeed $ T.pack $ getPrintableString x0
+        let g1 = mkSeed $ T.pack $ getPrintableString x1
+        es <- pick $ vectorOf len (arbitrary @Int)
+        ys0 <- run $ shuffleWith g0 es
+        ys1 <- run $ shuffleWith g1 es
+        monitor $ label (prettyLen es)
+        monitor $ cover 90 (ys0 /= ys1) "different"
+  where
+    prettyLen :: [a] -> String
+    prettyLen xs = case length xs of
+        n | n <= 1 -> "singleton"
+        n | n <= 10 -> "small list"
+        _ -> "big list"
