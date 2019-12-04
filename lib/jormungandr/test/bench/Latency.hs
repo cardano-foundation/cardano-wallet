560b3f5dbb749ef324623adcd614095eaa607250
Pawel Jakubas <pawel.jakubas@iohk.io>
2019-12-04 12:30:09 +0100

    move everything to test/bench
    remove Latency from integration tests
    
    nix patch

---
+++ lib/jormungandr/test/bench/Latency.hs
@@ -0,0 +1,141 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Main where
+
+import Prelude
+
+import Cardano.BM.Data.Severity
+    ( Severity (..) )
+import Cardano.BM.Trace
+    ( Trace )
+import Cardano.CLI
+    ( Port (..), withLogging )
+import Cardano.Faucet
+    ( initFaucet )
+import Cardano.Launcher
+    ( ProcessHasExited (..), withUtf8Encoding )
+import Cardano.Wallet.Api.Server
+    ( Listen (..) )
+import Cardano.Wallet.Jormungandr
+    ( serveWallet )
+import Cardano.Wallet.Jormungandr.Compatibility
+    ( Jormungandr )
+import Cardano.Wallet.Jormungandr.Launch
+    ( withConfig )
+import Cardano.Wallet.Jormungandr.Network
+    ( JormungandrBackend (..) )
+import Cardano.Wallet.Primitive.AddressDerivation
+    ( NetworkDiscriminant (..) )
+import Cardano.Wallet.Primitive.Fee
+    ( FeePolicy (..) )
+import Cardano.Wallet.Primitive.Model
+    ( BlockchainParameters (..) )
+import Cardano.Wallet.Primitive.Types
+    ( SyncTolerance (..) )
+import Control.Concurrent.Async
+    ( race )
+import Control.Concurrent.MVar
+    ( newEmptyMVar, putMVar, takeMVar )
+import Control.Exception
+    ( throwIO )
+import Data.Proxy
+    ( Proxy (..) )
+import Data.Quantity
+    ( Quantity (..) )
+import Data.Text
+    ( Text )
+import Network.HTTP.Client
+    ( defaultManagerSettings
+    , managerResponseTimeout
+    , newManager
+    , responseTimeoutMicro
+    )
+import Network.Socket
+    ( SockAddr (..) )
+import Numeric.Natural
+    ( Natural )
+import Test.Hspec
+    ( Spec, SpecWith, after, describe, hspec )
+import Test.Hspec.Extra
+    ( aroundAll )
+import Test.Integration.Framework.DSL
+    ( Context (..), TxDescription (..), tearDown )
+
+import qualified Cardano.BM.Configuration.Model as CM
+import qualified Data.Text as T
+import qualified Test.Integration.Scenario.API.Latency as ApiLatency
+
+main :: forall t. (t ~ Jormungandr) => IO ()
+main = withUtf8Encoding $ withLogging Nothing Info $ \logging ->
+    hspec $ do
+        describe "API Latency" $ specWithServer logging $ do
+            ApiLatency.spec
+
+specWithServer
+    :: (CM.Configuration, Trace IO Text)
+    -> SpecWith (Context Jormungandr)
+    -> Spec
+specWithServer logCfg = aroundAll withContext . after tearDown
+  where
+    withContext :: (Context Jormungandr -> IO ()) -> IO ()
+    withContext action = do
+        ctx <- newEmptyMVar
+        let setupContext wAddr nPort bp = do
+                let baseUrl = "http://" <> T.pack (show wAddr) <> "/"
+                let sixtySeconds = 60*1000*1000 -- 60s in microseconds
+                manager <- (baseUrl,) <$> newManager (defaultManagerSettings
+                    { managerResponseTimeout =
+                        responseTimeoutMicro sixtySeconds
+                    })
+                faucet <- initFaucet
+                putMVar ctx $ Context
+                    { _cleanup = pure ()
+                    , _manager = manager
+                    , _nodePort = nPort
+                    , _walletPort = sockAddrPort wAddr
+                    , _faucet = faucet
+                    , _feeEstimator = mkFeeEstimator (getFeePolicy bp)
+                    , _target = Proxy
+                    }
+        race (takeMVar ctx >>= action) (withServer setupContext) >>=
+            either pure (throwIO . ProcessHasExited "integration")
+
+    withServer setup = withConfig $ \jmCfg ->
+        serveWallet @'Testnet logCfg (SyncTolerance 10) Nothing "127.0.0.1"
+            ListenOnRandomPort (Launch jmCfg) setup
+
+sockAddrPort :: SockAddr -> Port a
+sockAddrPort addr = Port . fromIntegral $ case addr of
+    SockAddrInet p _ -> p
+    SockAddrInet6 p _ _ _ -> p
+    _ -> 0
+
+mkFeeEstimator :: FeePolicy -> TxDescription -> (Natural, Natural)
+mkFeeEstimator policy (TxDescription nInps nOuts) =
+    let
+        LinearFee (Quantity a) (Quantity b) (Quantity _c) = policy
+        nChanges = nOuts
+        -- NOTE¹
+        -- We safely round BEFORE the multiplication because we know that
+        -- Jormungandr' fee are necessarily naturals constants. We carry doubles
+        -- here because of the legacy with Byron. In the end, it matters not
+        -- because in the spectrum of numbers we're going to deal with, naturals
+        -- can be represented without any rounding issue using 'Double' (or,
+        -- transactions have suddenly become overly expensive o_O)
+        fee = fromIntegral $ (round a) + (nInps + nOuts + nChanges) * (round b)
+    in
+        -- NOTE²
+        -- We use a range (min, max) and call it an "estimator" because for the
+        -- bridge (and probably cardano-node on Shelley), it's not possible to
+        -- compute the fee precisely by only knowing the number of inputs and
+        -- ouputs since the exact fee cost depends on the values of the
+        -- outputs and the values of the input indexes.
+        (fee, fee)
