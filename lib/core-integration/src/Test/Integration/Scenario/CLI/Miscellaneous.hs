707b2d3515096f93f2786df274243d9f041e5ab7
Piotr Stachyra <piotr.stachyra@iohk.io>
2019-12-05 14:09:18 +0100

    CLI_VERSION test - modify to work with windows testing bundle

--- lib/core-integration/src/Test/Integration/Scenario/CLI/Miscellaneous.hs
+++ lib/core-integration/src/Test/Integration/Scenario/CLI/Miscellaneous.hs
@@ -11,7 +11,7 @@ module Test.Integration.Scenario.CLI.Miscellaneous
 import Prelude
 
 import Cardano.Wallet.Version
-    ( gitRevision, showFullVersion, version )
+    ( showVersion, version )
 import Control.Monad
     ( forM_ )
 import System.Command
@@ -32,7 +32,7 @@ spec = do
     it "CLI_VERSION - cardano-wallet shows version" $  do
         (Exit c, Stdout out) <- cardanoWalletCLI @t ["version"]
         let v = L.dropWhileEnd (== '\n') out
-        v `shouldBe` (showFullVersion version gitRevision)
+        v `shouldContain` (showVersion version <> " (git revision: " )
         c `shouldBe` ExitSuccess
 
     describe "CLI_HELP - cardano-wallet shows help on bad arg or param" $ do
