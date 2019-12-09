583951798997ddddf180ee210ecf74ca79baf3d8
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-12-09 16:19:08 +1000

    API bench: Take multiple measurements and use average

--- lib/jormungandr/test/bench/Latency.hs
+++ lib/jormungandr/test/bench/Latency.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -38,7 +40,7 @@ import Cardano.Wallet.Api.Types
     , ApiWallet
     )
 import Cardano.Wallet.Jormungandr
-    ( serveWallet )
+    ( ServerLog (..), serveWallet )
 import Cardano.Wallet.Jormungandr.Compatibility
     ( Jormungandr )
 import Cardano.Wallet.Jormungandr.Launch
@@ -68,11 +70,13 @@ import Data.Maybe
 import Data.Proxy
     ( Proxy (..) )
 import Data.Time
-    ( UTCTime )
+    ( NominalDiffTime )
 import Data.Time.Clock
     ( diffUTCTime )
+import Data.Time.Clock.System
+    ( SystemTime (..), getSystemTime )
 import Fmt
-    ( Builder, build, fixedF, fmtLn, (+|) )
+    ( Builder, build, fixedF, fmtLn, (+|), (|+) )
 import Network.HTTP.Client
     ( defaultManagerSettings
     , managerResponseTimeout
@@ -80,7 +84,7 @@ import Network.HTTP.Client
     , responseTimeoutMicro
     )
 import Network.Wai.Middleware.Logging
-    ( ApiLog (..), ServerLog (..), WithRequestId (..) )
+    ( ApiLog (..), HandlerLog (..) )
 import Numeric.Natural
     ( Natural )
 import Test.Integration.Framework.DSL
@@ -108,27 +112,44 @@ main :: forall t n. (t ~ Jormungandr, n ~ 'Testnet) => IO ()
 main = do
     tvar <- newTVarIO []
     logging <- setupLatencyLogging tvar
-    let toDiff :: UTCTime -> UTCTime -> Double
-        toDiff t t' = (1000 * ) $ realToFrac $ diffUTCTime t t'
-    let toMilliseconds :: UTCTime -> UTCTime -> Builder
-        toMilliseconds t t' = build $ fixedF 1 $ toDiff t t'
+
+    let meanAvg :: [NominalDiffTime] -> Double
+        meanAvg ts = sum (map realToFrac ts) * 1000 / fromIntegral (length ts)
+    let buildResult :: [NominalDiffTime] -> Builder
+        buildResult [] = "ERR"
+        buildResult ts = build $ fixedF 1 $ meanAvg ts
+    let fmtResult :: String -> [NominalDiffTime] -> IO ()
+        fmtResult title ts = fmtLn ("    "+|title|+" - "+|buildResult ts|+" ms")
+
+    fmtLn "Latencies for two fixture wallets scenario"
+
     withUtf8Encoding $ benchWithServer logging $ \ctx -> do
         (wal1, wal2) <- twoFixtureWallet ctx
 
-        (_res1, [t1, t1']) <- measureLatency tvar
+        t1 <- measureApiLogs tvar
             (request @[ApiWallet] ctx listWalletsEp Default Empty)
 
-        (_res2, [t2, t2']) <- measureLatency tvar
+        fmtResult "listWallets        " t1
+
+        t2 <- measureApiLogs tvar
             (request @ApiWallet ctx (getWalletEp wal1) Default Empty)
 
-        (_res3, [t3, t3']) <- measureLatency tvar
+        fmtResult "getWallet          " t2
+
+        t3 <- measureApiLogs tvar
             (request @ApiUtxoStatistics ctx (getWalletUtxoEp wal1) Default Empty)
 
-        (_res4, [t4, t4']) <- measureLatency tvar
+        fmtResult "getUTxOsStatistics " t3
+
+        t4 <- measureApiLogs tvar
             (request @[ApiAddress n] ctx (getAddressesEp wal1 "") Default Empty)
 
-        (_res5, [t5, t5']) <- measureLatency tvar
+        fmtResult "listAddresses      " t4
+
+        t5 <- measureApiLogs tvar
             (request @[ApiTransaction n] ctx (listTxEp wal1 "") Default Empty)
+
+        fmtResult "listTransactions   " t5
 
         addrs <- listAddresses ctx wal2
         let amt = 1 :: Natural
@@ -142,53 +163,105 @@ main = do
                     }
                 }]
             }|]
-        (_res6, [t6, t6']) <- measureLatency tvar
+        t6 <- measureApiLogs tvar
             (request @ApiFee ctx (postTxFeeEp wal1) Default payload)
 
-        (_res7, [t7, t7']) <- measureLatency tvar
+        fmtResult "postTransactionFee " t6
+
+        t7 <- measureApiLogs tvar
             (request @[ApiStakePool] ctx listStakePoolsEp Default Empty)
 
-        (_res8, [t8, t8']) <- measureLatency tvar
+        fmtResult "listStakePools     " t7
+
+        t8 <- measureApiLogs tvar
             (request @ApiNetworkInformation ctx networkInfoEp Default Empty)
 
-        fmtLn "Latencies for two fixture wallets scenario"
-        fmtLn ("    listWallets        - "+|(toMilliseconds t1 t1')+|" ms")
-        fmtLn ("    getWallet          - "+|(toMilliseconds t2 t2')+|" ms")
-        fmtLn ("    getUTxOsStatistics - "+|(toMilliseconds t3 t3')+|" ms")
-        fmtLn ("    listAddresses      - "+|(toMilliseconds t4 t4')+|" ms")
-        fmtLn ("    listTransactions   - "+|(toMilliseconds t5 t5')+|" ms")
-        fmtLn ("    postTransactionFee - "+|(toMilliseconds t6 t6')+|" ms")
-        fmtLn ("    listStakePools     - "+|(toMilliseconds t7 t7')+|" ms")
-        fmtLn ("    getNetworkInfo     - "+|(toMilliseconds t8 t8')+|" ms")
+        fmtResult "getNetworkInfo     " t8
 
         pure ()
   where
     twoFixtureWallet ctx =
         (,) <$> fixtureWallet ctx <*> fixtureWallet ctx
 
+isLogRequestStart :: ServerLog -> Bool
+isLogRequestStart = \case
+    LogApiServerMsg (ApiLog _ LogRequestStart) -> True
+    _ -> False
+
+isLogRequestFinish :: ServerLog -> Bool
+isLogRequestFinish = \case
+    LogApiServerMsg (ApiLog _ LogRequestFinish) -> True
+    _ -> False
+
+measureApiLogs
+    :: TVar [LogObject ServerLog] -- ^ Log message variable.
+    -> IO a -- ^ Action to run
+    -> IO [NominalDiffTime]
+measureApiLogs = measureLatency isLogRequestStart isLogRequestFinish
+
+-- | Run tests for at least this long to get accurate timings.
+sampleTimeSeconds :: Int
+sampleTimeSeconds = 5
+
+-- | Measure how long an action takes based on trace points and taking an
+-- average of results over a short time period.
 measureLatency
-    :: TVar [LogObject ServerLog]
-    -> IO a
-    -> IO (a, [UTCTime])
-measureLatency tvar action = do
+    :: (msg -> Bool) -- ^ Predicate for start message
+    -> (msg -> Bool) -- ^ Predicate for end message
+    -> TVar [LogObject msg] -- ^ Log message variable.
+    -> IO a -- ^ Action to run
+    -> IO [NominalDiffTime]
+measureLatency start finish tvar action = do
     atomically $ writeTVar tvar []
-    res <- action
-    let getTimestamp = tstamp . loMeta
-    let filterMsg logObj = case loContent logObj of
-            LogMessage (LogApiMsg (WithRequestId _ LogRequestStart)) ->
-                Just $ getTimestamp logObj
-            LogMessage (LogApiMsg (WithRequestId _ LogRequestFinish)) ->
-                Just $ getTimestamp logObj
-            _ -> Nothing
-    getTimes <- mapMaybe filterMsg <$> readTVarIO tvar
-    pure (res, getTimes)
+    _res <- repeatFor sampleTimeSeconds action
+    extractTimings start finish . reverse <$> readTVarIO tvar
+
+-- | Scan through iohk-monitoring logs and extract time differences between
+-- start and end messages.
+extractTimings
+    :: (a -> Bool) -- ^ Predicate for start message
+    -> (a -> Bool) -- ^ Predicate for end message
+    -> [LogObject a] -- ^ Log messages
+    -> [NominalDiffTime]
+extractTimings isStart isFinish msgs = map2 mkDiff filtered
+  where
+    map2 _ [] = []
+    map2 f (a:b:xs) = (f a b:map2 f xs)
+    map2 _ _ = error "start trace without matching finish trace"
+
+    mkDiff (False, start) (True, finish) = diffUTCTime finish start
+    mkDiff (False, _) _ = error "missing finish trace"
+    mkDiff (True, _) _ = error "missing start trace"
+
+    filtered = mapMaybe filterMsg msgs
+    filterMsg logObj = case loContent logObj of
+        LogMessage msg | isStart msg -> Just (False, getTimestamp logObj)
+        LogMessage msg | isFinish msg -> Just (True, getTimestamp logObj)
+        _ -> Nothing
+    getTimestamp = tstamp . loMeta
+
+-- | Repeatedly run an action, until total elapsed time in seconds is greater
+-- than the given amount.
+repeatFor :: Int -> IO a -> IO [a]
+repeatFor nSeconds action = do
+    start <- getSystemTime
+    let repeater rs = do
+            now <- getSystemTime
+            if finished start now
+                then pure rs
+                else do
+                    !r <- action
+                    repeater (r:rs)
+    reverse <$> repeater []
+  where
+    finished a b = systemSeconds b - systemSeconds a > fromIntegral nSeconds
 
 setupLatencyLogging
     :: TVar [LogObject ServerLog]
     -> IO (CM.Configuration, Trace IO ServerLog)
 setupLatencyLogging tvar = do
     cfg <- do
-        cfg' <-defaultConfigStdout
+        cfg' <- defaultConfigStdout
         CM.setMinSeverity cfg' Debug
         pure cfg'
     pure (cfg, traceInTVarIO tvar)
