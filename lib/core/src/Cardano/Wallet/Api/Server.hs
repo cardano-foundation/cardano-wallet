43ecd05debedbd856aee43dbf78cc3c745796569
KtorZ <matthias.benkort@gmail.com>
2019-12-16 12:54:16 +0100

    compute payment fee as the difference between inputs/outputs
    effectively re-using the existing coin selection function and removing the need for
    an extra 'estimateFee' function in the core engine

--- lib/core/src/Cardano/Wallet/Api/Server.hs
+++ lib/core/src/Cardano/Wallet/Api/Server.hs
@@ -50,7 +50,6 @@ import Cardano.Wallet
     ( ErrAdjustForFee (..)
     , ErrCoinSelection (..)
     , ErrDecodeSignedTx (..)
-    , ErrEstimatePaymentFee (..)
     , ErrFetchRewards (..)
     , ErrJoinStakePool (..)
     , ErrListTransactions (..)
@@ -157,8 +156,6 @@ import Cardano.Wallet.Primitive.AddressDiscovery.Sequential
     ( SeqState (..), defaultAddressPoolGap, mkSeqState )
 import Cardano.Wallet.Primitive.CoinSelection
     ( CoinSelection (..), changeBalance, feeBalance, inputBalance )
-import Cardano.Wallet.Primitive.Fee
-    ( Fee (..) )
 import Cardano.Wallet.Primitive.Model
     ( Wallet, availableBalance, currentTip, getState, totalBalance )
 import Cardano.Wallet.Primitive.Types
@@ -802,13 +799,11 @@ postTransactionFee
     -> Handler ApiFee
 postTransactionFee ctx (ApiT wid) body = do
     let outs = coerceCoin <$> (body ^. #payments)
-    (Fee fee) <- liftHandler $ withWorkerCtx ctx wid liftE $ \wrk ->
-        W.estimatePaymentFee @_ @s @t wrk wid outs
-    return ApiFee
-        { amount = Quantity (fromIntegral fee)
-        }
+    liftHandler $ withWorkerCtx ctx wid liftE $ \wrk ->
+        apiFee <$> W.selectCoinsForPayment @_ @s @t @k wrk wid outs
   where
-    liftE = throwE . ErrEstimatePaymentFeeNoSuchWallet
+    apiFee = ApiFee . Quantity . fromIntegral . feeBalance
+    liftE = throwE . ErrSelectForPaymentNoSuchWallet
 
 {-------------------------------------------------------------------------------
                                     Stake Pools
@@ -1599,11 +1594,6 @@ instance Buildable e => LiftHandler (ErrSelectForPayment e) where
         ErrSelectForPaymentNoSuchWallet e -> handler e
         ErrSelectForPaymentCoinSelection e -> handler e
         ErrSelectForPaymentFee e -> handler e
-
-instance Buildable e => LiftHandler (ErrEstimatePaymentFee e) where
-    handler = \case
-        ErrEstimatePaymentFeeNoSuchWallet e -> handler e
-        ErrEstimatePaymentFeeCoinSelection e -> handler e
 
 instance LiftHandler ErrListUTxOStatistics where
     handler = \case
