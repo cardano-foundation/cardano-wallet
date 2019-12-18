ecf60cd32e97dd5a11c91e38e4ad4a5686e327ef
KtorZ <matthias.benkort@gmail.com>
2019-12-18 18:31:01 +0100

    prune in the same db transaction as the rest... just in case..

--- lib/core/src/Cardano/Wallet.hs
+++ lib/core/src/Cardano/Wallet.hs
@@ -607,12 +607,7 @@ restoreBlocks ctx wid blocks nodeTip = db & \DBLayer{..} -> do
         liftIO $ logCheckpoint (NE.last cps)
         putCheckpoint (PrimaryKey wid) (NE.last cps)
 
-    -- NOTE
-    -- We prune the database in a separate db transaction since this is not
-    -- strictly required to maintain integrity of the data. It's quite a heavy
-    -- operation, so we may still want to unlock the database to allow some
-    -- other operations to flow in the meantime.
-    mapExceptT atomically $ prune (PrimaryKey wid)
+        prune (PrimaryKey wid)
 
     liftIO $ do
         progress <- walletSyncProgress @ctx ctx (NE.last cps)
