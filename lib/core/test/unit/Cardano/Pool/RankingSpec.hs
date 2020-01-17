fb5da90b6c330d6cb7d8ec9caf87f9d51114cf43
KtorZ <matthias.benkort@gmail.com>
2020-01-17 12:46:13 +0100

    simplifies allElseEqualProperty + apply coding standards
    - Favor explicit importsover qualified imports
    - Avoid duplication in function signatures and function body
    - Use type-signatures for intermediate functions
    - Wrap lines that are too long

--- lib/core/test/unit/Cardano/Pool/RankingSpec.hs
+++ lib/core/test/unit/Cardano/Pool/RankingSpec.hs
@@ -1,30 +1,56 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
+
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Cardano.Pool.RankingSpec
     ( spec
     ) where
 
 import Prelude
 
+import Cardano.Pool.Ranking
+    ( EpochConstants (..)
+    , Lovelace (..)
+    , Margin (..)
+    , Pool (..)
+    , RelativeStakeOf (..)
+    , desirability
+    , mkRelativeStake
+    , saturatedPoolRewards
+    , saturatedPoolSize
+    , unsafeMkMargin
+    , unsafeToNonNegative
+    )
+import Data.Bifunctor
+    ( bimap )
 import Data.Function
     ( (&) )
 import Data.Generics.Internal.VL.Lens
-    ( Lens', view, (.~) )
+    ( (.~), (^.) )
 import Data.Generics.Labels
     ()
+import Data.Generics.Product.Fields
+    ( HasField', field' )
 import Data.Ord
     ( Ordering (..) )
+import Data.Proxy
+    ( Proxy (..) )
 import Data.Word
     ( Word64 )
+import GHC.TypeLits
+    ( KnownSymbol, Symbol, symbolVal )
 import Test.Hspec
     ( Spec, describe, it )
 import Test.QuickCheck
@@ -49,53 +75,42 @@ spec :: Spec
 spec = do
     describe "Stake Pool Ranking" $ do
         describe "all else equal..." $ do
-            it "higher margin => lower desirability (or equal)" $
-                property $
-                    allElseEqualProperty
-                        "margin"
-                        #margin
-                        (show . R.getMargin)
-                        NegativeEffect
+            it "higher margin => lower desirability (or equal)"
+                $ property
+                $ allElseEqualProperty @"margin" NegativeEffect
 
-            it "higher cost => lower desirability (or equal)" $
-                property $ do
-                    allElseEqualProperty
-                        "cost"
-                        #cost
-                        (show . R.getLovelace)
-                        NegativeEffect
+            it "higher cost => lower desirability (or equal)"
+                $ property
+                $ allElseEqualProperty @"cost" NegativeEffect
 
-            it "higher performance => higher desirability (or equal)" $
-                property $
-                    allElseEqualProperty
-                        "performance"
-                        #recentAvgPerformance
-                        (show . R.getNonNegative)
-                        PositiveEffect
+            it "higher performance => higher desirability (or equal)"
+                $ property
+                $ allElseEqualProperty @"recentAvgPerformance" PositiveEffect
 
-            it "higher oversaturation => desirability NOT affected" $
-                property prop_oversaturationDoesNotInfluenceDesirability
+            it "higher oversaturation => desirability NOT affected"
+                $ property prop_oversaturationDoesNotInfluenceDesirability
 
-        describe "desiraility" $ do
+        describe "desirability" $ do
             it "desirability >= 0"
-                (property prop_desirabilityNonNegative)
+                $ property prop_desirabilityNonNegative
 
         describe "saturatedPoolRewards" $ do
             it "a0 == 0 => saturatedPoolRewards == p*R*z0"
-                (property prop_saturatedPoolRewardsReduces)
+                $ property prop_saturatedPoolRewardsReduces
+
             it "saturatedPoolRewards >= 0"
-                (property prop_saturatedPoolRewardsNonNegative)
+                $ property prop_saturatedPoolRewardsNonNegative
 
         describe "mkRelativeStake" $ do
             it "stake > R, R >= 1 => mkRelativeStake ∈ [0,1] (even with conversions)"
                 $ property prop_mkRelativeStakeBounds
 
-prop_mkRelativeStakeBounds :: R.EpochConstants -> Word64 -> Word64 -> Property
+prop_mkRelativeStakeBounds :: EpochConstants -> Word64 -> Word64 -> Property
 prop_mkRelativeStakeBounds constants' tot stake =
     let
-        rel = R.unRelativeStake
-            $ R.mkRelativeStake
-                (R.Lovelace $ fromIntegral stake)
+        rel = unRelativeStake
+            $ mkRelativeStake
+                (Lovelace $ fromIntegral stake)
                 constants
     in
         counterexample ("relative stake = " ++ show rel) $
@@ -104,42 +119,44 @@ prop_mkRelativeStakeBounds constants' tot stake =
                     property $ rel >= 0 && rel <= 1
 
   where
-    constants = constants'{R.totalRewards = R.Lovelace . R.unsafeToNonNegative $ fromIntegral tot}
+    constants = constants'
+        { totalRewards = Lovelace . unsafeToNonNegative $ fromIntegral tot }
 
-prop_desirabilityNonNegative :: R.EpochConstants -> R.Pool -> Property
+prop_desirabilityNonNegative :: EpochConstants -> Pool -> Property
 prop_desirabilityNonNegative constants pool = property $
-    R.desirability constants pool >= 0
+    desirability constants pool >= 0
 
-prop_saturatedPoolRewardsNonNegative :: R.EpochConstants -> R.Pool -> Property
+prop_saturatedPoolRewardsNonNegative :: EpochConstants -> Pool -> Property
 prop_saturatedPoolRewardsNonNegative constants pool = property $
-    R.saturatedPoolRewards constants pool >= 0
+    saturatedPoolRewards constants pool >= 0
 
-prop_saturatedPoolRewardsReduces :: R.EpochConstants -> R.Pool -> Property
+prop_saturatedPoolRewardsReduces :: EpochConstants -> Pool -> Property
 prop_saturatedPoolRewardsReduces constants' pool =
     let
-        constants = constants'{R.leaderStakeInfluence = R.unsafeToNonNegative 0 }
-        z0 = R.unRelativeStake $ R.saturatedPoolSize constants
-        _R =  R.getNonNegative $ R.getLovelace $ R.totalRewards constants
-        p = R.getNonNegative $ R.recentAvgPerformance pool
+        constants = constants'
+            { leaderStakeInfluence = unsafeToNonNegative 0 }
+        z0 = unRelativeStake  $ saturatedPoolSize constants
+        _R = R.getNonNegative $ getLovelace $ totalRewards constants
+        p  = R.getNonNegative $ recentAvgPerformance pool
     in
-        R.saturatedPoolRewards constants pool === p*_R*z0
+        saturatedPoolRewards constants pool === p*_R*z0
 
 prop_oversaturationDoesNotInfluenceDesirability
-    :: R.Pool
-    -> R.EpochConstants
+    :: Pool
+    -> EpochConstants
     -> Property
 prop_oversaturationDoesNotInfluenceDesirability pool constants =
     forAll arbitrary $ \v1 v2 -> do
         let (higherStake, lowerStake) = sortTuple (v1, v2)
-        let z0 = R.unRelativeStake $ R.saturatedPoolSize constants
+        let z0 = unRelativeStake $ saturatedPoolSize constants
         higherStake > z0 ==>
             des higherStake === des lowerStake
   where
     sortTuple (a, b)
-        | a > b     = (a,b)
+        | a > b     = (a, b)
         | otherwise = (b, a)
 
-    des _stake = R.desirability constants pool
+    des _stake = desirability constants pool
     -- TODO: set pool{R.stake = stake}, but that field doesn't even exist!
 
 data EffectOnDesirability
@@ -150,77 +167,88 @@ data EffectOnDesirability
 -- | All else equal, what effect on the desirability does changing a single
 -- field have?
 allElseEqualProperty
-    :: (Arbitrary a, Show a, Ord a)
-    => String
-    -> Lens' R.Pool a
-    -> (a -> String)
-       -- ^ Debug print a field-value
-    -> EffectOnDesirability
-    -> R.Pool
+    :: forall (field :: Symbol) a.
+        ( Arbitrary a
+        , Show a
+        , Ord a
+        , HasField' field Pool a
+        , KnownSymbol field
+        )
+    => EffectOnDesirability
+        -- ^ Expected effect on the desirability
+    -> Pool
        -- ^ The base pool which is later modified
-    -> R.EpochConstants
+    -> EpochConstants
        -- ^ Constants used
     -> Property
-allElseEqualProperty desc field showValue eff p constants =
-    forAll arbitrary $ \value1 value2 -> do
-        let (higherVal, lowerVal) = sortTuple (value1, value2)
+allElseEqualProperty expectedEffect pool constants =
+    forAll arbitrary $ \(value1 :: a) (value2 :: a) -> do
+        let (poolLower, poolHigher) = sortTuple (value1, value2)
+                & bimap poolWith poolWith
 
-        let poolRankDesc = "Pools:\n" ++ showPool 1 (poolWith higherVal) ++ "\n" ++ showPool 2 (poolWith lowerVal)
+        let (dLower, dHigher) =
+                ( desirability constants poolLower
+                , desirability constants poolHigher
+                )
 
-        case des (poolWith higherVal) `compare` des (poolWith lowerVal) of
-            GT -> eff === PositiveEffect
+        let poolRankDesc = unlines
+                [ "Pools:"
+                , showPool poolLower  dLower
+                , showPool poolHigher dHigher
+                ]
+
+        case dLower `compare` dHigher of
             EQ -> property True
             -- A reason we end up in this case could be that both pools are
             -- un-profitable. It would be interesting to re-run tests with
             -- a higher total reward pot. But as long we hit the other cases we
             -- should be fine, really.
-
-            LT -> eff === NegativeEffect
-
-            & counterexample poolRankDesc
-            & classify
-                    (des (poolWith higherVal) == des (poolWith lowerVal))
-                    "equal desirability"
-
+            GT -> expectedEffect === PositiveEffect
+            LT -> expectedEffect === NegativeEffect
+          & counterexample poolRankDesc
+          & classify (dLower == dHigher) "same desirability"
   where
-    poolWith val = p & field .~ val
+    poolWith :: a -> Pool
+    poolWith val = pool & (field' @field) .~ val
+
+    sortTuple :: (a, a) -> (a, a)
     sortTuple (a, b)
-        | a > b = (a,b)
-        | otherwise     = (b, a)
+        | a > b     = (a, b)
+        | otherwise = (b, a)
 
-    showPool :: Int -> R.Pool -> String
-    showPool rank x =
-        show rank
-            ++ ". "
-            ++ desc
-            ++ " = "
-            ++ showValue (view field x)
-            ++ ", desirability = "
-            ++ show (des x)
+    showPool :: Pool -> Double -> String
+    showPool x d = unwords
+        [ show (symbolVal $ Proxy @field)
+        , "="
+        , show (x ^. field' @field)
+        , ", desirability ="
+        , show d
+        ]
 
-    des = R.desirability constants
-
-instance Arbitrary R.Pool where
+instance Arbitrary Pool where
     arbitrary = genericArbitrary
     shrink = genericShrink
 
-instance Arbitrary (R.RelativeStakeOf a) where
-    arbitrary = R.RelativeStake <$> choose (0, 1)
-    shrink (R.RelativeStake x) = map R.RelativeStake $ filter (\a -> a >= 0 && a <= 1) $ shrink x
+instance Arbitrary (RelativeStakeOf a) where
+    arbitrary =
+        RelativeStake <$> choose (0, 1)
+
+    shrink (RelativeStake x) =
+        map RelativeStake $ filter (\a -> a >= 0 && a <= 1) $ shrink x
 
 -- TODO: We should ideally not export the NonNegative and Positive constructors,
 -- in which case we wouldn't be able to use derivingVia.
-deriving via (NonNegative Double) instance (Arbitrary R.Lovelace)
+deriving via (NonNegative Double) instance (Arbitrary Lovelace)
 deriving via (NonNegative Double) instance (Arbitrary (R.NonNegative Double))
 deriving via (Positive Int) instance (Arbitrary (R.Positive Int))
 
-instance Arbitrary R.Margin where
-    arbitrary = R.unsafeMkMargin <$> choose (0, 1)
-    shrink = map R.unsafeMkMargin
+instance Arbitrary Margin where
+    arbitrary = unsafeMkMargin <$> choose (0, 1)
+    shrink = map unsafeMkMargin
         . filter (\a -> a >= 0 && a <= 1)
-        . map R.getMargin
+        . map getMargin
         . shrink
 
-instance Arbitrary R.EpochConstants where
+instance Arbitrary EpochConstants where
     arbitrary = genericArbitrary
     shrink = genericShrink
