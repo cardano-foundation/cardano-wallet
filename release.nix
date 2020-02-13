519404e2a83ecfc11f2561905bd6aaa11163d379
Samuel Leathers <samuel.leathers@iohk.io>
2020-02-13 17:13:16 -0500

    release.nix: windows - change pc -> w64

--- release.nix
+++ release.nix
@@ -62,27 +62,27 @@ let
     daedalus-jormungandr = with jobs; {
       linux = native.cardano-wallet-jormungandr.x86_64-linux;
       macos = native.cardano-wallet-jormungandr.x86_64-darwin;
-      windows = x86_64-pc-mingw32.cardano-wallet-jormungandr.x86_64-linux;
+      windows = x86_64-w64-mingw32.cardano-wallet-jormungandr.x86_64-linux;
     };
 
     # Windows release ZIP archive
     cardano-wallet-jormungandr-win64 = import ./nix/windows-release.nix {
       inherit pkgs project;
-      cardano-wallet-jormungandr = jobs.x86_64-pc-mingw32.cardano-wallet-jormungandr.x86_64-linux;
+      cardano-wallet-jormungandr = jobs.x86_64-w64-mingw32.cardano-wallet-jormungandr.x86_64-linux;
     };
 
     # This is used for testing the build on windows.
     cardano-wallet-jormungandr-tests-win64 = import ./nix/windows-testing-bundle.nix {
       inherit pkgs project;
-      cardano-wallet-jormungandr = jobs.x86_64-pc-mingw32.cardano-wallet-jormungandr.x86_64-linux;
-      tests = collectTests jobs.x86_64-pc-mingw32.tests;
-      benchmarks = collectTests jobs.x86_64-pc-mingw32.benchmarks;
+      cardano-wallet-jormungandr = jobs.x86_64-w64-mingw32.cardano-wallet-jormungandr.x86_64-linux;
+      tests = collectTests jobs.x86_64-w64-mingw32.tests;
+      benchmarks = collectTests jobs.x86_64-w64-mingw32.benchmarks;
     };
 
     # For testing migration tests on windows
     migration-tests-win64 = import ./nix/windows-migration-tests-bundle.nix {
       inherit pkgs project;
-      migration-tests = jobs.x86_64-pc-mingw32.migration-tests.x86_64-linux;
+      migration-tests = jobs.x86_64-w64-mingw32.migration-tests.x86_64-linux;
     };
 
     # Fully-static linux binary (placeholder - does not build)
@@ -133,7 +133,7 @@ let
      collectTests jobs.native.benchmarks ++
      [ jobs.native.cardano-wallet-jormungandr.x86_64-linux
        jobs.native.cardano-wallet-jormungandr.x86_64-darwin
-       jobs.x86_64-pc-mingw32.cardano-wallet-jormungandr.x86_64-linux
+       jobs.x86_64-w64-mingw32.cardano-wallet-jormungandr.x86_64-linux
        jobs.native.shell.x86_64-linux
        jobs.native.shell.x86_64-darwin
        jobs.cardano-wallet-jormungandr-win64
