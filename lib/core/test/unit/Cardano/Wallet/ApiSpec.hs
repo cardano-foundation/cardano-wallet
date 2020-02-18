af4d38e15f3d1fd6cfe46054ec60a78e9f572792
KtorZ <matthias.benkort@gmail.com>
2020-02-18 18:19:06 +0100

    Make API capture test generic in terms of the API

--- lib/core/test/unit/Cardano/Wallet/ApiSpec.hs
+++ lib/core/test/unit/Cardano/Wallet/ApiSpec.hs
@@ -1,16 +1,29 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+-- The following is justified by the usage of the 'requestBody' field
+-- accessor on the 'Request' object from the Network.Wai.Internal module as a
+-- setter. The use of this field as a getter is deprecated, but we need it to
+-- mount an existing request body in a request.
+{-# OPTIONS_GHC -fno-warn-deprecations #-}
 
 module Cardano.Wallet.ApiSpec
     ( spec
@@ -18,114 +31,498 @@ module Cardano.Wallet.ApiSpec
 
 import Prelude
 
+import Cardano.Wallet.Api
+    ( Api )
 import Cardano.Wallet.Api.Server
     ( LiftHandler (..) )
 import Cardano.Wallet.Api.Types
-    ( ApiT, ApiTxId )
+    ( AllowedMnemonics
+    , ApiAddress
+    , ApiByronWallet
+    , ApiByronWalletMigrationInfo
+    , ApiCoinSelection
+    , ApiEpochNumber
+    , ApiFee
+    , ApiNetworkInformation
+    , ApiNetworkParameters
+    , ApiNetworkTip
+    , ApiSelectCoinsData
+    , ApiStakePool
+    , ApiT
+    , ApiTransaction
+    , ApiTxId
+    , ApiUtxoStatistics
+    , ApiWallet
+    , ApiWalletPassphrase
+    , BackwardCompatPlaceholder (..)
+    , ByronWalletPostData
+    , ByronWalletStyle (..)
+    , Iso8601Time
+    , PostExternalTransactionData
+    , PostTransactionData
+    , PostTransactionFeeData
+    , StyleSymbol
+    , WalletPostData
+    , WalletPutData
+    , WalletPutPassphraseData
+    )
+import Cardano.Wallet.Primitive.AddressDerivation
+    ( NetworkDiscriminant (..) )
 import Cardano.Wallet.Primitive.Types
     ( PoolId, WalletId )
+import Control.Arrow
+    ( first )
+import Control.Monad
+    ( forM_ )
+import Control.Monad.IO.Class
+    ( liftIO )
 import Data.Aeson.QQ
     ( aesonQQ )
+import Data.ByteString.Lazy
+    ( ByteString )
 import Data.Function
     ( (&) )
+import Data.IORef
+    ( IORef, atomicModifyIORef, modifyIORef, newIORef )
+import Data.Kind
+    ( Type )
 import Data.Proxy
     ( Proxy (..) )
+import Data.String
+    ( IsString )
 import Data.Text
     ( Text )
+import Data.Typeable
+    ( Typeable, typeRep )
+import Data.Word.Odd
+    ( Word31 )
+import GHC.TypeLits
+    ( ErrorMessage (..), KnownSymbol, Nat, Symbol, TypeError, symbolVal )
+import Network.HTTP.Types.Header
+    ( hAccept, hContentLength, hContentType )
+import Network.HTTP.Types.Method
+    ( Method )
 import Network.Wai
-    ( defaultRequest, pathInfo )
+    ( Request
+    , RequestBodyLength (..)
+    , defaultRequest
+    , pathInfo
+    , requestBody
+    , requestBodyLength
+    , requestHeaders
+    , requestMethod
+    )
 import Network.Wai.Middleware.ServerError
     ( handleRawError )
 import Network.Wai.Test
-    ( assertBody, assertStatus, request, runSession )
+    ( Session, assertBody, assertStatus, request, runSession )
 import Servant
-    ( Application, Server, err501, serve, throwError )
+    ( Application
+    , Header'
+    , ReqBody
+    , Server
+    , StdMethod (..)
+    , Verb
+    , err501
+    , serve
+    , throwError
+    )
 import Servant.API
     ( (:<|>) (..), (:>), Capture )
 import Servant.API.Verbs
-    ( GetNoContent )
+    ( DeleteNoContent, GetNoContent, NoContentVerb, ReflectMethod (..), Verb )
+import Servant.Links
+    ( URI (..), allLinks', linkURI )
+import System.IO.Unsafe
+    ( unsafePerformIO )
 import Test.Hspec
-    ( Spec, describe, it )
+    ( Spec, SpecWith, describe, it, runIO, xdescribe )
+import Web.HttpApiData
+    ( ToHttpApiData (..) )
 
 import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Char8 as B8
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
+import qualified Servant
+
 
 spec :: Spec
-spec = describe "Path params capture failures" $ do
-    describe "False wallet ids" $ do
-        spec_FalseWalletIds $ T.replicate 40 "ś"
-        spec_FalseWalletIds $ T.replicate 39 "1"
-        spec_FalseWalletIds $ T.replicate 41 "1"
+spec = do
+    gSpec (everyPathParams api) spec_MalformedParam
+    gSpec (everyBodyParams api) spec_MalformedParam
 
-    describe "False transaction ids" $ do
-        spec_FalseTxIds $ T.replicate 64 "ś"
-        spec_FalseTxIds $ T.replicate 63 "1"
-        spec_FalseTxIds $ T.replicate 65 "1"
-
-    describe "False stake pool ids" $ do
-        spec_FalseStakePoolIds $ T.replicate 64 "ś"
-        spec_FalseStakePoolIds $ T.replicate 63 "1"
-        spec_FalseStakePoolIds $ T.replicate 65 "1"
-
-spec_FalseWalletIds :: Text -> Spec
-spec_FalseWalletIds wid =
-    it (T.unpack wid) $ flip runSession application $ do
-        response <- request $ defaultRequest
-            { pathInfo = ["testCaptureWalletId", wid] }
-        response & assertStatus 400
-        response & assertBody (Aeson.encode [aesonQQ|
-            { "code": "bad_request"
-            , "message": "wallet id should be a hex-encoded string of 40 characters"
-            }|])
-
-spec_FalseTxIds :: Text -> Spec
-spec_FalseTxIds tid =
-    it (T.unpack tid) $ flip runSession application $ do
-        response <- request $ defaultRequest
-            { pathInfo = ["testCaptureTxId", tid] }
-        response & assertStatus 400
-        response & assertBody (Aeson.encode [aesonQQ|
-            { "code": "bad_request"
-            , "message": "Invalid tx hash: expecting a hex-encoded value that is 32 bytes in length."
-            }|])
-
-spec_FalseStakePoolIds :: Text -> Spec
-spec_FalseStakePoolIds tid =
-    it (T.unpack tid) $ flip runSession application $ do
-        response <- request $ defaultRequest
-            { pathInfo = ["testCaptureStakePoolId", tid] }
-        response & assertStatus 400
-        response & assertBody (Aeson.encode [aesonQQ|
-            { "code": "bad_request"
-            , "message": "Invalid stake pool id: expecting a hex-encoded value that is 32 bytes in length."
-            }|])
-
+spec_MalformedParam :: Request -> ExpectedError -> Session ()
+spec_MalformedParam malformedRequest (ExpectedError msg) = do
+    response <- request malformedRequest
+    response & assertStatus 400
+    response & assertBody (Aeson.encode [aesonQQ|
+        { "code": "bad_request"
+        , "message": #{msg}
+        }|])
 
 --
--- Test API
+-- Construction of well-formed and malformed parameters
+--
+
+newtype ExpectedError = ExpectedError String
+    deriving newtype (IsString)
+
+class WellFormed t where
+    wellformed :: t
+
+class Malformed t where
+    malformed :: [(t, ExpectedError)]
+    malformed = []
+
+instance WellFormed (PathParam (ApiT WalletId)) where
+    wellformed = PathParam $ T.replicate 40 "0"
+
+instance Malformed (PathParam (ApiT WalletId)) where
+    malformed = first PathParam <$>
+        [ (T.replicate 40 "ś", msg)
+        , (T.replicate 39 "1", msg)
+        , (T.replicate 41 "1", msg)
+        ]
+      where
+        msg = "wallet id should be a hex-encoded string of 40 characters"
+
+instance WellFormed (PathParam ApiTxId) where
+    wellformed = PathParam $ T.replicate 64 "0"
+
+instance Malformed (PathParam ApiTxId) where
+    malformed = first PathParam <$>
+        [ (T.replicate 64 "ś", msg)
+        , (T.replicate 63 "1", msg)
+        , (T.replicate 65 "1", msg)
+        ]
+      where
+        msg = "Invalid tx hash: expecting a hex-encoded value that is 32 bytes in length."
+
+instance WellFormed (PathParam (ApiT PoolId)) where
+    wellformed = PathParam $ T.replicate 64 "0"
+
+instance Malformed (PathParam (ApiT PoolId)) where
+    malformed = first PathParam <$>
+        [ (T.replicate 64 "ś", msg)
+        , (T.replicate 63 "1", msg)
+        , (T.replicate 65 "1", msg)
+        ]
+      where
+        msg = "Invalid stake pool id: expecting a hex-encoded value that is 32 bytes in length."
+
+instance WellFormed (PathParam (BackwardCompatPlaceholder (ApiT PoolId))) where
+    wellformed = PathParam $ T.replicate 64 "0"
+
+instance Malformed (PathParam (BackwardCompatPlaceholder (ApiT PoolId))) where
+    malformed = first PathParam <$>
+        [ (T.replicate 64 "ś", msg)
+        , (T.replicate 63 "1", msg)
+        , (T.replicate 65 "1", msg)
+        ]
+      where
+        msg = "Invalid stake pool id: expecting a hex-encoded value that is 32 bytes in length."
+
+instance WellFormed (PathParam ApiEpochNumber) where
+    wellformed = PathParam "latest"
+
+instance Malformed (PathParam ApiEpochNumber) where
+    malformed = first PathParam <$>
+        [ ("earliest", msg)
+        , (T.pack $ show $ (+1) $ fromIntegral @Word31 @Int maxBound, msg)
+        , ("invalid", msg)
+        , ("-1", msg)
+        ]
+      where
+        msg = "I couldn't parse the given epoch number. I am expecting either the word 'latest' or, an integer from 0 to 2147483647."
+
+instance Malformed (BodyParam WalletPostData) where
+    malformed = first (BodyParam . Aeson.encode) <$>
+        [ ( [aesonQQ|
+            { "name": "ads"
+            , "mnemonic_sentence": "album execute kingdom dumb trip all salute busy case bring spell ugly umbrella choice shy"
+            , "passphrase": "Secure Passphrase"
+            } |]
+          , "Error in $['mnemonic_sentence']: parsing [] failed, expected Array, but encountered String"
+          )
+        , ( [aesonQQ|
+            { "name": "ads"
+            , "mnemonic_sentence": 15
+            , "passphrase": "Secure Passphrase"
+            } |]
+          , "Error in $['mnemonic_sentence']: parsing [] failed, expected Array, but encountered Number"
+          )
+        , ( [aesonQQ|
+            { "name": "ads"
+            , "passphrase": "Secure Passphrase"
+            } |]
+          , "Error in $: parsing Cardano.Wallet.Api.Types.WalletPostData(WalletPostData) failed, key 'mnemonic_sentence' not found"
+          )
+        , ( [aesonQQ|
+            { "name": "Just a łallet"
+            , "mnemonic_sentence": #{mnemonics15}
+            , "mnemonic_second_factor": []
+            , "passphrase": "Secure Passphrase"
+            } |]
+          , "Error in $['mnemonic_second_factor']: Invalid number of words: 9 or 12 words are expected."
+          )
+        , ( [aesonQQ|
+            { "name": "Just a łallet"
+            , "mnemonic_sentence": #{mnemonics15}
+            , "mnemonic_second_factor": ["squirrel", "material", "silly", "twice", "direct", "slush", "pistol", "razor", "become"]
+            , "passphrase": "Secure Passphrase"
+            } |]
+          , "Error in $['mnemonic_second_factor']: Invalid entropy checksum: please double-check the last word of your mnemonic sentence."
+          )
+        -- TODO
+        -- and so forth...
+        -- We should move all existing "malformed" scenarios from the
+        -- integration level to here.
+        ]
+      where
+        mnemonics15 :: [Text]
+        mnemonics15 =
+            [ "network", "empty", "cause", "mean", "expire"
+            , "private", "finger", "accident", "session", "problem"
+            , "absurd", "banner", "stage", "void", "what"
+            ]
+
+instance Malformed (BodyParam (ApiSelectCoinsData 'Testnet))
+
+instance Malformed (BodyParam (PostTransactionData 'Testnet))
+
+instance Malformed (BodyParam (PostTransactionFeeData 'Testnet))
+
+instance Malformed (BodyParam (ByronWalletPostData (mw :: [Nat])))
+
+instance Malformed (BodyParam WalletPutData)
+
+instance Malformed (BodyParam WalletPutPassphraseData)
+
+instance Malformed (BodyParam ApiNetworkTip)
+
+instance Malformed (BodyParam ApiWalletPassphrase)
+
+instance Malformed (BodyParam PostExternalTransactionData)
+
+--
+-- Generic API Spec
+--
+
+class Typeable api => GenericApiSpec api where
+    gSpec :: api -> (Request -> ExpectedError -> Session ()) -> Spec
+    gSpec _ _ = xdescribe (show $ typeRep $ Proxy @api) (pure ())
+
+instance (GenericApiSpec a, GenericApiSpec b) => GenericApiSpec (a :<|> b) where
+    gSpec (a :<|> b) toS = gSpec a toS >> gSpec b toS
+
+instance GenericApiSpec Request where
+    gSpec req _ = pure ()
+
+instance
+    ( Typeable a, Malformed (PathParam a)
+    ) => GenericApiSpec (PathParam a -> Request)
+  where
+    gSpec toRequest toSession = describe "Malformed PathParam" $ do
+        forM_ (first toRequest <$> malformed) $ \(req, msg) ->
+            it (titleize (Proxy @a) req) $
+                runSession (toSession req msg) application
+
+instance
+    ( Typeable a, Malformed (PathParam a), WellFormed (PathParam a)
+    , GenericApiSpec (PathParam a -> Request)
+    , Typeable b, Malformed (PathParam b), WellFormed (PathParam b)
+    , GenericApiSpec (PathParam b -> Request)
+    ) => GenericApiSpec (PathParam a -> PathParam b -> Request)
+  where
+    gSpec toRequest toSession = do
+        gSpec (toRequest wellformed) toSession
+        gSpec (`toRequest` wellformed) toSession
+
+instance
+    ( Typeable a, Malformed (BodyParam a)
+    ) => GenericApiSpec (BodyParam a -> IO Request)
+  where
+    gSpec toRequest toSession = describe "Malformed BodyParam" $
+        forM_ (first toRequest <$> malformed) $ \(newReq, msg) -> do
+            req <- runIO newReq
+            it (titleize (Proxy @a) req) $
+                runSession (toSession req msg) application
+
+--
+-- Construct test cases from the API
 --
 
 application :: Application
-application = serve (Proxy @Api) server
+application = serve api server
     & handleRawError (curry handler)
 
-type Api = TestCaptureWalletId
-      :<|> TestCaptureTxId
-      :<|> TestCaptureStakePoolId
+api :: Proxy (Api Testnet)
+api = Proxy @(Api Testnet)
 
-type TestCaptureWalletId = "testCaptureWalletId"
-    :> Capture "walletId" (ApiT WalletId)
-    :> GetNoContent
+server :: Server (Api Testnet)
+server = error
+    "No test from this module should actually reach handlers of the server. \
+    \Tests are indeed all testing the internal machinery of Servant + Wai and \
+    \the way they interact with the outside world. Only valid requests are \
+    \delegated to our handlers."
 
-type TestCaptureTxId = "testCaptureTxId"
-    :> Capture "txId" ApiTxId
-    :> GetNoContent
+newtype PathParam t = PathParam Text
+    deriving (Typeable)
 
-type TestCaptureStakePoolId = "testCaptureStakePoolId"
-    :> Capture "poolId" (ApiT PoolId)
-    :> GetNoContent
+newtype BodyParam t = BodyParam ByteString
+    deriving (Typeable)
 
-server :: Server Api
-server = const (throwError err501)
-    :<|> const (throwError err501)
-    :<|> const (throwError err501)
+everyPathParams :: GEveryParams api => Proxy api -> MkPathRequest api
+everyPathParams proxy = gEveryPathParams proxy defaultRequest
+
+everyBodyParams :: GEveryParams api => Proxy api -> MkBodyRequest api
+everyBodyParams proxy = gEveryBodyParams proxy $ defaultRequest
+    { requestHeaders =
+        [ (hContentType, "application/json")
+        , (hAccept, "application/json")
+        ]
+    }
+
+class GEveryParams api where
+    type MkPathRequest api :: *
+    gEveryPathParams :: Proxy api -> Request -> MkPathRequest api
+
+    type MkBodyRequest api :: *
+    gEveryBodyParams :: Proxy api -> Request -> MkBodyRequest api
+
+    -- TODO
+    -- Capture request query params as QueryParam
+    --
+    -- newtype QueryParam t = QueryParam Text
+    --     deriving (Typeable)
+    --
+    -- type MkQueryRequest api :: *
+    -- gEveryQueryParams :: Proxy api -> Request -> MkQueryRequest api
+
+instance
+    ( GEveryParams a
+    , GEveryParams b
+    ) => GEveryParams (a :<|> b)
+  where
+    type MkPathRequest (a :<|> b) = MkPathRequest a :<|> MkPathRequest b
+    gEveryPathParams _ req = do
+        gEveryPathParams (Proxy @a) req
+      :<|>
+        gEveryPathParams (Proxy @b) req
+
+    type MkBodyRequest (a :<|> b) = MkBodyRequest a :<|> MkBodyRequest b
+    gEveryBodyParams _ req = do
+        gEveryBodyParams (Proxy @a) req
+      :<|>
+        gEveryBodyParams (Proxy @b) req
+
+instance
+    ( ReflectMethod m
+    ) => GEveryParams (Verb (m :: StdMethod) s ct a)
+  where
+    type MkPathRequest (Verb m s ct a) = Request
+    gEveryPathParams _ req =
+        req { requestMethod = reflectMethod (Proxy @m) }
+
+    type MkBodyRequest (Verb m s ct a) = Request
+    gEveryBodyParams _ req =
+        req { requestMethod = reflectMethod (Proxy @m) }
+
+instance
+    ( ReflectMethod m
+    ) => GEveryParams (NoContentVerb (m :: StdMethod))
+  where
+    type MkPathRequest (NoContentVerb m) = Request
+    gEveryPathParams _ req =
+        req { requestMethod = reflectMethod (Proxy @m) }
+
+    type MkBodyRequest (NoContentVerb m) = Request
+    gEveryBodyParams _ req =
+        req { requestMethod = reflectMethod (Proxy @m) }
+
+instance
+    ( WellFormed (PathParam t)
+    , GEveryParams sub
+    ) => GEveryParams (Capture p t :> sub)
+  where
+    type MkPathRequest (Capture p t :> sub) = PathParam t -> MkPathRequest sub
+    gEveryPathParams _ req t =
+        gEveryPathParams (Proxy @sub) (addPathFragment t req)
+
+    type MkBodyRequest (Capture p t :> sub) = MkBodyRequest sub
+    gEveryBodyParams _ req =
+        gEveryBodyParams (Proxy @sub) (addPathFragment t req)
+      where
+        t = wellformed :: PathParam t
+
+instance
+    ( KnownSymbol s
+    , GEveryParams sub
+    ) => GEveryParams (s :> sub)
+  where
+    type MkPathRequest (s :> sub) = MkPathRequest sub
+    gEveryPathParams _ req =
+        gEveryPathParams (Proxy @sub) (addPathFragment str req)
+      where
+        str = PathParam $ T.pack $ symbolVal (Proxy @s)
+
+    type MkBodyRequest (s :> sub) = MkBodyRequest sub
+    gEveryBodyParams _ req =
+        gEveryBodyParams (Proxy @sub) (addPathFragment str req)
+      where
+        str = PathParam $ T.pack $ symbolVal (Proxy @s)
+
+instance
+    ( GEveryParams sub
+    ) => GEveryParams (ReqBody a b :> sub)
+  where
+    type MkPathRequest (ReqBody a b :> sub) = MkPathRequest sub
+    gEveryPathParams _ =
+        gEveryPathParams (Proxy @sub)
+
+    type MkBodyRequest (ReqBody a b :> sub) = BodyParam b -> IO (MkBodyRequest sub)
+    gEveryBodyParams _ req b =
+        gEveryBodyParams (Proxy @sub) <$> (setRequestBody b req)
+
+instance
+    ( GEveryParams sub
+    ) => GEveryParams (Servant.QueryParam a b :> sub)
+  where
+    type MkPathRequest (Servant.QueryParam a b :> sub) = MkPathRequest sub
+    gEveryPathParams _ =
+        gEveryPathParams (Proxy @sub)
+
+    type MkBodyRequest (Servant.QueryParam a b :> sub) = MkBodyRequest sub
+    gEveryBodyParams _ =
+        gEveryBodyParams (Proxy @sub)
+
+--
+-- Helpers
+--
+
+addPathFragment :: PathParam t -> Request -> Request
+addPathFragment (PathParam fragment) req = req
+    { pathInfo = pathInfo req ++ [fragment] }
+
+setRequestBody :: BodyParam b -> Request -> IO Request
+setRequestBody (BodyParam bytes) req = do
+    ref <- newIORef $ BL.toChunks bytes
+    pure req
+        { requestBodyLength = KnownLength $ fromIntegral $ BL.length bytes
+        , requestBody = atomicModifyIORef ref $ \case
+            []  -> ([], mempty)
+            h:q -> (q, h)
+        }
+
+titleize
+    :: forall t. (Typeable t)
+    => Proxy t
+    -> Request
+    -> String
+titleize proxy req = unwords
+    [ "(" <> show (typeRep proxy) <> ")"
+    , B8.unpack (requestMethod req)
+    , "/" <> T.unpack (T.intercalate "/" $ pathInfo req)
+    ]
