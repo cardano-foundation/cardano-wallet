7c81311f5c8edbdd62b0b8f6362ccdc165855281
KtorZ <matthias.benkort@gmail.com>
2020-02-25 17:51:04 +0100

    remove malformed unit tests in Jörmungandr, moved to core/ApiSpec already

--- lib/jormungandr/test/unit/Cardano/Wallet/Jormungandr/ApiSpec.hs
+++ lib/jormungandr/test/unit/Cardano/Wallet/Jormungandr/ApiSpec.hs
@@ -147,59 +147,6 @@ spec = do
                     }
             return ()
 
-        it "invalid stake pool id in endpoint response gives expected error" $ do
-            let exampleStake = "{\"epoch\": 252054,\"stake\": {\"dangling\":0,\"\
-                    \pools\":[[\"b80fed0d2289d535a94f6870add0cf8b3\
-                    \74cfe6cae078320ec\",1]],\"unassigned\":100100000000000}}"
-            decodeJSON exampleStake `shouldBe`
-                Left "Error in $.stake.pools[0][0]: stake pool id invalid: \
-                     \expected 32 bytes but got 25"
-            return ()
-
-        it "invalid stake pool id in endpoint response gives expected error" $ do
-            let exampleStake = "{\"epoch\": 252054,\"stake\": {\"dangling\":0,\"\
-                    \pools\":[[12345,1]],\"unassigned\":100100000000000}}"
-            decodeJSON exampleStake `shouldBe`
-                Left "Error in $.stake.pools[0][0]: expected Text, encountered Number"
-            return ()
-
-        it "invalid stake pair in endpoint response gives expected error" $ do
-            let exampleStake = "{\"epoch\": 252054,\"stake\": {\"dangling\":0,\"\
-                    \pools\":[[\"7d749ef424507fb80fed0d2289d535a94f6870add0cf8b3\
-                    \74cfe6cae078320ec\",1, \"not needed field\"]],\"unassigned\":\
-                    \100100000000000}}"
-            decodeJSON exampleStake `shouldBe`
-                Left "Error in $.stake.pools[0]: cannot unpack array of length 3\
-                     \ into a tuple of length 2"
-            return ()
-
-        it "invalid numerical field value in endpoint response gives expected error" $ do
-            let exampleStake = "{\"epoch\": 252054,\"stake\": {\"dangling\":0,\"\
-                    \pools\":[[\"7d749ef424507fb80fed0d2289d535a94f6870add0cf8b3\
-                    \74cfe6cae078320ec\",1]],\"unassigned\":[]}}"
-            decodeJSON exampleStake `shouldBe`
-                Left "Error in $.stake.unassigned: expected Word64, \
-                     \encountered Array"
-            return ()
-
-        it "invalid non-numerical field value in endpoint response gives expected error" $ do
-            let exampleStake = "{\"epoch\": 252054,\"stake\": {\"dangling\":0,\"\
-                    \pools\":[[\"7d749ef424507fb80fed0d2289d535a94f6870add0cf8b3\
-                    \74cfe6cae078320ec\",1]],\"unassigned\":-10010000}}"
-            decodeJSON exampleStake `shouldBe`
-                Left "Error in $.stake.unassigned: Word64 is either floating or \
-                     \will cause over or underflow: -1.001e7"
-            return ()
-
-        it "invalid non-numerical field value in endpoint response gives expected error" $ do
-            let exampleStake = "{\"epoch\": 252054,\"stake\": {\"dangling\":0,\"\
-                    \pools\":[[\"7d749ef424507fb80fed0d2289d535a94f6870add0cf8b3\
-                    \74cfe6cae078320ec\",1]],\"unassigned\":10010000.23}}"
-            decodeJSON exampleStake `shouldBe`
-                Left "Error in $.stake.unassigned: Word64 is either floating or \
-                     \will cause over or underflow: 1.001000023e7"
-            return ()
-
         describe "MimeUnrender decoding" $ do
             it "returns 'Left' when encountering an invalid binary block" $ do
                 mimeUnrender (Proxy @JormungandrBinary) ""
