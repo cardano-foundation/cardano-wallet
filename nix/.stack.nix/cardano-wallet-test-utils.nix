6393f8b02aff95f5348a9afdebc0d98459035875
Johannes Lund <johannes.lund@iohk.io>
2020-04-01 14:14:34 +0200

    Bump from 2020.3.16 to 2020.4.1
    set OLD_VERSION "2020.3.16"
    set NEW_VERSION "2020.4.1"
    find nix -name "*.nix" -type file | xargs perl -i -pe "s/$OLD_VERSION/$NEW_VERSION/"
    find lib -name "*.cabal" -type file | xargs perl -i -pe "s/$OLD_VERSION/$NEW_VERSION/"

--- nix/.stack.nix/cardano-wallet-test-utils.nix
+++ nix/.stack.nix/cardano-wallet-test-utils.nix
@@ -44,7 +44,7 @@ in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
       specVersion = "1.10";
       identifier = {
         name = "cardano-wallet-test-utils";
-        version = "2020.3.16";
+        version = "2020.4.1";
         };
       license = "Apache-2.0";
       copyright = "2018-2020 IOHK";
