955ca23bf85918ade456f7c33dd88b6ac266d0a8
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-04-03 20:46:56 +1000

    Buildkite: tweak latency benchmark build output

--- .buildkite/bench-latency.sh
+++ .buildkite/bench-latency.sh
@@ -10,11 +10,14 @@ echo "--- Build"
 nix-build -A benchmarks.cardano-wallet-jormungandr.latency -o $bench_name_jormungandr
 nix-build -A benchmarks.cardano-wallet-byron.latency -o $bench_name_byron
 
-echo "+++ Run benchmark"
-
 # Note: the tracing will not work if program output is piped
 # to another process (e.g. "tee").
 # It says "Error: Switchboard's queue full, dropping log items!"
 
-./$bench_name_jormungandr/bin/latency
+echo "+++ Run benchmark - byron"
+
 ./$bench_name_byron/bin/latency
+
+echo "+++ Run benchmark - jormungandr"
+
+./$bench_name_jormungandr/bin/latency
