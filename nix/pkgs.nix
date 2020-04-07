0f127e79ea14478d93de8ccefe75d9b5675352e6
Johannes Lund <johannes.lund@iohk.io>
2020-04-07 10:24:48 +0200

    Retrieve cardano-node configs using forEnvironments (& add topologies)

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -7,22 +7,24 @@ let
 in pkgs: super: with pkgs; {
   jmPkgs = import ./jormungandr.nix { inherit (pkgs) commonLib; inherit pkgs; };
   cardanoNodePkgs = import sources.cardano-node { inherit system crossSystem config; };
-  cardano-node = cardanoNodePkgs.cardano-node // {
-    mainnet = {
-      configFile =
-        let
-          rawCfg = commonLib.cardanoLib.environments.mainnet.nodeConfig // {
-						GenesisFile = commonLib.cardanoLib.environments.mainnet.genesisFile;
-            minSeverity = "Error";
-					};
-				in
-          builtins.toFile "cardano-node-mainnet-config" (builtins.toJSON rawCfg);
-    };
+  cardano-node = cardanoNodePkgs.cardano-node
+    // commonLib.cardanoLib.forEnvironments (env:
+      env // {
+        configFile =
+          let
+            rawCfg = env.nodeConfig // {
+              GenesisFile = env.genesisFile;
+              minSeverity = "Error";
+            };
+          in
+          builtins.toFile "cardano-node-config" (builtins.toJSON rawCfg);
+        })
+        // { topologyFiles = {
+            # Note: It was easier to define as topologyFiles.testnet than testnet.topologyFile.
+            # TODO: Retrieve these from iohk-nix or cardano-node directly.
+            mainnet = builtins.toFile "topology" "{\"Producers\":[{\"addr\":\"relays-new.cardano-mainnet.iohk.io\",\"port\":3001,\"valency\":1}]}";
+            testnet = builtins.toFile "topology" "{\"Producers\":[{\"addr\":\"relays-new.cardano-testnet.iohkdev.io\",\"port\":3001,\"valency\":1}]}";
+          };};
 
-    # provide configuration directory as a convenience
-    configs = pkgs.runCommand "cardano-node-configs" {} ''
-      cp -R ${sources.cardano-node}/configuration $out;
-    '';
-  };
   inherit (pkgs1903) stack;
 }
