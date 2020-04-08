160c860d19c9fb3e6a00577687ca16b91222932c
Johannes Lund <johannes.lund@iohk.io>
2020-04-08 16:33:59 +0200

    Test Tip - BlockHeader conversions
    Example failure:
    
    Falsified (after 1 test):
      Hash {getHash = "\NUL\SOH\NUL\SOH\NUL\SOH\NUL\NUL\NUL\NUL\NUL\SOH\NUL\NUL\SOH\SOH\SOH\NUL\NUL\NUL\NUL\SOH\NUL\SOH\SOH\SOH\NUL\NUL\SOH\NUL\SOH\NUL"}
      Tip (SlotNo {unSlotNo = 0}) (ByronHash {unByronHash = AbstractHash 0000000000000000000000000000000000000000000000000000000000000000}) (BlockNo {unBlockNo = 0})
      Origin /= At (Block {blockPointSlot = SlotNo {unSlotNo = 0}, blockPointHash = ByronHash {unByronHash = AbstractHash 0000000000000000000000000000000000000000000000000000000000000000}

--- lib/byron/test/unit/Cardano/Wallet/Byron/CompatibilitySpec.hs
+++ lib/byron/test/unit/Cardano/Wallet/Byron/CompatibilitySpec.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Cardano.Wallet.Byron.CompatibilitySpec
     ( spec
     ) where
 
 import Prelude
 
+import Cardano.Crypto.Hashing
+    ( decodeHash )
 import Cardano.Wallet.Byron.Compatibility
-    ( toGenTx )
+    ( ByronBlock, fromTip, toGenTx, toPoint )
 import Cardano.Wallet.Byron.TransactionSpec
     ( goldenMainnet__1_1
     , goldenMainnet__1_25
@@ -17,13 +23,21 @@ import Cardano.Wallet.Byron.TransactionSpec
     , goldenTestnet__2_2
     )
 import Cardano.Wallet.Primitive.Types
-    ( SealedTx (..) )
+    ( EpochLength (..), Hash (..), SealedTx (..), SlotId (..), fromFlatSlot )
 import Cardano.Wallet.Unsafe
     ( unsafeFromHex )
 import Control.Monad
     ( forM_ )
+import Ouroboros.Consensus.Byron.Ledger.Block
+    ( ByronHash (..) )
+import Ouroboros.Network.Block
+    ( BlockNo (..), SlotNo (..), Tip (..), getTipPoint )
 import Test.Hspec
     ( Spec, describe, it )
+import Test.QuickCheck
+
+import qualified Data.ByteString as BS
+import qualified Data.Text as T
 
 spec :: Spec
 spec = do
@@ -40,3 +54,36 @@ spec = do
             ] $ \bytes ->
             let str = show $ toGenTx $ SealedTx $ unsafeFromHex bytes
             in it (take 23 str) (putStrLn str)
+
+    describe "Conversions" $
+        it "toPoint' . fromTip' == getTipPoint" $ property $ \gh tip -> do
+            let fromTip' = fromTip gh epochLength
+            let toPoint' = toPoint epochLength
+            toPoint' (fromTip' tip) === (getTipPoint tip)
+
+instance Arbitrary (Hash "Genesis") where
+    arbitrary = Hash . BS.pack <$> vector 32
+
+instance Arbitrary (Hash "BlockHeader") where
+    arbitrary = Hash . BS.pack <$> vector 32
+
+instance (Arbitrary (Tip ByronBlock)) where
+    arbitrary = oneof
+        [ return TipGenesis
+        , arbitraryTip
+        ]
+      where
+        arbitraryTip = do
+            n <- choose (0, 100)
+            return $ Tip (SlotNo n) hash (BlockNo n)
+        hash = ByronHash
+            . either (error . show) id
+            . decodeHash
+            . T.pack
+            $ replicate 64 '0'
+
+epochLength :: EpochLength
+epochLength = EpochLength 10
+
+instance Arbitrary SlotId where
+    arbitrary = fromFlatSlot epochLength <$> choose (0, 100)
