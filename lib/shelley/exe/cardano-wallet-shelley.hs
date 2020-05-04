f57878c6b52d7393232b8788963850966005c43b
Johannes Lund <johannes.lund@iohk.io>
2020-05-04 23:27:40 +0200

    Scaffold cardano-wallet-shelley from the byron one

---
+++ lib/shelley/exe/cardano-wallet-shelley.hs
@@ -0,0 +1,308 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+-- |
+-- Copyright: © 2018-2020 IOHK
+-- License: Apache-2.0
+--
+-- This module parses command line arguments for the wallet and executes
+-- corresponding commands.
+--
+-- In essence, it's a proxy to the wallet server, which is required for most
+-- commands. Commands are turned into corresponding API calls, and submitted
+-- to an up-and-running server. Some commands do not require an active server
+-- and can be run "offline".
+
+module Main where
+
+import Prelude
+
+import Cardano.BM.Data.Severity
+    ( Severity (..) )
+import Cardano.BM.Trace
+    ( Trace, appendName, logDebug, logError, logInfo, logNotice )
+import Cardano.CLI
+    ( LoggingOptions
+    , cli
+    , cmdAddress
+    , cmdKey
+    , cmdMnemonicShelley
+    , cmdNetwork
+    , cmdShelleyWalletCreate
+    , cmdTransaction
+    , cmdVersion
+    , cmdWallet
+    , databaseOption
+    , enableWindowsANSI
+    , helperTracing
+    , hostPreferenceOption
+    , listenOption
+    , loggingMinSeverity
+    , loggingOptions
+    , loggingSeverityOrOffReader
+    , loggingTracers
+    , runCli
+    , setupDirectory
+    , shutdownHandlerFlag
+    , syncToleranceOption
+    , tlsOption
+    , withLogging
+    )
+import Cardano.Startup
+    ( ShutdownHandlerLog
+    , installSignalHandlers
+    , withShutdownHandler
+    , withUtf8Encoding
+    )
+import Cardano.Wallet.Api.Client
+    ( byronAddressClient
+    , byronTransactionClient
+    , byronWalletClient
+    , networkClient
+    )
+import Cardano.Wallet.Api.Server
+    ( HostPreference, Listen (..), TlsConfiguration )
+import Cardano.Wallet.Logging
+    ( trMessage, transformTextTrace )
+import Cardano.Wallet.Primitive.Types
+    ( SyncTolerance )
+import Cardano.Wallet.Shelley
+    ( TracerSeverities
+    , Tracers
+    , Tracers' (..)
+    , serveWallet
+    , setupTracers
+    , tracerDescriptions
+    , tracerLabels
+    )
+import Cardano.Wallet.Shelley.Launch
+    ( NetworkConfiguration (..)
+    , networkConfigurationOption
+    , nodeSocketOption
+    , parseGenesisData
+    )
+import Cardano.Wallet.Version
+    ( GitRevision, Version, gitRevision, showFullVersion, version )
+import Control.Applicative
+    ( Const (..), optional )
+import Control.Monad
+    ( void )
+import Control.Monad.Trans.Except
+    ( runExceptT )
+import Control.Tracer
+    ( contramap )
+import Data.Text
+    ( Text )
+import Data.Text.Class
+    ( ToText (..) )
+import Network.Socket
+    ( SockAddr )
+import Options.Applicative
+    ( CommandFields
+    , Mod
+    , Parser
+    , command
+    , helper
+    , info
+    , internal
+    , long
+    , metavar
+    , option
+    , progDesc
+    , value
+    )
+import System.Environment
+    ( getArgs, getExecutablePath )
+import System.Exit
+    ( ExitCode (..), exitWith )
+
+import qualified Data.Text as T
+
+{-------------------------------------------------------------------------------
+                              Main entry point
+-------------------------------------------------------------------------------}
+
+main :: IO ()
+main = withUtf8Encoding $ do
+    enableWindowsANSI
+    runCli $ cli $ mempty
+        <> cmdServe
+        <> cmdMnemonicShelley
+        <> cmdKey
+        <> cmdWallet cmdShelleyWalletCreate byronWalletClient
+        <> cmdAddress byronAddressClient
+        <> cmdTransaction byronTransactionClient byronWalletClient
+        <> cmdNetwork networkClient
+        <> cmdVersion
+
+beforeMainLoop
+    :: Trace IO MainLog
+    -> SockAddr
+    -> IO ()
+beforeMainLoop tr =
+    logInfo tr . MsgListenAddress
+
+{-------------------------------------------------------------------------------
+                            Command - 'serve'
+-------------------------------------------------------------------------------}
+
+-- | Arguments for the 'serve' command
+data ServeArgs = ServeArgs
+    { _hostPreference :: HostPreference
+    , _listen :: Listen
+    , _tlsConfig :: Maybe TlsConfiguration
+    , _nodeSocket :: FilePath
+    , _networkConfiguration :: NetworkConfiguration
+    , _database :: Maybe FilePath
+    , _syncTolerance :: SyncTolerance
+    , _enableShutdownHandler :: Bool
+    , _logging :: LoggingOptions TracerSeverities
+    } deriving (Show)
+
+cmdServe
+    :: Mod CommandFields (IO ())
+cmdServe = command "serve" $ info (helper <*> helper' <*> cmd) $ mempty
+    <> progDesc "Serve API that listens for commands/actions."
+  where
+    helper' = helperTracing tracerDescriptions
+
+    cmd = fmap exec $ ServeArgs
+        <$> hostPreferenceOption
+        <*> listenOption
+        <*> optional tlsOption
+        <*> nodeSocketOption
+        <*> networkConfigurationOption
+        <*> optional databaseOption
+        <*> syncToleranceOption
+        <*> shutdownHandlerFlag
+        <*> loggingOptions tracerSeveritiesOption
+    exec
+        :: ServeArgs
+        -> IO ()
+    exec args@(ServeArgs
+      host
+      listen
+      tlsConfig
+      nodeSocket
+      networkConfig
+      databaseDir
+      sTolerance
+      enableShutdownHandler
+      logOpt) = do
+        withTracers logOpt $ \tr tracers -> do
+            installSignalHandlers (logNotice tr MsgSigTerm)
+            withShutdownHandlerMaybe tr enableShutdownHandler $ do
+                logDebug tr $ MsgServeArgs args
+
+                (discriminant, bp, vData, block0)
+                    <- runExceptT (parseGenesisData networkConfig) >>= \case
+                            Right x -> pure x
+                            Left err -> do
+                                logError tr (MsgFailedToParseGenesis $ T.pack err)
+                                exitWith $ ExitFailure 33
+
+                whenJust databaseDir $ setupDirectory (logInfo tr . MsgSetupDatabases)
+                exitWith =<< serveWallet
+                    discriminant
+                    tracers
+                    sTolerance
+                    databaseDir
+                    host
+                    listen
+                    tlsConfig
+                    nodeSocket
+                    block0
+                    (bp, vData)
+                    (beforeMainLoop tr)
+
+    whenJust m fn = case m of
+       Nothing -> pure ()
+       Just a  -> fn a
+    withShutdownHandlerMaybe :: Trace IO MainLog -> Bool -> IO () -> IO ()
+    withShutdownHandlerMaybe _ False = void
+    withShutdownHandlerMaybe tr True = void . withShutdownHandler trShutdown
+      where
+        trShutdown = trMessage $ contramap (\(n, x) -> (n, fmap MsgShutdownHandler x)) tr
+{-------------------------------------------------------------------------------
+                                    Logging
+-------------------------------------------------------------------------------}
+
+-- FIXME: reduce duplication. See 'cardano-wallet-jormungandr.hs'
+
+data MainLog
+    = MsgCmdLine String [String]
+    | MsgVersion Version GitRevision
+    | MsgSetupStateDir Text
+    | MsgSetupDatabases Text
+    | MsgServeArgs ServeArgs
+    | MsgListenAddress SockAddr
+    | MsgSigTerm
+    | MsgShutdownHandler ShutdownHandlerLog
+    | MsgFailedToParseGenesis Text
+    deriving (Show)
+
+instance ToText MainLog where
+    toText msg = case msg of
+        MsgCmdLine exe args ->
+            T.pack $ unwords ("Command line:":exe:args)
+        MsgVersion ver rev ->
+            "Running as v" <> T.pack (showFullVersion ver rev)
+        MsgSetupStateDir txt ->
+            "Wallet state: " <> txt
+        MsgSetupDatabases txt ->
+            "Wallet databases: " <> txt
+        MsgServeArgs args ->
+            T.pack $ show args
+        MsgListenAddress addr ->
+            "Wallet backend server listening on " <> T.pack (show addr)
+        MsgSigTerm ->
+            "Terminated by signal."
+        MsgShutdownHandler msg' ->
+            toText msg'
+        MsgFailedToParseGenesis hint -> T.unwords
+            [ "Failed to parse genesis configuration. You may want to check the"
+            , "filepath given via --genesis and make sure it points to a valid"
+            , "JSON genesis file. Here's (perhaps) some helpful hint:", hint
+            ]
+
+withTracers
+    :: LoggingOptions TracerSeverities
+    -> (Trace IO MainLog -> Tracers IO -> IO a)
+    -> IO a
+withTracers logOpt action =
+    withLogging Nothing (loggingMinSeverity logOpt) $ \(_, tr) -> do
+        let trMain = appendName "main" (transformTextTrace tr)
+        let tracers = setupTracers (loggingTracers logOpt) tr
+        logInfo trMain $ MsgVersion version gitRevision
+        logInfo trMain =<< MsgCmdLine <$> getExecutablePath <*> getArgs
+        action trMain tracers
+
+
+{-------------------------------------------------------------------------------
+                                 Options
+-------------------------------------------------------------------------------}
+
+
+tracerSeveritiesOption :: Parser TracerSeverities
+tracerSeveritiesOption = Tracers
+    <$> traceOpt applicationTracer (Just Info)
+    <*> traceOpt apiServerTracer (Just Info)
+    <*> traceOpt walletEngineTracer (Just Info)
+    <*> traceOpt walletDbTracer (Just Info)
+    <*> traceOpt ntpClientTracer (Just Info)
+    <*> traceOpt networkTracer (Just Info)
+  where
+    traceOpt field def = fmap Const . option loggingSeverityOrOffReader $ mempty
+        <> long ("trace-" <> T.unpack (getConst (field tracerLabels)))
+        <> value def
+        <> metavar "SEVERITY"
+        <> internal
+
---
+++ lib/shelley/src/Cardano/Wallet/Shelley/Launch.hs
@@ -0,0 +1,373 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+-- |
+-- Copyright: © 2018-2020 IOHK
+-- License: Apache-2.0
+--
+-- Provides a function to launch cardano-node for /testing/.
+
+module Cardano.Wallet.Byron.Launch
+    ( -- * Integration Launcher
+      withCardanoNode
+
+    , NetworkConfiguration (..)
+    , nodeSocketOption
+    , networkConfigurationOption
+    , parseGenesisData
+    ) where
+
+import Prelude
+
+import Cardano.BM.Data.Severity
+    ( Severity (..) )
+import Cardano.BM.Trace
+    ( Trace )
+import Cardano.Chain.Genesis
+    ( GenesisData (..), readGenesisData )
+import Cardano.CLI
+    ( optionT )
+import Cardano.Crypto.ProtocolMagic
+    ( ProtocolMagicId (..) )
+import Cardano.Launcher
+    ( Command (..), StdStream (..), withBackendProcess )
+import Cardano.Wallet.Byron
+    ( SomeNetworkDiscriminant (..) )
+import Cardano.Wallet.Byron.Compatibility
+    ( NodeVersionData
+    , emptyGenesis
+    , fromGenesisData
+    , fromProtocolMagicId
+    , mainnetBlockchainParameters
+    , mainnetVersionData
+    , testnetVersionData
+    )
+import Cardano.Wallet.Byron.Transaction
+    ( genesisBlockFromTxOuts )
+import Cardano.Wallet.Logging
+    ( trMessageText )
+import Cardano.Wallet.Network.Ports
+    ( getRandomPort )
+import Cardano.Wallet.Primitive.AddressDerivation
+    ( NetworkDiscriminant (..) )
+import Cardano.Wallet.Primitive.Types
+    ( Block (..), GenesisBlockParameters (..), ProtocolMagic (..) )
+import Cardano.Wallet.Unsafe
+    ( unsafeRunExceptT )
+import Control.Applicative
+    ( (<|>) )
+import Control.Exception
+    ( bracket, throwIO )
+import Control.Monad
+    ( forM_, unless, when )
+import Control.Monad.Fail
+    ( MonadFail )
+import Control.Monad.Trans.Except
+    ( ExceptT, withExceptT )
+import Data.Aeson
+    ( toJSON )
+import Data.Function
+    ( (&) )
+import Data.Proxy
+    ( Proxy (..) )
+import Data.Text
+    ( Text )
+import Data.Time.Clock.POSIX
+    ( getPOSIXTime )
+import GHC.TypeLits
+    ( SomeNat (..), someNatVal )
+import Options.Applicative
+    ( Parser, flag', help, long, metavar )
+import Ouroboros.Network.Magic
+    ( NetworkMagic (..) )
+import Ouroboros.Network.NodeToClient
+    ( NodeToClientVersionData (..), nodeToClientCodecCBORTerm )
+import System.Directory
+    ( copyFile, doesDirectoryExist, removeDirectoryRecursive )
+import System.Environment
+    ( lookupEnv )
+import System.FilePath
+    ( takeFileName, (</>) )
+import System.Info
+    ( os )
+import System.IO.Temp
+    ( createTempDirectory, getCanonicalTemporaryDirectory )
+
+import qualified Data.Aeson as Aeson
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Text as T
+import qualified Data.Yaml as Yaml
+
+data NetworkConfiguration where
+    MainnetConfig
+        :: (SomeNetworkDiscriminant, NodeVersionData)
+        -> NetworkConfiguration
+
+    TestnetConfig
+        :: FilePath
+        -> NetworkConfiguration
+
+-- | Hand-written as there's no Show instance for 'NodeVersionData'
+instance Show NetworkConfiguration where
+    show = \case
+        MainnetConfig{} ->
+            "MainnetConfig"
+        TestnetConfig genesisFile ->
+            "TestnetConfig " <> show genesisFile
+
+-- | --node-socket=FILE
+nodeSocketOption :: Parser FilePath
+nodeSocketOption = optionT $ mempty
+    <> long "node-socket"
+    <> metavar "FILE"
+    <> help "Path to the node's domain socket."
+
+-- | --mainnet | (--testnet=FILE)
+networkConfigurationOption :: Parser NetworkConfiguration
+networkConfigurationOption =
+    mainnetFlag <|> (TestnetConfig <$> testnetOption)
+  where
+    -- --mainnet
+    mainnetFlag = flag'
+        (MainnetConfig (SomeNetworkDiscriminant $ Proxy @'Mainnet, mainnetVersionData))
+        (long "mainnet")
+
+    -- | --testnet=FILE
+    testnetOption
+        :: Parser FilePath
+    testnetOption = optionT $ mempty
+        <> long "testnet"
+        <> metavar "FILE"
+        <> help "Path to the genesis .json file."
+
+someTestnetDiscriminant
+    :: ProtocolMagic
+    -> (SomeNetworkDiscriminant, NodeVersionData)
+someTestnetDiscriminant pm@(ProtocolMagic n) =
+    case someNatVal (fromIntegral n) of
+        Just (SomeNat proxy) ->
+            ( SomeNetworkDiscriminant $ mapProxy proxy
+            , testnetVersionData pm
+            )
+        _ -> error "networkDiscriminantFlag: failed to convert \
+            \ProtocolMagic to SomeNat."
+  where
+    mapProxy :: forall a. Proxy a -> Proxy ('Testnet a)
+    mapProxy _proxy = Proxy @('Testnet a)
+
+parseGenesisData
+    :: NetworkConfiguration
+    -> ExceptT String IO (SomeNetworkDiscriminant, GenesisBlockParameters, NodeVersionData, Block)
+parseGenesisData = \case
+    MainnetConfig (discriminant, vData) -> pure
+        ( discriminant
+        , mainnetBlockchainParameters
+        , vData
+        , emptyGenesis (staticParameters mainnetBlockchainParameters)
+        )
+    TestnetConfig genesisFile -> do
+        (genesisData, genesisHash) <-
+            withExceptT show $ readGenesisData genesisFile
+
+        let (discriminant, vData) = genesisData
+                & gdProtocolMagicId
+                & fromProtocolMagicId
+                & someTestnetDiscriminant
+
+        let (gbp, outs) = fromGenesisData (genesisData, genesisHash)
+        pure
+            ( discriminant
+            , gbp
+            , vData
+            , genesisBlockFromTxOuts (staticParameters gbp) outs
+            )
+
+--------------------------------------------------------------------------------
+-- For Integration
+--------------------------------------------------------------------------------
+
+data CardanoNodeConfig = CardanoNodeConfig
+    { nodeConfigFile   :: FilePath
+    , nodeDatabaseDir  :: FilePath
+    , nodeDlgCertFile  :: FilePath
+    , nodeSignKeyFile  :: FilePath
+    , nodeSocketFile   :: FilePath
+    , nodeTopologyFile :: FilePath
+    }
+
+-- | Spins up a @cardano-node@ in another process.
+--
+-- IMPORTANT: @cardano-node@ must be available on the current path.
+withCardanoNode
+    :: Trace IO Text
+    -- ^ Trace for subprocess control logging
+    -> FilePath
+    -- ^ Test directory in source tree
+    -> Severity
+    -- ^ Logging level for @cardano-node@
+    -> (FilePath -> Block -> (GenesisBlockParameters, NodeVersionData) -> IO a)
+    -- ^ Callback function with a socket filename and genesis params
+    -> IO a
+withCardanoNode tr tdir severity action =
+    orThrow $ withConfig tdir severity $ \cfg block0 (gbp, vData) -> do
+        nodePort <- getRandomPort
+        let args = mkArgs cfg nodePort
+        let cmd = Command "cardano-node" args (pure ()) Inherit Inherit
+        withBackendProcess (trMessageText tr) cmd $ do
+            action (nodeSocketFile cfg) block0 (gbp, vData)
+  where
+    orThrow = (=<<) (either throwIO pure)
+    mkArgs cfg port =
+        [ "run"
+        , "--config", nodeConfigFile cfg
+        , "--signing-key", nodeSignKeyFile cfg
+        , "--delegation-certificate", nodeDlgCertFile cfg
+        , "--topology", nodeTopologyFile cfg
+        , "--database-path", nodeDatabaseDir cfg
+        , "--socket-path", nodeSocketFile cfg
+        , "--port", show port
+        ]
+
+
+-- | Generate a new integration configuration based on a partial configuration
+-- located in @./test/data/cardano-node@.
+--
+-- The 'startTime' from the partial genesis file will be overriden with a new
+-- fresh recent one (resulting in a different genesis hash).
+--
+-- As a result, this function creates a temporary directory which is cleaned up
+-- after use (unless the ENV var NO_CLEANUP is set):
+--
+--     $ tree /tmp/cardano-wallet-byron-2cbb1ea94edb1cea/
+--       ├── genesis.json
+--       ├── node.cert
+--       ├── node.config
+--       ├── node.key
+--       └── node.topology
+--
+withConfig
+    :: FilePath
+    -- ^ Test data directory in source tree
+    -> Severity
+    -- ^ Logging level for @cardano-node@
+    -> (  CardanoNodeConfig
+       -> Block
+       -> (GenesisBlockParameters, NodeVersionData)
+       -> IO a
+       )
+    -- ^ Callback function with the node configuration and genesis params
+    -> IO a
+withConfig tdir minSeverity action =
+    bracket setupConfig teardownConfig $ \(_a,b,c,d) -> action b c d
+  where
+    source :: FilePath
+    source = tdir </> "cardano-node"
+
+    setupConfig
+        :: IO ( FilePath
+              , CardanoNodeConfig
+              , Block
+              , (GenesisBlockParameters, NodeVersionData)
+              )
+    setupConfig = do
+        dir <- getCanonicalTemporaryDirectory
+            >>= \tmpRoot -> createTempDirectory tmpRoot "cw-byron"
+
+        let nodeConfigFile   = dir </> "node.config"
+        let nodeDatabaseDir  = dir </> "node.db"
+        let nodeDlgCertFile  = dir </> "node.cert"
+        let nodeGenesisFile  = dir </> "genesis.json"
+        let nodeSignKeyFile  = dir </> "node.key"
+        let nodeTopologyFile = dir </> "node.topology"
+        let nodeSocketFile =
+                if os == "mingw32"
+                then "\\\\.\\pipe\\" ++ takeFileName dir
+                else dir </> "node.socket"
+
+        -- we need to specify genesis file location every run in tmp
+        Yaml.decodeFileThrow (source </> "node.config")
+            >>= withObject (addGenesisFilePath (T.pack nodeGenesisFile))
+            >>= withObject (addMinSeverity (T.pack $ show minSeverity))
+            >>= Yaml.encodeFile (dir </> "node.config")
+
+        Yaml.decodeFileThrow @_ @Aeson.Value (source </> "genesis.yaml")
+            >>= withObject updateStartTime
+            >>= Aeson.encodeFile nodeGenesisFile
+        forM_ ["node.topology", "node.key", "node.cert"] $ \f ->
+            copyFile (source </> f) (dir </> f)
+
+        (genesisData, genesisHash) <- unsafeRunExceptT $
+            readGenesisData nodeGenesisFile
+        let (gbp, outs) = fromGenesisData (genesisData, genesisHash)
+
+        let networkMagic = NetworkMagic $ unProtocolMagicId $ gdProtocolMagicId genesisData
+
+        pure
+            ( dir
+            , CardanoNodeConfig
+                { nodeConfigFile
+                , nodeDatabaseDir
+                , nodeDlgCertFile
+                , nodeSignKeyFile
+                , nodeSocketFile
+                , nodeTopologyFile
+                }
+            , genesisBlockFromTxOuts (staticParameters gbp) outs
+            , ( gbp
+              , ( NodeToClientVersionData { networkMagic }
+                , nodeToClientCodecCBORTerm
+                )
+              )
+            )
+
+    teardownConfig
+        :: (FilePath, b, c, d)
+        -> IO ()
+    teardownConfig (dir, _, _, _) = do
+        noCleanup <- maybe False (not . null) <$> lookupEnv "NO_CLEANUP"
+        exists <- doesDirectoryExist dir
+        unless noCleanup $ when exists $ removeDirectoryRecursive dir
+
+-- | Add a "startTime" field in a given object with the current POSIX time as a
+-- value.
+updateStartTime
+    :: Aeson.Object
+    -> IO Aeson.Object
+updateStartTime m = do
+    time <- round @_ @Int <$> getPOSIXTime
+    pure $ HM.insert "startTime" (toJSON time) m
+
+-- | Add a "GenesisFile" field in a given object with the current path of
+-- genesis.json in tmp dir as value.
+addGenesisFilePath
+    :: Text
+    -> Aeson.Object
+    -> IO Aeson.Object
+addGenesisFilePath path = pure . HM.insert "GenesisFile" (toJSON path)
+
+-- | Add a "minSeverity" field in a given object with the current path of
+-- genesis.json in tmp dir as value.
+addMinSeverity
+    :: Text
+    -> Aeson.Object
+    -> IO Aeson.Object
+addMinSeverity severity = pure . HM.insert "minSeverity" (toJSON severity)
+
+-- | Do something with an a JSON object. Fails if the given JSON value isn't an
+-- object.
+withObject
+    :: MonadFail m
+    => (Aeson.Object -> m Aeson.Object)
+    -> Aeson.Value
+    -> m Aeson.Value
+withObject action = \case
+    Aeson.Object m -> Aeson.Object <$> action m
+    _ -> fail
+        "withObject: was given an invalid JSON. Expected an Object but got \
+        \something else."
