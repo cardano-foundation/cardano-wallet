fda274f1776aa0211dbfc7b856e3148d2872733e
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-05-14 14:41:43 +1000

    nix: add util function for converting date version formats

--- nix/util.nix
+++ nix/util.nix
@@ -21,4 +21,15 @@ with lib; with haskell-nix.haskellLib;
   # cause errors when nix attempts to copy them into the store.
   removeSocketFilesFilter = _path: type:
     elem type ["regular" "directory" "symlink" ];
+
+  # Convert version strings from Cabal format (YYYY.M.D)
+  # to git tag format (vYYYY-MM-DD).
+  versionTag = cabal: let
+      parts = builtins.match "([[:digit:]]{4})\.([[:digit:]]{1,2})\.([[:digit:]]{1,2})" cabal;
+      leading0 = str: if stringLength str == 1 then "0" + str else str;
+    in
+      assert assertMsg (parts != null)
+        "versionTag: ${cabal} is not in YYYY.M.D format";
+      "v" + concatMapStringsSep "-" leading0 parts;
+
 }
