76123b0fd095023872d2be1fc0022b663286a644
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-06-23 19:14:50 +1000

    Buildkite: split all-tests-pass into {linux,windows}-tests-pass

--- .buildkite/push-branch.sh
+++ .buildkite/push-branch.sh
@@ -12,27 +12,40 @@
 
 set -euo pipefail
 
-branch="${1:-}"
-
-if [ -z "$branch" ]; then
-  echo "usage: $0 TARGET_BRANCH"
-  exit 1
-fi
+this_branch=linux-tests-pass
+other_branch=windows-tests-pass
+common_branch=all-tests-pass
 
 : "${sshkey:=/run/keys/buildkite-cardano-wallet-ssh-private}"
 remote="git@github.com:input-output-hk/cardano-wallet.git"
 
-git fetch origin "$branch" || true
+advance_branch() {
+  branch="$1"
+  to="$2"
 
-from=$(git show-ref -s "origin/$branch" || echo "-")
-to=$(git show-ref -s HEAD)
+  from=$(git show-ref -s "origin/$branch" || echo "-")
 
-echo "Advancing $branch from $from to $to"
+  echo "Advancing $branch from $from to $to"
+}
+
+git fetch origin "$this_branch" || true
+git fetch origin "$other_branch" || true
+
+advance_branch "$this_branch" $(git show-ref -s HEAD)
+
+common_ref=$(git merge-base "$this_branch" "$other_branch" || true)
+
+if [ -n "$common_ref" ]; then
+  advance_branch "$common_branch" "$common_ref"
+fi
 
 if [ -e $sshkey ]; then
   echo "Authenticating using SSH with $sshkey"
   export GIT_SSH_COMMAND="ssh -i $sshkey -F /dev/null"
-  git push $remote HEAD:refs/heads/$branch
+  git push $remote HEAD:refs/heads/$this_branch
+  if [ -n "$common_ref" ]; then
+    git push $remote "$common_ref":refs/heads/$common_branch
+  fi
   exit 0
 else
   echo "There is no SSH key at $sshkey"
