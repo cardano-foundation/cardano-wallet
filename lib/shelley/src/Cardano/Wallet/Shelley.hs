8fd81925f5b9372c069b551627942cc70cb349d6
KtorZ <matthias.benkort@gmail.com>
2020-07-21 15:13:23 +0200

    remove fallback when a pool metadata proxy is supplied.
    The fallback made sense in the early days, but we can now make it a proper switch: it's either fetching from pools, or it's fetching from the registry

--- lib/shelley/src/Cardano/Wallet/Shelley.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley.hs
@@ -353,9 +353,9 @@ serveWallet
         tr = contramap (MsgFromWorker mempty) poolsEngineTracer
         trFetch = contramap MsgFetchPoolMetadata tr
         onExit = defaultWorkerAfter poolsEngineTracer
-        fetchStrategies = maybe
-            [identityUrlBuilder]
-            (\proxyUrl -> [registryUrlBuilder proxyUrl, identityUrlBuilder])
+        fetchStrategies = pure $ maybe
+            identityUrlBuilder
+            registryUrlBuilder
             poolMetadataProxy
 
     apiLayer
