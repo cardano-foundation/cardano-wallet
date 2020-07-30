168af751f4c1da6925f7acc608bf13f38f49b7f3
Jonathan Knowles <jonathan.knowles@iohk.io>
2020-07-30 07:22:33 +0000

    Add function `nominalDiffTimeToMicroseconds`.

--- lib/core/src/Data/Time/Utils.hs
+++ lib/core/src/Data/Time/Utils.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NumericUnderscores #-}
+
 -- |
 -- Copyright: © 2018-2020 IOHK
 -- License: Apache-2.0
@@ -5,14 +7,21 @@
 -- Utility functions for manipulating time values.
 
 module Data.Time.Utils
-    ( utcTimePred
+    ( nominalDiffTimeToMicroseconds
+    , utcTimePred
     , utcTimeSucc
     ) where
 
 import Prelude
 
 import Data.Time
-    ( UTCTime, addUTCTime )
+    ( NominalDiffTime, UTCTime, addUTCTime )
+
+-- | Converts the specified time difference into an integral number of
+--   microseconds.
+--
+nominalDiffTimeToMicroseconds :: Integral i => NominalDiffTime -> i
+nominalDiffTimeToMicroseconds = ceiling . (* 1_000_000) . toRational
 
 -- | For a given time 't0', get the closest representable time 't1' to 't0'
 --   for which 't0 < t1'.
