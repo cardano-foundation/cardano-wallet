940b5d16adfe9b436445b7a8b3748b23dd172979
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-07-31 20:03:23 +1000

    Remove byron latency bench

--- .buildkite/bench-latency.sh
+++ .buildkite/bench-latency.sh
@@ -6,17 +6,12 @@ set -euo pipefail
 cd `dirname $0`/..
 
 echo "--- Build"
-nix-build -A benchmarks.cardano-wallet-jormungandr.latency -o bench-latency-jormungandr
-nix-build -A benchmarks.cardano-wallet-byron.latency -o bench-latency-byron
 nix-build -A benchmarks.cardano-wallet-shelley.latency -o bench-latency-shelley
+nix-build -A benchmarks.cardano-wallet-jormungandr.latency -o bench-latency-jormungandr
 
 # Note: the tracing will not work if program output is piped
 # to another process (e.g. "tee").
 # It says "Error: Switchboard's queue full, dropping log items!"
-
-echo "+++ Run benchmark - byron"
-
-( cd lib/byron && ../../bench-latency-byron/bin/latency )
 
 echo "+++ Run benchmark - shelley"
 
