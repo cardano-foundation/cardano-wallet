7e6559af019b9936fa0ced6a45d442b2fa58c47a
Jonathan Knowles <jonathan.knowles@iohk.io>
2020-08-13 08:40:19 +0000

    Add logging to SQLite `removePools` DB operation.

--- lib/core/src/Cardano/Pool/DB/Sqlite.hs
+++ lib/core/src/Cardano/Pool/DB/Sqlite.hs
@@ -87,6 +87,8 @@ import Data.String.QQ
     ( s )
 import Data.Text
     ( Text )
+import Data.Text.Class
+    ( toText )
 import Data.Time.Clock
     ( UTCTime, addUTCTime, getCurrentTime )
 import Data.Word
@@ -382,6 +384,10 @@ newDBLayer trace fp timeInterpreter = do
             -- TODO: remove dangling metadata no longer attached to a pool
 
         , removePools = mapM_ $ \pool -> do
+            liftIO
+                $ traceWith trace
+                $ MsgRemovingDatabaseEntity
+                $ T.unwords [ "pool", toText pool ]
             deleteWhere [ PoolProductionPoolId ==. pool ]
             deleteWhere [ PoolOwnerPoolId ==. pool ]
             deleteWhere [ PoolRegistrationPoolId ==. pool ]
