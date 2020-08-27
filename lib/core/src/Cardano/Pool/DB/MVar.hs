c24b83b3088b597c7cee0acae30fc91a9cc5cc0b
Jonathan Knowles <jonathan.knowles@iohk.io>
2020-08-27 04:30:31 +0000

    Use `RecordWildCards` in `newDBLayer` functions.
    Justification:
    
    Some `DBLayer` functions are defined in terms of other `DBLayer`
    functions, such as `readPoolLifeCycleStatus`, which is defined in terms
    of `readPoolRegistration` and `readPoolRetirement`.
    
    Since mutual recursion between functions defined as fields of a record
    is somewhat awkward, we currently define auxilliary functions for each
    of the functions that we need to reuse, and then refer to these
    auxilliary functions when building the record. However, this creates a
    layer of indirection and duplication.
    
    By using `RecordWildCards`, we can instead build a `DBLayer` object in
    terms of ordinary functions defined in a `where` clause. Since ordinary
    functions can refer to one another using mutual recursion, we no longer
    need auxilliary functons, saving a layer of indirection.

--- lib/core/src/Cardano/Pool/DB/MVar.hs
+++ lib/core/src/Cardano/Pool/DB/MVar.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeFamilies #-}
 
 -- |
@@ -66,79 +67,81 @@ import Data.Tuple
 newDBLayer :: TimeInterpreter Identity -> IO (DBLayer IO)
 newDBLayer timeInterpreter = do
     db <- newMVar emptyPoolDatabase
-    let readPoolRegistration_ =
+    pure $ mkDBLayer db
+  where
+    mkDBLayer db = DBLayer {..}
+      where
+        readPoolRegistration_ =
             readPoolDB db . mReadPoolRegistration
-    let readPoolRetirement_ =
+        readPoolRetirement_ =
             readPoolDB db . mReadPoolRetirement
-    return $ DBLayer
 
-        { putPoolProduction = \sl pool -> ExceptT $ do
+        putPoolProduction sl pool = ExceptT $
             pool `deepseq`
                 alterPoolDB errPointAlreadyExists db (mPutPoolProduction sl pool)
 
-        , readPoolProduction =
+        readPoolProduction =
             readPoolDB db . mReadPoolProduction timeInterpreter
 
-        , readTotalProduction =
+        readTotalProduction =
             readPoolDB db mReadTotalProduction
 
-        , putStakeDistribution = \a0 a1 ->
+        putStakeDistribution a0 a1 =
             void $ alterPoolDB (const Nothing) db (mPutStakeDistribution a0 a1)
 
-        , readStakeDistribution =
+        readStakeDistribution =
             readPoolDB db . mReadStakeDistribution
 
-        , readPoolProductionCursor =
+        readPoolProductionCursor =
             readPoolDB db . mReadCursor
 
-        , putPoolRegistration = \cpt cert -> void
+        putPoolRegistration cpt cert = void
               $ alterPoolDB (const Nothing) db
               $ mPutPoolRegistration cpt cert
 
-        , readPoolLifeCycleStatus = \poolId ->
+        readPoolLifeCycleStatus poolId =
             determinePoolLifeCycleStatus
                 <$> readPoolRegistration_ poolId
                 <*> readPoolRetirement_ poolId
 
-        , readPoolRegistration = readPoolRegistration_
+        readPoolRegistration = readPoolRegistration_
 
-        , putPoolRetirement = \cpt cert -> void
+        putPoolRetirement cpt cert = void
             $ alterPoolDB (const Nothing) db
             $ mPutPoolRetirement cpt cert
 
-        , readPoolRetirement = readPoolRetirement_
+        readPoolRetirement = readPoolRetirement_
 
-        , unfetchedPoolMetadataRefs =
+        unfetchedPoolMetadataRefs =
             readPoolDB db . mUnfetchedPoolMetadataRefs
 
-        , putFetchAttempt =
+        putFetchAttempt =
             void . alterPoolDB (const Nothing) db . mPutFetchAttempt
 
-        , listRegisteredPools =
+        listRegisteredPools =
             modifyMVar db (pure . swap . mListRegisteredPools)
 
-        , listRetiredPools = \epochNo ->
+        listRetiredPools epochNo =
             modifyMVar db (pure . swap . mListRetiredPools epochNo)
 
-        , putPoolMetadata = \a0 a1 ->
+        putPoolMetadata a0 a1 =
             void $ alterPoolDB (const Nothing) db (mPutPoolMetadata a0 a1)
 
-        , readSystemSeed =
+        readSystemSeed =
             modifyMVar db (fmap swap . mReadSystemSeed)
 
-        , rollbackTo =
+        rollbackTo =
             void . alterPoolDB (const Nothing) db . mRollbackTo timeInterpreter
 
-        , removePools =
+        removePools =
             void . alterPoolDB (const Nothing) db . mRemovePools
 
-        , cleanDB =
+        cleanDB =
             void $ alterPoolDB (const Nothing) db mCleanPoolProduction
 
-        , readPoolMetadata = readPoolDB db mReadPoolMetadata
+        readPoolMetadata = readPoolDB db mReadPoolMetadata
 
-        , atomically = id
-        }
+        atomically = id
 
 alterPoolDB
     :: (PoolErr -> Maybe err)
