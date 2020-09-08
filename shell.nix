f2ebd75890a654f5caf4c98da03bd4801468655f
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-09-08 14:36:15 +1000

    nix: Add shell with profiled haskell packages

--- shell.nix
+++ shell.nix
@@ -1,1 +1,13 @@
-(import ./default.nix {}).shell
+# This file is used by nix-shell.
+# It just plucks the "shell" attribute from default.nix.
+# See that file for more info.
+
+let cardanoWallet = import ./default.nix {};
+
+# If you want Haskell dependencies built with profiling, then use:
+# nix-shell --arg profiling true
+in { profiling ? false }:
+
+  if profiling
+    then cardanoWallet.shell-prof
+    else cardanoWallet.shell
