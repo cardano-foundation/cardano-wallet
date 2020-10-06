076f525a0d9705ae02d7da9918601ca79443c042
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-10-06 13:12:23 +1000

    nix/regenerate.sh: add a default value for locale config

--- nix/regenerate.sh
+++ nix/regenerate.sh
@@ -2,4 +2,8 @@
 
 set -euo pipefail
 
+# Provide default value for LOCALE_ARCHIVE environment variable
+: "${LOCALE_ARCHIVE:=/usr/lib/locale/locale-archive}"
+export LOCALE_ARCHIVE
+
 exec $(nix-build `dirname $0`/. -A stackNixRegenerate --no-out-link)
