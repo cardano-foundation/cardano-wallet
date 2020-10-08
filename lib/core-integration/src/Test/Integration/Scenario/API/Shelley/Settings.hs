246417e66b1dd7390c1d7c2c52c2b63ca19dc24d
Piotr Stachyra <piotr.stachyra@iohk.io>
2020-10-08 13:04:15 +0200

    Integration test checking metadata is re-synced after settings update

--- lib/core-integration/src/Test/Integration/Scenario/API/Shelley/Settings.hs
+++ lib/core-integration/src/Test/Integration/Scenario/API/Shelley/Settings.hs
@@ -12,12 +12,18 @@
 
 module Test.Integration.Scenario.API.Shelley.Settings
     ( spec
+    , updateMetadataSource
     ) where
 
 import Prelude
 
 import Cardano.Wallet.Api.Types
-    ( ApiT (..), DecodeAddress, DecodeStakeAddress, EncodeAddress (..) )
+    ( ApiStakePool
+    , ApiT (..)
+    , DecodeAddress
+    , DecodeStakeAddress
+    , EncodeAddress (..)
+    )
 import Cardano.Wallet.Primitive.AddressDerivation
     ( PaymentAddress )
 import Cardano.Wallet.Primitive.AddressDerivation.Byron
@@ -27,11 +33,17 @@ import Cardano.Wallet.Primitive.AddressDerivation.Icarus
 import Cardano.Wallet.Primitive.AddressDerivation.Shelley
     ( ShelleyKey )
 import Cardano.Wallet.Primitive.Types
-    ( PoolMetadataSource, Settings )
+    ( Coin (..), PoolMetadataSource, Settings )
+import Data.Generics.Internal.VL.Lens
+    ( view )
+import Data.Maybe
+    ( isJust, isNothing )
+import Data.Text
+    ( Text )
 import Data.Text.Class
     ( fromText )
 import Test.Hspec
-    ( SpecWith, describe, shouldBe )
+    ( SpecWith, describe, shouldBe, shouldSatisfy )
 import Test.Hspec.Extra
     ( it )
 import Test.Integration.Framework.DSL
@@ -43,6 +55,7 @@ import Test.Integration.Framework.DSL
     , expectResponseCode
     , json
     , request
+    , unsafeRequest
     , verify
     )
 
@@ -60,14 +73,7 @@ spec :: forall n t.
 spec = describe "SHELLEY_SETTINGS" $ do
     it "SETTINGS_01 - Can put and read settings" $ \ctx -> do
         let uri = "http://smash.it"
-            payload = Json [json| {
-                "settings": {
-                    "pool_metadata_source": #{uri}
-                     }
-                } |]
-        r <- request @(ApiT Settings) ctx Link.putSettings Default
-            payload
-        expectResponseCode @IO HTTP.status204 r
+        updateMetadataSource ctx uri
         eventually "The settings are applied" $ do
             r2 <- request @(ApiT Settings) ctx Link.getSettings Default Empty
             verify r2
@@ -76,3 +82,37 @@ spec = describe "SHELLEY_SETTINGS" $ do
                     (`shouldBe` (either (const (error "no")) id $ fromText
                         @PoolMetadataSource uri))
                 ]
+
+    it "SETTINGS_02 - Changing pool_metadata_source re-syncs metadata" $ \ctx -> do
+        let toNone = "none"
+            toDirect = "direct"
+            getMetadata = fmap (view #metadata) . snd <$> unsafeRequest
+                @[ApiStakePool] ctx (Link.listStakePools arbitraryStake) Empty
+
+        updateMetadataSource ctx toNone
+        eventually "1. There is no metadata" $
+            getMetadata >>= (`shouldSatisfy` all isNothing)
+
+        updateMetadataSource ctx toDirect
+        eventually "2. There is metadata" $
+            getMetadata >>= (`shouldSatisfy` all isJust)
+
+        updateMetadataSource ctx toNone
+        eventually "3. There is no metadata" $
+            getMetadata >>= (`shouldSatisfy` all isNothing)
+
+
+updateMetadataSource :: Context t -> Text -> IO ()
+updateMetadataSource ctx t = do
+    r <- request @(ApiT Settings) ctx Link.putSettings Default payload
+    expectResponseCode @IO HTTP.status204 r
+ where
+   payload = Json [json| {
+       "settings": {
+           "pool_metadata_source": #{t}
+            }
+       } |]
+
+arbitraryStake :: Maybe Coin
+arbitraryStake = Just $ ada 10_000
+  where ada = Coin . (1000*1000*)
