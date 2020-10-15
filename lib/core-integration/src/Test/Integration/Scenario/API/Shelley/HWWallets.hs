c32efdbd97928bc9826a6a0956f583be0663112b
Jonathan Knowles <jonathan.knowles@iohk.io>
2020-10-15 07:22:43 +0000

    Update integration test `SHELLEY_HW_WALLETS/HW_WALLETS_04`.
    Update integration test `SHELLEY_HW_WALLETS/HW_WALLETS_04` to make
    multiple payments.
    
    In addition, we expect that the resulting selection will have:
    
    * an output for each of the original payments.
    * at least one change output.

--- lib/core-integration/src/Test/Integration/Scenario/API/Shelley/HWWallets.hs
+++ lib/core-integration/src/Test/Integration/Scenario/API/Shelley/HWWallets.hs
@@ -37,8 +37,6 @@ import Control.Monad
     ( forM_ )
 import Data.Generics.Internal.VL.Lens
     ( view, (^.) )
-import Data.List.NonEmpty
-    ( NonEmpty ((:|)) )
 import Data.Quantity
     ( Quantity (..) )
 import Data.Text
@@ -77,6 +75,8 @@ import Test.Integration.Framework.TestData
     ( errMsg403NoRootKey, payloadWith, updateNamePayload, updatePassPayload )
 
 import qualified Cardano.Wallet.Api.Link as Link
+import qualified Data.List as L
+import qualified Data.List.NonEmpty as NE
 import qualified Network.HTTP.Types.Status as HTTP
 
 
@@ -297,20 +297,26 @@ spec = describe "SHELLEY_HW_WALLETS" $ do
             r <- request
                 @ApiWallet ctx (Link.deleteWallet @'Shelley w) Default Empty
             expectResponseCode @IO HTTP.status204 r
-
             source <- restoreWalletFromPubKey
                 @ApiWallet @'Shelley ctx pubKey restoredWalletName
             target <- emptyWallet ctx
-            targetAddress : _ <- fmap (view #id) <$> listAddresses @n ctx target
-
-            let amount = Quantity minUTxOValue
-            let payment = AddressAmount targetAddress amount
-            let output = ApiCoinSelectionOutput targetAddress amount
-            selectCoins @n @'Shelley ctx source (payment :| []) >>= flip verify
+            let paymentCount = 4
+            targetAddresses <- take paymentCount .
+                fmap (view #id) <$> listAddresses @n ctx target
+            let targetAmounts = take paymentCount $
+                    Quantity <$> [minUTxOValue ..]
+            let payments = NE.fromList $
+                    zipWith AddressAmount targetAddresses targetAmounts
+            let outputs =
+                    zipWith ApiCoinSelectionOutput targetAddresses targetAmounts
+            selectCoins @n @'Shelley ctx source payments >>= flip verify
                 [ expectResponseCode HTTP.status200
-                , expectField #inputs (`shouldSatisfy` (not . null))
-                , expectField #outputs (`shouldSatisfy` ((> 1) . length))
-                , expectField #outputs (`shouldSatisfy` (output `elem`))
+                , expectField #inputs
+                    (`shouldSatisfy` (not . null))
+                , expectField #outputs
+                    (`shouldSatisfy` ((L.sort outputs ==) . L.sort))
+                , expectField #change
+                    (`shouldSatisfy` (not . null))
                 ]
 
     describe "HW_WALLETS_05 - Wallet from pubKey is available" $ do
