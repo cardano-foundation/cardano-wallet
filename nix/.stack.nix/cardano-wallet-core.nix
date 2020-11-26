a92126b689c4ff5a3cb01926c6051b0d3b14efa0
KtorZ <matthias.benkort@gmail.com>
2020-11-26 10:51:04 +0100

    Bump version from 2020.11.17 to 2020.11.26

--- nix/.stack.nix/cardano-wallet-core.nix
+++ nix/.stack.nix/cardano-wallet-core.nix
@@ -11,7 +11,7 @@
     flags = { release = false; };
     package = {
       specVersion = "1.10";
-      identifier = { name = "cardano-wallet-core"; version = "2020.11.17"; };
+      identifier = { name = "cardano-wallet-core"; version = "2020.11.26"; };
       license = "Apache-2.0";
       copyright = "2018-2020 IOHK";
       maintainer = "operations@iohk.io";
--- nix/.stack.nix/cardano-wallet-test-utils.nix
+++ nix/.stack.nix/cardano-wallet-test-utils.nix
@@ -13,7 +13,7 @@
       specVersion = "1.10";
       identifier = {
         name = "cardano-wallet-test-utils";
-        version = "2020.11.17";
+        version = "2020.11.26";
         };
       license = "Apache-2.0";
       copyright = "2018-2020 IOHK";
