996783d710104e0e8daecba00f9ae887c33e55c1
Jonathan Knowles <jonathan.knowles@iohk.io>
2020-12-01 07:52:31 +0000

    Generate token bundles with non-ada assets in wallet property tests.
    Previously, the wallet property tests were restricted to generating token
    bundles with only ada coin quantities.
    
    This change modifies the `TxOut` arbitrary instance to use a full token
    bundle generator.

--- lib/core/test/unit/Cardano/Wallet/Primitive/Types/TokenBundleSpec.hs
+++ lib/core/test/unit/Cardano/Wallet/Primitive/Types/TokenBundleSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TypeApplications #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
@@ -14,6 +15,8 @@ import Cardano.Wallet.Primitive.Types.TokenBundle
     ( TokenBundle (..) )
 import Cardano.Wallet.Primitive.Types.TokenBundle.TokenMapSpec
     ()
+import Control.Monad
+    ( join )
 import Test.Hspec
     ( Spec, describe )
 import Test.Hspec.Core.QuickCheck
@@ -40,8 +43,19 @@ spec =
 -- Arbitrary instances
 --------------------------------------------------------------------------------
 
-genSmallCoin :: Gen Coin
-genSmallCoin = Coin <$> choose (0, 63)
+genStrictlyPositiveCoin :: Gen Coin
+genStrictlyPositiveCoin = Coin <$> choose (1, 64)
+
+shrinkStrictlyPositiveCoin :: Coin -> [Coin]
+shrinkStrictlyPositiveCoin (Coin c) = Coin <$> filter (> 0) (shrink c)
 
 instance Arbitrary TokenBundle where
-    arbitrary = TokenBundle <$> genSmallCoin <*> arbitrary
+    arbitrary = TokenBundle <$> genStrictlyPositiveCoin <*> arbitrary
+    shrink TokenBundle {coin, tokens} = join
+        [ [ TokenBundle coin' tokens
+          | coin' <- shrinkStrictlyPositiveCoin coin
+          ]
+        , [ TokenBundle coin tokens'
+          | tokens' <- shrink tokens
+          ]
+        ]
