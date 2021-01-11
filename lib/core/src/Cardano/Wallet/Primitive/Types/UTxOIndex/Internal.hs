3a567e2659363bff08961acbac16d35efa1fc015
Jonathan Knowles <jonathan.knowles@iohk.io>
2021-01-11 09:56:53 +0000

    Add motivation to the module header comment for type `UTxOIndex`.
    In response to review feedback:
    
    https://github.com/input-output-hk/cardano-wallet/pull/2431#discussion_r553767667
    https://github.com/input-output-hk/cardano-wallet/pull/2431#discussion_r553769984

--- lib/core/src/Cardano/Wallet/Primitive/Types/UTxOIndex/Internal.hs
+++ lib/core/src/Cardano/Wallet/Primitive/Types/UTxOIndex/Internal.hs
@@ -6,8 +6,12 @@
 -- Copyright: © 2018-2021 IOHK
 -- License: Apache-2.0
 --
--- Provides internal functions for the 'UTxOIndex' type, which stores a UTxO
--- set indexed by asset ID.
+-- Provides internal functions for the 'UTxOIndex' type, which indexes a UTxO
+-- set by asset ID.
+--
+-- The index makes it possible to efficiently compute the subset of a UTxO
+-- set containing a particular asset, or to select a UTxO entry containing
+-- that asset, without having to search through the entire UTxO set.
 --
 -- See the documentation for 'UTxOIndex' for more details.
 --
