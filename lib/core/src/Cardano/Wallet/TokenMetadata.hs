04984ea05bdd921fd4b9d3ebf65ba035a663e529
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2021-02-06 19:28:57 +0800

    Add skeleton client for cardano metadata server

--- lib/core/src/Cardano/Wallet/TokenMetadata.hs
+++ lib/core/src/Cardano/Wallet/TokenMetadata.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
+
 module Cardano.Wallet.TokenMetadata
     ( TokenMetadataServer (..)
     , tokenMetadataServerFromFile
@@ -9,6 +11,10 @@ module Cardano.Wallet.TokenMetadata
 
     -- * Convenience
     , fillMetadata
+
+    -- * Client
+    , metadataClient
+    , getTokenMetadata
     ) where
 
 import Prelude
@@ -18,18 +24,25 @@ import Cardano.Wallet.Primitive.Types.TokenMap
 import Cardano.Wallet.Primitive.Types.TokenPolicy
     ( AssetMetadata (..), TokenPolicyId )
 import Control.Monad
-    ( mapM )
+    ( mapM, (<=<) )
 import Data.Aeson
     ( FromJSON (..)
     , Object
+    , ToJSON (..)
     , Value
     , eitherDecodeFileStrict
+    , eitherDecodeStrict'
+    , encode
     , withArray
     , withObject
     , (.:)
     )
 import Data.Aeson.Types
     ( Parser )
+import Data.Bifunctor
+    ( bimap )
+import Data.ByteString
+    ( ByteString )
 import Data.Foldable
     ( toList )
 import Data.Map
@@ -38,10 +51,20 @@ import Data.Set
     ( Set )
 import Data.Text
     ( Text )
+import GHC.Generics
+    ( Generic )
+import Network.HTTP.Client
+import UnliftIO.Exception
+    ( Exception, throwIO, tryAny )
 
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import qualified Data.Text as T
+
+{-------------------------------------------------------------------------------
+   Token Metadata
+-------------------------------------------------------------------------------}
 
 -- | Helper for adding metadata to sets of assets.
 fillMetadata
@@ -93,8 +116,8 @@ parseTokenMetadataMap = fmap Map.fromList . withArray "list of subjects"
     parseTokenMetadataEntry :: Value -> Parser (TokenPolicyId, AssetMetadata)
     parseTokenMetadataEntry = withObject "token metadata" $ \o -> do
         subject <- o .: "subject"
-        name' <- AssetMetadata <$> o `getProperty` "name"
-        return (subject, name')
+        md <- AssetMetadata <$> o `getProperty` "name" <*> o `getProperty` "description"
+        return (subject, md)
       where
         -- | Properties contain both a value an a signature.
         getProperty :: Object -> Text -> Parser Text
@@ -106,3 +129,97 @@ parseTokenMetadataMap = fmap Map.fromList . withArray "list of subjects"
                 --    <- propObj .: "signatures" -- maybe "anSignatures" in server?
                 return val
                 )
+
+{-------------------------------------------------------------------------------
+                            Cardano Metadata Server
+-------------------------------------------------------------------------------}
+
+-- | Models a request to the @POST /metadata/query@ endpoint of the metadata
+-- server -- the only one that we need.
+data BatchRequest = BatchRequest
+    { subjects :: [Subject]
+    , properties :: [PropertyName]
+    } deriving (Generic, Show, Eq)
+
+type BatchResponse = [Properties]
+
+data Properties = Properties
+    { subject :: Subject
+    , owner :: Signature
+    , properties :: [(PropertyName, (PropertyValue, [Signature]))]
+    } deriving (Generic, Show, Eq)
+
+newtype Subject = Subject { unSubject :: Text }
+    deriving (Generic, Show, Eq)
+
+newtype PropertyName = PropertyName { unPropertyName :: Text }
+    deriving (Generic, Show, Eq)
+
+-- Properties can actually have any json value.
+-- But for now, limit to text.
+newtype PropertyValue = PropertyValue { unPropertyValue :: Text }
+    deriving (Generic, Show, Eq)
+
+newtype Hex = Hex ByteString
+ deriving (Generic, Show, Eq)
+data Signature = Signature
+    { signature :: Hex
+    , publicKey :: Hex
+    } deriving (Generic, Show, Eq)
+
+-- todo: use generic
+instance ToJSON BatchRequest where
+instance ToJSON PropertyName where
+instance FromJSON PropertyName where
+instance ToJSON Subject where
+instance FromJSON Subject where
+instance FromJSON Properties where
+instance FromJSON Signature where
+instance FromJSON Hex where
+    parseJSON = error "hex decode bytestring"
+instance FromJSON PropertyValue where
+
+newtype JSONParseError = JSONParseError String
+    deriving (Show, Eq)
+instance Exception JSONParseError
+
+metadataClient :: String -> Manager -> BatchRequest -> IO BatchResponse
+metadataClient baseURL manager =
+    parseResponse <=< flip httpLbs manager <=< makeHttpReq
+  where
+    makeHttpReq query = do
+        req <- parseRequest (baseURL ++ "metadata/query")
+        pure req {
+            method = "POST",
+            requestBody = RequestBodyLBS $ encode query
+            }
+    parseResponse = either (throwIO . JSONParseError) pure
+        . eitherDecodeStrict'
+        . BL.toStrict
+        . responseBody
+
+{-------------------------------------------------------------------------------
+                           Requesting token metadata
+-------------------------------------------------------------------------------}
+
+data TokenMetadataError = TokenMetadataError String -- todo: constructors
+
+getTokenMetadata
+    :: (BatchRequest -> IO BatchResponse)
+    -> [AssetId]
+    -> IO (Either TokenMetadataError [(AssetId, AssetMetadata)])
+getTokenMetadata client =
+    fmap (bimap (TokenMetadataError . show) fromResponse) . tryAny . client . toRequest
+  where
+    toRequest :: [AssetId] -> BatchRequest
+    toRequest as = BatchRequest (map toSubject as)
+        [PropertyName "name", PropertyName "description"]
+    toSubject = error "todo: hex-encode policyId + assetName"
+    fromResponse :: BatchResponse -> [(AssetId, AssetMetadata)]
+    fromResponse = error "todo"
+
+testIt :: IO ()
+testIt = do
+    client <- metadataClient "http://localhost:8000/api/" <$> newManager defaultManagerSettings
+    Right r <- getTokenMetadata client []
+    pure ()
