942eb74b207bcca8c66aad19236161425182382b
Pawel Jakubas <pawel.jakubas@iohk.io>
2021-02-08 16:41:08 +0100

    add mkSharedState
    improve mkSharedState and SharedState

--- lib/core/src/Cardano/Wallet/Primitive/AddressDiscovery/Shared.hs
+++ lib/core/src/Cardano/Wallet/Primitive/AddressDiscovery/Shared.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -27,6 +28,8 @@ module Cardano.Wallet.Primitive.AddressDiscovery.Shared
     (
     -- ** State
       SharedState (..)
+    , KeyNumberScope (..)
+    , mkSharedState
     ) where
 
 import Prelude
@@ -36,11 +39,17 @@ import Cardano.Address.Script
 import Cardano.Crypto.Wallet
     ( XPub )
 import Cardano.Wallet.Primitive.AddressDerivation
-    ( Depth (..), DerivationPrefix (..), NetworkDiscriminant (..) )
+    ( NetworkDiscriminant (..) )
+import Cardano.Wallet.Primitive.AddressDerivation
+    ( Depth (..), DerivationType (..), Index (..) )
+import Cardano.Wallet.Primitive.AddressDiscovery.Sequential
+    ( DerivationPrefix (..), coinTypeAda, purposeBIP44 )
 import Cardano.Wallet.Primitive.Types.Address
     ( Address (..) )
 import Control.DeepSeq
     ( NFData )
+import Data.Maybe
+    ( isNothing )
 import Data.Word
     ( Word8 )
 import GHC.Generics
@@ -57,12 +66,12 @@ data SharedState (n :: NetworkDiscriminant) k = SeqState
         -- ^ Reward account public key associated with this wallet
     , derivationPrefix :: DerivationPrefix
         -- ^ Derivation path prefix from a root key up to the account key
-    , derivationKeyNumber :: Word8
+    , derivationKeyNumber :: KeyNumberScope
         -- ^ Number of first keys that are used to produce candidate addresses
-    , paymentScriptTemplate :: !ScriptTemplate
+    , paymentScriptTemplate :: !(Maybe ScriptTemplate)
         -- ^ Script template together with a map of account keys and cosigners
         -- for payment credential
-    , stakingScriptTemplate :: !ScriptTemplate
+    , stakingScriptTemplate :: !(Maybe ScriptTemplate)
         -- ^ Script template together with a map of account keys and cosigners
         -- for staking credential
     , addressCandidates :: [Address]
@@ -81,3 +90,28 @@ instance
     ( NFData (k 'AccountK XPub)
     )
     => NFData (SharedState n k)
+
+newtype KeyNumberScope =
+    KeyNumberScope { unKeyNumberScope :: Word8 }
+    deriving (Eq, Generic, Show)
+    deriving anyclass NFData
+
+-- | Construct a SharedState for a wallet from public account key, script templates
+-- for both staking and spending, and the number of keys to generate key candidates.
+mkSharedState
+    :: forall (n :: NetworkDiscriminant) k. k 'AccountK XPub
+    -> Index 'Hardened 'AccountK
+    -> Maybe ScriptTemplate
+    -> Maybe ScriptTemplate
+    -> KeyNumberScope
+    -> Maybe (SharedState n k)
+mkSharedState accXPub accIx spendingTemplate stakingTemplate keyIx =
+    let
+        prefix =
+            DerivationPrefix ( purposeBIP44, coinTypeAda, accIx )
+        addressDerived = []
+    in
+        if all isNothing [spendingTemplate, stakingTemplate] then
+            Nothing
+        else
+            Just $ SeqState accXPub prefix keyIx spendingTemplate stakingTemplate addressDerived
