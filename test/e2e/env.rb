9dee63af83104b26575ea7051d09167ef7c09d37
Piotr Stachyra <piotr.stachyra@iohk.io>
2021-03-09 11:08:33 +0100

    move gpg file to fixtures subdir

--- test/e2e/env.rb
+++ test/e2e/env.rb
@@ -1,5 +1,5 @@
-ENV['TESTS_E2E_FIXTURES_FILE'] ||= "./fixture_wallets.json"
 ENV['TESTS_E2E_FIXTURES'] ||= "this_is_wrong_secret"
+ENV['TESTS_E2E_FIXTURES_FILE'] ||= "./fixtures/fixture_wallets.json"
 ENV['TESTS_E2E_NODE_DB'] ||= "./node_db"
 ENV['TESTS_E2E_WALLET_DB'] ||= "./wallet_db"
 ENV['TESTS_E2E_LOGS'] ||= "./logs"
