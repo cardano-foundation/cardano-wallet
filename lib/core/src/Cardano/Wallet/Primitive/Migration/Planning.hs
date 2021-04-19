c9ecdab84354b9611af4464dbfed536ea4c93ef7
Jonathan Knowles <jonathan.knowles@iohk.io>
2021-04-19 08:57:26 +0000

    Add module `Migration.Planning`.
    This module contains an algorithm for planning migrations at a high
    level. It determines how to partition the UTxO set into entries of
    different types, and in which order to add UTxO entries to selections.

---
+++ lib/core/src/Cardano/Wallet/Primitive/Migration/Planning.hs
@@ -0,0 +1,271 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- |
+-- Copyright: © 2021 IOHK
+-- License: Apache-2.0
+--
+-- This module contains an algorithm for planning migrations at a high level.
+--
+-- It determines how to partition the UTxO set into entries of different types,
+-- and in which order to add entries to selections, in order to maximize the
+-- number of entries that can be successfully migrated.
+--
+-- Use 'createPlan' to create a migration plan.
+--
+module Cardano.Wallet.Primitive.Migration.Planning
+    (
+    -- * Migration planning
+      createPlan
+    , MigrationPlan (..)
+
+    -- * UTxO entry categorization
+    , CategorizedUTxO (..)
+    , UTxOEntryCategory (..)
+    , categorizeUTxO
+    , categorizeUTxOEntries
+    , categorizeUTxOEntry
+    , uncategorizeUTxO
+    , uncategorizeUTxOEntries
+
+    ) where
+
+import Prelude
+
+import Cardano.Wallet.Primitive.Migration.Selection
+    ( RewardWithdrawal (..), Selection (..), SelectionError (..), TxSize (..) )
+import Cardano.Wallet.Primitive.Types.Coin
+    ( Coin (..) )
+import Cardano.Wallet.Primitive.Types.TokenBundle
+    ( TokenBundle (..) )
+import Cardano.Wallet.Primitive.Types.Tx
+    ( TxConstraints (..), TxIn, TxOut )
+import Cardano.Wallet.Primitive.Types.UTxO
+    ( UTxO (..) )
+import Data.Either
+    ( isRight )
+import Data.Functor
+    ( (<&>) )
+import Data.Generics.Internal.VL.Lens
+    ( view )
+import Data.Generics.Labels
+    ()
+import GHC.Generics
+    ( Generic )
+
+import qualified Cardano.Wallet.Primitive.Migration.Selection as Selection
+import qualified Cardano.Wallet.Primitive.Types.TokenBundle as TokenBundle
+import qualified Data.Foldable as F
+import qualified Data.List as L
+import qualified Data.Map.Strict as Map
+
+--------------------------------------------------------------------------------
+-- Migration planning
+--------------------------------------------------------------------------------
+
+data MigrationPlan i s = MigrationPlan
+    { selections :: ![Selection i s]
+    , unselected :: !(CategorizedUTxO i)
+    , totalFee :: !Coin
+    }
+    deriving (Eq, Generic, Show)
+
+createPlan
+    :: TxSize s
+    => TxConstraints s
+    -> CategorizedUTxO i
+    -> RewardWithdrawal
+    -> MigrationPlan i s
+createPlan constraints =
+    run []
+  where
+    run !selections !utxo !reward =
+        case createSelection constraints utxo reward of
+            Just (utxo', selection) ->
+                run (selection : selections) utxo' (RewardWithdrawal $ Coin 0)
+            Nothing -> MigrationPlan
+                { selections
+                , unselected = utxo
+                , totalFee = F.foldMap (view #fee) selections
+                }
+
+createSelection
+    :: TxSize s
+    => TxConstraints s
+    -> CategorizedUTxO i
+    -> RewardWithdrawal
+    -> Maybe (CategorizedUTxO i, Selection i s)
+createSelection constraints utxo rewardWithdrawal =
+    initializeSelection constraints utxo rewardWithdrawal
+    <&> extendSelection constraints
+
+initializeSelection
+    :: forall i s. TxSize s
+    => TxConstraints s
+    -> CategorizedUTxO i
+    -> RewardWithdrawal
+    -> Maybe (CategorizedUTxO i, Selection i s)
+initializeSelection constraints utxoAtStart reward =
+    initializeWith =<< utxoAtStart `select` Supporter
+  where
+    initializeWith (entry, utxo) =
+        case Selection.create constraints reward [entry] of
+            Right selection -> Just (utxo, selection)
+            Left _ -> Nothing
+
+extendSelection
+    :: TxSize s
+    => TxConstraints s
+    -> (CategorizedUTxO i, Selection i s)
+    -> (CategorizedUTxO i, Selection i s)
+extendSelection constraints = extendWithFreerider
+  where
+    extendWithFreerider (!utxo, !selection) =
+        case extendWith Freerider constraints (utxo, selection) of
+            Right (utxo', selection') ->
+                extendWithFreerider (utxo', selection')
+            Left ExtendSelectionAdaInsufficient ->
+                extendWithSupporter (utxo, selection)
+            Left ExtendSelectionEntriesExhausted ->
+                extendWithSupporter (utxo, selection)
+            Left ExtendSelectionFull ->
+                (utxo, selection)
+
+    extendWithSupporter (!utxo, !selection) =
+        case extendWith Supporter constraints (utxo, selection) of
+            Right (utxo', selection') ->
+                extendWithFreerider (utxo', selection')
+            Left ExtendSelectionAdaInsufficient ->
+                (utxo, selection)
+            Left ExtendSelectionEntriesExhausted ->
+                (utxo, selection)
+            Left ExtendSelectionFull ->
+                (utxo, selection)
+
+data ExtendSelectionError
+    = ExtendSelectionAdaInsufficient
+    | ExtendSelectionEntriesExhausted
+    | ExtendSelectionFull
+
+extendWith
+    :: TxSize s
+    => UTxOEntryCategory
+    -> TxConstraints s
+    -> (CategorizedUTxO i, Selection i s)
+    -> Either ExtendSelectionError (CategorizedUTxO i, Selection i s)
+extendWith category constraints (utxo, selection) =
+    case utxo `select` category of
+        Just (entry, utxo') ->
+            case Selection.extend constraints selection entry of
+                Right selection' ->
+                    Right (utxo', selection')
+                Left SelectionAdaInsufficient ->
+                    Left ExtendSelectionAdaInsufficient
+                Left SelectionFull {} ->
+                    Left ExtendSelectionFull
+        Nothing ->
+            Left ExtendSelectionEntriesExhausted
+
+select
+    :: CategorizedUTxO i
+    -> UTxOEntryCategory
+    -> Maybe ((i, TokenBundle), CategorizedUTxO i)
+select utxo = \case
+    Supporter -> selectSupporter
+    Freerider -> selectFreerider
+    Ignorable -> selectIgnorable
+  where
+    selectSupporter = case supporters utxo of
+        entry : remaining -> Just (entry, utxo {supporters = remaining})
+        [] -> Nothing
+    selectFreerider = case freeriders utxo of
+        entry : remaining -> Just (entry, utxo {freeriders = remaining})
+        [] ->  Nothing
+    selectIgnorable =
+        -- We never select an entry that should be ignored:
+        Nothing
+
+--------------------------------------------------------------------------------
+-- Categorization of UTxO entries
+--------------------------------------------------------------------------------
+
+data UTxOEntryCategory
+    = Supporter
+    -- ^ A coin or bundle that is capable of paying for its own marginal fee
+    -- and the base transaction fee.
+    | Freerider
+    -- ^ A coin or bundle that is not capable of paying for itself.
+    | Ignorable
+    -- ^ A coin that should not be added to a selection, because its value is
+    -- lower than the marginal fee for an input.
+    deriving (Eq, Show)
+
+data CategorizedUTxO i = CategorizedUTxO
+    { supporters :: ![(i, TokenBundle)]
+    , freeriders :: ![(i, TokenBundle)]
+    , ignorables :: ![(i, TokenBundle)]
+    }
+    deriving (Eq, Show)
+
+categorizeUTxO
+    :: TxSize s
+    => TxConstraints s
+    -> UTxO
+    -> CategorizedUTxO (TxIn, TxOut)
+categorizeUTxO constraints (UTxO u) = categorizeUTxOEntries constraints $
+    (\(i, o) -> ((i, o), view #tokens o)) <$> Map.toList u
+
+categorizeUTxOEntries
+    :: forall i s. TxSize s
+    => TxConstraints s
+    -> [(i, TokenBundle)]
+    -> CategorizedUTxO i
+categorizeUTxOEntries constraints uncategorizedEntries = CategorizedUTxO
+    { supporters = entriesMatching Supporter
+    , freeriders = entriesMatching Freerider
+    , ignorables = entriesMatching Ignorable
+    }
+  where
+    categorizedEntries :: [(i, (TokenBundle, UTxOEntryCategory))]
+    categorizedEntries = uncategorizedEntries
+        <&> (\(i, b) -> (i, (b, categorizeUTxOEntry constraints b)))
+
+    entriesMatching :: UTxOEntryCategory -> [(i, TokenBundle)]
+    entriesMatching category =
+        fmap fst <$> L.filter ((== category) . snd . snd) categorizedEntries
+
+categorizeUTxOEntry
+    :: TxSize s
+    => TxConstraints s
+    -> TokenBundle
+    -> UTxOEntryCategory
+categorizeUTxOEntry constraints b
+    | Just c <- TokenBundle.toCoin b, coinIsIgnorable c =
+        Ignorable
+    | bundleIsSupporter =
+        Supporter
+    | otherwise =
+        Freerider
+  where
+    bundleIsSupporter :: Bool
+    bundleIsSupporter = isRight $
+        Selection.create constraints (RewardWithdrawal $ Coin 0) [((), b)]
+
+    coinIsIgnorable :: Coin -> Bool
+    coinIsIgnorable c = c <= txInputCost constraints
+
+uncategorizeUTxO :: CategorizedUTxO (TxIn, TxOut) -> UTxO
+uncategorizeUTxO = UTxO . Map.fromList . fmap fst . uncategorizeUTxOEntries
+
+uncategorizeUTxOEntries :: CategorizedUTxO i -> [(i, TokenBundle)]
+uncategorizeUTxOEntries utxo = mconcat
+    [ supporters utxo
+    , freeriders utxo
+    , ignorables utxo
+    ]
---
+++ lib/core/test/unit/Cardano/Wallet/Primitive/Migration/PlanningSpec.hs
@@ -0,0 +1,367 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Cardano.Wallet.Primitive.Migration.PlanningSpec
+    where
+
+import Prelude
+
+import Cardano.Wallet.Primitive.Migration.Planning
+    ( CategorizedUTxO (..)
+    , MigrationPlan (..)
+    , UTxOEntryCategory (..)
+    , categorizeUTxOEntries
+    , categorizeUTxOEntry
+    , createPlan
+    , uncategorizeUTxOEntries
+    )
+import Cardano.Wallet.Primitive.Migration.Selection
+    ( RewardWithdrawal (..), Selection (..) )
+import Cardano.Wallet.Primitive.Migration.SelectionSpec
+    ( MockInputId
+    , MockTxConstraints (..)
+    , conjoinMap
+    , counterexampleMap
+    , genCoinRange
+    , genMockInput
+    , genMockInputAdaOnly
+    , genMockTxConstraints
+    , genTokenBundleMixed
+    , unMockTxConstraints
+    )
+import Cardano.Wallet.Primitive.Types.Coin
+    ( Coin (..) )
+import Cardano.Wallet.Primitive.Types.TokenBundle
+    ( TokenBundle (..) )
+import Control.Monad
+    ( replicateM )
+import Data.Either
+    ( isLeft, isRight )
+import Data.Generics.Internal.VL.Lens
+    ( view )
+import Data.Generics.Labels
+    ()
+import Data.Set
+    ( Set )
+import Fmt
+    ( padLeftF, pretty )
+import Test.Hspec
+    ( Spec, describe, it )
+import Test.Hspec.Core.QuickCheck
+    ( modifyMaxSuccess )
+import Test.Hspec.Extra
+    ( parallel )
+import Test.QuickCheck
+    ( Arbitrary (..)
+    , Blind (..)
+    , Gen
+    , Property
+    , checkCoverage
+    , choose
+    , counterexample
+    , cover
+    , label
+    , oneof
+    , property
+    , withMaxSuccess
+    )
+
+import qualified Cardano.Wallet.Primitive.Migration.Selection as Selection
+import qualified Data.Foldable as F
+import qualified Data.List as L
+import qualified Data.List.NonEmpty as NE
+import qualified Data.Set as Set
+
+spec :: Spec
+spec = describe "Cardano.Wallet.Primitive.Migration.PlanningSpec" $
+
+    modifyMaxSuccess (const 1000) $ do
+
+    parallel $ describe "Creating migration plans" $ do
+
+        describe "Small migrations" $
+            it "prop_createPlan_small" $
+                property prop_createPlan_small
+
+        describe "Large migrations" $
+            it "prop_createPlan_large" $
+                property prop_createPlan_large
+
+        describe "Giant migrations" $
+            it "prop_createPlan_giant" $
+                property prop_createPlan_giant
+
+    parallel $ describe "Categorizing UTxO entries" $ do
+
+        it "prop_categorizeUTxOEntry" $
+            property prop_categorizeUTxOEntry
+
+--------------------------------------------------------------------------------
+-- Creating migration plans
+--------------------------------------------------------------------------------
+
+data ArgsForCreatePlan = ArgsForCreatePlan
+    { mockConstraints :: MockTxConstraints
+    , mockInputs :: [(MockInputId, TokenBundle)]
+    , mockRewardWithdrawal :: Coin
+    }
+    deriving (Eq, Show)
+
+newtype Small a = Small { unSmall :: a }
+    deriving (Eq, Show)
+
+newtype Large a = Large { unLarge :: a }
+    deriving (Eq, Show)
+
+newtype Giant a = Giant { unGiant :: a }
+    deriving (Eq, Show)
+
+instance Arbitrary (Small ArgsForCreatePlan) where
+    arbitrary = Small <$> genArgsForCreatePlan
+        (0, 100) genMockInput
+
+instance Arbitrary (Large ArgsForCreatePlan) where
+    arbitrary = Large <$> genArgsForCreatePlan
+        (1_000, 1_000) genMockInput
+
+instance Arbitrary (Giant ArgsForCreatePlan) where
+    arbitrary = Giant <$> genArgsForCreatePlan
+        (100_000, 100_000) genMockInputAdaOnly
+
+prop_createPlan_small :: Blind (Small ArgsForCreatePlan) -> Property
+prop_createPlan_small (Blind (Small args)) =
+    withMaxSuccess 1000 $
+    prop_createPlan args
+
+prop_createPlan_large :: Blind (Large ArgsForCreatePlan) -> Property
+prop_createPlan_large (Blind (Large args)) =
+    withMaxSuccess 100 $
+    prop_createPlan args
+
+prop_createPlan_giant :: Blind (Giant ArgsForCreatePlan) -> Property
+prop_createPlan_giant (Blind (Giant args)) =
+    withMaxSuccess 1 $
+    prop_createPlan args
+
+genArgsForCreatePlan
+    :: (Int, Int)
+    -- ^ Input count range
+    -> (MockTxConstraints -> Gen (MockInputId, TokenBundle))
+    -- ^ Genenator for inputs
+    -> Gen ArgsForCreatePlan
+genArgsForCreatePlan (inputCountMin, inputCountMax) genInput = do
+    mockConstraints <- genMockTxConstraints
+    mockInputCount <- choose (inputCountMin, inputCountMax)
+    mockInputs <- replicateM mockInputCount (genInput mockConstraints)
+    mockRewardWithdrawal <- oneof
+        [ pure (Coin 0)
+        , genCoinRange (Coin 1) (Coin 1_000_000)
+        ]
+    pure ArgsForCreatePlan
+        { mockConstraints
+        , mockInputs
+        , mockRewardWithdrawal
+        }
+
+prop_createPlan :: ArgsForCreatePlan -> Property
+prop_createPlan mockArgs =
+    label labelTransactionCount $
+    label labelMeanTransactionInputCount $
+    label labelMeanTransactionOutputCount $
+    label (labelNotSelectedPercentage "freeriders" freeriders) $
+    label (labelNotSelectedPercentage "supporters" supporters) $
+    label (labelNotSelectedPercentage "ignorables" ignorables) $
+
+    counterexample counterexampleText $
+    conjoinMap
+        [ ( "inputs are not preserved"
+          , inputIdsAll == Set.union inputIdsSelected inputIdsNotSelected )
+        , ( "total fee is incorrect"
+          , totalFee result == totalFeeExpected )
+        , ( "more than one transaction has reward withdrawal"
+          , rewardWithdrawalCount <= 1 )
+        , ( "reward withdrawal amount incorrect"
+          , rewardWithdrawalAmount == rewardWithdrawalExpected )
+        , ( "one or more supporters not selected"
+          , null (supporters (unselected result)) )
+        ]
+  where
+    labelTransactionCount = pretty $ mconcat
+        [ "number of transactions required: ["
+        , padLeftF 3 '0' (10 * selectionCountDiv10)
+        , " – "
+        , padLeftF 3 '0' (10 * (selectionCountDiv10 + 1) - 1)
+        , "]"
+        ]
+      where
+        selectionCountDiv10 = selectionCount `div` 10
+
+    labelMeanTransactionInputCount = pretty $ mconcat
+        [ "mean number of inputs per transaction: ["
+        , padLeftF 3 '0' (10 * meanTxInputCountDiv10)
+        , " – "
+        , padLeftF 3 '0' (10 * (meanTxInputCountDiv10 + 1) - 1)
+        , "]"
+        ]
+      where
+        meanTxInputCountDiv10 = meanTxInputCount `div` 10
+        meanTxInputCount :: Int
+        meanTxInputCount
+            | selectionCount == 0 =
+                0
+            | otherwise =
+                totalSelectedInputCount `div` selectionCount
+        totalSelectedInputCount :: Int
+        totalSelectedInputCount =
+            L.sum $ L.length . view #inputIds <$> selections result
+
+    labelMeanTransactionOutputCount = pretty $ mconcat
+        [ "mean number of outputs per transaction: "
+        , padLeftF 3 '0' meanTxOutputCount
+        ]
+      where
+        meanTxOutputCount :: Int
+        meanTxOutputCount
+            | selectionCount == 0 =
+                0
+            | otherwise =
+                totalSelectedOutputCount `div` selectionCount
+        totalSelectedOutputCount :: Int
+        totalSelectedOutputCount =
+            L.sum $ L.length . view #outputs <$> selections result
+
+    labelNotSelectedPercentage categoryName category = pretty $ mconcat
+        [ categoryName
+        , " not selected: "
+        , maybe
+            ("no entries available")
+            (\p -> padLeftF 3 '0' p <> "%")
+            (percentage)
+        ]
+      where
+        percentage
+            | entriesAvailable == 0 =
+                Nothing
+            | otherwise =
+                Just $ (entriesNotSelected * 100) `div` entriesAvailable
+
+        entriesAvailable :: Int
+        entriesAvailable = length $ category categorizedUTxO
+        entriesNotSelected :: Int
+        entriesNotSelected = length $ category $ unselected result
+
+    ArgsForCreatePlan
+        { mockConstraints
+        , mockInputs
+        , mockRewardWithdrawal
+        } = mockArgs
+
+    constraints = unMockTxConstraints mockConstraints
+    result = createPlan constraints categorizedUTxO
+        (RewardWithdrawal mockRewardWithdrawal)
+
+    categorizedUTxO = categorizeUTxOEntries constraints mockInputs
+
+    inputIdsAll :: Set MockInputId
+    inputIdsAll = Set.fromList (fst <$> mockInputs)
+
+    inputIdsSelected :: Set MockInputId
+    inputIdsSelected = Set.fromList
+        [ i
+        | s <- selections result
+        , i <- NE.toList (view #inputIds s)
+        ]
+
+    inputIdsNotSelected :: Set MockInputId
+    inputIdsNotSelected = Set.fromList
+        $ fmap fst
+        $ uncategorizeUTxOEntries
+        $ unselected result
+
+    rewardWithdrawalCount =
+        length $ filter (> Coin 0) (rewardWithdrawal <$> selections result)
+    rewardWithdrawalAmount =
+        F.foldMap rewardWithdrawal (selections result)
+    rewardWithdrawalExpected
+        | selectionCount == 0 =
+            Coin 0
+        | otherwise =
+            mockRewardWithdrawal
+
+    selectionCount = length (selections result)
+
+    totalFeeExpected :: Coin
+    totalFeeExpected = F.foldMap fee (selections result)
+
+    counterexampleText = counterexampleMap
+        [ ( "mockConstraints"
+          , show mockConstraints )
+        , ( "count of supporters available"
+          , show (length $ supporters categorizedUTxO) )
+        , ( "count of supporters not selected"
+          , show (length $ supporters $ unselected result) )
+        , ( "count of freeriders available"
+          , show (length $ freeriders categorizedUTxO) )
+        , ( "count of freeriders not selected"
+          , show (length $ freeriders $ unselected result) )
+        , ( "count of ignorables available"
+          , show (length $ ignorables categorizedUTxO) )
+        , ( "count of ignorables not selected"
+          , show (length $ ignorables $ unselected result) )
+        , ( "count of reward withdrawals"
+          , show rewardWithdrawalCount )
+        ]
+
+--------------------------------------------------------------------------------
+-- Categorizing UTxO entries
+--------------------------------------------------------------------------------
+
+data ArgsForCategorizeUTxOEntry = ArgsForCategorizeUTxOEntry
+    { mockConstraints :: MockTxConstraints
+    , mockEntry :: TokenBundle
+    }
+    deriving (Eq, Show)
+
+instance Arbitrary ArgsForCategorizeUTxOEntry where
+    arbitrary = genArgsForCategorizeUTxOEntry
+
+genArgsForCategorizeUTxOEntry :: Gen ArgsForCategorizeUTxOEntry
+genArgsForCategorizeUTxOEntry = do
+    mockConstraints <- genMockTxConstraints
+    mockEntry <- genTokenBundleMixed mockConstraints
+    pure ArgsForCategorizeUTxOEntry {..}
+
+prop_categorizeUTxOEntry :: ArgsForCategorizeUTxOEntry -> Property
+prop_categorizeUTxOEntry mockArgs =
+    checkCoverage $
+    cover 5 (result == Supporter) "Supporter" $
+    cover 5 (result == Freerider) "Freerider" $
+    cover 5 (result == Ignorable) "Ignorable" $
+    property
+        $ selectionCreateExpectation
+        $ Selection.create constraints
+            (RewardWithdrawal $ Coin 0) [((), mockEntry)]
+  where
+    ArgsForCategorizeUTxOEntry
+        { mockConstraints
+        , mockEntry
+        } = mockArgs
+    constraints = unMockTxConstraints mockConstraints
+    result = categorizeUTxOEntry constraints mockEntry
+    selectionCreateExpectation = case result of
+        Supporter -> isRight
+        Freerider -> isLeft
+        Ignorable -> isLeft
+
+--------------------------------------------------------------------------------
+-- Miscellaneous types and functions
+--------------------------------------------------------------------------------
+
+coinToInteger :: Coin -> Integer
+coinToInteger = fromIntegral . unCoin
