d7b8d5da9f409299c12a68a2567e6eb53e29dffe
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2021-05-04 19:42:22 +1000

    release.nix: Improve evaluation time

--- release.nix
+++ release.nix
@@ -5,20 +5,16 @@
 # The purpose of this file is to select jobs defined in default.nix and map
 # them to all supported build platforms.
 #
-# The layout is TARGET-SYSTEM.ATTR-PATH.BUILD-SYSTEM where
+# The layout is PLATFORM.ATTR-PATH where
 #
-#   * TARGET-SYSTEM is one of
-#     - native - build the job for BUILD-SYSTEM
-#     - x86_64-w64-mingw32 - build the job for windows
+#   * PLATFORM is one of
+#     - linux.native - normal glibc build
+#     - linux.musl - fully static build with musl libc
+#     - windows - cross-compiled windows
 #     - musl64 - build the job for Linux, but statically linked with musl libc
 #
-#   * ATTR-PATH is an attribute from default.nix
-#
-#   * BUILD-SYSTEM is the system where the derivation is built. Hydra
-#     uses this to distribute builds to its build slaves. If building
-#     locally then it must reflect your local system. The value is one of:
-#     - x86_64-linux - Linux
-#     - x86_64-darwin - macOS
+#   * ATTR-PATH is usually an attribute from default.nix. Some
+#     release-only attributes are added and others are filtered out.
 #
 # Discover jobs by using tab completion in your shell:
 #   nix-build release.nix -A <TAB>
@@ -67,66 +63,140 @@
 
 # Platform filter string for jobset.
 , platform ? "all"
+
+# Enable debug tracing
+, debug ? false
 }:
+
+with pkgs.lib;
 
 assert pkgs.lib.asserts.assertOneOf "platform" platform
   ["all" "linux" "macos" "windows"];
 
 let
-  buildNative  = builtins.elem builtins.currentSystem supportedSystems;
-  buildLinux   = builtins.elem "x86_64-linux" supportedSystems && buildForPlatform "linux";
-  buildMacOS   = builtins.elem "x86_64-darwin" supportedSystems && buildForPlatform "macos";
-  buildMusl    = builtins.elem "x86_64-linux" supportedCrossSystems && buildLinux;
-  buildWindows = builtins.elem builtins.currentSystem supportedCrossSystems && buildForPlatform "windows";
-  buildForPlatform = name: builtins.elem platform ["all" name];
-in
+  buildNative  = elem builtins.currentSystem supportedSystems;
+  buildLinux   = elem names.linux supportedSystems && buildForPlatform "linux";
+  buildMacOS   = elem names.macos supportedSystems && buildForPlatform "macos";
+  buildMusl    = elem names.linux supportedCrossSystems && buildLinux;
+  buildWindows = elem builtins.currentSystem supportedCrossSystems && buildForPlatform "windows";
+  buildForPlatform = name: elem platform ["all" name];
 
-with (import pkgs.iohkNix.release-lib) {
-  inherit pkgs;
-  inherit supportedCrossSystems scrubJobs projectArgs;
-  supportedSystems =
-    pkgs.lib.optional buildLinux "x86_64-linux" ++
-    pkgs.lib.optional buildMacOS "x86_64-darwin";
-  packageSet = import cardano-wallet;
-  gitrev = cardano-wallet.rev;
-};
-
-with pkgs.lib;
-
-let
-  ############################################################################
-  # Mapping the default.nix derivations to build platforms
-
-  inherit (systems.examples) mingwW64 musl64;
-
-  mappedJobs = optionalAttrs buildNative {
-    native = mapTestOn (filterMappedNative (packagePlatformsOrig (filterJobsNative project)));
-  } // optionalAttrs buildWindows {
-    "${mingwW64.config}" = mapTestOnCross mingwW64
-      (packagePlatformsCross (filterJobsWindows project));
-  } // optionalAttrs buildMusl {
-    musl64 = mapTestOnCross musl64
-      (packagePlatformsCross (filterJobsCross project));
+  makeReleaseLib = system: (import pkgs.iohkNix.release-lib) {
+    inherit pkgs scrubJobs projectArgs;
+    supportedSystems = [ system ];
+    supportedCrossSystems = filter (s: s == system) supportedCrossSystems;
+    packageSet = import cardano-wallet;
+    gitrev = cardano-wallet.rev;
   };
 
-  ############################################################################
-  # Collecting and filtering jobs
+  # Shorthand system names
+  names = {
+    linux = "x86_64-linux";
+    macos = "x86_64-darwin";
+  };
 
-  testsSupportedSystems =
-    optional buildLinux "x86_64-linux" ++
-    optional buildMacOS "x86_64-darwin";
+  # Debug tracing function
+  tr = val: if debug then __trace (__toJSON val) val else val;
 
-  # Recurse through an attrset, returning all test derivations in a list.
-  collectTests' = ds: filter (d: elem d.system testsSupportedSystems) (collect isDerivation ds);
-  # Adds the package name to the test derivations for windows-testing-bundle.nix
-  # (passthru.identifier.name does not survive mapTestOn)
-  collectTests = ds: concatLists (
-    mapAttrsToList (packageName: package:
-      map (drv: drv // { inherit packageName; }) (collectTests' package)
-    ) ds);
+  # Instantiate project package sets for linux and macos.
+  releaseLib = mapAttrs (const makeReleaseLib) names // {
+    windows = if (elem names.linux supportedCrossSystems)
+              then releaseLib.linux
+              else releaseLib.macos;
+  };
+
+  ####################################################################
+  # The main jobset which maps derivations from default.nix to build
+  # platforms
+
+  platformJobs = optionalAttrs buildLinux (with releaseLib.linux; rec {
+    linux = optionalAttrs buildNative {
+      native = untag (mapTestOn
+        (tr (packagePlatforms (filterJobs.linux.native project))));
+
+      # Build and cache the build script used on Buildkite
+      buildkiteScript = import ./.buildkite/default.nix {
+        inherit pkgs;
+      };
+    } // optionalAttrs buildMusl {
+      musl = untag (mapTestOnCross systems.examples.musl64
+        (tr (packagePlatformsCross (filterJobs.linux.musl project))));
+    };
+
+    cardano-wallet-linux64 = import ./nix/release-package.nix {
+      inherit pkgs;
+      exes = releaseContents (if buildMusl then linux.musl else linux.native);
+      platform = "linux64";
+      format = "tar.gz";
+    };
+  }) // optionalAttrs (buildMacOS && buildNative) (with releaseLib.macos; (rec {
+    macos = untag (mapTestOn (tr (packagePlatforms (filterJobs.macos project))));
+
+    cardano-wallet-macos64 = hydraJob' (import ./nix/release-package.nix {
+      inherit ((pkgsFor names.macos).private) pkgs;
+      exes = releaseContents macos;
+      platform = "macos64";
+      format = "tar.gz";
+    });
+  })) // optionalAttrs buildWindows (with releaseLib.windows; (rec {
+    windows = untag (mapTestOnCross systems.examples.mingwW64
+      (tr (packagePlatformsCross (filterJobs.windows project))));
+
+    cardano-wallet-win64 = import ./nix/release-package.nix {
+      inherit pkgs;
+      exes = releaseContents windows;
+      platform = "win64";
+      format = "zip";
+    };
+
+    # This is used for testing the build on windows.
+    cardano-wallet-tests-win64 = import ./nix/windows-testing-bundle.nix {
+      inherit pkgs project;
+      cardano-wallet = windows.cardano-wallet;
+      cardano-node = windows.cardano-node;
+      cardano-cli = windows.cardano-cli;
+      tests = collectTestsWithPackageName windows.tests;
+      benchmarks = collectTestsWithPackageName windows.benchmarks;
+    };
+  }));
+
+  # Which exes should be put in the release archive.
+  releaseContents = jobs: map (exe: jobs.${exe}) [
+    "cardano-wallet"
+    "bech32"
+    "cardano-address"
+    "cardano-cli"
+    "cardano-node"
+  ];
+
+  ####################################################################
+  # Job filters
+
+  filterJobs = {
+    linux = {
+      native = makeJobFilter filterJobsNative {
+        # Don't run tests on linux native, because they are run for linux musl.
+        checks = [];
+        testCoverageReport = [];
+      };
+      musl = makeJobFilter filterJobsCross {};
+    };
+    macos = makeJobFilter filterJobsNative {
+      dockerImage = [];
+    };
+    windows = makeJobFilter filterJobsCross {
+      # Remove cardano-node integration tests for Windows because
+      # ouroboros-network doesn't work under wine.
+      checks.cardano-wallet.integration = [];
+      # Remove the test coverage report - only generate that for Linux musl.
+      testCoverageReport = [];
+    };
+  };
+
+  makeJobFilter = base: filt: js: recursiveUpdate (recursiveUpdate js base) filt;
 
   # Remove build jobs for which cross compiling does not make sense.
-  filterJobsCross = js: recursiveUpdate js {
+  filterJobsCross = {
     dockerImage = [];
     private = {
       shell = [];
@@ -137,133 +207,96 @@ let
     };
   };
 
-  # Remove cardano-node integration tests for Windows because
-  # ouroboros-network doesn't work under wine.
-  filterJobsWindows = let
-      f = path: value: if (isCardanoNodeIntegration path) then {} else value;
-      isCardanoNodeIntegration = path:
-         path == ["checks" "cardano-wallet" "integration"];
-    in
-      js: mapAttrsRecursive f (filterJobsCross js);
-
-  # Filters jobs for non-cross builds after platform mapping.
-  # 1. Don't run tests on linux native, because they are also run for linux musl.
-  filterMappedNative = let
-    removeLinuxNativeChecks = path: value:
-      if (head path == "checks" && builtins.typeOf value == "list")
-        then remove "x86_64-linux" value
-        else value;
-  in mapAttrsRecursive removeLinuxNativeChecks;
-
   # Filters the derivations from default.nix for non-cross builds.
-  # 1. Build profiled packages for the master branch, so that they are cached.
-  #    But don't make profiled builds for PRs because this is a waste of time.
-  # 2. Remove the test coverage report - only generate that for Linux musl.
-  filterJobsNative = let
-    removeProfiledBuildForPRs = if (pr == null)
-      then id
-      else js: recursiveUpdate js { private.shell-prof = null; };
-    removeCoverageReport = filterAttrs (n: _: n != "testCoverageReport");
-  in
-    drvs: removeCoverageReport (removeProfiledBuildForPRs drvs);
+  filterJobsNative = (optionalAttrs (pr != null) {
+    # Build profiled packages for the master branch, so that they are
+    # cached. But don't make profiled builds for PRs because this is
+    # a waste of time.
+    private.shell-prof = [];
+  });
 
   ############################################################################
   # This aggregate job is what IOHK Hydra uses to update the CI status
   # in GitHub.
 
-  required = mkRequiredJob (
-    optionals buildNative (
-      collectTests jobs.native.checks ++
-      collectTests jobs.native.benchmarks ++
-      optionals buildLinux [
-        jobs.native.private.shell.x86_64-linux
-        # executables for linux
-        jobs.native.cardano-wallet.x86_64-linux
-      ] ++
-      optionals buildMacOS [
-        jobs.native.private.shell.x86_64-darwin
+  required = releaseLib.linux.mkRequiredJob (
+    withPlatform "linux.native" (path: js:
+      collectTestNames path "checks" js ++
+      collectTestNames path "benchmarks" js ++
+      [ "${path}.private.shell"
+        "${path}.cardano-wallet"
+      ]) ++
+    withPlatform "linux.musl" (path: js:
+      collectTestNames path "checks" js ++
+      # Release package for Linux
+      [ "cardano-wallet-linux64"
+      ]) ++
+    withPlatform "macos" (path: js:
+      collectTestNames path "checks" js ++
+      collectTestNames path "benchmarks" js ++
+      [ "${path}.private.shell"
         # executables for macOS
-        jobs.native.cardano-wallet.x86_64-darwin
+        "${path}.cardano-wallet"
 
-        # Release packages for macOS
-        jobs.cardano-wallet-macos64
+        # Release package for macOS
+        "cardano-wallet-macos64"
       ]) ++
-    optionals buildMusl (
-      collectTests jobs.musl64.checks ++
-      # Release packages for Linux
-      [ jobs.cardano-wallet-linux64
-      ]) ++
-    optionals buildWindows (
-      collectTests jobs.x86_64-w64-mingw32.checks ++
-      [ jobs.x86_64-w64-mingw32.cardano-wallet.x86_64-linux
-
-        # Release packages for Windows
-        jobs.cardano-wallet-win64
-
+    withPlatform "windows" (path: js:
+      collectTestNames path "checks" js ++
+      [ "${path}.cardano-wallet"
+        # Release package for Windows
+        "cardano-wallet-win64"
         # Windows testing package - is run nightly in CI.
-        jobs.cardano-wallet-tests-win64
-      ]));
+        "cardano-wallet-tests-win64"
+      ])
+  );
+
+  ####################################################################
+  # Jobset munging
+
+  # Removes redundant system tags from the jobset
+  untag = let
+    getLeaf = attrs: let
+      names = attrNames attrs;
+      name = head (attrNames attrs);
+    in
+      if (length names == 1 && elem name supportedSystems)
+        then attrs.${name}
+        else null;
+  in mapAttrsRecursiveCond
+    (as: !(isDerivation as) && getLeaf as == null)
+    (path: attrs:
+      let leaf = getLeaf attrs;
+      in if leaf == null then attrs else leaf);
 
   ############################################################################
-  # Release distribution jobs - these all have a Hydra download link.
+  # Collecting jobs
 
-  # Which exes should be put in the release archive.
-  releaseContents = [
-    "cardano-wallet"
-    "bech32"
-    "cardano-address"
-    "cardano-cli"
-    "cardano-node"
-  ];
+  testsSupportedSystems =
+    optional buildLinux names.linux ++
+    optional buildMacOS names.macos;
+  isTest = drv: isDerivation drv && elem drv.system testsSupportedSystems;
 
-  # function to take a list of jobs by name from a jobset.
-  selectExes = subjobs: system: map (exe: subjobs.${exe}.${system});
+  # Adds the package name to the test derivations for windows-testing-bundle.nix
+  # (passthru.identifier.name does not survive mapTestOn)
+  collectTestsWithPackageName = ds: concatLists
+    (mapAttrsToList (packageName: package: map
+      (drv: drv // { inherit packageName; })
+      (filter isTest (collect isDerivation package))) ds);
 
-  releaseDistJobs = optionalAttrs buildMusl {
-    cardano-wallet-linux64 = import ./nix/release-package.nix {
-      inherit pkgs;
-      exes = selectExes jobs.musl64 "x86_64-linux" releaseContents;
-      platform = "linux64";
-      format = "tar.gz";
-    };
-  } // optionalAttrs buildMacOS {
-    cardano-wallet-macos64 = hydraJob' (import ./nix/release-package.nix {
-      inherit ((pkgsFor "x86_64-darwin").private) pkgs;
-      exes = selectExes jobs.native "x86_64-darwin" releaseContents;
-      platform = "macos64";
-      format = "tar.gz";
-    });
-  } // optionalAttrs buildWindows {
-    cardano-wallet-win64 = import ./nix/release-package.nix {
-      inherit pkgs;
-      exes = selectExes jobs.x86_64-w64-mingw32 builtins.currentSystem releaseContents;
-      platform = "win64";
-      format = "zip";
-    };
+  collectTestNames = base: attr: js:
+    optionals (hasAttr attr js)
+    (collect isString
+      (mapAttrsRecursiveCond
+        (as: !(isDerivation as))
+        (path: drv: if isTest drv
+          then concatStringsSep "." ([base attr] ++ path)
+          else null)
+        js.${attr}));
 
-    # This is used for testing the build on windows.
-    cardano-wallet-tests-win64 = let
-      winJobs = jobs."${mingwW64.config}";
-    in import ./nix/windows-testing-bundle.nix {
-      inherit pkgs project;
-      cardano-wallet = winJobs.cardano-wallet.x86_64-linux;
-      cardano-node = winJobs.cardano-node.x86_64-linux;
-      cardano-cli = winJobs.cardano-cli.x86_64-linux;
-      tests = collectTests winJobs.tests;
-      benchmarks = collectTests winJobs.benchmarks;
-    };
-  };
+  withPlatform = path: f:
+    let jobset = attrByPath (splitString "." path) null platformJobs;
+    in if (jobset != null) then f path jobset else [];
 
-  ############################################################################
-  # Miscellaneous extra jobs
-
-  otherJobs = optionalAttrs buildLinux {
-    # Build and cache the build script used on Buildkite
-    buildkiteScript = import ./.buildkite/default.nix {
-      inherit pkgs;
-    };
-  };
-
-  jobs = mappedJobs // required // releaseDistJobs // otherJobs;
-
-in jobs
+in
+  platformJobs // required
