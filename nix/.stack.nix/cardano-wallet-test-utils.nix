f91364e06d5cd2fca846f4cd6c9ee5fa19879ebb
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2021-06-11 17:26:25 +1000

    cabal: Bump version from 2021.5.26 to 2021.6.11

--- nix/.stack.nix/cardano-wallet-test-utils.nix
+++ nix/.stack.nix/cardano-wallet-test-utils.nix
@@ -13,7 +13,7 @@
       specVersion = "1.10";
       identifier = {
         name = "cardano-wallet-test-utils";
-        version = "2021.5.26";
+        version = "2021.6.11";
         };
       license = "Apache-2.0";
       copyright = "2018-2020 IOHK";
