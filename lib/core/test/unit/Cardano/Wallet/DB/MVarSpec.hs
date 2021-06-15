96b6cc73a309ac7e56969d58cf50a7e91a67f1bb
Johannes Lund <johannes.lund@iohk.io>
2021-06-15 20:23:25 +0200

    Mark MVar DB properties pending on macOS
    Unit tests on macOS hydra builds are regularly timing out.
    
    Rough analysis suggests more than 50% of recent unit timeouts occur in
    the MVar DB properties:
    
    > Broken down by tags/issues:
    > 21 times (100%) #2472 Unit tests sometimes time out.
    > 19 times (90%) mac
    > 19 times (90%) hydra
    > 10 times (48%) MVar
    > 2 times (10%) buildkite
    
    (./scripts/bors-stats.rb list --fetch-system --tag "#2472" --details true --annotate MVar)
    
    I believe as we run the DB properties on the real Sqlite DB, and
    the MVar properties in linux, we shouldn't lose much from disabling them on macOS.

--- lib/core/test/unit/Cardano/Wallet/DB/MVarSpec.hs
+++ lib/core/test/unit/Cardano/Wallet/DB/MVarSpec.hs
@@ -35,12 +35,16 @@ import Test.Hspec
     ( Spec, before, describe )
 import Test.QuickCheck
     ( Arbitrary (..) )
+import Test.Utils.Darwin
+    ( pendingOnMacOS )
 
 import qualified Cardano.Wallet.DB.MVar as MVar
 
 spec :: Spec
-spec = before (MVar.newDBLayer @IO @(SeqState 'Mainnet ShelleyKey) ti) $
-    describe "MVar" properties
+spec =
+    before (pendingOnMacOS "#2472: timeouts in hydra mac builds")
+    $ before (MVar.newDBLayer @IO @(SeqState 'Mainnet ShelleyKey) ti)
+    $ describe "MVar" properties
   where
     ti = dummyTimeInterpreter
 
