d57ae988e1f0626dbe2eebf5b89e79287dd5e206
Johannes Lund <johannes.lund@iohk.io>
2021-06-17 19:39:40 +0200

    Use LineBuffering in unit tests
    We use it in the integration tests. Maybe it will improve the unit
    timeouts or our understanding of it - could be worth a shot.

--- lib/core/test/unit/Main.hs
+++ lib/core/test/unit/Main.hs
@@ -4,7 +4,12 @@ import Cardano.Startup
     ( withUtf8Encoding )
 import Prelude
 import qualified Spec
+import System.IO
+    ( BufferMode (..), hSetBuffering, stderr, stdout )
 import Test.Hspec.Runner
 
 main :: IO ()
-main = withUtf8Encoding $ hspecWith defaultConfig Spec.spec
+main = do
+    hSetBuffering stdout LineBuffering
+    hSetBuffering stderr LineBuffering
+    withUtf8Encoding $ hspecWith defaultConfig Spec.spec
