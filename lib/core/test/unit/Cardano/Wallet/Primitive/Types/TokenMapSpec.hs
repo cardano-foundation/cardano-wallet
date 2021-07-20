983f393a9d19ca1ebbdb3320bae3aa678f418ca6
Jonathan Knowles <jonathan.knowles@iohk.io>
2021-07-20 07:12:48 +0000

    Restore shrinking behaviour for `TokenQuantity` in `TokenMapSpec`.
    The shrinker for `Arbitrary TokenQuantity` should allow zero values.
    
    See:
    
    https://github.com/input-output-hk/cardano-wallet/pull/2765#pullrequestreview-710210825

--- lib/core/test/unit/Cardano/Wallet/Primitive/Types/TokenMapSpec.hs
+++ lib/core/test/unit/Cardano/Wallet/Primitive/Types/TokenMapSpec.hs
@@ -41,6 +41,7 @@ import Cardano.Wallet.Primitive.Types.TokenQuantity
 import Cardano.Wallet.Primitive.Types.TokenQuantity.Gen
     ( genTokenQuantitySmall
     , genTokenQuantitySmallPositive
+    , shrinkTokenQuantitySmall
     , shrinkTokenQuantitySmallPositive
     )
 import Control.Monad
@@ -1050,7 +1051,7 @@ instance Arbitrary TokenQuantity where
     -- verify that the token map invariant (that a map contains no
     -- zero-valued tokens) is maintained.
     arbitrary = genTokenQuantitySmall
-    shrink = shrinkTokenQuantitySmallPositive
+    shrink = shrinkTokenQuantitySmall
 
 instance Arbitrary (Positive TokenQuantity) where
     arbitrary = Positive <$> genTokenQuantitySmallPositive
