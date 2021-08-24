2915d8d69600b5b828ec26c145ca98acb2f0435b
Jonathan Knowles <jonathan.knowles@iohk.io>
2021-08-24 11:20:36 +0000

    Add module `Primitive.Types.RewardAccount.Gen`.

---
+++ lib/core/src/Cardano/Wallet/Primitive/Types/RewardAccount/Gen.hs
@@ -0,0 +1,38 @@
+module Cardano.Wallet.Primitive.Types.RewardAccount.Gen
+    ( genRewardAccount
+    , shrinkRewardAccount
+    )
+    where
+
+import Prelude
+
+import Cardano.Wallet.Primitive.Types.RewardAccount
+    ( RewardAccount (..) )
+import Test.QuickCheck
+    ( Gen, elements, sized )
+
+import qualified Data.ByteString.Char8 as B8
+
+--------------------------------------------------------------------------------
+-- Reward accounts generated according to the size parameter
+--------------------------------------------------------------------------------
+
+genRewardAccount :: Gen (RewardAccount)
+genRewardAccount = sized $ \size -> elements $ take (max 1 size) addresses
+
+shrinkRewardAccount :: RewardAccount -> [RewardAccount]
+shrinkRewardAccount a
+    | a == simplest = []
+    | otherwise = [simplest]
+  where
+    simplest = head addresses
+
+addresses :: [RewardAccount]
+addresses = mkRewardAccount <$> ['0' ..]
+
+--------------------------------------------------------------------------------
+-- Internal utilities
+--------------------------------------------------------------------------------
+
+mkRewardAccount :: Char -> RewardAccount
+mkRewardAccount c = RewardAccount $ "Reward" `B8.snoc` c
