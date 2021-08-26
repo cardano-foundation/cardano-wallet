03a405412ab19f4df6a12ac945af9806154cffff
Johannes Lund <johannes.lund@iohk.io>
2021-08-26 16:33:09 +0200

    Bump version from 2021.8.11 to 2021.8.27

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -19,9 +19,9 @@ set -euo pipefail
 ################################################################################
 # Release-specific parameters. Can be changed interactively by running the script.
 # Release tags must follow format vYYYY-MM-DD.
-GIT_TAG="v2021-08-11"
-OLD_GIT_TAG="v2021-07-30"
-CARDANO_NODE_TAG="alonzo-purple-1.0.1"
+GIT_TAG="v2021-08-27"
+OLD_GIT_TAG="v2021-08-11"
+CARDANO_NODE_TAG="1.29.0"
 
 ################################################################################
 # Tag munging functions
