c9f4b340ffc263d209cdbbdb17e59272b6c0b41c
Pawel Jakubas <pawel.jakubas@iohk.io>
2021-10-15 19:46:42 +0200

    deal with validity interval

--- lib/core/src/Cardano/Wallet/Api/Server.hs
+++ lib/core/src/Cardano/Wallet/Api/Server.hs
@@ -2267,7 +2267,7 @@ decodeTransaction ctx _wid (ApiSerialisedTransaction (ApiT sealed)) = do
         , collateral = map toInp colls
         , withdrawals = map toWrdl $ Map.assocs wdrlMap
         , metadata = ApiTxMetadata $ ApiT <$> meta
-        , scriptValidity = Nothing
+        , scriptValidity = ApiT <$> vldt
         }
   where
     tl = ctx ^. W.transactionLayer @k
