158fc72886058f60ae8d379c7cef8cc0d85fa0a2
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2021-10-22 16:02:36 +0800

    Improve scripts for haskell-language-server and ghci

---
+++ scripts/cabal-lib.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+######################################################################
+# Functions for querying Cabal projects
+
+# shellcheck disable=SC2034
+
+builddir=dist-newstyle
+cabal_opts=("--builddir=$builddir")
+plan_json=$builddir/cache/plan.json
+
+list_cabal_files() {
+  git ls-files '*.cabal'
+}
+
+list_packages() {
+  list_cabal_files | xargs basename -a | sed 's/\.cabal//'
+}
+
+get_cabal_version() {
+  awk '/^version:/ { print $2; }' "$(git ls-files '*.cabal' | head -n1)"
+}
+
+list_sources() {
+  git ls-files 'lib/**/*.hs' | grep -v Main.hs
+}
+
+# usage: query_plan_json PACKAGE COMP:NAME KEY
+query_plan_json() {
+  jq -r '.["install-plan"][]|select(."pkg-name"=="'"$1"'")|select(.["component-name"]=="'"$2"'")["'"$3"'"]' < $plan_json
+}
+
+# usage: get_dist_dir PACKAGE
+get_dist_dir() {
+  relpath "$(query_plan_json "$1" lib dist-dir)"
+}
+
+# usage: get_bin_dir PACKAGE exe:NAME
+get_bin_dir() {
+  dirname "$(relpath "$(query_plan_json "$1" "$2" bin-file)")"
+}
+
+relpath() {
+  # path must exist
+  # realpath "--relative-to=$(pwd)" "$1"
+  # shellcheck disable=SC2001
+  echo "$1" | sed "s=$(pwd)/=="
+}
+
+setup_cabal_plan() {
+  if [ ! -f $plan_json ]; then
+    echo "$0: Running Cabal to generate build plan $plan_json"
+    cabal "${cabal_opts[@]}" update
+    cabal "${cabal_opts[@]}" configure
+  fi
+}
+
+######################################################################
+# Config file generation
+
+ghci_flags() {
+  cat <<EOF
+-XOverloadedStrings
+-XNoImplicitPrelude
+-XTypeApplications
+-XDataKinds
+-fwarn-unused-binds
+-fwarn-unused-imports
+-fwarn-orphans
+-Wno-missing-home-modules
+EOF
+
+  # Add all source directories to ghci command-line
+  mapfile -t sources < <(list_sources)
+  dirname "${sources[@]}" | sed -e 's/^\([^A-Z]*\).*$/-i\1/' | sed 's=/$==' | sort -u
+
+  # Add Cabal-generated Paths sources
+  list_packages | while read -r pkg; do
+    echo "-i$(get_dist_dir "$pkg")/build/autogen"
+  done
+}
+
+# List all modules for ghci command-line
+list_modules() {
+  list_sources | sed -e 's/^[^A-Z]*\(.*\)\.hs$/\1/' | grep / | sed 'y=/=.='
+  list_packages | sed -e 'y/-/_/' -e 's/^/Paths_/'
+}
---
+++ scripts/hie-bios.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+######################################################################
+# Generates a cradle config for haskell-language-server,
+# This script is used by ../hie-direnv.yaml.
+######################################################################
+
+set -euo pipefail
+
+# See https://github.com/haskell/hie-bios#bios
+make_hie_bios() {
+  ghci_flags
+  list_modules
+}
+
+# See https://github.com/haskell/hie-bios#cradle-dependencies
+make_hie_bios_deps() {
+  list_cabal_files
+
+  cat<<EOF
+cabal.project
+default.nix
+shell.nix
+EOF
+}
+
+: "${HIE_BIOS_OUTPUT:=/dev/stdout}"
+: "${HIE_BIOS_ARG:=${1:-}}"
+: "${HIE_BIOS_DEPS:=/dev/null}"
+
+cd "$(dirname "$0")"/.. || exit
+
+source_file=${HIE_BIOS_ARG//$(pwd)/}
+
+if [[ "$source_file" =~ ^lib/.*\.hs$ || -z "$source_file" ]]; then
+  echo "$0: Generating hie-bios for source file $source_file."
+  . "$(dirname "$0")/cabal-lib.sh"
+
+  setup_cabal_plan
+
+  echo "Writing cradle config to $HIE_BIOS_OUTPUT"
+  make_hie_bios > "$HIE_BIOS_OUTPUT"
+
+  echo "Writing dependencies to $HIE_BIOS_DEPS"
+  make_hie_bios_deps > "$HIE_BIOS_DEPS"
+
+  echo "Finished."
+else
+  echo "Ignoring $source_file."
+fi
