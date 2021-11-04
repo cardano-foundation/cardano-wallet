640fd73cf9a41f3f0fbc808f5f1a9be86ace4426
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2021-11-04 16:50:57 +0800

    Buildkite: Add haskell-language-server check

---
+++ scripts/buildkite/check-haskell-nix-cabal.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "--- Cabal update"
+cabal update
+echo
+
+echo "+++ Cabal configure -frelease"
+cabal configure -frelease --enable-tests --enable-benchmarks
+echo
+
+echo "+++ Cabal configure"
+cabal configure --enable-tests --enable-benchmarks
+echo
+
+echo "+++ haskell-language-server"
+ln -sf hie-direnv.yaml hie.yaml
+haskell-language-server lib/core/src/Cardano/Wallet.hs
+echo
