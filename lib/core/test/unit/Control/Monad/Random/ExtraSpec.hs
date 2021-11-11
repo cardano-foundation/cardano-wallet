c6cd695889f0e4386382b9a36cc4391da1fa3325
Jonathan Knowles <jonathan.knowles@iohk.io>
2021-11-11 08:28:19 +0000

    Test properties of `MonadRandomState` class.

--- lib/core/test/unit/Control/Monad/Random/ExtraSpec.hs
+++ lib/core/test/unit/Control/Monad/Random/ExtraSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
@@ -6,13 +8,19 @@ module Control.Monad.Random.ExtraSpec
 
 import Prelude
 
+import Control.Monad.Random.Class
+    ( MonadRandom (..) )
 import Control.Monad.Random.Extra
-    ( StdGenSeed (..)
+    ( MonadRandomState (..)
+    , NonRandom (..)
+    , StdGenSeed (..)
     , stdGenFromSeed
     , stdGenToSeed
     )
+import Data.Proxy
+    ( Proxy (..) )
 import System.Random
-    ( mkStdGen )
+    ( Random (..), mkStdGen )
 import Test.Hspec
     ( Spec, describe, it )
 import Test.QuickCheck
@@ -27,15 +35,72 @@ import Test.QuickCheck
     , shrinkIntegral
     , (===)
     )
+import Test.QuickCheck.Monadic
+    ( assert, monadic, run )
+import Test.QuickCheck.Property
+    ( ioProperty )
 
 spec :: Spec
 spec = describe "Control.Monad.Random.ExtraSpec" $ do
+
+    describe "MonadRandomState" $ do
+        describe "IO" $ do
+            it "prop_MonadRandomState_setRandomSeed_getRandom" $
+                property $
+                prop_MonadRandomState_setRandomSeed_getRandom
+                    (Proxy @Int) ioProperty
+            it "prop_MonadRandomState_setRandomSeed_getRandomSeed" $
+                property $
+                prop_MonadRandomState_setRandomSeed_getRandomSeed ioProperty
+        describe "NonRandom" $ do
+            it "prop_MonadRandomState_setRandomSeed_getRandom" $
+                property $
+                prop_MonadRandomState_setRandomSeed_getRandom
+                    (Proxy @Int) runNonRandom
+            it "prop_MonadRandomState_setRandomSeed_getRandomSeed" $
+                property $
+                prop_MonadRandomState_setRandomSeed_getRandomSeed runNonRandom
 
     describe "StdGenSeed" $ do
         it "prop_stdGenToSeed_stdGenFromSeed" $
             property prop_stdGenToSeed_stdGenFromSeed
         it "prop_stdGenFromSeed_stdGenToSeed" $
             property prop_stdGenFromSeed_stdGenToSeed
+
+--------------------------------------------------------------------------------
+-- Random number generator states
+--------------------------------------------------------------------------------
+
+-- Verifies that when we use a constant seed value, we always generate the same
+-- random number.
+--
+prop_MonadRandomState_setRandomSeed_getRandom
+    :: forall m a. (MonadRandomState m, Eq a, Random a)
+    => Proxy a
+    -> (m Property -> Property)
+    -> RandomSeed m
+    -> Property
+prop_MonadRandomState_setRandomSeed_getRandom _proxy extractMonadicValue seed =
+    monadic extractMonadicValue $ do
+        run $ setRandomSeed seed
+        (randomValue1 :: a) <- run getRandom
+        run $ setRandomSeed seed
+        (randomValue2 :: a) <- run getRandom
+        assert $ randomValue1 == randomValue2
+
+-- Verifies that when we set the seed and read it back again, we get the same
+-- seed value.
+--
+prop_MonadRandomState_setRandomSeed_getRandomSeed
+    :: (MonadRandomState m, Eq (RandomSeed m))
+    => (m Property -> Property)
+    -> RandomSeed m
+    -> Property
+prop_MonadRandomState_setRandomSeed_getRandomSeed extractMonadicValue seed =
+    monadic extractMonadicValue $ do
+        run $ setRandomSeed seed
+        seed' <- run getRandomSeed
+        assert $ seed == seed'
 
 --------------------------------------------------------------------------------
 -- Random number generator seeds
