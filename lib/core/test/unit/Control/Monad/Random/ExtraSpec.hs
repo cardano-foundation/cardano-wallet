27ce78c5e0fa0a371002ca152bc0fa38c4beeb4f
Jonathan Knowles <jonathan.knowles@iohk.io>
2021-11-11 05:48:59 +0000

    Test properties of `StdGenSeed` type.

---
+++ lib/core/test/unit/Control/Monad/Random/ExtraSpec.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Control.Monad.Random.ExtraSpec
+    where
+
+import Prelude
+
+import Control.Monad.Random.Extra
+    ( StdGenSeed (..)
+    , stdGenFromSeed
+    , stdGenToSeed
+    )
+import System.Random
+    ( mkStdGen )
+import Test.Hspec
+    ( Spec, describe, it )
+import Test.QuickCheck
+    ( Arbitrary (..)
+    , Gen
+    , Property
+    , arbitraryBoundedIntegral
+    , checkCoverage
+    , cover
+    , frequency
+    , property
+    , shrinkIntegral
+    , (===)
+    )
+
+spec :: Spec
+spec = describe "Control.Monad.Random.ExtraSpec" $ do
+
+    describe "StdGenSeed" $ do
+        it "prop_stdGenToSeed_stdGenFromSeed" $
+            property prop_stdGenToSeed_stdGenFromSeed
+        it "prop_stdGenFromSeed_stdGenToSeed" $
+            property prop_stdGenFromSeed_stdGenToSeed
+
+--------------------------------------------------------------------------------
+-- Random number generator seeds
+--------------------------------------------------------------------------------
+
+prop_stdGenToSeed_stdGenFromSeed :: StdGenSeed -> Property
+prop_stdGenToSeed_stdGenFromSeed s =
+    checkCoverage $
+    cover 1 (s == minBound) "s == minBound" $
+    cover 1 (s == maxBound) "s == maxBound" $
+    stdGenToSeed (stdGenFromSeed s) === s
+
+prop_stdGenFromSeed_stdGenToSeed :: MkStdGenInt -> Property
+prop_stdGenFromSeed_stdGenToSeed (MkStdGenInt i) =
+    checkCoverage $
+    cover 1 (i == minBound) "i == minBound" $
+    cover 1 (i == maxBound) "i == maxBound" $
+    stdGenFromSeed (stdGenToSeed (mkStdGen i)) === mkStdGen i
+
+instance Arbitrary StdGenSeed where
+    arbitrary = genStdGenSeed
+    shrink = shrinkStdGenSeed
+
+genStdGenSeed :: Gen StdGenSeed
+genStdGenSeed = StdGenSeed <$> genBoundedIntegralWithUniformPriority 16
+
+shrinkStdGenSeed :: StdGenSeed -> [StdGenSeed]
+shrinkStdGenSeed (StdGenSeed s) = StdGenSeed <$> shrinkIntegral s
+
+newtype MkStdGenInt = MkStdGenInt Int
+    deriving (Eq, Show)
+
+instance Arbitrary MkStdGenInt where
+    arbitrary = genMkStdGenInt
+    shrink = shrinkMkStdGenInt
+
+genMkStdGenInt :: Gen MkStdGenInt
+genMkStdGenInt = MkStdGenInt <$> genBoundedIntegralWithUniformPriority 16
+
+shrinkMkStdGenInt :: MkStdGenInt -> [MkStdGenInt]
+shrinkMkStdGenInt (MkStdGenInt i) = MkStdGenInt <$> shrinkIntegral i
+
+--------------------------------------------------------------------------------
+-- Utilities
+--------------------------------------------------------------------------------
+
+-- | Generates a distribution that explicitly includes boundary values.
+--
+-- The given uniform priority parameter determines how uniform the distribution
+-- should be:
+--
+--  - Higher values of this parameter produce distributions that are more
+--    uniform, where boundary values are deprioritized.
+--  - Lower values of this parameter produce distributions that are less
+--    uniform, where boundary values are prioritized.
+--
+genBoundedIntegralWithUniformPriority
+    :: Bounded a => Integral a => Int -> Gen a
+genBoundedIntegralWithUniformPriority uniformPriority = frequency
+    [ (1, pure minBound)
+    , (1, pure maxBound)
+    , (1, pure (minBound + 1))
+    , (1, pure (maxBound - 1))
+    , (uniformPriority, arbitraryBoundedIntegral)
+    ]
