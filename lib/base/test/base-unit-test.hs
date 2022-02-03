86f093fcbe2b56e2063d9a7c71f62d2b30b26670
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2022-02-04 00:42:53 +1000

    Fix cardano-wallet-base tests when running under wine

--- lib/base/test/base-unit-test.hs
+++ lib/base/test/base-unit-test.hs
@@ -1,1 +1,13 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Main where
+
+import Cardano.Wallet.Base
+
+import Cardano.Wallet.Startup
+    ( withUtf8Encoding )
+import Test.Hspec.Extra
+    ( hspecMain )
+
+import qualified Spec
+
+main :: IO ()
+main = withUtf8Encoding $ hspecMain Spec.spec
