79ee45c3d915ead7fd9b0192ea9847f37b680e40
Yuriy Lazaryev <yuriy.lazaryev@iohk.io>
2022-02-21 13:54:59 +0100

    chore: avoid whitespace between a record constructor and its curly braces

--- lib/core/src/Cardano/Wallet.hs
+++ lib/core/src/Cardano/Wallet.hs
@@ -2507,7 +2507,7 @@ listAssets ctx wid = db & \DBLayer{..} -> do
             . map (TokenBundle.getAssets . view #tokens)
             . filter ourOut
             . txInfoOutputs
-        ourOut TxOut {address} = ourAddress address
+        ourOut TxOut{address} = ourAddress address
         ourAddress addr = isJust . fst . isOurs addr $ getState cp
     pure $ Set.unions $ map txAssets txs
   where
