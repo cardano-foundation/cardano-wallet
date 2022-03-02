3d85f1b30672b7b9cef43265993c357dfebeedb9
Pawel Jakubas <pawel.jakubas@iohk.io>
2022-03-02 21:33:48 +0100

    accomodate minting/burning in constructUnsignedTx

--- lib/shelley/src/Cardano/Wallet/Shelley/Transaction.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley/Transaction.hs
@@ -173,7 +173,7 @@ import Cardano.Wallet.Transaction
     , withdrawalToCoin
     )
 import Cardano.Wallet.Util
-    ( modifyM )
+    ( internalError, modifyM )
 import Codec.Serialise
     ( deserialiseOrFail )
 import Control.Arrow
@@ -327,13 +327,25 @@ constructUnsignedTx
     -- ^ Finalized asset selection
     -> Coin
     -- ^ Explicit fee amount
+    -> (TokenMap, Map AssetId (Script KeyHash))
+    -- ^ Assets to be minted
+    -> (TokenMap, Map AssetId (Script KeyHash))
+    -- ^ Assets to be burnt
     -> ShelleyBasedEra era
     -> Either ErrMkTransaction SealedTx
-constructUnsignedTx networkId (md, certs) ttl rewardAcnt wdrl cs fee era =
+constructUnsignedTx networkId (md, certs) ttl rewardAcnt wdrl cs fee toMint toBurn era =
     sealedTxFromCardanoBody <$> tx
   where
     tx = mkUnsignedTx era ttl cs md wdrls certs (toCardanoLovelace fee)
     wdrls = mkWithdrawals networkId rewardAcnt wdrl
+    allScripts = Map.union (snd toMint) (snd toBurn)
+    decorateWithScript (assetId, quantity) =
+        case Map.lookup assetId allScripts of
+            Just script -> (assetId, quantity, script)
+            Nothing -> internalError "constructUnsignedTx: each assetId at this point should have accompanying script"
+    extractData = map decorateWithScript . TokenMap.toFlatList . fst
+    mintData = extractData toMint
+    burnData = extractData toBurn
 
 mkTx
     :: forall k era.
@@ -529,19 +541,21 @@ newTransactionLayer networkId = TransactionLayer
         let wdrl  = withdrawalToCoin $ view #txWithdrawal ctx
         let delta = selectionDelta txOutCoin selection
         let rewardAcct = toRewardAccountRaw stakeXPub
+        let assetsToBeMinted = view #txAssetsToMint ctx
+        let assetsToBeBurnt = view #txAssetsToBurn ctx
         case view #txDelegationAction ctx of
             Nothing -> do
                 withShelleyBasedEra era $ do
                     let md = view #txMetadata ctx
                     constructUnsignedTx networkId (md, []) ttl rewardAcct wdrl
-                        selection delta
+                        selection delta assetsToBeMinted assetsToBeBurnt
 
             Just action -> do
                 withShelleyBasedEra era $ do
                     let certs = mkDelegationCertificates action stakeXPub
                     let payload = (view #txMetadata ctx, certs)
                     constructUnsignedTx networkId payload ttl rewardAcct wdrl
-                        selection delta
+                        selection delta assetsToBeMinted assetsToBeBurnt
 
     , estimateSignedTxSize = \pp tx -> do
         anyShelleyTx <- inAnyShelleyBasedEra (cardanoTx tx)
