32d17a5599e3755b780b166e95c7be42bccc9a6f
Yuriy Lazaryev <yuriy.lazaryev@iohk.io>
2022-03-18 18:46:14 +0100

    Rework CLI error reporting for the --blockfrost-token-file

--- lib/shelley/src/Cardano/Wallet/Shelley/Launch/Blockfrost.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley/Launch/Blockfrost.hs
@@ -1,23 +1,37 @@
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Cardano.Wallet.Shelley.Launch.Blockfrost
-    ( TokenFile
+    ( TokenFile (..)
     , readToken
     , tokenFileOption
+    , TokenFileException(..)
     ) where
 
 import Prelude
 
-import Blockfrost.Client.Core
-    ( projectFromFile )
 import Blockfrost.Client.Types
     ( Project (..) )
+import Blockfrost.Env
+    ( parseEnv )
+import Control.Exception
+    ( Exception, throw )
+import Data.Text
+    ( Text )
 import Options.Applicative
     ( Parser, help, long, metavar, option, str )
 
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+
 newtype TokenFile = TokenFile FilePath
     deriving newtype (Eq, Show)
+
+newtype TokenFileException = TokenFileException FilePath
+    deriving stock (Eq, Show)
+    deriving anyclass (Exception)
 
 -- | --blockfrost-token-file FILE
 tokenFileOption :: Parser TokenFile
@@ -31,4 +45,14 @@ tokenFileOption = option (TokenFile <$> str) $ mconcat
     ]
 
 readToken :: TokenFile -> IO Project
-readToken (TokenFile fp) = projectFromFile fp
+readToken (TokenFile fp) = Text.readFile fp >>=
+    either (throw (TokenFileException fp) . const) pure . mkProject
+  where
+    -- Can't use `Blockfrost.Client.Core.projectFromFile` as it uses `error`
+    -- and it leads to an unnecessary output that pollutes stdout.
+    mkProject :: Text -> Either Text Project
+    mkProject t =
+      let st = Text.strip t
+          tEnv = Text.dropEnd 32 st
+          token = Text.drop (Text.length tEnv) st
+      in Project <$> parseEnv tEnv <*> pure token
