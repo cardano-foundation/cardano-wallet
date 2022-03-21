9b9f395095800e2ad32f0b8c5f426fa5c079a4ca
Yuriy Lazaryev <yuriy.lazaryev@iohk.io>
2022-03-21 10:47:42 +0100

    test: readToken

--- lib/shelley/src/Cardano/Wallet/Shelley/Launch/Blockfrost.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley/Launch/Blockfrost.hs
@@ -7,6 +7,7 @@ module Cardano.Wallet.Shelley.Launch.Blockfrost
     ( TokenFile (..)
     , readToken
     , tokenFileOption
+    , TokenException(..)
     , TokenFileException(..)
     ) where
 
@@ -17,17 +18,21 @@ import Blockfrost.Client.Types
 import Blockfrost.Env
     ( parseEnv )
 import Control.Exception
-    ( Exception, throw )
-import Data.Text
-    ( Text )
+    ( Exception, IOException, catch, throw )
+import Control.Monad
+    ( when )
 import Options.Applicative
     ( Parser, help, long, metavar, option, str )
 
-import qualified Data.Text.IO as T
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
 newtype TokenFile = TokenFile FilePath
     deriving newtype (Eq, Show)
+
+data TokenException = EmptyToken FilePath | InvalidToken FilePath
+    deriving stock (Eq, Show)
+    deriving anyclass (Exception)
 
 newtype TokenFileException = TokenFileException FilePath
     deriving stock (Eq, Show)
@@ -45,14 +50,15 @@ tokenFileOption = option (TokenFile <$> str) $ mconcat
     ]
 
 readToken :: TokenFile -> IO Project
-readToken (TokenFile fp) = T.readFile fp >>=
-    either (throw (TokenFileException fp) . const) pure . mkProject
-  where
+readToken (TokenFile fp) = do
     -- Can't use `Blockfrost.Client.Core.projectFromFile` as it uses `error`
     -- and it leads to an unnecessary output that pollutes stdout.
-    mkProject :: Text -> Either Text Project
-    mkProject t =
-      let st = T.strip t
-          tEnv = T.dropEnd 32 st
-          token = T.drop (T.length tEnv) st
-      in Project <$> parseEnv tEnv <*> pure token
+    line <- T.readFile fp `catch` \(_ :: IOException) ->
+        throw $ TokenFileException fp
+    let tokenSrc = T.strip line
+    when (T.null tokenSrc) $ throw $ EmptyToken fp
+    let tEnv = T.dropEnd 32 tokenSrc
+        token = T.drop (T.length tEnv) tokenSrc
+    case Project <$> parseEnv tEnv <*> pure token of
+      Left _ -> throw $ InvalidToken fp
+      Right project -> pure project
