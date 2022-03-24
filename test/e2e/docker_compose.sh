2837462f507da0284ea566468a7832be79e06fb1
Piotr Stachyra <piotr.stachyra@iohk.io>
2022-03-24 12:41:52 +0100

    Fix docker wallet db cache

--- test/e2e/docker_compose.sh
+++ test/e2e/docker_compose.sh
@@ -1,7 +1,10 @@
+#!/usr/bin/env bash
+
 NETWORK=testnet \
 TESTS_E2E_TOKEN_METADATA=https://metadata.cardano-testnet.iohkdev.io/ \
 WALLET=dev-master \
-NODE=1.28.0 \
+NODE=1.34.1 \
 NODE_CONFIG_PATH=`pwd`/state/configs/$NETWORK \
-DATA=`pwd`/state/node_db/$NETWORK
+DATA=`pwd`/state/node_db/$NETWORK \
+WALLET_DATA=`pwd`/state/wallet_db/$NETWORK \
 docker-compose -f docker-compose-test.yml $1
