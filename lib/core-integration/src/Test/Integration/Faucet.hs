b11a6363f07d5931e887c29a1a108f1d1ef0ed79
Samuel Evans-Powell <mail@sevanspowell.net>
2022-05-06 09:02:51 +0200

    Add more Shelly mnemonics as a buffer
    - Tests are failing due to not enough Shelley mnemonics, add more than necessary
      to stave off future failures.

--- lib/core-integration/src/Test/Integration/Faucet.hs
+++ lib/core-integration/src/Test/Integration/Faucet.hs
@@ -965,6 +965,22 @@ seqMnemonics = unsafeMkMnemonic <$>
       , "trend" , "fine" , "hybrid" , "risk" , "mail"
       , "fan" , "damage" , "push" , "shrug" , "boost"
       ]
+    , [ "sleep" , "sword" , "chaos" , "taste" , "emotion"
+      , "achieve" , "prosper" , "scrap" , "delay" , "wing"
+      , "ketchup" , "neglect" , "foam" , "muffin" , "brisk"
+      ]
+    , [ "lawn" , "bring" , "diamond" , "network" , "remember"
+      , "slight" , "auto" , "select" , "vivid" , "silent"
+      , "letter" , "usual" , "pen" , "yellow" , "equip"
+      ]
+    , [ "coast" , "under" , "relax" , "vault" , "hip"
+      , "plastic" , "boat" , "dice" , "genuine" , "inject"
+      , "have" , "swim" , "miss" , "expire" , "tornado"
+      ]
+    , [ "damp" , "fabric" , "witness" , "height" , "hurdle"
+      , "office" , "midnight" , "gaze" , "engage" , "eagle"
+      , "into" , "cup" , "about" , "velvet" , "acquire"
+      ]
     ]
 
 maMnemonics :: [Mnemonic 24]
