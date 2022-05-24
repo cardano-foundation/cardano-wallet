8989644a45b02b1180cb92271391476810d0b531
Johannes Lund <johannes.lund@iohk.io>
2022-05-24 16:33:02 +0200

    use nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -33,8 +33,8 @@
   ############################################################################
 
   inputs = {
-    nixpkgs.follows = "haskellNix/nixpkgs-2111";
-    hostNixpkgs.follows = "nixpkgs-unstable";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    hostNixpkgs.follows = "nixpkgs";
     haskellNix = {
       url = "github:input-output-hk/haskell.nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -57,7 +57,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, hostNixpkgs, nixpkgs-unstable, flake-utils, haskellNix, iohkNix, customConfig, emanote, ... }:
+  outputs = { self, nixpkgs, hostNixpkgs, flake-utils, haskellNix, iohkNix, customConfig, emanote, ... }:
     let
       inherit (nixpkgs) lib;
       config = import ./nix/config.nix lib customConfig;
