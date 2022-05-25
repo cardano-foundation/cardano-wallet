80ec1574d1e7c1a00c29d3ba63105608a2ee682b
Heinrich Apfelmus <heinrich.apfelmus@iohk.io>
2022-05-25 15:41:41 +0200

    Add documentation to `Cardano.Wallet.Api.Aeson.Variant`

--- lib/core/src/Cardano/Wallet/Api/Aeson/Variant.hs
+++ lib/core/src/Cardano/Wallet/Api/Aeson/Variant.hs
@@ -1,5 +1,11 @@
 {-# LANGUAGE DeriveFunctor #-}
 
+-- |
+-- Copyright: © 2022 IOHK
+-- License: Apache-2.0
+--
+-- This module provides a utility function 'variants' for parsing
+-- JSON values using one out of many parsers.
 module Cardano.Wallet.Api.Aeson.Variant
     ( variants
     , Variant
@@ -11,6 +17,7 @@ import Prelude
 import Data.Aeson.Types
     ( Object, Parser, Value, modifyFailure, withObject )
 
+-- | Specification of a JSON parser suitable for 'variants'.
 data Variant a = Variant
     { _errContext :: String
     , _acceptance :: Object -> Bool
@@ -18,19 +25,33 @@ data Variant a = Variant
     }
     deriving (Functor)
 
--- | Define a variant for the object json parsing
+-- | Define a 'Variant' for parsing a JSON value.
+--
+-- A predicate checks whether a given 'Value' belongs to this variant;
+-- the 'Value' is parsed only if this this check succeeds.
 variant
-    :: String -- ^ variant error context
-    -> (Object -> Bool) -- ^ variance acceptance 
-    -> (Value -> Parser a) -- ^ variance parser 
+    :: String -- ^ Error message suffix in case of parse failure.
+    -> (Object -> Bool) -- ^ Check whether this variant applies.
+    -> (Value -> Parser a) -- ^ Parser for this variant.
     -> Variant a
 variant = Variant
 
--- | Compute a parser for `a` given parsers for its variants
+-- | Construct a parser for @a@ from parsers for its variants.
+--
+-- The parser succeeds iff exactly one of the predicates of the
+-- variants succeeds and the parser of that variant also succeeds.
+-- Using the predicate in this way improves error messages in case
+-- of parse failure.
+--
+-- For example, 'variants' can be used to parse a JSON value
+-- into a disjoint sum ('Either') without needing a tag
+-- representing the 'Left'/'Right' cases.
+-- Instead, the predicates of the variants can be used to disambiguate a
+-- 'Value' by checking the presence of absence of certain JSON object keys.
 variants 
-    :: String -- ^ external parsing error context 
-    -> [Variant a] -- ^ possible variants
-    -> Value -- ^ value to parse
+    :: String -- ^ Error message suffix in case of parse failure.
+    -> [Variant a] -- ^ Possible variants.
+    -> Value -- ^ Value to parse.
     -> Parser a
 variants ctx xs v = withObject ctx run v
   where
