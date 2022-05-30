59ab382c3c337b9d448934bddf17a8343e215b09
Johannes Lund <johannes.lund@iohk.io>
2022-05-30 16:44:55 +0200

    Remove stack-nix materialization
    This fixes this error from running ./nix/regenerate.sh:
    
    ```
    error: attribute 'stack-nix' missing
    
           at /nix/store/flpvyc5zbczv3zmspqylpmpmq2sh3wwq-source/flake.nix:371:24:
    
              370|               inherit (pkgs) sha256map-regenerate checkStackProject;
              371|               inherit (project.stack-nix.passthru) generateMaterialized;
                 |                        ^
              372|               buildToolsGenerateMaterialized = pkgs.haskell-build-tools.regenerateMaterialized;
    ```
    
    Which we need to be able to run to bump dependencies (ran into this when
    bumping cardano-addresses in another, to-be future commit)

--- nix/regenerate.sh
+++ nix/regenerate.sh
@@ -7,11 +7,6 @@ cd $(dirname "$0")/..
 # Regenerate sha256map.nix
 nix run .#sha256map-regenerate < stack.yaml > nix/sha256map.nix
 
-# Regenerate stack-to-nix files in ./.stack-nix
-rm -f nix/materialized/stack-nix/.stack-to-nix.cache #https://github.com/input-output-hk/haskell.nix/issues/57
-nix build .#generateMaterialized
-./result nix/materialized/stack-nix/
-
 # Regenerate materialized haskell-build-tools in ./materialized
 nix build .#buildToolsGenerateMaterialized
 ./result/bin/regenerate-materialized-nix
