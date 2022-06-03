7ca11a49009fc2205898fc5e5f82a1bfc6209666
Pawel Jakubas <jakubas.pawel@gmail.com>
2022-06-03 12:39:53 +0200

    clean fromLedgerAlonzoPParams

--- lib/shelley/src/Cardano/Wallet/Shelley/Compatibility.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley/Compatibility.hs
@@ -91,7 +91,6 @@ module Cardano.Wallet.Shelley.Compatibility
     , fromAlonzoPParams
     , fromLedgerExUnits
     , toLedgerExUnits
-    , fromLedgerAlonzoPParams
     , toAlonzoPParams
     , fromCardanoAddress
     , toSystemStart
@@ -368,7 +367,6 @@ import qualified Cardano.Ledger.Babbage.TxBody as Babbage
 import qualified Cardano.Ledger.BaseTypes as BT
 import qualified Cardano.Ledger.BaseTypes as Ledger
 import qualified Cardano.Ledger.BaseTypes as SL
-import qualified Cardano.Ledger.Coin as Ledger
 import qualified Cardano.Ledger.Core as SL.Core
 import qualified Cardano.Ledger.Credential as SL
 import qualified Cardano.Ledger.Crypto as SL
@@ -885,102 +883,6 @@ txParametersFromPParams maxBundleSize getMaxExecutionUnits pp = W.TxParameters
 -- Copied from cardano-api
 -- To be removed when once again exposed.
 --------------------------------------------------------------------------------
-
-fromShelleyLovelace :: Ledger.Coin -> Cardano.Lovelace
-fromShelleyLovelace (Ledger.Coin c) = Cardano.Lovelace c
-
-fromLedgerNonce :: Ledger.Nonce -> Maybe Cardano.PraosNonce
-fromLedgerNonce Ledger.NeutralNonce = Nothing
-fromLedgerNonce (Ledger.Nonce h) =
-    Just (Cardano.makePraosNonce $ Crypto.hashToBytes h)
-
-
-fromLedgerAlonzoPParams
-    :: Alonzo.PParams ledgerera
-    -> Cardano.ProtocolParameters
-fromLedgerAlonzoPParams
-    Alonzo.PParams {
-      Alonzo._minfeeA
-    , Alonzo._minfeeB
-    , Alonzo._maxBBSize
-    , Alonzo._maxTxSize
-    , Alonzo._maxBHSize
-    , Alonzo._keyDeposit
-    , Alonzo._poolDeposit
-    , Alonzo._eMax
-    , Alonzo._nOpt
-    , Alonzo._a0
-    , Alonzo._rho
-    , Alonzo._tau
-    , Alonzo._d
-    , Alonzo._extraEntropy
-    , Alonzo._protocolVersion
-    , Alonzo._minPoolCost
-    , Alonzo._coinsPerUTxOWord
-    , Alonzo._costmdls
-    , Alonzo._prices
-    , Alonzo._maxTxExUnits
-    , Alonzo._maxBlockExUnits
-    , Alonzo._maxValSize
-    , Alonzo._collateralPercentage
-    , Alonzo._maxCollateralInputs
-    } = Cardano.ProtocolParameters {
-      protocolParamProtocolVersion     = (\(BT.ProtVer a b) -> (a,b))
-                                           _protocolVersion
-    , protocolParamDecentralization    = Just $ SL.unboundRational _d
-    , protocolParamExtraPraosEntropy   = fromLedgerNonce _extraEntropy
-    , protocolParamMaxBlockHeaderSize  = _maxBHSize
-    , protocolParamMaxBlockBodySize    = _maxBBSize
-    , protocolParamMaxTxSize           = _maxTxSize
-    , protocolParamTxFeeFixed          = _minfeeB
-    , protocolParamTxFeePerByte        = _minfeeA
-    , protocolParamMinUTxOValue        = Nothing
-    , protocolParamStakeAddressDeposit = fromShelleyLovelace _keyDeposit
-    , protocolParamStakePoolDeposit    = fromShelleyLovelace _poolDeposit
-    , protocolParamMinPoolCost         = fromShelleyLovelace _minPoolCost
-    , protocolParamPoolRetireMaxEpoch  = _eMax
-    , protocolParamStakePoolTargetNum  = _nOpt
-    , protocolParamPoolPledgeInfluence = SL.unboundRational _a0
-    , protocolParamMonetaryExpansion   = SL.unboundRational _rho
-    , protocolParamTreasuryCut         = SL.unboundRational _tau
-    , protocolParamUTxOCostPerWord     = Just (fromShelleyLovelace _coinsPerUTxOWord)
-    , protocolParamCostModels          = fromAlonzoCostModels $ Alonzo.unCostModels _costmdls
-    , protocolParamPrices              = Just (fromAlonzoPrices _prices)
-    , protocolParamMaxTxExUnits        = Just (fromAlonzoExUnits _maxTxExUnits)
-    , protocolParamMaxBlockExUnits     = Just (fromAlonzoExUnits _maxBlockExUnits)
-    , protocolParamMaxValueSize        = Just _maxValSize
-    , protocolParamCollateralPercent   = Just _collateralPercentage
-    , protocolParamMaxCollateralInputs = Just _maxCollateralInputs
-    }
-  where
-      fromAlonzoPrices :: Alonzo.Prices -> Cardano.ExecutionUnitPrices
-      fromAlonzoPrices Alonzo.Prices{Alonzo.prSteps, Alonzo.prMem} = Cardano.ExecutionUnitPrices
-          { priceExecutionSteps  = Ledger.unboundRational prSteps
-          , priceExecutionMemory = Ledger.unboundRational prMem
-          }
-
-      fromAlonzoExUnits :: Alonzo.ExUnits -> Cardano.ExecutionUnits
-      fromAlonzoExUnits Alonzo.ExUnits{Alonzo.exUnitsSteps, Alonzo.exUnitsMem} = Cardano.ExecutionUnits
-          { executionSteps  = exUnitsSteps
-          , executionMemory = exUnitsMem
-          }
-
-      fromAlonzoScriptLanguage :: Alonzo.Language -> Cardano.AnyPlutusScriptVersion
-      fromAlonzoScriptLanguage Alonzo.PlutusV1 =
-          Cardano.AnyPlutusScriptVersion Cardano.PlutusScriptV1
-      fromAlonzoScriptLanguage Alonzo.PlutusV2 =
-          Cardano.AnyPlutusScriptVersion Cardano.PlutusScriptV2
-
-      fromAlonzoCostModel :: Alonzo.CostModel -> Cardano.CostModel
-      fromAlonzoCostModel m = Cardano.CostModel $ Alonzo.getCostModelParams m
-
-      fromAlonzoCostModels
-          :: Map Alonzo.Language Alonzo.CostModel
-          -> Map Cardano.AnyPlutusScriptVersion Cardano.CostModel
-      fromAlonzoCostModels =
-          Map.fromList
-          . map (bimap fromAlonzoScriptLanguage fromAlonzoCostModel)
-          . Map.toList
 
 toAlonzoPParams
     :: Cardano.ProtocolParameters
