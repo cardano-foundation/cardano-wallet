7dc642065c9fe6c5a5140b40455766ef7f0786b8
Johannes Lund <johannes.lund@iohk.io>
2022-06-11 00:13:06 +0200

    Try to fix nix `error: attribute 'defaultPackage' missing`
    related to emanote. Looking with `nix repl`, `:lf .` it seems the
    attribute was renamed/moved and that this commit should work...
    hopefully...

--- flake.nix
+++ flake.nix
@@ -241,7 +241,7 @@
               });
               docs = pkgs.mkShell {
                 name = "cardano-wallet-docs";
-                nativeBuildInputs = [ emanote.defaultPackage.${system} pkgs.yq ];
+                nativeBuildInputs = [ emanote.packages.${system}.default pkgs.yq ];
                 # allow building the shell so that it can be cached in hydra
                 phases = [ "installPhase" ];
                 installPhase = "echo $nativeBuildInputs > $out";
