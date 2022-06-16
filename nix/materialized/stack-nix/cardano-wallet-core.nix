eb29745049e23becc4934edcff4fd6067aeeb43e
IOHK <devops+stack-project@iohk.io>
2022-06-16 09:38:20 +0000

    Regenerate nix

--- nix/materialized/stack-nix/cardano-wallet-core.nix
+++ nix/materialized/stack-nix/cardano-wallet-core.nix
@@ -194,6 +194,7 @@
           "Cardano/Wallet/DB/Sqlite/Stores"
           "Cardano/Wallet/DB/Sqlite/Types"
           "Cardano/Wallet/DB/WalletState"
+          "Cardano/Wallet/DB/WalletState"
           "Cardano/Wallet/Logging"
           "Cardano/Wallet/Network"
           "Cardano/Wallet/Network/Light"
