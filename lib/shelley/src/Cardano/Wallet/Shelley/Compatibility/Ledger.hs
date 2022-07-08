763a0d3b81bcf570783bbd5866808bd9a509f03f
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-07-08 07:09:15 +0000

    Remove legacy function `computeMinimumAdaQuantity` from `Compatibility.Ledger`.

--- lib/shelley/src/Cardano/Wallet/Shelley/Compatibility/Ledger.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley/Compatibility/Ledger.hs
@@ -14,11 +14,8 @@
 --
 module Cardano.Wallet.Shelley.Compatibility.Ledger
     (
-      -- * Exported ledger functions
-      computeMinimumAdaQuantity
-
       -- * Conversions from wallet types to ledger specification types
-    , toLedgerCoin
+      toLedgerCoin
     , toLedgerTokenBundle
     , toLedgerTokenPolicyId
     , toLedgerTokenName
@@ -38,9 +35,6 @@ module Cardano.Wallet.Shelley.Compatibility.Ledger
       --   types
     , Convert (..)
 
-      -- * Internal functions
-    , computeMinimumAdaQuantityInternal
-
     ) where
 
 import Prelude
@@ -51,8 +45,6 @@ import Cardano.Crypto.Hash
     ( hashFromBytes, hashToBytes )
 import Cardano.Ledger.SafeHash
     ( unsafeMakeSafeHash )
-import Cardano.Wallet.Primitive.Types
-    ( MinimumUTxOValue (..) )
 import Cardano.Wallet.Primitive.Types.Address
     ( Address (..) )
 import Cardano.Wallet.Primitive.Types.Coin
@@ -61,8 +53,6 @@ import Cardano.Wallet.Primitive.Types.Hash
     ( Hash (..) )
 import Cardano.Wallet.Primitive.Types.TokenBundle
     ( TokenBundle (..) )
-import Cardano.Wallet.Primitive.Types.TokenMap
-    ( TokenMap )
 import Cardano.Wallet.Primitive.Types.TokenPolicy
     ( TokenName (..), TokenPolicyId (..) )
 import Cardano.Wallet.Primitive.Types.TokenQuantity
@@ -93,7 +83,6 @@ import Ouroboros.Consensus.Shelley.Eras
 import qualified Cardano.Crypto.Hash.Class as Crypto
 import qualified Cardano.Ledger.Address as Ledger
 import qualified Cardano.Ledger.Alonzo as Alonzo
-import qualified Cardano.Ledger.Alonzo.Rules.Utxo as Alonzo
 import qualified Cardano.Ledger.Alonzo.TxBody as Alonzo
 import qualified Cardano.Ledger.Babbage as Babbage
 import qualified Cardano.Ledger.Babbage.TxBody as Babbage
@@ -101,40 +90,13 @@ import qualified Cardano.Ledger.Crypto as Ledger
 import qualified Cardano.Ledger.Keys as Ledger
 import qualified Cardano.Ledger.Mary.Value as Ledger
 import qualified Cardano.Ledger.Shelley.API as Ledger
-import qualified Cardano.Ledger.ShelleyMA.Rules.Utxo as Ledger
 import qualified Cardano.Ledger.ShelleyMA.Timelocks as MA
 import qualified Cardano.Wallet.Primitive.Types.Coin as Coin
 import qualified Cardano.Wallet.Primitive.Types.TokenBundle as TokenBundle
 import qualified Cardano.Wallet.Primitive.Types.TokenMap as TokenMap
-import qualified Data.ByteString as BS
 import qualified Data.Map.Strict as Map
 import qualified Data.Map.Strict.NonEmptyMap as NonEmptyMap
 import qualified Ouroboros.Network.Block as O
-
---------------------------------------------------------------------------------
--- Public functions
---------------------------------------------------------------------------------
-
--- | Uses the ledger specification to compute the minimum required ada quantity
---   for a token map.
---
-computeMinimumAdaQuantity
-    :: MinimumUTxOValue
-    -- ^ The absolute minimum ada quantity specified by the protocol.
-    -> TokenMap
-    -- ^ The token map to evaluate.
-    -> Coin
-    -- ^ The minimum ada quantity for the given token map.
-computeMinimumAdaQuantity protocolMinimum m =
-    -- Note:
-    --
-    -- We assume here that 'computeMinimumAdaQuantityInternal' has the property
-    -- of being constant w.r.t. to the ada value. Assuming this property holds,
-    -- it is safe to call it with an ada value of 0.
-    --
-    -- See 'prop_computeMinimumAdaQuantity_agnosticToAdaQuantity'.
-    --
-    computeMinimumAdaQuantityInternal protocolMinimum (TokenBundle (Coin 0) m)
 
 --------------------------------------------------------------------------------
 -- Roundtrip conversion between wallet types and ledger specification types
@@ -342,54 +304,3 @@ toWalletScript keyrole = fromLedgerScript
         ActiveUntilSlot $ fromIntegral slot
     fromLedgerScript (MA.RequireTimeStart (O.SlotNo slot)) =
         ActiveFromSlot $ fromIntegral slot
-
---------------------------------------------------------------------------------
--- Internal functions
---------------------------------------------------------------------------------
-
--- | Uses the ledger specification to compute the minimum required ada quantity
---   for a token bundle.
---
--- This function is intended to be constant with respect to:
---
---    - the ada quantity;
---    - the quantities of individual assets.
---
--- See the following properties:
---
---    - 'prop_computeMinimumAdaQuantity_agnosticToAdaQuantity';
---    - 'prop_computeMinimumAdaQuantity_agnosticToAssetQuantities'.
---
--- TODO: [ADP-954] Datum hashes are currently not taken into account.
-computeMinimumAdaQuantityInternal
-    :: MinimumUTxOValue
-    -- ^ The absolute minimum ada quantity specified by the protocol.
-    -> TokenBundle
-    -- ^ The token bundle to evaluate.
-    -> Coin
-    -- ^ The minimum ada quantity for the given token bundle.
-computeMinimumAdaQuantityInternal (MinimumUTxOValue protocolMinimum) bundle =
-    toWalletCoin $
-        Ledger.scaledMinDeposit
-            (toLedgerTokenBundle bundle)
-            (toLedgerCoin protocolMinimum)
-computeMinimumAdaQuantityInternal (MinimumUTxOValueCostPerWord (Coin perWord)) bundle =
-    let
-        outputSize = Alonzo.utxoEntrySize $
-            toAlonzoTxOut (TxOut dummyAddr bundle) Nothing
-    in
-        Coin $ fromIntegral outputSize * perWord
-  where
-    -- We just need an address the ledger can deserialize. It doesn't actually
-    -- use the length of it.
-    --
-    -- This should not change (if Alonzo is already in-use, it would have to be
-    -- changed in a new era).
-    --
-    -- Regardless, the dummy address is a payment / enterprise address -- can't
-    -- get any shorter than that. The integration tests use longer addresses.
-    -- They should break if this were to be wrong.
-    --
-    -- Because the ledger function is pure and not taking a network, passing in
-    -- a mainnet network should be fine regardless of network.
-    dummyAddr = Address $ BS.pack $ 97 : replicate 28 0
--- lib/shelley/test/unit/Cardano/Wallet/Shelley/Compatibility/LedgerSpec.hs
+++ lib/shelley/test/unit/Cardano/Wallet/Shelley/Compatibility/LedgerSpec.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
@@ -10,12 +9,10 @@ module Cardano.Wallet.Shelley.Compatibility.LedgerSpec
 
 import Prelude
 
-import Cardano.Wallet.Primitive.Types
-    ( MinimumUTxOValue (..) )
 import Cardano.Wallet.Primitive.Types.Coin
     ( Coin (..) )
 import Cardano.Wallet.Primitive.Types.TokenBundle
-    ( Flat (..), TokenBundle )
+    ( TokenBundle )
 import Cardano.Wallet.Primitive.Types.TokenBundle.Gen
     ( genTokenBundleSmallRange, shrinkTokenBundleSmallRange )
 import Cardano.Wallet.Primitive.Types.TokenPolicy
@@ -26,50 +23,20 @@ import Cardano.Wallet.Primitive.Types.TokenQuantity
     ( TokenQuantity (..) )
 import Cardano.Wallet.Primitive.Types.TokenQuantity.Gen
     ( genTokenQuantityFullRange, shrinkTokenQuantityFullRange )
-import Cardano.Wallet.Primitive.Types.Tx
-    ( txOutMaxCoin
-    , txOutMaxTokenQuantity
-    , txOutMinCoin
-    , txOutMinTokenQuantity
-    )
 import Cardano.Wallet.Primitive.Types.Tx.Gen
-    ( genTxOutCoin, genTxOutTokenBundle, shrinkTxOutCoin )
+    ( genTxOutCoin, shrinkTxOutCoin )
 import Cardano.Wallet.Shelley.Compatibility.Ledger
-    ( Convert (..), computeMinimumAdaQuantityInternal )
-import Data.Bifunctor
-    ( second )
+    ( Convert (..) )
 import Data.Proxy
     ( Proxy (..) )
 import Data.Typeable
     ( Typeable, typeRep )
-import Data.Word
-    ( Word64 )
-import Fmt
-    ( pretty )
 import Test.Hspec
     ( Spec, describe, it, parallel )
 import Test.Hspec.Core.QuickCheck
     ( modifyMaxSuccess )
 import Test.QuickCheck
-    ( Arbitrary (..)
-    , Blind (..)
-    , Positive (..)
-    , Property
-    , checkCoverage
-    , conjoin
-    , counterexample
-    , cover
-    , genericShrink
-    , oneof
-    , property
-    , withMaxSuccess
-    , (=/=)
-    , (===)
-    )
-
-import qualified Cardano.Wallet.Primitive.Types.Coin as Coin
-import qualified Cardano.Wallet.Primitive.Types.TokenBundle as TokenBundle
-import qualified Data.Set as Set
+    ( Arbitrary (..), property, (===) )
 
 spec :: Spec
 spec = describe "Cardano.Wallet.Shelley.Compatibility.LedgerSpec" $
@@ -84,160 +51,9 @@ spec = describe "Cardano.Wallet.Shelley.Compatibility.LedgerSpec" $
         ledgerRoundtrip $ Proxy @TokenPolicyId
         ledgerRoundtrip $ Proxy @TokenQuantity
 
-    parallel $ describe "Properties for computeMinimumAdaQuantity" $ do
-
-        it "prop_computeMinimumAdaQuantity_forCoin" $
-            property prop_computeMinimumAdaQuantity_forCoin
-        it "prop_computeMinimumAdaQuantity_agnosticToAdaQuantity" $
-            property prop_computeMinimumAdaQuantity_agnosticToAdaQuantity
-        it "prop_computeMinimumAdaQuantity_agnosticToAssetQuantities" $
-            property prop_computeMinimumAdaQuantity_agnosticToAssetQuantities
-
-    parallel $ describe "Unit tests for computeMinimumAdaQuantity" $ do
-
-        it "unit_computeMinimumAdaQuantity_emptyBundle" $
-            property unit_computeMinimumAdaQuantity_emptyBundle
-        it "unit_computeMinimumAdaQuantity_fixedSizeBundle_8" $
-            property unit_computeMinimumAdaQuantity_fixedSizeBundle_8
-        it "unit_computeMinimumAdaQuantity_fixedSizeBundle_64" $
-            property unit_computeMinimumAdaQuantity_fixedSizeBundle_64
-        it "unit_computeMinimumAdaQuantity_fixedSizeBundle_256" $
-            property unit_computeMinimumAdaQuantity_fixedSizeBundle_256
-
---------------------------------------------------------------------------------
--- Properties
---------------------------------------------------------------------------------
-
-prop_computeMinimumAdaQuantity_forCoin
-    :: MinimumUTxOValue
-    -> Coin
-    -> Property
-prop_computeMinimumAdaQuantity_forCoin minParam c =
-    computeMinimumAdaQuantityInternal
-        minParam
-        (TokenBundle.fromCoin c)
-        === expectedMinimumAdaQuantity
-  where
-    expectedMinimumAdaQuantity = case minParam of
-        MinimumUTxOValue c' -> c'
-        MinimumUTxOValueCostPerWord (Coin x) -> Coin $ x * 29
-
-prop_computeMinimumAdaQuantity_agnosticToAdaQuantity
-    :: Blind TokenBundle
-    -> MinimumUTxOValue
-    -> Property
-prop_computeMinimumAdaQuantity_agnosticToAdaQuantity
-    (Blind bundle) minParam =
-        counterexample counterexampleText $ conjoin
-            [ compute bundle === compute bundleWithCoinMinimized
-            , compute bundle === compute bundleWithCoinMaximized
-            , bundleWithCoinMinimized =/= bundleWithCoinMaximized
-            ]
-  where
-    bundleWithCoinMinimized = TokenBundle.setCoin bundle txOutMinCoin
-    bundleWithCoinMaximized = TokenBundle.setCoin bundle txOutMaxCoin
-    compute = computeMinimumAdaQuantityInternal minParam
-    counterexampleText = unlines
-        [ "bundle:"
-        , pretty (Flat bundle)
-        , "bundle minimized:"
-        , pretty (Flat bundleWithCoinMinimized)
-        , "bundle maximized:"
-        , pretty (Flat bundleWithCoinMaximized)
-        ]
-
-prop_computeMinimumAdaQuantity_agnosticToAssetQuantities
-    :: Blind TokenBundle
-    -> MinimumUTxOValue
-    -> Property
-prop_computeMinimumAdaQuantity_agnosticToAssetQuantities
-    (Blind bundle) minVal =
-        checkCoverage $
-        cover 40 (assetCount >= 1)
-            "Token bundle has at least 1 non-ada asset" $
-        cover 20 (assetCount >= 2)
-            "Token bundle has at least 2 non-ada assets" $
-        cover 10 (assetCount >= 4)
-            "Token bundle has at least 4 non-ada assets" $
-        counterexample counterexampleText $ conjoin
-            [ compute bundle === compute bundleMinimized
-            , compute bundle === compute bundleMaximized
-            , assetCount === assetCountMinimized
-            , assetCount === assetCountMaximized
-            , if assetCount == 0
-                then bundleMinimized === bundleMaximized
-                else bundleMinimized =/= bundleMaximized
-            ]
-  where
-    assetCount = Set.size $ TokenBundle.getAssets bundle
-    assetCountMinimized = Set.size $ TokenBundle.getAssets bundleMinimized
-    assetCountMaximized = Set.size $ TokenBundle.getAssets bundleMaximized
-    bundleMinimized = bundle `setAllQuantitiesTo` txOutMinTokenQuantity
-    bundleMaximized = bundle `setAllQuantitiesTo` txOutMaxTokenQuantity
-    compute = computeMinimumAdaQuantityInternal minVal
-    setAllQuantitiesTo = flip (adjustAllQuantities . const)
-    counterexampleText = unlines
-        [ "bundle:"
-        , pretty (Flat bundle)
-        , "bundle minimized:"
-        , pretty (Flat bundleMinimized)
-        , "bundle maximized:"
-        , pretty (Flat bundleMaximized)
-        ]
-
---------------------------------------------------------------------------------
--- Unit tests
---------------------------------------------------------------------------------
-
--- | Creates a test to compute the minimum ada quantity for a token bundle with
---   a fixed number of assets, where the expected result is a constant.
---
--- Policy identifiers, asset names, token quantities are all allowed to vary.
---
-unit_computeMinimumAdaQuantity_fixedSizeBundle
-    :: TokenBundle
-    -- ^ Fixed size bundle
-    -> Coin
-    -- ^ Expected minimum ada quantity
-    -> Property
-unit_computeMinimumAdaQuantity_fixedSizeBundle bundle expectation =
-    withMaxSuccess 100 $
-    computeMinimumAdaQuantityInternal (MinimumUTxOValue protocolMinimum) bundle === expectation
-  where
-    protocolMinimum = Coin 1_000_000
-
-unit_computeMinimumAdaQuantity_emptyBundle :: Property
-unit_computeMinimumAdaQuantity_emptyBundle =
-    unit_computeMinimumAdaQuantity_fixedSizeBundle TokenBundle.empty $
-        Coin 1000000
-
-unit_computeMinimumAdaQuantity_fixedSizeBundle_8
-    :: Blind (FixedSize8 TokenBundle) -> Property
-unit_computeMinimumAdaQuantity_fixedSizeBundle_8 (Blind (FixedSize8 b)) =
-    unit_computeMinimumAdaQuantity_fixedSizeBundle b $
-        Coin 3888885
-
-unit_computeMinimumAdaQuantity_fixedSizeBundle_64
-    :: Blind (FixedSize64 TokenBundle) -> Property
-unit_computeMinimumAdaQuantity_fixedSizeBundle_64 (Blind (FixedSize64 b)) =
-    unit_computeMinimumAdaQuantity_fixedSizeBundle b $
-        Coin 22555533
-
-unit_computeMinimumAdaQuantity_fixedSizeBundle_256
-    :: Blind (FixedSize256 TokenBundle) -> Property
-unit_computeMinimumAdaQuantity_fixedSizeBundle_256 (Blind (FixedSize256 b)) =
-    unit_computeMinimumAdaQuantity_fixedSizeBundle b $
-        Coin 86555469
-
 --------------------------------------------------------------------------------
 -- Utilities
 --------------------------------------------------------------------------------
-
-adjustAllQuantities
-    :: (TokenQuantity -> TokenQuantity) -> TokenBundle -> TokenBundle
-adjustAllQuantities adjust b = uncurry TokenBundle.fromFlatList $ second
-    (fmap (fmap adjust))
-    (TokenBundle.toFlatList b)
 
 ledgerRoundtrip
     :: forall w l. (Arbitrary w, Eq w, Show w, Typeable w, Convert w l)
@@ -253,19 +69,6 @@ ledgerRoundtrip proxy = it title $
         ]
 
 --------------------------------------------------------------------------------
--- Adaptors
---------------------------------------------------------------------------------
-
-newtype FixedSize8 a = FixedSize8 { unFixedSize8 :: a }
-    deriving (Eq, Show)
-
-newtype FixedSize64 a = FixedSize64 { unFixedSize64 :: a }
-    deriving (Eq, Show)
-
-newtype FixedSize256 a = FixedSize256 { unFixedSize256 :: a }
-    deriving (Eq, Show)
-
---------------------------------------------------------------------------------
 -- Arbitraries
 --------------------------------------------------------------------------------
 
@@ -275,30 +78,9 @@ instance Arbitrary Coin where
     arbitrary = genTxOutCoin
     shrink = shrinkTxOutCoin
 
-instance Arbitrary MinimumUTxOValue where
-    arbitrary = oneof
-        [ MinimumUTxOValue . Coin.fromWord64 . (* 1_000_000) <$> genSmallWord
-        , MinimumUTxOValueCostPerWord . Coin.fromWord64 <$> genSmallWord
-        ]
-      where
-      genSmallWord = fromIntegral @Int @Word64 . getPositive <$> arbitrary
-    shrink = genericShrink
-
 instance Arbitrary TokenBundle where
     arbitrary = genTokenBundleSmallRange
     shrink = shrinkTokenBundleSmallRange
-
-instance Arbitrary (FixedSize8 TokenBundle) where
-    arbitrary = FixedSize8 <$> genTxOutTokenBundle 8
-    -- No shrinking
-
-instance Arbitrary (FixedSize64 TokenBundle) where
-    arbitrary = FixedSize64 <$> genTxOutTokenBundle 64
-    -- No shrinking
-
-instance Arbitrary (FixedSize256 TokenBundle) where
-    arbitrary = FixedSize256 <$> genTxOutTokenBundle 256
-    -- No shrinking
 
 instance Arbitrary TokenName where
     arbitrary = genTokenNameLargeRange
