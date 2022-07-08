4f348ca748b232cd0805918fe62b60ba2855b7e5
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-07-08 05:42:41 +0000

    Add golden tests for minimum UTxO values.
    We add golden minimum UTxO values for all Shelley-based eras.

--- lib/shelley/test/unit/Cardano/Wallet/Shelley/MinimumUTxOSpec.hs
+++ lib/shelley/test/unit/Cardano/Wallet/Shelley/MinimumUTxOSpec.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+{- HLINT ignore "Use camelCase" -}
 
 module Cardano.Wallet.Shelley.MinimumUTxOSpec
     ( spec
@@ -9,6 +14,8 @@ module Cardano.Wallet.Shelley.MinimumUTxOSpec
 
 import Prelude
 
+import Cardano.Api
+    ( ShelleyBasedEra (..) )
 import Cardano.Api.Gen
     ( genAddressAny )
 import Cardano.Wallet.Primitive.Types.Address
@@ -31,11 +38,15 @@ import Cardano.Wallet.Primitive.Types.TokenBundle
 import Cardano.Wallet.Primitive.Types.TokenBundle.Gen
     ( shrinkTokenBundle )
 import Cardano.Wallet.Primitive.Types.TokenMap
-    ( TokenMap )
+    ( AssetId (..), TokenMap )
 import Cardano.Wallet.Primitive.Types.TokenMap.Gen
     ( genTokenMap, shrinkTokenMap )
+import Cardano.Wallet.Primitive.Types.TokenPolicy
+    ( TokenName (UnsafeTokenName) )
+import Cardano.Wallet.Primitive.Types.TokenPolicy.Gen
+    ( mkTokenPolicyId )
 import Cardano.Wallet.Primitive.Types.Tx
-    ( TxOut (..) )
+    ( TxOut (..), txOutMaxTokenQuantity, txOutMinTokenQuantity )
 import Cardano.Wallet.Primitive.Types.Tx.Gen
     ( genTxOutTokenBundle )
 import Cardano.Wallet.Shelley.Compatibility
@@ -47,8 +58,16 @@ import Cardano.Wallet.Shelley.MinimumUTxO
     , unsafeLovelaceToWalletCoin
     , unsafeValueToLovelace
     )
+import Control.Monad
+    ( forM_ )
+import Data.Default
+    ( Default (..) )
 import Data.Function
     ( (&) )
+import Data.Generics.Internal.VL.Lens
+    ( view )
+import GHC.Generics
+    ( Generic )
 import Test.Hspec
     ( Spec, describe, it )
 import Test.QuickCheck
@@ -57,6 +76,8 @@ import Test.QuickCheck
     , checkCoverage
     , conjoin
     , cover
+    , elements
+    , frequency
     , property
     , sized
     )
@@ -66,10 +87,19 @@ import Test.QuickCheck.Extra
     ( report, verify )
 import Test.Utils.Laws
     ( testLawsMany )
+import Test.Utils.Pretty
+    ( (====) )
 
 import qualified Cardano.Api.Shelley as Cardano
+import qualified Cardano.Ledger.Alonzo.PParams as Alonzo
+import qualified Cardano.Ledger.Babbage.PParams as Babbage
+import qualified Cardano.Ledger.Coin as Ledger
+import qualified Cardano.Ledger.Shelley.PParams as Shelley
 import qualified Cardano.Wallet.Primitive.Types.TokenBundle as TokenBundle
+import qualified Cardano.Wallet.Primitive.Types.TokenMap as TokenMap
 import qualified Data.ByteString as BS
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 
 spec :: Spec
 spec = do
@@ -80,15 +110,36 @@ spec = do
             ]
 
     describe "computeMinimumCoinForUTxO" $ do
-        it "prop_computeMinimumCoinForUTxO_evaluation" $
-            prop_computeMinimumCoinForUTxO_evaluation
-                & property
-        it "prop_computeMinimumCoinForUTxO_shelleyBasedEra_bounds" $
-            prop_computeMinimumCoinForUTxO_shelleyBasedEra_bounds
-                & property
-        it "prop_computeMinimumCoinForUTxO_shelleyBasedEra_stability" $
-            prop_computeMinimumCoinForUTxO_shelleyBasedEra_stability
-                & property
+
+        describe "Properties" $ do
+
+            it "prop_computeMinimumCoinForUTxO_evaluation" $
+                prop_computeMinimumCoinForUTxO_evaluation
+                    & property
+            it "prop_computeMinimumCoinForUTxO_shelleyBasedEra_bounds" $
+                prop_computeMinimumCoinForUTxO_shelleyBasedEra_bounds
+                    & property
+            it "prop_computeMinimumCoinForUTxO_shelleyBasedEra_stability" $
+                prop_computeMinimumCoinForUTxO_shelleyBasedEra_stability
+                    & property
+
+        describe "Golden Tests" $ do
+
+            goldenTests_computeMinimumCoinForUTxO "Shelley"
+                goldenMinimumUTxO_Shelley
+                goldenMinimumCoins_Shelley
+            goldenTests_computeMinimumCoinForUTxO "Allegra"
+                goldenMinimumUTxO_Allegra
+                goldenMinimumCoins_Allegra
+            goldenTests_computeMinimumCoinForUTxO "Mary"
+                goldenMinimumUTxO_Mary
+                goldenMinimumCoins_Mary
+            goldenTests_computeMinimumCoinForUTxO "Alonzo"
+                goldenMinimumUTxO_Alonzo
+                goldenMinimumCoins_Alonzo
+            goldenTests_computeMinimumCoinForUTxO "Babbage"
+                goldenMinimumUTxO_Babbage
+                goldenMinimumCoins_Babbage
 
 -- Check that it's possible to evaluate 'computeMinimumCoinForUTxO' without
 -- any run-time error.
@@ -236,6 +287,220 @@ prop_computeMinimumCoinForUTxO_shelleyBasedEra_stability
     ourComputeMinCoin :: TokenMap -> Coin
     ourComputeMinCoin =
         computeMinimumCoinForUTxO (minimumUTxOForShelleyBasedEra era pp)
+
+--------------------------------------------------------------------------------
+-- Golden tests
+--------------------------------------------------------------------------------
+
+goldenTests_computeMinimumCoinForUTxO
+    :: String
+    -- ^ The era name.
+    -> MinimumUTxO
+    -- ^ The minimum UTxO function.
+    -> [(TokenMap, Coin)]
+    -- ^ Mappings from 'TokenMap' values to expected minimum 'Coin' values.
+    -> Spec
+goldenTests_computeMinimumCoinForUTxO
+    eraName minimumUTxO expectedMinimumCoins =
+        goldenTests title
+            (uncurry computeMinimumCoinForUTxO)
+            (mkTest <$> expectedMinimumCoins)
+  where
+    mkTest
+        :: (TokenMap, Coin) -> GoldenTestData (MinimumUTxO, TokenMap) Coin
+    mkTest (tokenMap, coinExpected) = GoldenTestData
+        { params = (minimumUTxO, tokenMap)
+        , result = coinExpected
+        }
+    title = unwords
+        ["goldenTests_computeMinimumCoinForUTxO", eraName]
+
+--------------------------------------------------------------------------------
+-- Golden 'MinimumUTxO' values
+--------------------------------------------------------------------------------
+
+goldenMinimumUTxO_Shelley :: MinimumUTxO
+goldenMinimumUTxO_Shelley =
+    minimumUTxOForShelleyBasedEra ShelleyBasedEraShelley
+        -- Value derived from 'mainnet-shelley-genesis.json':
+        def {Shelley._minUTxOValue = Ledger.Coin 1_000_000}
+
+goldenMinimumUTxO_Allegra :: MinimumUTxO
+goldenMinimumUTxO_Allegra =
+    minimumUTxOForShelleyBasedEra ShelleyBasedEraAllegra
+        -- Value derived from 'mainnet-shelley-genesis.json':
+        def {Shelley._minUTxOValue = Ledger.Coin 1_000_000}
+
+goldenMinimumUTxO_Mary :: MinimumUTxO
+goldenMinimumUTxO_Mary =
+    minimumUTxOForShelleyBasedEra ShelleyBasedEraMary
+        -- Value derived from 'mainnet-shelley-genesis.json':
+        def {Shelley._minUTxOValue = Ledger.Coin 1_000_000}
+
+goldenMinimumUTxO_Alonzo :: MinimumUTxO
+goldenMinimumUTxO_Alonzo =
+    minimumUTxOForShelleyBasedEra ShelleyBasedEraAlonzo
+        -- Value derived from 'mainnet-alonzo-genesis.json':
+        def {Alonzo._coinsPerUTxOWord = Ledger.Coin 34_482}
+
+goldenMinimumUTxO_Babbage :: MinimumUTxO
+goldenMinimumUTxO_Babbage =
+    minimumUTxOForShelleyBasedEra ShelleyBasedEraBabbage
+        -- Value derived from 'mainnet-alonzo-genesis.json':
+        def {Babbage._coinsPerUTxOByte = Ledger.Coin 4_310}
+
+--------------------------------------------------------------------------------
+-- Golden minimum 'Coin' values
+--------------------------------------------------------------------------------
+
+goldenMinimumCoins_Shelley :: [(TokenMap, Coin)]
+goldenMinimumCoins_Shelley =
+    [ (goldenTokenMap_0, Coin 1_000_000)
+    , (goldenTokenMap_1, Coin 1_000_000)
+    , (goldenTokenMap_2, Coin 1_000_000)
+    , (goldenTokenMap_3, Coin 1_000_000)
+    , (goldenTokenMap_4, Coin 1_000_000)
+    ]
+
+goldenMinimumCoins_Allegra :: [(TokenMap, Coin)]
+goldenMinimumCoins_Allegra =
+    [ (goldenTokenMap_0, Coin 1_000_000)
+    , (goldenTokenMap_1, Coin 1_000_000)
+    , (goldenTokenMap_2, Coin 1_000_000)
+    , (goldenTokenMap_3, Coin 1_000_000)
+    , (goldenTokenMap_4, Coin 1_000_000)
+    ]
+
+goldenMinimumCoins_Mary :: [(TokenMap, Coin)]
+goldenMinimumCoins_Mary =
+    [ (goldenTokenMap_0, Coin 1_000_000)
+    , (goldenTokenMap_1, Coin 1_444_443)
+    , (goldenTokenMap_2, Coin 1_555_554)
+    , (goldenTokenMap_3, Coin 1_740_739)
+    , (goldenTokenMap_4, Coin 1_999_998)
+    ]
+
+goldenMinimumCoins_Alonzo :: [(TokenMap, Coin)]
+goldenMinimumCoins_Alonzo =
+    [ (goldenTokenMap_0, Coin   999_978)
+    , (goldenTokenMap_1, Coin 1_344_798)
+    , (goldenTokenMap_2, Coin 1_448_244)
+    , (goldenTokenMap_3, Coin 1_620_654)
+    , (goldenTokenMap_4, Coin 1_862_028)
+    ]
+
+goldenMinimumCoins_Babbage :: [(TokenMap, Coin)]
+goldenMinimumCoins_Babbage =
+    [ (goldenTokenMap_0, Coin   995_610)
+    , (goldenTokenMap_1, Coin 1_150_770)
+    , (goldenTokenMap_2, Coin 1_323_170)
+    , (goldenTokenMap_3, Coin 1_323_170)
+    , (goldenTokenMap_4, Coin 2_012_770)
+    ]
+
+--------------------------------------------------------------------------------
+-- Golden 'TokenMap' values
+--------------------------------------------------------------------------------
+
+goldenTokenMaps :: [TokenMap]
+goldenTokenMaps =
+    [ goldenTokenMap_0
+    , goldenTokenMap_1
+    , goldenTokenMap_2
+    , goldenTokenMap_3
+    , goldenTokenMap_4
+    ]
+
+goldenTokenMap_0 :: TokenMap
+goldenTokenMap_0 = TokenMap.empty
+
+goldenTokenMap_1 :: TokenMap
+goldenTokenMap_1 = TokenMap.fromFlatList
+    [ (goldenAssetId_A_1_short, txOutMinTokenQuantity)
+    ]
+
+goldenTokenMap_2 :: TokenMap
+goldenTokenMap_2 = TokenMap.fromFlatList
+    [ (goldenAssetId_A_1_long, txOutMaxTokenQuantity)
+    ]
+
+goldenTokenMap_3 :: TokenMap
+goldenTokenMap_3 = TokenMap.fromFlatList
+    [ (goldenAssetId_A_1_short, txOutMinTokenQuantity)
+    , (goldenAssetId_A_2_short, txOutMinTokenQuantity)
+    , (goldenAssetId_B_1_short, txOutMinTokenQuantity)
+    , (goldenAssetId_B_2_short, txOutMinTokenQuantity)
+    ]
+
+goldenTokenMap_4 :: TokenMap
+goldenTokenMap_4 = TokenMap.fromFlatList
+    [ (goldenAssetId_A_1_long, txOutMaxTokenQuantity)
+    , (goldenAssetId_A_2_long, txOutMaxTokenQuantity)
+    , (goldenAssetId_B_1_long, txOutMaxTokenQuantity)
+    , (goldenAssetId_B_2_long, txOutMaxTokenQuantity)
+    ]
+
+--------------------------------------------------------------------------------
+-- Golden 'AssetId' values
+--------------------------------------------------------------------------------
+
+goldenAssetId_A_1_short :: AssetId
+goldenAssetId_A_1_short = mkAssetId 'A' "1"
+
+goldenAssetId_A_2_short :: AssetId
+goldenAssetId_A_2_short = mkAssetId 'A' "2"
+
+goldenAssetId_B_1_short :: AssetId
+goldenAssetId_B_1_short = mkAssetId 'B' "1"
+
+goldenAssetId_B_2_short :: AssetId
+goldenAssetId_B_2_short = mkAssetId 'B' "2"
+
+goldenAssetId_A_1_long :: AssetId
+goldenAssetId_A_1_long = mkAssetId 'A' (replicate 32 '1')
+
+goldenAssetId_A_2_long :: AssetId
+goldenAssetId_A_2_long = mkAssetId 'A' (replicate 32 '2')
+
+goldenAssetId_B_1_long :: AssetId
+goldenAssetId_B_1_long = mkAssetId 'B' (replicate 32 '1')
+
+goldenAssetId_B_2_long :: AssetId
+goldenAssetId_B_2_long = mkAssetId 'B' (replicate 32 '2')
+
+mkAssetId :: Char -> String -> AssetId
+mkAssetId pid name = AssetId
+    (mkTokenPolicyId pid)
+    (UnsafeTokenName $ T.encodeUtf8 $ T.pack name)
+
+--------------------------------------------------------------------------------
+-- Golden test support
+--------------------------------------------------------------------------------
+
+data GoldenTestData params result = GoldenTestData
+    { params :: params
+    , result :: result
+    }
+    deriving (Eq, Generic, Show)
+
+goldenTests
+    :: (Eq result, Show result)
+    => String
+    -> (params -> result)
+    -> [GoldenTestData params result]
+    -> Spec
+goldenTests title f goldenTestData =
+    describe title $
+    forM_ (zip testNumbers goldenTestData) $
+        \(testNumber :: Int, test) -> do
+            let subtitle = "golden test #" <> show testNumber
+            it subtitle $
+                let resultExpected = view #result test in
+                let resultActual = f (view #params test) in
+                property $ resultExpected ==== resultActual
+  where
+    testNumbers :: [Int]
+    testNumbers = [0 ..]
 
 --------------------------------------------------------------------------------
 -- Utility functions
