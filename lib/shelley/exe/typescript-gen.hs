82928993b99c345121a6ec566b612b217ede8d60
Michal Rus <michal.rus@iohk.io>
2022-07-29 17:31:10 +0200

    Don’t use endpoints taking `n :: NetworkDiscriminant` or `apiPool`

--- lib/shelley/exe/typescript-gen.hs
+++ lib/shelley/exe/typescript-gen.hs
@@ -8,6 +8,9 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 import Prelude
 
@@ -17,40 +20,46 @@ import Cardano.Wallet.Api
 import Servant.TypeScript
 import Cardano.Wallet.Api.Types (ApiStakePool)
 import qualified Cardano.Wallet.Api.Types
+import qualified Cardano.Wallet.Primitive.Types
 import Cardano.Wallet.Primitive.AddressDerivation (NetworkDiscriminant(..))
 import Data.Aeson.TypeScript.Recursive (recursivelyDeriveMissingTypeScriptInstancesFor)
 import Data.Aeson.TypeScript.TH (deriveJSONAndTypeScript, TypeScript)
+import Servant ((:<|>)(..), (:>)(..))
 
-type MainnetApi = Cardano.Wallet.Api.ApiV2 'Mainnet ApiStakePool
+type SmallerApiV2 = "v2" :> SmallerApi
 
-$(recursivelyDeriveMissingTypeScriptInstancesFor ''MainnetApi (deriveJSONAndTypeScript A.defaultOptions))
+-- FIXME: cardano-wallet API without:
+--   • parts referencing (n :: NetworkDiscriminant)
+--   • and apiPool
+--   • and endpoints returning `OctetStream`
+--       So for now, we commented out `PostExternalTransaction` and `SignMetadata`
+--
+--       We would (maybe) like to propose a JSON representation of OctetStream along the lines of:
+--
+--       > {"data":"base64-encoded-string"}
+type SmallerApi =
+         Wallets
+    :<|> WalletKeys
+    :<|> Assets
+    :<|> ByronWallets
+    :<|> ByronAssets
+    :<|> Network
+    :<|> Settings
+    :<|> SMASH
+    :<|> SharedWallets
+    :<|> SharedWalletKeys
 
--- Hmmm… The 'Mainnet parameter is not handled by `recursivelyDeriveMissingTypeScriptInstancesFor`?
-instance TypeScript (Cardano.Wallet.Api.Types.ApiWalletMigrationPostData 'Mainnet "lenient")
-instance TypeScript (Cardano.Wallet.Api.Types.ApiWalletMigrationPostData 'Mainnet "user")
-instance TypeScript (Cardano.Wallet.Api.Types.ApiAddress 'Mainnet)
--- etc. …
--- No instance for (TypeScript (Cardano.Wallet.Api.Types.ApiSelectCoinsData 'Mainnet))
--- etc. … so we’d have to specify all of them by hand…
+-- TODO: pls, help us get the whole `MainnetApi` instead of `SmallerApi` here :pray:
+--type MainnetApi = Cardano.Wallet.Api.ApiV2 'Mainnet ApiStakePool
 
--- Why is this needed???
-instance TypeScript (Proxy 'Mainnet)
+deriving instance TypeScript Cardano.Wallet.Primitive.Types.WalletId
+
+-- ↑ results in:
+-- • No instance for (TypeScript
+--                      (cryptonite-0.27:Crypto.Hash.Types.Digest
+--                         cryptonite-0.27:Crypto.Hash.Blake2b.Blake2b_160))
+
+$(recursivelyDeriveMissingTypeScriptInstancesFor ''SmallerApi (deriveJSONAndTypeScript A.defaultOptions))
 
 main :: IO ()
-main = writeTypeScriptLibrary (Proxy :: Proxy MainnetApi) "tmp"
-
--- But OctetStream (ByteString) cannot be processed, since it’s not JSON:
---
--- > exe/typescript-gen.hs:28:8: error:
--- >     • servant-0.19:Servant.API.ContentTypes.JSON expected in list '[servant-0.19:Servant.API.ContentTypes.OctetStream]
---
--- And `Data.Aeson.TypeScript.TH` is a “library provides a way to
--- generate TypeScript .d.ts files that match your existing Aeson
--- ToJSON instances.”,
--- cf. <https://hackage.haskell.org/package/aeson-typescript-0.4.0.0/docs/Data-Aeson-TypeScript-TH.html>
---
--- So for now, we commented out `PostExternalTransaction` and `SignMetadata`
---
--- We would (maybe) like to propose a JSON representation of OctetStream along the lines of:
---
--- > {"data":"base64-encoded-string"}
+main = writeTypeScriptLibrary (Proxy :: Proxy SmallerApi) "tmp"
