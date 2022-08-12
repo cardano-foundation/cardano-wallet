55f40ca0851ce73e09e87f59a5950e348734b5b9
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-08-12 02:50:39 +0000

    Add function `applyNM`.
    This commit adds the `applyNM` function and property tests.
    
    This function applies a monadic function @n@ times to an initial value,
    returning the final result.
    
    Its behaviour is similar to `applyN`, but applies a monadic function
    instead of a pure function, hence the addition of the `M` suffix:
    
    ```
    applyN  :: forall   a.            Int -> (a ->   a) -> a ->   a
    applyNM :: forall m a. Monad m => Int -> (a -> m a) -> a -> m a
    ```

---
+++ lib/core/test/unit/Control/Monad/UtilSpec.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ViewPatterns #-}
+
+-- |
+-- Copyright: © 2022 IOHK
+-- License: Apache-2.0
+--
+module Control.Monad.UtilSpec
+    ( spec
+    )
+    where
+
+import Prelude
+
+import Control.Monad
+    ( (<=<) )
+import Control.Monad.Identity
+    ( Identity (..) )
+import Control.Monad.Util
+    ( applyNM )
+import Data.Function
+    ( (&) )
+import Data.Function.Utils
+    ( applyN )
+import Test.Hspec
+    ( Spec, describe, it )
+import Test.QuickCheck
+    ( Fun (..), Property, applyFun, conjoin, property, (===) )
+
+spec :: Spec
+spec = describe "Control.Monad.UtilSpec" $ do
+
+    describe "applyNM" $ do
+        it "prop_applyNM_applyN @Int" $
+            prop_applyNM_applyN @Int
+                & property
+        it "prop_applyNM_unit @Identity @Int" $
+            prop_applyNM_unit @Identity @Int
+                & property
+        it "prop_applyNM_unit @Maybe @Int" $
+            prop_applyNM_unit @Maybe @Int
+                & property
+        it "prop_applyNM_unit @[] @Int" $
+            prop_applyNM_unit @[] @Int
+                & property
+
+--------------------------------------------------------------------------------
+-- applyNM
+--------------------------------------------------------------------------------
+
+prop_applyNM_applyN
+    :: (Eq a, Show a) => Int -> Fun a a -> a -> Property
+prop_applyNM_applyN n (applyFun -> f) a =
+    applyNM n (Identity <$> f) a === Identity (applyN n f a)
+
+prop_applyNM_unit
+    :: (Monad m, Eq (m a), Show (m a)) => Fun a (m a) -> a -> Property
+prop_applyNM_unit (applyFun -> f) a = conjoin
+    [ applyNM 0 f a === pure a
+    , applyNM 1 f a === f a
+    , applyNM 2 f a === (f <=< f) a
+    , applyNM 3 f a === (f <=< f <=< f) a
+    , applyNM 4 f a === (f <=< f <=< f <=< f) a
+    ]
