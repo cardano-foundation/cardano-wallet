73bcc85b1a9c894a450396417148cf14ed19a1b6
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-08-24 01:29:35 +0000

    Make `unsafeCoinFromCardanoApiCalculateMinimumUTxOResult` an inner function.
    This function is now only used by `computeMinimumCoinForUTxOCardanoApi`.
    
    We can safely make it an inner function.

--- lib/shelley/src/Cardano/Wallet/Shelley/MinimumUTxO/Internal.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley/MinimumUTxO/Internal.hs
@@ -8,7 +8,6 @@
 --
 module Cardano.Wallet.Shelley.MinimumUTxO.Internal
     ( computeMinimumCoinForUTxOCardanoApi
-    , unsafeCoinFromCardanoApiCalculateMinimumUTxOResult
     ) where
 
 import Prelude
@@ -37,41 +36,37 @@ computeMinimumCoinForUTxOCardanoApi
     -> Coin
 computeMinimumCoinForUTxOCardanoApi
     (MinimumUTxOForShelleyBasedEra era pp) txOut =
-        unsafeCoinFromCardanoApiCalculateMinimumUTxOResult $
+        unsafeCoinFromResult $
             Cardano.calculateMinimumUTxO era
                 (toCardanoTxOut era txOut)
                 (Cardano.fromLedgerPParams era pp)
-
--- | Extracts a 'Coin' value from the result of calling the Cardano API
---   function 'calculateMinimumUTxO'.
---
-unsafeCoinFromCardanoApiCalculateMinimumUTxOResult
-    :: HasCallStack
-    => Either Cardano.MinimumUTxOError Cardano.Value
-    -> Coin
-unsafeCoinFromCardanoApiCalculateMinimumUTxOResult = \case
-    Right value ->
-        -- We assume that the returned value is a non-negative ada quantity
-        -- with no other assets. If this assumption is violated, we have no
-        -- way to continue, and must raise an error:
-        value
-            & unsafeValueToLovelace
-            & unsafeLovelaceToWalletCoin
-    Left e ->
-        -- The 'Cardano.calculateMinimumUTxO' function should only return
-        -- an error if a required protocol parameter is missing.
-        --
-        -- However, given that values of 'MinimumUTxOForShelleyBasedEra'
-        -- can only be constructed by supplying an era-specific protocol
-        -- parameters record, it should be impossible to trigger this
-        -- condition.
-        --
-        -- Any violation of this assumption indicates a programming error.
-        -- If this condition is triggered, we have no way to continue, and
-        -- must raise an error:
-        --
-        error $ unwords
-            [ "unsafeCoinFromCardanoApiCalculateMinimumUTxOResult:"
-            , "unexpected error:"
-            , show e
-            ]
+  where
+    unsafeCoinFromResult
+        :: Either Cardano.MinimumUTxOError Cardano.Value
+        -> Coin
+    unsafeCoinFromResult = \case
+        Right value ->
+            -- We assume that the returned value is a non-negative ada quantity
+            -- with no other assets. If this assumption is violated, we have no
+            -- way to continue, and must raise an error:
+            value
+                & unsafeValueToLovelace
+                & unsafeLovelaceToWalletCoin
+        Left e ->
+            -- The 'Cardano.calculateMinimumUTxO' function should only return
+            -- an error if a required protocol parameter is missing.
+            --
+            -- However, given that values of 'MinimumUTxOForShelleyBasedEra'
+            -- can only be constructed by supplying an era-specific protocol
+            -- parameters record, it should be impossible to trigger this
+            -- condition.
+            --
+            -- Any violation of this assumption indicates a programming error.
+            -- If this condition is triggered, we have no way to continue, and
+            -- must raise an error:
+            --
+            error $ unwords
+                [ "computeMinimumCoinForUTxOCardanoApi:"
+                , "unexpected error:"
+                , show e
+                ]
