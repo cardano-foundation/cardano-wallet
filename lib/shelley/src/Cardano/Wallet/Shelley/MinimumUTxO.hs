7d0415539b2aef504c9274ca599edf979008506a
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-08-24 03:19:13 +0000

    Delete redundant code from public `MinimumUTxO` module.
    These functions are no longer used.

--- lib/shelley/src/Cardano/Wallet/Shelley/MinimumUTxO.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley/MinimumUTxO.hs
@@ -13,10 +13,6 @@ module Cardano.Wallet.Shelley.MinimumUTxO
 
 import Prelude
 
-import Cardano.Ledger.Babbage.Rules.Utxo
-    ( babbageMinUTxOValue )
-import Cardano.Ledger.Serialization
-    ( mkSized )
 import Cardano.Wallet.Primitive.Types.Address
     ( Address (..) )
 import Cardano.Wallet.Primitive.Types.Coin
@@ -29,15 +25,7 @@ import Cardano.Wallet.Primitive.Types.TokenMap
     ( TokenMap )
 import Cardano.Wallet.Primitive.Types.Tx
     ( TxOut (..), txOutMaxCoin )
-import Cardano.Wallet.Shelley.Compatibility
-    ( toCardanoTxOut )
-import Cardano.Wallet.Shelley.Compatibility.Ledger
-    ( toBabbageTxOut, toWalletCoin )
-import Ouroboros.Consensus.Cardano.Block
-    ( StandardBabbage )
 
-import qualified Cardano.Api.Shelley as Cardano
-import qualified Cardano.Ledger.Babbage.PParams as Babbage
 import qualified Cardano.Wallet.Primitive.Types.TokenBundle as TokenBundle
 import qualified Cardano.Wallet.Shelley.MinimumUTxO.Internal as Internal
 
@@ -77,47 +65,3 @@ isBelowMinimumCoinForUTxO minimumUTxO addr tokenBundle =
             TokenBundle.getCoin tokenBundle <
                 Internal.computeMinimumCoinForUTxOCardanoLedger
                     minimumUTxOShelley (TxOut addr tokenBundle)
-
--- | Embeds a 'TokenMap' within a padded 'Cardano.TxOut' value.
---
--- When computing the minimum UTxO quantity for a given 'TokenMap', we do not
--- have access to an address or to an ada quantity.
---
--- However, in order to compute a minimum UTxO quantity through the Cardano
--- API, we must supply a 'TxOut' value with a valid address and ada quantity.
---
--- It's imperative that we do not underestimate minimum UTxO quantities, as
--- this may result in the creation of transactions that are unacceptable to
--- the ledger. In the case of change generation, this would be particularly
--- problematic, as change outputs are generated automatically, and users do
--- not have direct control over the ada quantities generated.
---
--- However, while we cannot underestimate minimum UTxO quantities, we are at
--- liberty to moderately overestimate them.
---
--- Since the minimum UTxO quantity function is monotonically increasing w.r.t.
--- the size of the address and ada quantity, if we supply a 'TxOut' with an
--- address and ada quantity whose serialized lengths are the maximum possible
--- lengths, we can be confident that the resultant value will not be an
--- underestimate.
---
-_embedTokenMapWithinPaddedTxOut
-    :: Cardano.ShelleyBasedEra era
-    -> Address
-    -> TokenMap
-    -> Cardano.TxOut Cardano.CtxTx era
-_embedTokenMapWithinPaddedTxOut era addr m =
-    toCardanoTxOut era $ TxOut addr $ TokenBundle txOutMaxCoin m
-
--- | Uses the ledger to compute a minimum ada quantity for the Babbage era.
---
-_computeLedgerMinimumCoinForBabbage
-    :: Babbage.PParams StandardBabbage
-    -> Address
-    -> TokenBundle
-    -> Coin
-_computeLedgerMinimumCoinForBabbage pp addr tokenBundle =
-    toWalletCoin
-        $ babbageMinUTxOValue pp
-        $ mkSized
-        $ toBabbageTxOut (TxOut addr tokenBundle) Nothing
