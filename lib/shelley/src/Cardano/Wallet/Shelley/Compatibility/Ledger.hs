8450f1dbde9019ada163d45dd0295db35416807b
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-08-24 02:37:48 +0000

    Add era-specific `TxOut` conversion functions to `Compatibility.Ledger`.

--- lib/shelley/src/Cardano/Wallet/Shelley/Compatibility/Ledger.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley/Compatibility/Ledger.hs
@@ -20,8 +20,6 @@ module Cardano.Wallet.Shelley.Compatibility.Ledger
     , toLedgerTokenPolicyId
     , toLedgerTokenName
     , toLedgerTokenQuantity
-    , toAlonzoTxOut
-    , toBabbageTxOut
 
       -- * Conversions from ledger specification types to wallet types
     , toWalletCoin
@@ -34,6 +32,13 @@ module Cardano.Wallet.Shelley.Compatibility.Ledger
       -- * Roundtrip conversion between wallet types and ledger specification
       --   types
     , Convert (..)
+
+      -- * Conversions for transaction outputs
+    , toShelleyTxOut
+    , toAllegraTxOut
+    , toMaryTxOut
+    , toAlonzoTxOut
+    , toBabbageTxOut
 
     ) where
 
@@ -78,7 +83,7 @@ import GHC.Stack
 import Numeric.Natural
     ( Natural )
 import Ouroboros.Consensus.Shelley.Eras
-    ( StandardCrypto )
+    ( StandardAllegra, StandardCrypto, StandardMary, StandardShelley )
 
 import qualified Cardano.Crypto.Hash.Class as Crypto
 import qualified Cardano.Ledger.Address as Ledger
@@ -90,6 +95,7 @@ import qualified Cardano.Ledger.Crypto as Ledger
 import qualified Cardano.Ledger.Keys as Ledger
 import qualified Cardano.Ledger.Mary.Value as Ledger
 import qualified Cardano.Ledger.Shelley.API as Ledger
+import qualified Cardano.Ledger.Shelley.TxBody as Shelley
 import qualified Cardano.Ledger.ShelleyMA.Timelocks as MA
 import qualified Cardano.Wallet.Primitive.Types.Coin as Coin
 import qualified Cardano.Wallet.Primitive.Types.TokenBundle as TokenBundle
@@ -247,6 +253,28 @@ instance Convert Address (Ledger.Addr StandardCrypto) where
             , pretty (Address bytes)
             ]
     toWallet = Address . Ledger.serialiseAddr
+
+--------------------------------------------------------------------------------
+-- Conversions for 'TxOut'
+--------------------------------------------------------------------------------
+
+toShelleyTxOut
+    :: TxOut
+    -> Shelley.TxOut StandardShelley
+toShelleyTxOut (TxOut addr bundle) =
+    Shelley.TxOut (toLedger addr) (toLedger (TokenBundle.coin bundle))
+
+toAllegraTxOut
+    :: TxOut
+    -> Shelley.TxOut StandardAllegra
+toAllegraTxOut (TxOut addr bundle) =
+    Shelley.TxOut (toLedger addr) (toLedger (TokenBundle.coin bundle))
+
+toMaryTxOut
+    :: TxOut
+    -> Shelley.TxOut StandardMary
+toMaryTxOut (TxOut addr bundle) =
+    Shelley.TxOut (toLedger addr) (toLedger bundle)
 
 toAlonzoTxOut
     :: TxOut
