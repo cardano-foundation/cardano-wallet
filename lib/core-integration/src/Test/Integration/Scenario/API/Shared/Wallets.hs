7adcdbe08531e441f6f5c097de449bad87544334
Pawel Jakubas <jakubas.pawel@gmail.com>
2022-09-16 12:54:04 +0200

    add test for active wallet from xprv

--- lib/core-integration/src/Test/Integration/Scenario/API/Shared/Wallets.hs
+++ lib/core-integration/src/Test/Integration/Scenario/API/Shared/Wallets.hs
@@ -572,9 +572,17 @@ spec = describe "SHARED_WALLETS" $ do
             [ expectResponseCode HTTP.status201
             ]
 
-        let walWithSelf = getFromResponse id rPostWithSelf
+        let walWithSelf@(ApiSharedWallet (Right activeWal)) = getFromResponse id rPostWithSelf
 
         getWalletIdFromSharedWallet walWithSelf `shouldBe` getWalletIdFromSharedWallet wal
+
+        rAddrsActive <- request @[ApiAddress n] ctx
+            (Link.listAddresses @'Shared activeWal) Default Empty
+        expectResponseCode HTTP.status200 rAddrsActive
+        let g = fromIntegral $ getAddressPoolGap defaultAddressPoolGap
+        expectListSize g rAddrsActive
+        forM_ [0..(g-1)] $ \addrNum -> do
+            expectListField addrNum (#state . #getApiT) (`shouldBe` Unused) rAddrsActive
 
     it "SHARED_WALLETS_CREATE_06 - Create an active shared wallet from account xpub with self" $ \ctx -> runResourceT $ do
         (_, accXPubTxt):_ <- liftIO $ genXPubs 1
