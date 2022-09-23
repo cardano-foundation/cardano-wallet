114d164f5cd355cd217a149330e211f4850827ae
Johannes Lund <johannes.lund@iohk.io>
2022-09-23 17:38:21 +0200

    Copy `fromShelleyBasedScript` from cardano-api

--- lib/wallet/src/Cardano/Api/Extra.hs
+++ lib/wallet/src/Cardano/Api/Extra.hs
@@ -11,6 +11,7 @@ module Cardano.Api.Extra
     ( withShelleyBasedTx
     , inAnyCardanoEra
     , asAnyShelleyBasedEra
+    , fromShelleyBasedScript
     ) where
 
 import Prelude
@@ -21,9 +22,25 @@ import Cardano.Api
     , InAnyShelleyBasedEra (..)
     , IsCardanoEra (cardanoEra)
     , IsShelleyBasedEra
+    , PlutusScriptVersion (..)
+    , Script (..)
+    , ScriptInEra (..)
+    , ScriptLanguageInEra (..)
     , ShelleyBasedEra (..)
-    , Tx
+    , SimpleScriptVersion (..)
+    , TimeLocksSupported (TimeLocksInSimpleScriptV2)
+    , Tx (..)
     )
+import Cardano.Api.Shelley
+    ( PlutusScript (PlutusScriptSerialised)
+    , ShelleyLedgerEra
+    , fromAllegraTimelock
+    , fromShelleyMultiSig
+    )
+
+import qualified Cardano.Ledger.Alonzo.Language as Alonzo
+import qualified Cardano.Ledger.Alonzo.Scripts as Alonzo
+import qualified Cardano.Ledger.Core as Ledger
 
 -- | Apply an era-parameterized function to an existentially-wrapped
 -- tx.
@@ -56,3 +73,48 @@ asAnyShelleyBasedEra = \case
         Just $ InAnyShelleyBasedEra ShelleyBasedEraAlonzo a
     InAnyCardanoEra BabbageEra a ->
         Just $ InAnyShelleyBasedEra ShelleyBasedEraBabbage a
+
+-- Copied from cardano-api because it is not exported.
+fromShelleyBasedScript  :: ShelleyBasedEra era
+                        -> Ledger.Script (ShelleyLedgerEra era)
+                        -> ScriptInEra era
+fromShelleyBasedScript era script =
+  case era of
+    ShelleyBasedEraShelley ->
+      ScriptInEra SimpleScriptV1InShelley $
+      SimpleScript SimpleScriptV1 $
+      fromShelleyMultiSig script
+    ShelleyBasedEraAllegra ->
+      ScriptInEra SimpleScriptV2InAllegra $
+      SimpleScript SimpleScriptV2 $
+      fromAllegraTimelock TimeLocksInSimpleScriptV2 script
+    ShelleyBasedEraMary ->
+      ScriptInEra SimpleScriptV2InMary $
+      SimpleScript SimpleScriptV2 $
+      fromAllegraTimelock TimeLocksInSimpleScriptV2 script
+    ShelleyBasedEraAlonzo ->
+      case script of
+        Alonzo.TimelockScript s ->
+          ScriptInEra SimpleScriptV2InAlonzo $
+          SimpleScript SimpleScriptV2 $
+          fromAllegraTimelock TimeLocksInSimpleScriptV2 s
+        Alonzo.PlutusScript Alonzo.PlutusV1 s ->
+          ScriptInEra PlutusScriptV1InAlonzo $
+          PlutusScript PlutusScriptV1 $
+          PlutusScriptSerialised s
+        Alonzo.PlutusScript Alonzo.PlutusV2 _ ->
+          error "fromShelleyBasedScript: PlutusV2 not supported in Alonzo era"
+    ShelleyBasedEraBabbage ->
+      case script of
+        Alonzo.TimelockScript s ->
+          ScriptInEra SimpleScriptV2InBabbage $
+          SimpleScript SimpleScriptV2 $
+          fromAllegraTimelock TimeLocksInSimpleScriptV2 s
+        Alonzo.PlutusScript Alonzo.PlutusV1 s ->
+          ScriptInEra PlutusScriptV1InBabbage $
+          PlutusScript PlutusScriptV1 $
+          PlutusScriptSerialised s
+        Alonzo.PlutusScript Alonzo.PlutusV2 s ->
+          ScriptInEra PlutusScriptV2InBabbage $
+          PlutusScript PlutusScriptV2 $
+          PlutusScriptSerialised s
