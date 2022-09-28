0c92e8faa67e1f130ef7e936d393b5fbe04e2a50
paolo veronelli <paolo.veronelli@gmail.com>
2022-09-28 21:11:28 +0200

    add mapping from `Cardano.Tx`

---
+++ lib/wallet/src/Cardano/Wallet/Read/Tx/Cardano.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+
+-- |
+-- Copyright: © 2020-2022 IOHK
+-- License: Apache-2.0
+--
+
+module Cardano.Wallet.Read.Tx.Cardano
+    ( fromCardanoApiTx
+    ) where
+
+import Prelude
+
+import Cardano.Wallet.Read.Eras
+    ( EraValue, allegra, alonzo, babbage, byron, inject, mary, shelley )
+import Cardano.Wallet.Read.Tx
+    ( Tx (..) )
+
+import qualified Cardano.Api as Cardano
+import qualified Cardano.Api.Byron as Cardano
+import qualified Cardano.Api.Shelley as Cardano
+
+fromCardanoApiTx :: Cardano.Tx era -> EraValue Tx
+fromCardanoApiTx = \case
+    Cardano.ShelleyTx era tx -> case era of
+        Cardano.ShelleyBasedEraShelley -> inject shelley $ Tx tx
+        Cardano.ShelleyBasedEraAllegra -> inject allegra $ Tx tx
+        Cardano.ShelleyBasedEraMary -> inject mary $ Tx tx
+        Cardano.ShelleyBasedEraAlonzo -> inject alonzo $ Tx tx
+        Cardano.ShelleyBasedEraBabbage -> inject babbage $ Tx tx
+    Cardano.ByronTx tx -> inject byron $ Tx $ () <$ tx
