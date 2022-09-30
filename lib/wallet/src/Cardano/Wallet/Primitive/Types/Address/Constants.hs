08a73f1116e6698ab9e079fda8aa0bd8972e98f5
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-09-30 01:05:04 +0000

    Remove now-unused constant `minLengthAddress`.

--- lib/wallet/src/Cardano/Wallet/Primitive/Types/Address/Constants.hs
+++ lib/wallet/src/Cardano/Wallet/Primitive/Types/Address/Constants.hs
@@ -10,7 +10,6 @@
 --
 module Cardano.Wallet.Primitive.Types.Address.Constants
     ( maxLengthAddress
-    , minLengthAddress
     ) where
 
 import Prelude
@@ -49,19 +48,3 @@ maxLengthAddress = L.maximumBy (compare `on` (BS.length . unAddress))
     , maxLengthAddressFor $ Proxy @ShelleyKey
     , maxLengthAddressFor $ Proxy @SharedKey
     ]
-
--- | A dummy 'Address' of the shortest length that the wallet can generate.
---
--- Please note that this address should:
---
---  - never be used for anything besides its length and validity properties.
---  - never be used as a payment target within a real transaction.
---
-minLengthAddress :: Address
-minLengthAddress = minLengthAddressShelley
-  where
-    minLengthAddressShelley =
-        Address $ BS.singleton enterpriseAddressHeaderByte <> payload
-      where
-        enterpriseAddressHeaderByte = 0b01100000
-        payload = BS.replicate 28 0
