76c99cdb288b4d3481edcc1d63387319d6b72f68
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-10-26 01:29:48 +0000

    Make it possible to specify a sample size for JSON golden tests.

--- lib/test-utils/src/Test/Utils/Roundtrip.hs
+++ lib/test-utils/src/Test/Utils/Roundtrip.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Test.Utils.Roundtrip
@@ -11,10 +12,14 @@ import Data.Aeson
     ( FromJSON (..), ToJSON (..) )
 import Data.Char
     ( isAlphaNum )
+import Data.Maybe
+    ( fromMaybe )
 import Data.Proxy
     ( Proxy (..) )
 import Data.Typeable
     ( Typeable, splitTyConApp, tyConName, typeRep )
+import System.Environment
+    ( lookupEnv )
 import Test.Aeson.GenericSpecs
     ( GoldenDirectoryOption (CustomDirectoryName)
     , Settings
@@ -38,6 +43,8 @@ import Test.Hspec
     ( Spec, it, runIO, shouldBe )
 import Test.QuickCheck
     ( Arbitrary (..), property )
+import Text.Read
+    ( readMaybe )
 import Web.HttpApiData
     ( FromHttpApiData (..), ToHttpApiData (..) )
 
@@ -61,8 +68,24 @@ jsonRoundtripAndGolden
 jsonRoundtripAndGolden dir proxy = do
     roundtripSpecs proxy
     typeNameInfo <- runIO mkCompatibleTypeNameInfo
-    goldenSpecsWithNotePlain settings typeNameInfo Nothing
+    sampleSize <- runIO getSampleSize
+    goldenSpecsWithNotePlain (settings {sampleSize}) typeNameInfo Nothing
   where
+    -- When generating new JSON golden files for some type, it is occasionally
+    -- useful to generate a different number of samples to the default number.
+    --
+    -- Specifying the environment variable "GOLDEN_SAMPLE_SIZE=<n>" together
+    -- with "CREATE_MISSING_GOLDEN=TRUE" will cause the newly-created golden
+    -- file to have 'n' samples.
+    --
+    getSampleSize :: IO Int
+    getSampleSize =
+        fromMaybe sampleSizeDefault . (readMaybe =<<) <$>
+        lookupEnv sampleSizeVariable
+      where
+        sampleSizeDefault = 10
+        sampleSizeVariable = "GOLDEN_SAMPLE_SIZE"
+
     -- NOTE
     -- We use a custom 'TypeNameInfo' instead of the default one provided by
     -- @hspec-golden-aeson@ because the defaults generates names that are
@@ -83,7 +106,6 @@ jsonRoundtripAndGolden dir proxy = do
     settings = defaultSettings
         { goldenDirectoryOption = CustomDirectoryName dir
         , useModuleNameAsSubDirectory = False
-        , sampleSize = 10
         -- Note that we fail the test if the random seed does not produce the
         -- same values as those within the golden file. It's important that
         -- we do fail, because otherwise we may inadvertently fail to cover
