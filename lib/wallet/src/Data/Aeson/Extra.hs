b141f693520947603702fc557038da25a7f3bdc7
Jonathan Knowles <jonathan.knowles@iohk.io>
2022-10-27 06:35:11 +0000

    Add function `objectUnion` to `Data.Aeson.Extra`.

--- lib/wallet/src/Data/Aeson/Extra.hs
+++ lib/wallet/src/Data/Aeson/Extra.hs
@@ -10,20 +10,30 @@
 -- the 'aeson' package.
 --
 module Data.Aeson.Extra
-    ( parseBoundedIntegral
+    ( objectUnion
+    , parseBoundedIntegral
     , aesonFromText
     ) where
 
 import Prelude
 
 import Data.Aeson
-    ( Value (Number), withText )
+    ( Value (Number, Object), withText )
 import Data.Aeson.Types
     ( Parser )
 import Data.Text.Class
     ( FromText (fromText) )
 
 import qualified Data.Scientific as Scientific
+
+-- | Performs the union of two JSON 'Object' values.
+--
+-- If either of the given values is not an 'Object', then this function will
+-- return 'Nothing'.
+--
+objectUnion :: Value -> Value -> Maybe Value
+objectUnion (Object a) (Object b) = Just $ Object $ a <> b
+objectUnion _ _ = Nothing
 
 parseBoundedIntegral
     :: forall a. (Bounded a, Integral a) => String -> Value -> Parser a
