e7a0b03262f9b75ee127fb44b90e2ff2a7d80fe9
paolo veronelli <paolo.veronelli@gmail.com>
2022-11-22 13:43:51 +0100

    Add API operations for the new submissions store

---
+++ lib/wallet/src/Cardano/Wallet/Submissions/Operations.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTSyntax #-}
+
+{- |
+Copyright: © 2022 IOHK
+License: Apache-2.0
+
+Define the high level operations admitted on the 'Submissions' store.
+
+This operations are intended to leave the state of the store valid as to
+invariants found in the specifications.
+
+-}
+module Cardano.Wallet.Submissions.Operations
+    ( applyOperations
+    , Operation (..)
+    ) where
+
+import Prelude
+
+import Cardano.Wallet.Submissions.Primitives
+    ( Primitive (MoveFinality, MoveTip), applyPrimitive )
+import Data.Foldable
+    ( Foldable (..) )
+
+import qualified Cardano.Wallet.Submissions.Primitives as DP
+import Cardano.Wallet.Submissions.Submissions
+    ( Submissions )
+import Cardano.Wallet.Submissions.TxStatus
+    ( HasTxId (..) )
+
+-- High Level, invariant respectful operations over the 'Submissions' store.
+data Operation slot tx where
+    -- | Insert tx new transaction in the local submission store.
+    AddSubmission :: slot -> tx -> Operation slot tx
+    -- | Move transactions in the in-ledger state, removing them from
+    -- in-submission.
+    RollForward
+      :: slot -- ^ New tip.
+      -> [(slot, tx)] -- ^ Transactions that were found in the ledder.
+      -> Operation slot tx
+    -- | Move transactions from the in-ledger state to in-submission state,
+    -- when their acceptance slot falls after the new tip.
+    RollBack
+      :: slot -- ^ new tip
+      -> Operation slot tx
+    -- | Remove transactions that cannot be rolled back in the ledger
+    -- and transaction that cannot make it to the ledger due to expiration
+    -- and max rollback time.
+    Prune :: slot -> Operation slot tx
+    -- | Remove a transaction from the tracked set.
+    Forget :: tx -> Operation slot tx
+    deriving (Show)
+
+
+-- | Apply a high level operation to the submission store.
+applyOperations
+    :: (Ord slot, Ord (TxId tx), HasTxId tx)
+    => Operation slot tx
+    -> Submissions slot tx
+    -> Submissions slot tx
+applyOperations (AddSubmission expiring tx)
+    = applyPrimitive (DP.AddSubmission expiring tx)
+applyOperations (RollForward newtip txs) = \x ->
+    applyPrimitive (MoveTip newtip)
+        . foldl'
+            ( \x' (s, tx) ->
+                    applyPrimitive (DP.MoveToLedger s tx) x'
+            )
+            x
+        $ txs
+applyOperations (RollBack t) = applyPrimitive (MoveTip t)
+applyOperations (Prune t) = applyPrimitive (MoveFinality t)
+applyOperations (Forget tx) = applyPrimitive (DP.Forget tx)
---
+++ lib/wallet/src/Cardano/Wallet/Submissions/Properties/Operations.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Cardano.Wallet.Submissions.Properties.Operations
+    ( properties
+    ) where
+
+import Prelude
+
+import Cardano.Wallet.Submissions.Operations
+    ( Operation (..) )
+import Cardano.Wallet.Submissions.Properties.Common
+    ( Step (Step), forAllIn, that, verify )
+import Cardano.Wallet.Submissions.Properties.Primitives
+    ( txIds )
+import Cardano.Wallet.Submissions.Submissions
+    ( Submissions, finality, tip, transactions )
+import Cardano.Wallet.Submissions.TxStatus
+    ( HasTxId (TxId), TxStatus (Expired, InLedger, InSubmission), status )
+import Data.Function
+    ( (&) )
+import Test.QuickCheck
+    ( Property, counterexample, property, (.&.) )
+
+status' :: Ord (TxId tx) => TxId tx -> Submissions slot tx -> TxStatus slot tx
+status' x = status x . transactions
+
+-- | As described in the specification:
+-- For every transaction in the data structure,
+-- we can define a 'slotObservation'@ :: Slot@.
+-- The comparison to 'finality' and 'tip'
+-- will partition the transaction statuses.
+properties
+    :: (Ord (TxId tx), Ord slot, Show (TxId tx))
+    => Step Operation slot tx -> Property
+properties (Step _ xs' _)
+    = counterexample "submissions invariants" $ verify $ do
+        that "finality precedes tip"
+            $ property $ finality xs' <= tip xs'
+        that "transactions are observed partitioned between finality and tip"
+            $ let
+                t = tip xs'
+                f = finality xs'
+                included w =  w > f .&. w <= t
+            in forAllIn (txIds xs') $ \x -> case status' x xs' of
+                InSubmission expiring _ ->
+                    (expiring > t)
+                    & counterexample "any in-submission transaction\
+                        \ should be after the tip"
+                InLedger _ acceptance _ ->
+                    included acceptance
+                    & counterexample "any in ledger transaction should be\
+                        \ included between finality and tip"
+                Expired expiring _ ->
+                    included expiring
+                    & counterexample "any expired transaction should be\
+                        \ included between finality and tip"
+                _ -> property True
