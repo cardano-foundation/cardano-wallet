7ebf0afc85ae9d2ad679b276362d64e7e1729cb3
Pawel Jakubas <pawel.jakubas@iohk.io>
2022-12-03 15:33:53 +0100

    stylish-haskell fix

--- prototypes/light-mode-test/src/Light/ReadBlocks.hs
+++ prototypes/light-mode-test/src/Light/ReadBlocks.hs
@@ -136,12 +136,12 @@ lightSync follower = do
                 fromBlock <$> (BF.getBlock . Left $ height - stabilityWindow)
     nextBlockFrom prev@(At height _) = do
         b <- hasBeenRolledBack prev
-       (if b then
-            (do target <- BF.getBlock . Left $ height - stabilityWindow
-                old <- rollBackward follower $ fromBlock target
-                nextBlockFrom old)
-        else
-            fromBlock <$> BF.getLatestBlock)
+        (if b then
+             (do target <- BF.getBlock . Left $ height - stabilityWindow
+                 old <- rollBackward follower $ fromBlock target
+                 nextBlockFrom old)
+         else
+             fromBlock <$> BF.getLatestBlock)
     stabilityWindow = 1_000 :: Integer
 
 -- | Test whether a given 'ChainPoint' has become invalid due
