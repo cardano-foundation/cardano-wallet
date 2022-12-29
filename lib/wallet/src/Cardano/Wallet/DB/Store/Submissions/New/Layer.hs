ad5f3a0e23106b348e10508a5cb738f914bf292d
paolo veronelli <paolo.veronelli@gmail.com>
2022-12-29 11:36:47 +0100

    Implement `putLocalTxSubmission_`

--- lib/wallet/src/Cardano/Wallet/DB/Store/Submissions/New/Layer.hs
+++ lib/wallet/src/Cardano/Wallet/DB/Store/Submissions/New/Layer.hs
@@ -12,8 +12,12 @@ module Cardano.Wallet.DB.Store.Submissions.New.Layer
 
 import Prelude
 
+import Cardano.Wallet
+    ( ErrNoSuchWallet (..) )
 import Cardano.Wallet.DB
-    ( DBPendingTxs (..) )
+    ( DBPendingTxs (..)
+    , ErrPutLocalTxSubmission (ErrPutLocalTxSubmissionNoSuchWallet)
+    )
 import Cardano.Wallet.DB.Sqlite.Types
     ( TxId (..) )
 import Cardano.Wallet.DB.Store.Submissions.New.Operations
@@ -25,6 +29,8 @@ import Cardano.Wallet.Primitive.Types
     ( WalletId )
 import Cardano.Wallet.Primitive.Types.Tx
     ( LocalTxSubmissionStatus (LocalTxSubmissionStatus), SealedTx )
+import Cardano.Wallet.Submissions.Operations
+    ( Operation (..) )
 import Cardano.Wallet.Submissions.Submissions
     ( TxStatusMeta (..), transactionsL )
 import Cardano.Wallet.Submissions.TxStatus
@@ -34,9 +40,9 @@ import Control.Lens
 import Control.Monad.Except
     ( ExceptT (ExceptT) )
 import Data.DBVar
-    ( DBVar, readDBVar )
+    ( DBVar, modifyDBMaybe, readDBVar )
 import Data.DeltaMap
-    ( DeltaMap )
+    ( DeltaMap (..) )
 import Database.Persist.Sql
     ( SqlPersistT )
 
@@ -47,8 +53,20 @@ mkDbPendingTxs
     :: DBVar (SqlPersistT IO) (DeltaMap WalletId DeltaTxSubmissions)
     -> DBPendingTxs (SqlPersistT IO)
 mkDbPendingTxs dbvar = DBPendingTxs
-    { putLocalTxSubmission_ = \wid txid tx sl ->
-        error "putLocalTxSubmissions not implemented"
+    { putLocalTxSubmission_ = \wid txid tx sl -> do
+        let errNoSuchWallet = ErrPutLocalTxSubmissionNoSuchWallet $
+                ErrNoSuchWallet wid
+        ExceptT $ modifyDBMaybe dbvar $ \ws -> do
+            case Map.lookup wid ws of
+                Nothing -> (Nothing, Left errNoSuchWallet)
+                Just _  ->
+                    let
+                        delta = Just
+                            $ Adjust wid
+                            $ AddSubmission sl (TxId txid, tx)
+                            $ error "pls pass meta to putLocalTxSubmission!"
+                    in  (delta, Right ())
+
     , readLocalTxSubmissionPending_ = \wid -> do
             v <- readDBVar dbvar
             pure $ case Map.lookup wid v of
