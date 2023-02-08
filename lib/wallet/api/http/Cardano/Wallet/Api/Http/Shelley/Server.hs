bbc59fda472d4fb75480a68bb8f46760e8c0f403
Yura Lazarev <Unisay@users.noreply.github.com>
2023-02-08 20:30:53 +0100

    selectCoinsForQuit uses balanceTransaction

--- lib/wallet/api/http/Cardano/Wallet/Api/Http/Shelley/Server.hs
+++ lib/wallet/api/http/Cardano/Wallet/Api/Http/Shelley/Server.hs
@@ -580,7 +580,14 @@ import Data.List.NonEmpty
 import Data.Map.Strict
     ( Map )
 import Data.Maybe
-    ( catMaybes, fromJust, fromMaybe, isJust, isNothing, mapMaybe )
+    ( catMaybes
+    , fromJust
+    , fromMaybe
+    , isJust
+    , isNothing
+    , mapMaybe
+    , maybeToList
+    )
 import Data.Proxy
     ( Proxy (..) )
 import Data.Quantity
@@ -1841,53 +1848,45 @@ selectCoinsForJoin ctx knownPools getPoolStatus pid wid = do
 selectCoinsForQuit
     :: forall s n k.
         ( s ~ SeqState n k
-        , DelegationAddress n k 'CredFromKeyK
+        , AddressBookIso s
         , Seq.SupportsDiscovery n k
+        , BoundedAddressLength k
+        , DelegationAddress n k 'CredFromKeyK
         , Typeable s
         , Typeable k
-        , BoundedAddressLength k
         )
     => ApiLayer (SeqState n k) k 'CredFromKeyK
     -> ApiT WalletId
-    -> Handler (Api.ApiCoinSelection n)
-selectCoinsForQuit ctx@ApiLayer{..} (ApiT wid) =
-    withWorkerCtx ctx wid liftE liftE $ \wrk -> do
-        let db = wrk ^. typed @(DBLayer IO s k)
-        era <- liftIO $ NW.currentNodeEra netLayer
-        pp <- liftIO $ NW.currentProtocolParameters netLayer
-        wdrl <-
-            liftIO $ W.shelleyOnlyMkSelfWithdrawal
-                @_ @_ @_ @_ @n netLayer txLayer era db wid
-        action <- liftIO $ WD.quitStakePoolDelegationAction db wid wdrl
-
-        let txCtx = defaultTransactionCtx
-                { txDelegationAction = Just action
-                , txWithdrawal = wdrl
-                }
-
+    -> Handler (ApiCoinSelection n)
+selectCoinsForQuit ctx@ApiLayer{..} (ApiT walletId) = do
+    era <- liftIO $ NW.currentNodeEra netLayer
+    recentEra@(AnyRecentEra (_ :: WriteTx.RecentEra e)) <- guardIsRecentEra era
+    withWorkerCtx ctx walletId liftE liftE $ \workerCtx -> liftIO $ do
+        let db = workerCtx ^. typed @(DBLayer IO s k)
+            ti = timeInterpreter netLayer
+        pp <- NW.currentProtocolParameters netLayer
+        withdrawal <- W.shelleyOnlyMkSelfWithdrawal @_ @_ @_ @_ @n
+            netLayer txLayer era db walletId
+        action <- WD.quitStakePoolDelegationAction db walletId withdrawal
         let genChange = W.defaultChangeAddressGen (delegationAddress @n)
-        let transform s sel =
-                W.assignChangeAddresses genChange sel s
-                & uncurry (W.selectionToUnsignedTx (txWithdrawal txCtx))
-        (utxoAvailable, wallet, pendingTxs) <-
-            liftHandler $ W.readWalletUTxOIndex @_ @_ @k wrk wid
-        let selectAssetsParams = W.SelectAssetsParams
-                { outputs = []
-                , pendingTxs
-                , randomSeed = Nothing
-                , txContext = txCtx
-                , utxoAvailableForInputs = UTxOSelection.fromIndex utxoAvailable
-                , utxoAvailableForCollateral = UTxOIndex.toMap utxoAvailable
-                , wallet
-                , selectionStrategy = SelectionStrategyOptimal
+        W.CoinSelection{..} <- W.selectCoinsForTransaction @s @k @n @e
+            recentEra db txLayer ti walletId genChange pp
+            defaultTransactionCtx
+                { txDelegationAction = Just action
+                , txWithdrawal = withdrawal
                 }
-        utx <-
-            liftHandler $ W.selectAssets @_ @_ @s @k @'CredFromKeyK
-                wrk era pp selectAssetsParams transform
-        (_, _, path) <- liftHandler
-            $ W.shelleyOnlyReadRewardAccount @s @k @n db wid
-        let refund = W.stakeKeyDeposit pp
-        pure $ mkApiCoinSelection [] [refund] (Just (action, path)) Nothing utx
+        pure ApiCoinSelection
+            { inputs = mkApiCoinSelectionInput <$> inputs
+            , outputs = mkApiCoinSelectionOutput <$> outputs
+            , change = mkApiCoinSelectionChange <$> change
+            , collateral = mkApiCoinSelectionCollateral <$> collateral
+            , certificates = uncurry mkApiCoinSelectionCerts <$>
+                delegationAction
+            , withdrawals = mkApiCoinSelectionWithdrawal <$> withdrawals
+            , depositsTaken = maybeToList $ mkApiCoin <$> deposit
+            , depositsReturned = maybeToList $ mkApiCoin <$> refund
+            , metadata = Nothing
+            }
 
 {-------------------------------------------------------------------------------
                                      Assets
@@ -4223,7 +4222,7 @@ mkApiCoinSelection deps refunds mcerts metadata unsignedTx =
             <$> unsignedTx ^. #unsignedCollateral
         , withdrawals = mkApiCoinSelectionWithdrawal
             <$> unsignedTx ^. #unsignedWithdrawals
-        , certificates = uncurry mkCertificates
+        , certificates = uncurry mkApiCoinSelectionCerts
             <$> mcerts
         , depositsTaken = mkApiCoin
             <$> deps
@@ -4232,74 +4231,79 @@ mkApiCoinSelection deps refunds mcerts metadata unsignedTx =
         , metadata = ApiBytesT. serialiseToCBOR
             <$> metadata
         }
+
+mkApiCoinSelectionCerts
+    :: DelegationAction
+    -> NonEmpty DerivationIndex
+    -> NonEmpty Api.ApiCertificate
+mkApiCoinSelectionCerts action ixs =
+    case action of
+        Join pid -> pure $ Api.JoinPool apiStakePath (ApiT pid)
+        Quit -> pure $ Api.QuitPool apiStakePath
+        JoinRegisteringKey pid -> NE.fromList
+            [ Api.RegisterRewardAccount apiStakePath
+            , Api.JoinPool apiStakePath (ApiT pid)
+            ]
   where
-    mkCertificates
-        :: DelegationAction
-        -> NonEmpty DerivationIndex
-        -> NonEmpty Api.ApiCertificate
-    mkCertificates action ixs =
-        case action of
-            Join pid -> pure $ Api.JoinPool apiStakePath (ApiT pid)
-            Quit -> pure $ Api.QuitPool apiStakePath
-            JoinRegisteringKey pid -> NE.fromList
-                [ Api.RegisterRewardAccount apiStakePath
-                , Api.JoinPool apiStakePath (ApiT pid)
-                ]
-      where
-        apiStakePath = ApiT <$> ixs
+    apiStakePath = ApiT <$> ixs
 
-    mkApiCoinSelectionInput :: input -> ApiWalletInput n
-    mkApiCoinSelectionInput
-        (TxIn txid index, TxOut addr (TokenBundle amount assets), path) =
-        ApiWalletInput
-            { id = ApiT txid
-            , index = index
-            , address = (ApiT addr, Proxy @n)
-            , amount = Coin.toQuantity amount
-            , assets = ApiT assets
-            , derivationPath = ApiT <$> path
-            }
+mkApiCoinSelectionInput
+    :: forall n
+     . (TxIn, TxOut, NonEmpty DerivationIndex)
+     -> ApiWalletInput n
+mkApiCoinSelectionInput
+    (TxIn txid index, TxOut addr (TokenBundle amount assets), path) =
+    ApiWalletInput
+        { id = ApiT txid
+        , index = index
+        , address = (ApiT addr, Proxy @n)
+        , amount = Coin.toQuantity amount
+        , assets = ApiT assets
+        , derivationPath = ApiT <$> path
+        }
 
-    mkApiCoinSelectionOutput :: output -> ApiCoinSelectionOutput n
-    mkApiCoinSelectionOutput (TxOut addr (TokenBundle amount assets)) =
-        ApiCoinSelectionOutput (ApiT addr, Proxy @n)
-        (Coin.toQuantity amount)
-        (ApiT assets)
+mkApiCoinSelectionOutput :: forall n. TxOut -> ApiCoinSelectionOutput n
+mkApiCoinSelectionOutput (TxOut addr (TokenBundle amount assets)) =
+    ApiCoinSelectionOutput (ApiT addr, Proxy @n)
+    (Coin.toQuantity amount)
+    (ApiT assets)
 
-    mkApiCoinSelectionChange :: change -> ApiCoinSelectionChange n
-    mkApiCoinSelectionChange txChange =
-        ApiCoinSelectionChange
-            { address =
-                (ApiT $ view #address txChange, Proxy @n)
-            , amount =
-                Coin.toQuantity $ view #amount txChange
-            , assets =
-                ApiT $ view #assets txChange
-            , derivationPath =
-                ApiT <$> view #derivationPath txChange
-            }
+mkApiCoinSelectionChange
+    :: forall n
+     . TxChange (NonEmpty DerivationIndex)
+    -> ApiCoinSelectionChange n
+mkApiCoinSelectionChange txChange =
+    ApiCoinSelectionChange
+        { address = (ApiT $ view #address txChange, Proxy @n)
+        , amount = Coin.toQuantity $ view #amount txChange
+        , assets = ApiT $ view #assets txChange
+        , derivationPath = ApiT <$> view #derivationPath txChange
+        }
 
-    mkApiCoinSelectionCollateral :: input -> ApiCoinSelectionCollateral n
-    mkApiCoinSelectionCollateral
-        (TxIn txid index, TxOut addr (TokenBundle amount _), path) =
-        ApiCoinSelectionCollateral
-            { id = ApiT txid
-            , index = index
-            , address = (ApiT addr, Proxy @n)
-            , amount = Coin.toQuantity amount
-            , derivationPath = ApiT <$> path
-            }
+mkApiCoinSelectionCollateral
+    :: forall n
+     . (TxIn, TxOut, NonEmpty DerivationIndex)
+    -> ApiCoinSelectionCollateral n
+mkApiCoinSelectionCollateral
+    (TxIn txid index, TxOut addr (TokenBundle amount _), path) =
+    ApiCoinSelectionCollateral
+        { id = ApiT txid
+        , index = index
+        , address = (ApiT addr, Proxy @n)
+        , amount = Coin.toQuantity amount
+        , derivationPath = ApiT <$> path
+        }
 
-    mkApiCoinSelectionWithdrawal :: withdrawal -> ApiCoinSelectionWithdrawal n
-    mkApiCoinSelectionWithdrawal (rewardAcct, wdrl, path) =
-        ApiCoinSelectionWithdrawal
-            { stakeAddress =
-                (ApiT rewardAcct, Proxy @n)
-            , amount =
-                Coin.toQuantity wdrl
-            , derivationPath =
-                ApiT <$> path
-            }
+mkApiCoinSelectionWithdrawal
+    :: forall n
+     . (RewardAccount, Coin, NonEmpty DerivationIndex)
+    -> ApiCoinSelectionWithdrawal n
+mkApiCoinSelectionWithdrawal (rewardAcct, wdrl, path) =
+    ApiCoinSelectionWithdrawal
+        { stakeAddress = (ApiT rewardAcct, Proxy @n)
+        , amount = Coin.toQuantity wdrl
+        , derivationPath = ApiT <$> path
+        }
 
 data MkApiTransactionParams = MkApiTransactionParams
     { txId :: Hash "Tx"
@@ -4456,9 +4460,7 @@ toAddressAmount
 toAddressAmount (TxOut addr (TokenBundle.TokenBundle coin assets)) =
     AddressAmount (ApiT addr, Proxy @n) (mkApiCoin coin) (ApiT assets)
 
-mkApiCoin
-    :: Coin
-    -> Quantity "lovelace" Natural
+mkApiCoin :: Coin -> Quantity "lovelace" Natural
 mkApiCoin (Coin c) = Quantity $ fromIntegral c
 
 mkApiFee :: Maybe Coin -> [Coin] -> FeeEstimation -> ApiFee
