aa5e1b60730727b64a3c92d6bc24e30af6849868
Pawel Jakubas <pawel.jakubas@iohk.io>
2023-02-24 17:00:10 +0100

    enhance submitSharedTransaction

--- lib/wallet/integration/src/Test/Integration/Scenario/API/Shared/Transactions.hs
+++ lib/wallet/integration/src/Test/Integration/Scenario/API/Shared/Transactions.hs
@@ -1747,6 +1747,7 @@ spec = describe "SHARED_TRANSACTIONS" $ do
         rDecodedTx4 <- request @(ApiDecodedTransaction n) ctx
             (Link.decodeTransaction @'Shared party2) Default decodePayload2
 
+        -- there is now 1 witness for staking and 1 witness for payment from party1
         let witsExp2 =
                 [ expectField (#witnessCount . #scripts)
                       (`shouldContain` [delegationScript])
@@ -1759,6 +1760,23 @@ spec = describe "SHARED_TRANSACTIONS" $ do
 
         verify rDecodedTx3 (decodedExpectations ++ certExpectation1 ++ witsExp2)
         verify rDecodedTx4 (decodedExpectations ++ certExpectation2 ++ witsExp2)
+
+        --missing party2' witness for script staking
+        submittedTx1 <- submitSharedTxWithWid ctx party1 signedTx1
+        verify submittedTx1
+            [ expectResponseCode HTTP.status403
+            , expectErrorMessage (errMsg403MissingWitsInTransaction 3 2)
+            ]
+
+        let (ApiSerialisedTransaction apiTx2 _) = signedTx1
+        signedTx2 <-
+            signSharedTx ctx party2 apiTx2
+                [ expectResponseCode HTTP.status202 ]
+
+        submittedTx2 <- submitSharedTxWithWid ctx party1 signedTx2
+        verify submittedTx2
+            [ expectResponseCode HTTP.status202
+            ]
   where
      listSharedTransactions ctx w mStart mEnd mOrder = do
          let path = Link.listTransactions' @'Shared w
