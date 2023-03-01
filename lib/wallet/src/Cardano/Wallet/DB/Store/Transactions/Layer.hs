a7d884bd28fc92f85fe3769e73eefe99dee33942
Heinrich Apfelmus <heinrich.apfelmus@iohk.io>
2023-03-01 17:20:39 +0100

    Add `GetTxOut` query to `TxSet` queries

--- lib/wallet/src/Cardano/Wallet/DB/Store/Transactions/Layer.hs
+++ lib/wallet/src/Cardano/Wallet/DB/Store/Transactions/Layer.hs
@@ -21,24 +21,36 @@ import Prelude
 import Cardano.Wallet.DB.Sqlite.Types
     ( TxId )
 import Cardano.Wallet.DB.Store.Transactions.Model
-    ( DeltaTxSet, TxRelation )
+    ( DeltaTxSet, TxRelation, fromTxCollateralOut, fromTxOut )
+import Control.Applicative
+    ( (<|>) )
+import Data.Word
+    ( Word32 )
 import Database.Persist.Sql
     ( SqlPersistT )
 
 import Cardano.Wallet.DB.Store.QueryStore
     ( QueryStore (..) )
 import qualified Cardano.Wallet.DB.Store.Transactions.Store as TxSet
+import qualified Cardano.Wallet.Primitive.Types.Tx.TxOut as W
 
 {-----------------------------------------------------------------------------
     DB for 'TxSet'
 ------------------------------------------------------------------------------}
 data QueryTxSet b where
     GetByTxId :: TxId -> QueryTxSet (Maybe TxRelation)
+    GetTxOut :: (TxId, Word32) -> QueryTxSet (Maybe W.TxOut)
 
 -- | Implementation of a 'QueryStore' for 'TxSet'.
 mkQueryStoreTxSet :: QueryStore (SqlPersistT IO) QueryTxSet DeltaTxSet
 mkQueryStoreTxSet = QueryStore
     { queryS = \case
         GetByTxId txid -> TxSet.selectTx txid
+        GetTxOut key -> do
+            mout <- TxSet.selectTxOut key
+            mcollateralOut <- TxSet.selectTxCollateralOut key
+            pure $
+                    (fromTxOut <$> mout)
+                <|> (fromTxCollateralOut <$> mcollateralOut)
     , store = TxSet.mkStoreTransactions
     }
