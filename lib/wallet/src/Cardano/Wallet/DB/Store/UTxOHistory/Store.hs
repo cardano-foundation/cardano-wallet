df236d3037c52ab61e3f909d3e8ae805520526c2
paolino <paolo.veronelli@gmail.com>
2023-03-13 16:07:37 +0000

    Implement load of DeltaUTxOStore.

--- lib/wallet/src/Cardano/Wallet/DB/Store/UTxOHistory/Store.hs
+++ lib/wallet/src/Cardano/Wallet/DB/Store/UTxOHistory/Store.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Cardano.Wallet.DB.Store.UTxOHistory.Store
@@ -6,16 +8,39 @@ module Cardano.Wallet.DB.Store.UTxOHistory.Store
 
 import Prelude
 
+import Cardano.Wallet.DB.Sqlite.Schema
+    ( DeltaUTxOSlots (..), DeltaUTxOValue (..), EntityField (..) )
+import Cardano.Wallet.DB.Sqlite.Types
+    ( getTxId )
 import Cardano.Wallet.DB.Store.UTxOHistory.Model
-    ( DeltaUTxOHistory (..), UTxOHistory)
+    ( DeltaUTxOHistory (..), Spent (..) )
+import Cardano.Wallet.DB.Store.UTxOHistory.Model.Internal
+    ( UTxOHistory (..) )
+import Cardano.Wallet.DB.Store.UTxOHistory.TxOutCBOR
+    ( deserializeTxOut )
 import Cardano.Wallet.Primitive.Types
     ( WalletId )
+import Cardano.Wallet.Primitive.Types.Tx.TxIn
+    ( TxIn (TxIn) )
+import Cardano.Wallet.Primitive.Types.UTxO
+    ( UTxO (..) )
+import Control.Lens
+    ( lazy, view, (<&>) )
+import Data.Either.Extra
+    ( eitherToMaybe, maybeToEither )
+import Data.Foldable
+    ( foldl' )
 import Data.Store
     ( UpdateStore, mkUpdateStore )
 import Database.Persist.Sql
-    ( SqlPersistT )
+    ( SqlPersistT, entityVal, selectFirst, selectList, (==.) )
 import GHC.Exception
-    ( SomeException )
+    ( Exception, SomeException )
+import GHC.Exception.Type
+    ( toException )
+
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
 
 -- | Create a 'Store' for 'UTxOHistory' using the given 'WalletId' as a key.
 mkStoreUTxOHistory
@@ -26,8 +51,99 @@ mkStoreUTxOHistory wid = mkUpdateStore
     (write wid)
     (update wid)
 
+-- | Issues with UTxOHistory operations.
+data UTxOHistoryError
+    = UTxOHistoryNotFound WalletId
+    | UTxOHistoryNotDeserializationError WalletId
+    deriving (Show, Eq, Exception)
+
 load :: WalletId -> SqlPersistT IO (Either SomeException UTxOHistory)
-load _wid = undefined
+load wid = do
+    slots :: Maybe DeltaUTxOSlots <-
+        fmap entityVal
+            <$> selectFirst [DeltaUTxOSlotsWallet ==. wid] []
+    case slots of
+        Nothing -> pure $ Left $ toException $ UTxOHistoryNotFound wid
+        Just DeltaUTxOSlots {deltaUTxOSlotsTip, deltaUTxOSlotsFinality} -> do
+            xs <-
+                fmap entityVal
+                    <$> selectList [DeltaUTxOValueWalletId ==. wid] []
+            pure
+                $ maybeToEither
+                    (toException $ UTxOHistoryNotDeserializationError wid)
+                $ foldl'
+                    patchByRow
+                    ( Just $
+                        UTxOHistory
+                            mempty
+                            mempty
+                            mempty
+                            mempty
+                            mempty
+                            deltaUTxOSlotsTip
+                            deltaUTxOSlotsFinality
+                            mempty
+                    )
+                    xs
+
+patchByRow :: Maybe UTxOHistory -> DeltaUTxOValue -> Maybe UTxOHistory
+patchByRow Nothing _ = Nothing
+patchByRow
+    ( Just
+            UTxOHistory
+                { history
+                , creationSlots
+                , creationTxIns
+                , spentSlots
+                , spentTxIns
+                , finality
+                , tip
+                , boot
+                }
+        )
+    DeltaUTxOValue
+        { deltaUTxOValueTxInTx
+        , deltaUTxOValueTxInIx
+        , deltaUTxOValueTxOut
+        , deltaUTxOValueCreation
+        , deltaUTxOValueSpent
+        , deltaUTxOValueBoot
+        } =
+        decodeTxOut deltaUTxOValueTxOut <&> \txOut ->
+            UTxOHistory
+                { history =
+                    onNotBoot
+                        history
+                        (<> (UTxO $ Map.singleton txIn txOut))
+                , creationSlots =
+                    onNotBoot creationSlots $
+                        Map.insertWith (<>) deltaUTxOValueCreation txInSingleton
+                , creationTxIns =
+                    onNotBoot creationTxIns $
+                        Map.insert txIn deltaUTxOValueCreation
+                , spentSlots = case deltaUTxOValueSpent of
+                    Unspent -> spentSlots
+                    Spent slot ->
+                        onNotBoot spentSlots $
+                            Map.insertWith (<>) slot txInSingleton
+                , spentTxIns = case deltaUTxOValueSpent of
+                    Unspent -> spentTxIns
+                    Spent slot ->
+                        onNotBoot spentTxIns $
+                            Map.insert txIn slot
+                , tip
+                , finality
+                , boot =
+                    boot
+                        <> if deltaUTxOValueBoot
+                            then UTxO $ Map.singleton txIn txOut
+                            else mempty
+                }
+      where
+        txIn = TxIn (getTxId deltaUTxOValueTxInTx) deltaUTxOValueTxInIx
+        txInSingleton = Set.singleton txIn
+        decodeTxOut = eitherToMaybe . deserializeTxOut . view lazy
+        onNotBoot y f = if deltaUTxOValueBoot then y else f y
 
 write :: WalletId -> UTxOHistory -> SqlPersistT IO ()
 write _wid = undefined
