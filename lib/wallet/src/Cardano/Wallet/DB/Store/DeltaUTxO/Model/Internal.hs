f4cee93b323573fc367833a8b497b95174be5dba
paolino <paolo.veronelli@gmail.com>
2023-03-15 13:51:48 +0000

    Add UTxOHistory datatype as internal module.

---
+++ lib/wallet/src/Cardano/Wallet/DB/Store/DeltaUTxO/Model/Internal.hs
@@ -0,0 +1,45 @@
+module Cardano.Wallet.DB.Store.DeltaUTxO.Model.Internal
+    ( UTxOHistory (..)
+    , Pruned (..)
+    )
+where
+
+import Prelude
+
+import Cardano.Wallet.Primitive.Types
+    ( Slot, SlotNo )
+import Cardano.Wallet.Primitive.Types.Tx.TxIn
+    ( TxIn )
+import Cardano.Wallet.Primitive.Types.UTxO
+    ( UTxO )
+import Data.Map.Strict
+    ( Map )
+import Data.Set
+    ( Set )
+
+-- | The finality of the UTxO history.
+data Pruned = PrunedUpTo SlotNo | NotPruned
+    deriving (Show, Eq)
+
+-- | UTxO history. Abstract history of the UTxO. We keep track of the creation
+-- and spending of slot of each TxIn. This allows us to rollback to a given slot
+-- and prune the history to a given slot.
+data UTxOHistory = UTxOHistory
+    { history :: UTxO
+    -- ^ All UTxO , spent and unspent.
+    , creationSlots :: Map Slot (Set TxIn)
+    -- ^ Reverse map of the `creationSlots` map
+    , creationTxIns :: Map TxIn Slot
+    -- ^ All TxIn, indexed by creation slot.
+    , spentSlots :: Map SlotNo (Set TxIn)
+    -- ^ All spent TxIn, indexed by spent slot.
+    , spentTxIns :: Map TxIn SlotNo
+    -- ^ Reverse map of the `spentSlots` map.
+    , tip :: Slot
+    -- ^ Current tip slot.
+    , finality :: Pruned
+    -- ^ Finality slot.
+    , boot :: UTxO
+    -- ^ UTxO created at genesis.
+    }
+    deriving (Show, Eq)
