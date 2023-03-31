efb05382f32d9bdbbaf2f830376ddc2003a23cb8
paolino <paolo.veronelli@gmail.com>
2023-03-31 15:34:42 +0000

    Remove wallet id index from meta store

--- lib/wallet/src/Cardano/Wallet/DB/Store/Wallets/Model.hs
+++ lib/wallet/src/Cardano/Wallet/DB/Store/Wallets/Model.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE TypeFamilies #-}
 
 {- |
@@ -15,36 +14,16 @@ in a collection of wallets.
 module Cardano.Wallet.DB.Store.Wallets.Model
     ( DeltaTxWalletsHistory (..)
     , TxWalletsHistory
-    , walletsLinkedTransactions
-    , transactionsToDeleteOnRollback
-    , transactionsToDeleteOnRemoveWallet
-    , inAnyWallet
-
-    -- * Testing
-    , garbageCollectEmptyWallets
-    , garbageCollectTxWalletsHistory
     ) where
 
 import Prelude
 
-import Cardano.Wallet.DB.Sqlite.Types
-    ( TxId (..) )
 import Cardano.Wallet.DB.Store.Meta.Model
-    ( TxMetaHistory (..), WalletsMeta, mkTxMetaHistory )
+    ( TxMetaHistory, mkTxMetaHistory )
 import Cardano.Wallet.DB.Store.Transactions.Model
     ( TxSet (..), mkTxSet )
 import Data.Delta
     ( Delta (..) )
-import Data.DeltaMap
-    ( DeltaMap (Adjust, Insert) )
-import Data.Foldable
-    ( toList )
-import Data.Generics.Internal.VL
-    ( view )
-import Data.Map.Strict
-    ( Map )
-import Data.Set
-    ( Set )
 import Fmt
     ( Buildable, build )
 
@@ -52,106 +31,30 @@ import qualified Cardano.Wallet.DB.Store.Meta.Model as TxMetaStore
 import qualified Cardano.Wallet.DB.Store.Transactions.Model as TxStore
 import qualified Cardano.Wallet.Primitive.Types as W
 import qualified Cardano.Wallet.Primitive.Types.Tx as WT
-import qualified Data.Map.Strict as Map
-import qualified Data.Set as Set
 
 data DeltaTxWalletsHistory
     = ExpandTxWalletsHistory W.WalletId [(WT.Tx, WT.TxMeta)]
-    | RollbackTxWalletsHistory W.WalletId W.SlotNo
+    | RollbackTxWalletsHistory W.SlotNo
         -- ^ Roll back a single wallet
-    | RemoveWallet W.WalletId
     deriving ( Show, Eq )
 
 instance Buildable DeltaTxWalletsHistory where
     build = build . show
 
 type TxWalletsHistory =
-    (TxSet, WalletsMeta)
+    (TxSet, TxMetaHistory)
 
 instance Delta DeltaTxWalletsHistory where
     type Base DeltaTxWalletsHistory = TxWalletsHistory
     apply (ExpandTxWalletsHistory wid cs) (txh,mtxmh) =
         ( apply (TxStore.Append $ mkTxSet $ fst <$> cs) txh
-        , flip apply mtxmh $ case Map.lookup wid mtxmh of
-              Nothing -> Insert wid (mkTxMetaHistory wid cs)
-              Just _ ->
-                  Adjust wid
-                  $ TxMetaStore.Expand
-                  $ mkTxMetaHistory wid cs)
-    apply (RollbackTxWalletsHistory wid slot) (x, mtxmh) =
+        , apply (TxMetaStore.Expand $ mkTxMetaHistory wid cs) mtxmh
+        )
+    apply (RollbackTxWalletsHistory slot) (txset, mtxmh) =
         -- Roll back all wallets to a given slot (number)
         -- and garbage collect transactions that no longer
         -- have a 'TxMeta' associated with them.
-        garbageCollectTxWalletsHistory
-            (x, garbageCollectEmptyWallets $ apply (Adjust wid change) mtxmh)
-      where
-        change
-            = TxMetaStore.Rollback slot
-    apply (RemoveWallet wid) (x , mtxmh) =
-        garbageCollectTxWalletsHistory (x, Map.delete wid mtxmh)
-
--- | Garbage collect all transactions that are no longer referenced
--- by any 'TxMeta'.
-garbageCollectTxWalletsHistory :: TxWalletsHistory -> TxWalletsHistory
-garbageCollectTxWalletsHistory (TxSet txh, mtxmh) = (TxSet (gc txh), mtxmh)
-  where
-    gc :: Map TxId x -> Map TxId x
-    gc x = Map.restrictKeys x $ walletsLinkedTransactions mtxmh
-
--- | List of transactions that are to be deleted from the 'TxSet'
--- when rolling back the collection of wallets.
---
--- These are precisely those transactions that have been rolled
--- back, but are not referenced from any other wallet.
-transactionsToDeleteOnRollback
-    :: W.WalletId -> W.SlotNo -> WalletsMeta -> Set TxId
-transactionsToDeleteOnRollback wid slot wmetas =
-    case Map.lookup wid wmetas of
-        Nothing -> Set.empty
-        Just metas ->
-            keepNotInOtherWallets wid wmetas
-            . snd
-            $ TxMetaStore.rollbackTxMetaHistory slot metas
-
--- | List of transactions that are to be deleted from the 'TxSet'
--- when deleting a wallet.
-transactionsToDeleteOnRemoveWallet
-    :: W.WalletId -> WalletsMeta -> Set TxId
-transactionsToDeleteOnRemoveWallet wid wmetas =
-    case Map.lookup wid wmetas of
-        Nothing -> Set.empty
-        Just metas ->
-            keepNotInOtherWallets wid wmetas
-            $ Map.keysSet (TxMetaStore.relations metas)
-
--- | Filter a list of transactions — keep only those transactions
--- that are not in another wallet.
-keepNotInOtherWallets
-    :: W.WalletId -> WalletsMeta -> Set TxId -> Set TxId
-keepNotInOtherWallets thisWallet allWallets = Set.filter keep
-  where
-    otherWallets = Map.delete thisWallet allWallets
-    keep txid = not $ inAnyWallet txid otherWallets
-
--- necessary because database will not distinguish between
--- a missing wallet in the map
--- and a wallet that has no meta-transactions
-garbageCollectEmptyWallets :: Map k TxMetaHistory
-    -> Map k TxMetaHistory
-garbageCollectEmptyWallets = Map.filter (not . null . view #relations)
-
-linkedTransactions :: TxMetaHistory -> Set TxId
-linkedTransactions (TxMetaHistory m) = Map.keysSet m
-
-walletsLinkedTransactions
-    :: Map W.WalletId TxMetaHistory -> Set TxId
-walletsLinkedTransactions = Set.unions . toList . fmap linkedTransactions
-
--- | Is a transaction present in any wallet?
-inAnyWallet
-    :: TxId
-    -> Map W.WalletId TxMetaHistory
-    -> Bool
-inAnyWallet txid = any inAnyTxMetaHistory
-  where
-    inAnyTxMetaHistory (TxMetaHistory m) = Map.member txid m
+        let ( metas', toBeDeletedTxSet)
+                = TxMetaStore.rollbackTxMetaHistory slot mtxmh
+            txSet' = apply (TxStore.DeleteTxs toBeDeletedTxSet) txset
+        in (txSet',  metas')
