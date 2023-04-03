4a25f58f24d3a1d538a1073f357b468bae2a6888
paolino <paolo.veronelli@gmail.com>
2023-04-03 10:36:29 +0000

    Add QueryStore for TxMeta.

---
+++ lib/wallet/src/Cardano/Wallet/DB/Store/Meta/Layer.hs
@@ -0,0 +1,136 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Cardano.Wallet.DB.Store.Meta.Layer
+    ( QueryTxMeta (..)
+    , mkQueryStoreTxMeta
+    ) where
+
+import Prelude
+
+import Cardano.Slotting.Slot
+    ( SlotNo )
+import Cardano.Wallet.DB.Sqlite.Schema
+    ( EntityField (..), TxMeta (..) )
+import Cardano.Wallet.DB.Sqlite.Types
+    ( TxId (..) )
+import Cardano.Wallet.DB.Store.Meta.Model
+    ( DeltaTxMetaHistory, TxMetaHistory (..) )
+import Cardano.Wallet.DB.Store.Meta.Store
+    ( mkStoreMetaTransactions )
+import Cardano.Wallet.DB.Store.QueryStore
+    ( Query (..), QueryStore (..) )
+import Cardano.Wallet.Primitive.Types
+    ( Range (..), SortOrder (..) )
+import Data.Foldable
+    ( toList )
+import Data.List
+    ( sortOn )
+import Data.Maybe
+    ( catMaybes )
+import Data.Ord
+    ( Down (..) )
+import Data.Set
+    ( Set )
+import Database.Persist.Sql
+    ( Entity (entityVal)
+    , Filter
+    , PersistQueryRead (selectFirst)
+    , SelectOpt (..)
+    , SqlPersistT
+    , selectList
+    , (<=.)
+    , (==.)
+    , (>.)
+    , (>=.)
+    )
+import GHC.Natural
+    ( Natural )
+
+import qualified Cardano.Wallet.DB.Sqlite.Schema as DB
+import qualified Cardano.Wallet.Primitive.Types as W
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+
+{-----------------------------------------------------------------------------
+    DB for 'TxMeta'
+------------------------------------------------------------------------------}
+
+-- | A query to the 'TxMeta' database.
+data QueryTxMeta b where
+    GetSome
+        :: Range SlotNo
+        -> Maybe Natural
+        -> SortOrder
+        -> QueryTxMeta [DB.TxMeta]
+    GetAfterSlot :: SlotNo -> QueryTxMeta (Set TxId)
+    GetOne :: TxId -> QueryTxMeta (Maybe DB.TxMeta)
+
+filterMetas :: Bool -> Range SlotNo -> [Filter TxMeta]
+filterMetas _ (Range Nothing Nothing) = []
+filterMetas True (Range (Just low) Nothing) = [TxMetaSlot >=. low]
+filterMetas False (Range (Just low) Nothing) = [TxMetaSlot >. low]
+filterMetas _ (Range  Nothing (Just high)) = [TxMetaSlot <=. high]
+filterMetas b (Range low high) = filterMetas b (Range Nothing high)
+    <> filterMetas b (Range low Nothing)
+
+limitMetas :: Maybe Natural -> [SelectOpt record]
+limitMetas Nothing = []
+limitMetas (Just l) = [LimitTo $ fromIntegral l]
+
+orderMetas :: SortOrder -> [SelectOpt TxMeta]
+orderMetas Ascending = [Asc TxMetaSlot, Asc TxMetaTxId]
+orderMetas Descending = [Desc TxMetaSlot, Asc TxMetaTxId]
+
+-- | A 'QueryStore' for 'TxMeta'.
+mkQueryStoreTxMeta :: QueryStore (SqlPersistT IO) QueryTxMeta DeltaTxMetaHistory
+mkQueryStoreTxMeta =
+    QueryStore
+        { queryS = \case
+            GetSome range limit order ->
+                fmap entityVal
+                    <$> selectList @DB.TxMeta
+                        (filterMetas True range)
+                        (limitMetas limit <> orderMetas order)
+            GetOne txId ->
+                fmap entityVal
+                    <$> selectFirst @_ @_ @DB.TxMeta
+                        [TxMetaTxId ==. txId]
+                        []
+            GetAfterSlot slot ->
+                foldMap ((Set.singleton . txMetaTxId) . entityVal)
+                    <$> selectList @DB.TxMeta
+                        (filterMetas False $ Range (Just slot) Nothing)
+                        []
+        , store = mkStoreMetaTransactions
+        }
+
+instance Query QueryTxMeta where
+    type World QueryTxMeta = TxMetaHistory
+    query :: QueryTxMeta b -> World QueryTxMeta -> b
+    query q (TxMetaHistory allTransactions) = case q of
+        GetSome range mlimit order ->
+            let whichMeta DB.TxMeta{..} =
+                    and
+                        $ catMaybes
+                            [ (txMetaSlot >=) <$> W.inclusiveLowerBound range
+                            , (txMetaSlot <=) <$> W.inclusiveUpperBound range
+                            ]
+                reorder = case order of
+                    Ascending -> sortOn ((,) <$> txMetaSlot <*> txMetaTxId)
+                    Descending -> sortOn ((,) <$> Down . txMetaSlot <*> txMetaTxId)
+            in  maybe id (take . fromIntegral) mlimit
+                    $ reorder
+                    $ filter whichMeta
+                    $ toList allTransactions
+        GetOne ti -> Map.lookup ti allTransactions
+        GetAfterSlot slot ->
+            let whichMeta DB.TxMeta{..} = txMetaSlot > slot
+            in  Set.fromList
+                    $ fmap txMetaTxId
+                    $ filter whichMeta
+                    $ toList allTransactions
