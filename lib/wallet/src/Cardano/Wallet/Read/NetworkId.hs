dd15623fe91d64c013259d1dcecf3af8c70253d6
paolino <paolo.veronelli@gmail.com>
2023-04-11 10:43:58 +0000

    Add singleton type to NetworkId module.

--- lib/wallet/src/Cardano/Wallet/Read/NetworkId.hs
+++ lib/wallet/src/Cardano/Wallet/Read/NetworkId.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE BinaryLiterals #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Cardano.Wallet.Read.NetworkId
     ( NetworkDiscriminant (..)
@@ -15,6 +19,12 @@ module Cardano.Wallet.Read.NetworkId
     , networkDiscriminantBits
     , NetworkDiscriminantCheck (..)
     , networkDescription
+    , SNetworkId (..)
+    , HasSNetworkId (..)
+    , fromSNat
+    , toSNat
+    , fromSNetworkId
+    , toSNetworkId
     )
 where
 
@@ -26,8 +36,10 @@ import Data.Typeable
     ( Proxy (..), Typeable )
 import Data.Word
     ( Word8 )
-import GHC.TypeLits
-    ( KnownNat, Nat, natVal )
+import GHC.Natural
+    ( Natural )
+import GHC.TypeNats
+    ( KnownNat, Nat, SomeNat (..), natVal, someNatVal )
 
 import qualified Data.Text as T
 
@@ -76,3 +88,58 @@ class NetworkDiscriminantCheck (n :: NetworkDiscriminant) k where
 networkDescription
     :: forall n. NetworkDiscriminantVal n => Proxy n -> Text
 networkDescription _ = networkDiscriminantVal @n
+
+{-----------------------------------------------------------------------------
+    Value level network discrimination
+------------------------------------------------------------------------------}
+
+data NetworkId
+    = NMainnet
+    | NTestnet Natural
+    deriving (Eq, Show)
+
+data SNat (n :: Nat) where
+    SNat :: KnownNat n => Proxy n -> SNat n
+
+fromSNat :: SNat n -> Natural
+fromSNat p@(SNat _) = natVal p
+
+toSNat :: Natural -> (forall (n :: Nat). SNat n -> a) -> a
+toSNat nat f = case someNatVal nat of
+    SomeNat proxy -> f (SNat proxy)
+
+{-----------------------------------------------------------------------------
+    singleton for NetworkDiscriminant
+------------------------------------------------------------------------------}
+
+-- | A singleton for 'NetworkDiscriminant'.
+data SNetworkId (n :: NetworkDiscriminant) where
+    SMainnet :: SNetworkId 'Mainnet
+    STestnet :: SNat i -> SNetworkId ('Testnet i)
+
+-- | A class for extracting the singleton for 'NetworkDiscriminant'.
+class HasSNetworkId n where
+    sNetworkId :: SNetworkId n
+
+instance HasSNetworkId 'Mainnet where
+    sNetworkId = SMainnet
+
+instance KnownNat i => HasSNetworkId ('Testnet i) where
+    sNetworkId = STestnet $ SNat Proxy
+
+{-----------------------------------------------------------------------------
+   conversions
+------------------------------------------------------------------------------}
+
+-- | Convert a 'NetworkDiscriminant' singleton to a 'NetworkId' value .
+fromSNetworkId :: SNetworkId n -> NetworkId
+fromSNetworkId SMainnet = NMainnet
+fromSNetworkId (STestnet p) = NTestnet $ fromSNat p
+
+-- | Run a function on a 'NetworkDiscriminant' singleton given a network id.
+toSNetworkId
+    :: NetworkId
+    -> (forall (n :: NetworkDiscriminant). SNetworkId n -> a)
+    -> a
+toSNetworkId NMainnet f = f SMainnet
+toSNetworkId (NTestnet i) f = toSNat i $  f . STestnet
