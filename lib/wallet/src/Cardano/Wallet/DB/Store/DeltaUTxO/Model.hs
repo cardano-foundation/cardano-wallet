e39cfdadc3b454b6cf6af5dca397d4c1ec24d374
Heinrich Apfelmus <heinrich.apfelmus@iohk.io>
2023-04-12 15:35:32 +0200

    Reorder `Cardano.Wallet.DB.Store.DeltaUTxO.Model`

--- lib/wallet/src/Cardano/Wallet/DB/Store/DeltaUTxO/Model.hs
+++ lib/wallet/src/Cardano/Wallet/DB/Store/DeltaUTxO/Model.hs
@@ -83,12 +83,6 @@ empty utxo =
   where
     creationSlots = Map.singleton Origin $ dom utxo
 
-reverseMapOfSets :: Ord v => Map k (Set v) -> Map v k
-reverseMapOfSets m = Map.fromList $ do
-    (k, vs) <- Map.toList m
-    v <- Set.toList vs
-    pure (v, k)
-
 -- | Returns the UTxO.
 getUTxO :: UTxOHistory -> UTxO
 getUTxO UTxOHistory {history, spentSlots} = history `excluding` fold spentSlots
@@ -105,162 +99,164 @@ getFinality UTxOHistory {finality} = finality
 getSpent :: UTxOHistory -> Map TxIn SlotNo
 getSpent UTxOHistory {spentTxIns} = spentTxIns
 
-onNotNull :: Foldable t => t a -> b -> (t a -> b -> b) -> b
-onNotNull x d f =
-    if null x
-        then d
-        else f x d
-
 -- how to apply a DeltaUTxOHistory to a UTxOHistory
 instance Delta DeltaUTxOHistory where
     type Base DeltaUTxOHistory = UTxOHistory
-    apply
-        (AppendBlock newTip delta)
-        noop@UTxOHistory
-            { history
-            , spentSlots
-            , creationSlots
-            , creationTxIns
-            , spentTxIns
-            , finality
-            , boot
-            } =
-            constrainingAppendBlock noop noop newTip $
-                UTxOHistory
-                    { history = history <> received delta
-                    , creationSlots = onNotNull
-                        (dom $ received delta)
-                        creationSlots
-                        $ \received' ->
-                            Map.insert
-                                (At newTip)
-                                (received' `Set.difference` dom history)
-                    , creationTxIns =
-                        foldl'
-                            (\m txIn -> Map.insert txIn (At newTip) m)
-                            creationTxIns
-                            $ dom
-                            $ received delta
-                    , spentSlots = onNotNull
-                        ( (excluded delta `Set.intersection` dom history)
-                            `Set.difference` fold spentSlots
-                        )
+    apply (AppendBlock newTip delta) = appendBlock newTip delta
+    apply (Rollback newTip) = rollback newTip
+    apply (Prune newFinality) = prune newFinality
+
+appendBlock :: SlotNo -> DeltaUTxO -> UTxOHistory -> UTxOHistory
+appendBlock newTip delta
+    noop@UTxOHistory
+        { history
+        , spentSlots
+        , creationSlots
+        , creationTxIns
+        , spentTxIns
+        , finality
+        , boot
+        }
+  = constrainingAppendBlock noop noop newTip $
+    UTxOHistory
+        { history = history <> received delta
+        , creationSlots = onNotNull
+            (dom $ received delta)
+            creationSlots
+            $ \received' ->
+                Map.insert
+                    (At newTip)
+                    (received' `Set.difference` dom history)
+        , creationTxIns =
+            foldl'
+                (\m txIn -> Map.insert txIn (At newTip) m)
+                creationTxIns
+                $ dom
+                $ received delta
+        , spentSlots = onNotNull
+            ( (excluded delta `Set.intersection` dom history)
+                `Set.difference` fold spentSlots
+            )
+            spentSlots
+            $ \excluded' -> Map.insert newTip excluded'
+        , spentTxIns =
+            foldl'
+                (\m txIn -> Map.insert txIn newTip m)
+                spentTxIns
+                (excluded delta)
+        , tip = At newTip
+        , finality = finality
+        , boot = boot
+        }
+
+rollback :: Slot -> UTxOHistory -> UTxOHistory
+rollback newTip
+    noop@UTxOHistory
+        { history
+        , spentSlots
+        , creationSlots
+        , creationTxIns
+        , spentTxIns
+        , finality
+        , boot
+        }
+  = constrainingRollback noop noop newTip $ \case
+    Just newTip' ->
+        let
+            (leftCreationSlots, rolledCreatedSlots) =
+                Map.spanAntitone (<= newTip') creationSlots
+            rolledSpentTxIns = fold $ case newTip' of
+                Origin -> spentSlots
+                At slot'' ->
+                    Map.dropWhileAntitone
+                        (<= slot'')
                         spentSlots
-                        $ \excluded' -> Map.insert newTip excluded'
-                    , spentTxIns =
-                        foldl'
-                            (\m txIn -> Map.insert txIn newTip m)
-                            spentTxIns
-                            (excluded delta)
-                    , tip = At newTip
-                    , finality = finality
-                    , boot = boot
-                    }
-    apply
-        (Rollback newTip)
-        noop@UTxOHistory
-            { history
-            , spentSlots
-            , creationSlots
-            , creationTxIns
-            , spentTxIns
-            , finality
-            , boot
-            } = constrainingRollback noop noop newTip $ \case
-            Just newTip' ->
-                let
-                    (leftCreationSlots, rolledCreatedSlots) =
-                        Map.spanAntitone (<= newTip') creationSlots
-                    rolledSpentTxIns = fold $ case newTip' of
-                        Origin -> spentSlots
-                        At slot'' ->
-                            Map.dropWhileAntitone
-                                (<= slot'')
-                                spentSlots
-                    rolledCreatedTxIns = fold rolledCreatedSlots
-                in
-                    UTxOHistory
-                        { history = history `excluding` rolledCreatedTxIns
-                        , spentSlots = case newTip' of
-                            Origin -> mempty
-                            At slot'' ->
-                                Map.takeWhileAntitone
-                                    (<= slot'')
-                                    spentSlots
-                        , creationSlots = leftCreationSlots
-                        , creationTxIns =
-                            Map.withoutKeys
-                                creationTxIns
-                                rolledCreatedTxIns
-                        , spentTxIns =
-                            Map.withoutKeys
-                                spentTxIns
-                                rolledSpentTxIns
-                        , tip = newTip'
-                        , finality = finality
-                        , boot = boot
-                        }
-            Nothing -> empty boot
-    apply
-        (Prune newFinality)
-        noop@UTxOHistory
-            { history
-            , spentSlots
-            , creationSlots
-            , creationTxIns
-            , spentTxIns
-            , tip
-            , boot
-            } = constrainingPrune noop noop newFinality $ \newFinality' ->
-            let
-                (prunedSpentSlots, leftSpentSlots) =
-                    Map.spanAntitone
-                        (<= newFinality')
-                        spentSlots
-                prunedTxIns = fold prunedSpentSlots
-                fixCreationSlot
-                    (txIn, slotNo) = Map.alter f slotNo
-                      where
-                        f Nothing = Nothing
-                        f (Just txIns) =
-                            let
-                                txIns' = Set.delete txIn txIns
-                            in
-                                if null txIns'
-                                    then Nothing
-                                    else Just txIns'
-            in
-                UTxOHistory
-                    { history = history `excluding` prunedTxIns
-                    , creationSlots =
-                        foldl'
-                            (flip fixCreationSlot)
-                            creationSlots
-                            $ Map.assocs
-                            $ Map.restrictKeys creationTxIns prunedTxIns
-                    , creationTxIns =
-                        Map.withoutKeys
-                            creationTxIns
-                            prunedTxIns
-                    , spentSlots = leftSpentSlots
-                    , spentTxIns =
-                        Map.withoutKeys
-                            spentTxIns
-                            prunedTxIns
-                    , tip = tip
-                    , finality = PrunedUpTo newFinality'
-                    , boot = boot
-                    }
+            rolledCreatedTxIns = fold rolledCreatedSlots
+        in
+            UTxOHistory
+                { history = history `excluding` rolledCreatedTxIns
+                , spentSlots = case newTip' of
+                    Origin -> mempty
+                    At slot'' ->
+                        Map.takeWhileAntitone
+                            (<= slot'')
+                            spentSlots
+                , creationSlots = leftCreationSlots
+                , creationTxIns =
+                    Map.withoutKeys
+                        creationTxIns
+                        rolledCreatedTxIns
+                , spentTxIns =
+                    Map.withoutKeys
+                        spentTxIns
+                        rolledSpentTxIns
+                , tip = newTip'
+                , finality = finality
+                , boot = boot
+                }
+    Nothing -> empty boot
+
+prune :: SlotNo -> UTxOHistory -> UTxOHistory
+prune newFinality
+    noop@UTxOHistory
+        { history
+        , spentSlots
+        , creationSlots
+        , creationTxIns
+        , spentTxIns
+        , tip
+        , boot
+        }
+  = constrainingPrune noop noop newFinality $ \newFinality' ->
+    let
+        (prunedSpentSlots, leftSpentSlots) =
+            Map.spanAntitone
+                (<= newFinality')
+                spentSlots
+        prunedTxIns = fold prunedSpentSlots
+        fixCreationSlot
+            (txIn, slotNo) = Map.alter f slotNo
+                where
+                f Nothing = Nothing
+                f (Just txIns) =
+                    let
+                        txIns' = Set.delete txIn txIns
+                    in
+                        if null txIns'
+                            then Nothing
+                            else Just txIns'
+    in
+        UTxOHistory
+            { history = history `excluding` prunedTxIns
+            , creationSlots =
+                foldl'
+                    (flip fixCreationSlot)
+                    creationSlots
+                    $ Map.assocs
+                    $ Map.restrictKeys creationTxIns prunedTxIns
+            , creationTxIns =
+                Map.withoutKeys
+                    creationTxIns
+                    prunedTxIns
+            , spentSlots = leftSpentSlots
+            , spentTxIns =
+                Map.withoutKeys
+                    spentTxIns
+                    prunedTxIns
+            , tip = tip
+            , finality = PrunedUpTo newFinality'
+            , boot = boot
+            }
 
 -- | Helper to constraint the slot of an AppendBlock.
 constrainingAppendBlock :: a -> UTxOHistory -> SlotNo -> a -> a
-constrainingAppendBlock noop UTxOHistory {tip} newTip f
+constrainingAppendBlock noop UTxOHistory{tip} newTip f
     | At newTip <= tip = noop
     | otherwise = f
 
 -- | Helper to constraint the slot of a Rollback.
 constrainingRollback :: a -> UTxOHistory -> Slot -> (Maybe Slot -> a) -> a
-constrainingRollback noop UTxOHistory {finality, tip} newTip f
+constrainingRollback noop UTxOHistory{finality, tip} newTip f
     | newTip >= tip = noop
     | otherwise = f $ case finality of
         NotPruned -> Just newTip
@@ -271,7 +267,7 @@ constrainingRollback noop UTxOHistory {finality, tip} newTip f
 
 -- | Helper to constraint the slot of a Prune.
 constrainingPrune :: a -> UTxOHistory -> SlotNo -> (SlotNo -> a) -> a
-constrainingPrune noop UTxOHistory {finality, tip} newFinality f =
+constrainingPrune noop UTxOHistory{finality, tip} newFinality f =
     fromMaybe noop $ do
         case finality of
             NotPruned -> pure ()
@@ -283,3 +279,18 @@ constrainingPrune noop UTxOHistory {finality, tip} newFinality f =
 -- | A datatype that represents the spent time of a TxOut.
 data Spent = Spent SlotNo | Unspent
     deriving (Eq, Show)
+
+{-----------------------------------------------------------------------------
+    Helper functions
+------------------------------------------------------------------------------}
+onNotNull :: Foldable t => t a -> b -> (t a -> b -> b) -> b
+onNotNull x d f =
+    if null x
+        then d
+        else f x d
+
+reverseMapOfSets :: Ord v => Map k (Set v) -> Map v k
+reverseMapOfSets m = Map.fromList $ do
+    (k, vs) <- Map.toList m
+    v <- Set.toList vs
+    pure (v, k)
