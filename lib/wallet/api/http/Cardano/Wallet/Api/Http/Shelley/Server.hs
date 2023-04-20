f68c4a13470a5ed15e248fbe2ea795791b3795e0
Jonathan Knowles <jonathan.knowles@iohk.io>
2023-04-20 10:11:55 +0000

    Change `Write.ProtocolParameters` to use ledger protocol parameters.
    Instead of node protocol parameters.

--- lib/wallet/api/http/Cardano/Wallet/Api/Http/Shelley/Server.hs
+++ lib/wallet/api/http/Cardano/Wallet/Api/Http/Shelley/Server.hs
@@ -706,7 +706,6 @@ import qualified Network.Ntp as Ntp
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified Network.Wai.Handler.WarpTLS as Warp
 
-
 -- | How the server should listen for incoming requests.
 data Listen
     = ListenOnPort Port
@@ -3144,22 +3143,12 @@ balanceTransaction
                         txLayer
                         genInpScripts
                         mScriptTemplate)
-                    (Write.ProtocolParameters pp nodePParams)
+                    (Write.unsafeFromWalletProtocolParameters pp)
                     ti
                     utxoIndex
                     (W.defaultChangeAddressGen argGenChange (Proxy @k))
                     (getState wallet)
                     partialTx
-              where
-                nodePParams = maybe
-                    (error $ unwords
-                        [ "balanceTransaction: no nodePParams."
-                        , "Should only be possible in Byron, where"
-                        , "withRecentEra should prevent this being reached."
-                        ])
-                    (Cardano.bundleProtocolParams
-                        (WriteTx.fromRecentEra (WriteTx.recentEra @era)))
-                    $ W.currentNodeProtocolParameters pp
 
         anyRecentTx <- maybeToHandler (Write.ErrOldEraNotSupported era)
             . WriteTx.asAnyRecentEra
