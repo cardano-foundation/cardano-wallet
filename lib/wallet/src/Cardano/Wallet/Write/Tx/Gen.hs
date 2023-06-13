bfd3dcfc61f5d851e52e6efb24d3520c70e30dbd
Johannes Lund <johannes.lund@iohk.io>
2023-06-13 14:48:55 +0200

    Drop unused generators and conversions in Write.Tx
    The idea behind them was to make it easy for the wallet API to represent
    tx outputs in their full form with inline scripts and datums included.
    
    This is currently unused, and is starting to show signs of neglect (when
    to use 'era' and when 'LatestLedgerEra'?). Probably it makes sense to
    define binary/JSON conversions in a separate module anyway. Possibly it
    also makes sense to base this upon a third module where ledger types are
    redefined without the `era` type parameters.
    
    Let's re-add this when needed.

--- lib/wallet/src/Cardano/Wallet/Write/Tx/Gen.hs
+++ lib/wallet/src/Cardano/Wallet/Write/Tx/Gen.hs
@@ -1,7 +1,4 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 
 -- |
@@ -9,105 +6,36 @@
 -- License: Apache-2.0
 --
 module Cardano.Wallet.Write.Tx.Gen
-    ( genDatum
-    , genBinaryData
-    , genDatumHash
-    , shrinkBinaryData
-    , shrinkDatum
+    ( genDatumHash
     , genTxOut
     )
     where
 
 import Prelude
 
-import Cardano.Ledger.Alonzo.Scripts.Data
-    ( Data (..), dataToBinaryData )
 import Cardano.Wallet.Write.Tx
-    ( BinaryData
-    , Datum (..)
-    , DatumHash
-    , LatestLedgerEra
+    ( DatumHash
     , RecentEra
     , ShelleyLedgerEra
     , TxOut
     , cardanoEraFromRecentEra
-    , datumFromCardanoScriptData
     , datumHashFromBytes
-    , datumToCardanoScriptData
     , shelleyBasedEraFromRecentEra
-    , withConstraints
     )
-import Data.ByteString
-    ( ByteString )
 import Data.Maybe
     ( fromMaybe )
 import Test.QuickCheck
-    ( Gen
-    , arbitrary
-    , choose
-    , listOf
-    , oneof
-    , scale
-    , shrinkMapBy
-    , sized
-    , vector
-    , vectorOf
-    )
+    ( Gen, arbitrary, vectorOf )
 
 import qualified Cardano.Api.Gen as Cardano
 import qualified Cardano.Api.Shelley as Cardano
 import qualified Data.ByteString as BS
-import qualified PlutusLedgerApi.V1 as PV1
-
-genDatum :: RecentEra era -> Gen (Datum (ShelleyLedgerEra era))
-genDatum recentEra = withConstraints recentEra $
-    oneof
-        [ Datum <$> genBinaryData recentEra
-        , DatumHash <$> genDatumHash
-        , pure NoDatum
-        ]
-
--- Originally from https://github.com/input-output-hk/cardano-ledger/blob/c7c63dabdb215ebdaed8b63274965966f2bf408f/eras/alonzo/test-suite/src/Test/Cardano/Ledger/Alonzo/Serialisation/Generators.hs#L66-L79
-genBinaryData :: RecentEra era -> Gen (BinaryData (ShelleyLedgerEra era))
-genBinaryData recentEra = withConstraints recentEra $
-    dataToBinaryData . Data <$> scale (`div` 10) (sized gendata)
-  where
-    gendata n | n > 0 = oneof
-        [ PV1.I <$> arbitrary
-        , PV1.B <$> genByteString
-        , PV1.Map
-            <$> listOf ((,) <$> gendata (n `div` 2) <*> gendata (n `div` 2))
-        , PV1.Constr
-            <$> fmap abs (arbitrary :: Gen Integer)
-            <*> listOf (gendata (n `div` 2))
-        , PV1.List
-            <$> listOf (gendata (n `div` 2))
-        ]
-    gendata _ = oneof [PV1.I <$> arbitrary, PV1.B <$> genByteString]
-
-shrinkDatum :: Datum LatestLedgerEra -> [Datum LatestLedgerEra]
-shrinkDatum (Datum x) = NoDatum : map Datum (shrinkBinaryData _recentEra x)
-shrinkDatum (DatumHash _) = [NoDatum]
-shrinkDatum NoDatum = []
-
-shrinkBinaryData :: RecentEra era -> BinaryData era -> [BinaryData era]
-shrinkBinaryData recentEra = withConstraints recentEra $
-    shrinkMapBy
-        datumFromCardanoScriptData
-        datumToCardanoScriptData $
-        shrinkMapBy
-            Cardano.unsafeHashableScriptData
-            Cardano.getScriptData
-            Cardano.shrinkScriptData
 
 genDatumHash :: Gen DatumHash
 genDatumHash =
     fromMaybe (error "genDatumHash should always generate valid hashes")
     . datumHashFromBytes
     . BS.pack <$> vectorOf 32 arbitrary
-
-genByteString :: Gen ByteString
-genByteString = BS.pack <$> (choose (0, 64) >>= vector)
 
 genTxOut :: RecentEra era -> Gen (TxOut (ShelleyLedgerEra era))
 genTxOut era = Cardano.toShelleyTxOut (shelleyBasedEraFromRecentEra era)
