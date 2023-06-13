97798417d09e81615e3bd051312e0886b557292d
Yura Lazarev <Unisay@users.noreply.github.com>
2023-06-13 09:59:04 +0200

    chore: removed warning

--- lib/wallet/src/Cardano/Wallet/Read/Tx/Withdrawals.hs
+++ lib/wallet/src/Cardano/Wallet/Read/Tx/Withdrawals.hs
@@ -43,13 +43,13 @@ import Control.Lens
     ( view )
 import Data.Map
     ( Map )
+
 import qualified Cardano.Ledger.Api as Ledger
-import qualified Cardano.Wallet.Primitive.Types.RewardAccount as Wallet
 import qualified Cardano.Wallet.Primitive.Types.Coin as Wallet
-import qualified Data.Map.Strict as Map
-import Cardano.Wallet.Read.Primitive.Tx.Features.Certificates (fromStakeCredential)
+import qualified Cardano.Wallet.Primitive.Types.RewardAccount as Wallet
 import qualified Cardano.Wallet.Read.Primitive.Tx.Features.Certificates as Certificates
 import qualified Cardano.Wallet.Read.Primitive.Tx.Features.Fee as Fee
+import qualified Data.Map.Strict as Map
 
 type family WithdrawalsType era where
   WithdrawalsType ByronEra = ()
@@ -85,7 +85,7 @@ getEraWithdrawals =
         Withdrawals . unWithdrawals . view (bodyTxL . withdrawalsTxBodyL)
 
 fromLedgerWithdrawals
-    ::Ledger.Withdrawals crypto -> Map Wallet.RewardAccount Wallet.Coin
+    :: Ledger.Withdrawals crypto -> Map Wallet.RewardAccount Wallet.Coin
 fromLedgerWithdrawals (Ledger.Withdrawals withdrawals) = Map.fromList
     [ (Certificates.fromStakeCredential cred, Fee.fromShelleyCoin coin)
     | (Ledger.RewardAcnt _network cred, coin) <- Map.toList withdrawals
