09ff1f7237d238d52ca45224bd97c0b67d76f703
paolino <paolo.veronelli@gmail.com>
2023-06-14 10:49:08 +0000

    fix MinimumUTxO.Internal issues

--- lib/wallet/src/Cardano/Wallet/Shelley/MinimumUTxO/Internal.hs
+++ lib/wallet/src/Cardano/Wallet/Shelley/MinimumUTxO/Internal.hs
@@ -15,8 +15,10 @@ module Cardano.Wallet.Shelley.MinimumUTxO.Internal
 
 import Prelude
 
-import Cardano.Ledger.Shelley.API.Wallet
-    ( evaluateMinLovelaceOutput )
+import Cardano.Api
+    ( ProtocolParametersConversionError )
+import Cardano.Ledger.Api
+    ( getMinCoinTxOut )
 import Cardano.Wallet.Primitive.Types.Coin
     ( Coin )
 import Cardano.Wallet.Primitive.Types.MinimumUTxO
@@ -34,6 +36,8 @@ import Cardano.Wallet.Shelley.Compatibility.Ledger
     , toShelleyTxOut
     , toWalletCoin
     )
+import Control.Lens
+    ( (<&>) )
 import Data.Function
     ( (&) )
 import GHC.Stack
@@ -47,49 +51,51 @@ import qualified Cardano.Api.Shelley as Cardano
 --
 -- This function does /not/ attempt to reach a fixed point before returning its
 -- result.
---
 computeMinimumCoinForUTxO_CardanoApi
     :: HasCallStack
     => MinimumUTxOForShelleyBasedEra
     -> TxOut
     -> Coin
 computeMinimumCoinForUTxO_CardanoApi
-    (MinimumUTxOForShelleyBasedEra era pp) txOut =
-        unsafeCoinFromResult $
-            Cardano.calculateMinimumUTxO era
+    (MinimumUTxOForShelleyBasedEra era pp)
+    txOut =
+        unsafeCoinFromResult
+            $ Cardano.bundleProtocolParams
+                (Cardano.shelleyBasedToCardanoEra era)
+                (Cardano.fromLedgerPParams era pp)
+            <&> Cardano.calculateMinimumUTxO
+                era
                 (toCardanoTxOut era txOut)
-                (Cardano.bundleProtocolParams
-                    (Cardano.shelleyBasedToCardanoEra era)
-                    (Cardano.fromLedgerPParams era pp))
-  where
-    unsafeCoinFromResult
-        :: Either Cardano.MinimumUTxOError Cardano.Lovelace
-        -> Coin
-    unsafeCoinFromResult = \case
-        Right value ->
-            -- We assume that the returned value is a non-negative ada quantity
-            -- with no other assets. If this assumption is violated, we have no
-            -- way to continue, and must raise an error:
-            value
-                & unsafeLovelaceToWalletCoin
-        Left e ->
-            -- The 'Cardano.calculateMinimumUTxO' function should only return
-            -- an error if a required protocol parameter is missing.
-            --
-            -- However, given that values of 'MinimumUTxOForShelleyBasedEra'
-            -- can only be constructed by supplying an era-specific protocol
-            -- parameters record, it should be impossible to trigger this
-            -- condition.
-            --
-            -- Any violation of this assumption indicates a programming error.
-            -- If this condition is triggered, we have no way to continue, and
-            -- must raise an error:
-            --
-            error $ unwords
-                [ "computeMinimumCoinForUTxO_CardanoApi:"
-                , "unexpected error:"
-                , show e
-                ]
+      where
+        unsafeCoinFromResult
+            :: Either ProtocolParametersConversionError Cardano.Lovelace
+            -> Coin
+        unsafeCoinFromResult = \case
+            Right value ->
+                -- We assume that the returned value is a non-negative ada quantity
+                -- with no other assets. If this assumption is violated, we have no
+                -- way to continue, and must raise an error:
+                value
+                    & unsafeLovelaceToWalletCoin
+            Left e ->
+                -- The 'Cardano.calculateMinimumUTxO' function should only return
+                -- an error if a required protocol parameter is missing.
+                --
+                -- However, given that values of 'MinimumUTxOForShelleyBasedEra'
+                -- can only be constructed by supplying an era-specific protocol
+                -- parameters record, it should be impossible to trigger this
+                -- condition.
+                --
+                -- Any violation of this assumption indicates a programming error.
+                -- If this condition is triggered, we have no way to continue, and
+                -- must raise an error:
+                --
+                error
+                    $ unwords
+                        [ "computeMinimumCoinForUTxO_CardanoApi:"
+                        , "unexpected error:"
+                        , show e
+                        ]
 
 -- | Computes a minimum UTxO value with Cardano Ledger.
 --
@@ -97,29 +103,29 @@ computeMinimumCoinForUTxO_CardanoApi
 --
 -- This function does /not/ attempt to reach a fixed point before returning its
 -- result.
---
 computeMinimumCoinForUTxO_CardanoLedger
     :: MinimumUTxOForShelleyBasedEra
     -> TxOut
     -> Coin
 computeMinimumCoinForUTxO_CardanoLedger
-    (MinimumUTxOForShelleyBasedEra era pp) txOut =
+    (MinimumUTxOForShelleyBasedEra era pp)
+    txOut =
         toWalletCoin $ case era of
             Cardano.ShelleyBasedEraShelley ->
-                evaluateMinLovelaceOutput pp
+                getMinCoinTxOut pp
                     $ toShelleyTxOut txOut
             Cardano.ShelleyBasedEraAllegra ->
-                evaluateMinLovelaceOutput pp
+                getMinCoinTxOut pp
                     $ toAllegraTxOut txOut
             Cardano.ShelleyBasedEraMary ->
-                evaluateMinLovelaceOutput pp
+                getMinCoinTxOut pp
                     $ toMaryTxOut txOut
             Cardano.ShelleyBasedEraAlonzo ->
-                evaluateMinLovelaceOutput pp
+                getMinCoinTxOut pp
                     $ toAlonzoTxOut txOut
             Cardano.ShelleyBasedEraBabbage ->
-                evaluateMinLovelaceOutput pp
+                getMinCoinTxOut pp
                     $ toBabbageTxOut txOut
             Cardano.ShelleyBasedEraConway ->
-                evaluateMinLovelaceOutput pp
+                getMinCoinTxOut pp
                     $ toConwayTxOut txOut
