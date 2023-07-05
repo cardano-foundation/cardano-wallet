9ea2e4413ce0a8d4c4736bd4215f3b6263554768
Heinrich Apfelmus <heinrich.apfelmus@iohk.io>
2023-07-05 13:50:43 +0200

    Migrate database files for `api` benchmark

--- .buildkite/bench-api.sh
+++ .buildkite/bench-api.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 # C.UTF-8 = UTF-8 based locale that is not associated with a natural language.
 # This seems necessary so that programs compiled with GHC don't choke,
 # see also <https://stackoverflow.com/a/63751678>.
-export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
 
 if [ -n "${SCRATCH_DIR:-}" ]; then
   mkdir -pv "$SCRATCH_DIR"
