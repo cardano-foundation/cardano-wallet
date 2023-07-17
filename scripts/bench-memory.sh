6b7dbf75c294d43407126019503191245337818b
paolino <paolo.veronelli@gmail.com>
2023-07-17 16:01:45 +0000

    Fix bash linting issues.

--- scripts/bench-memory.sh
+++ scripts/bench-memory.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
 echo "------------------------ Setup nix ---------------------------------------"
-wallet_exe="`which cardano-wallet`"
+wallet_exe="$(which cardano-wallet)"
 echo "wallet_exe: $wallet_exe"
-node_exe="`which cardano-node`"
+node_exe="$(which cardano-node)"
 echo "node_exe: $node_exe"
-node_db="`pwd`/lib/wallet-benchmarks/data/membench-snapshot.tgz"
+node_db="$(pwd)/lib/wallet-benchmarks/data/membench-snapshot.tgz"
 echo "node_db: $node_db"
-work_dir=`pwd`
+work_dir="$(pwd)"
 echo "work_dir: $work_dir"
 bench="memory \
     --snapshot=$node_db \
@@ -17,6 +17,5 @@ echo "bench command: $bench"
 echo "------------------------ Setup nix done ---------------------------------"
 
 echo "------------------------ Run --------------------------------------------"
-echo "log: $log"
-$bench 2>$error_log 1>$log
+$bench 2>"${error_log:?}" 1>"${log:?}"
 echo "------------------------ Run done ---------------------------------------"
