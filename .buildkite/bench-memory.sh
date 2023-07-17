b16fb3bf813ee707cb9b47b8a16e549b274c227f
paolino <paolo.veronelli@gmail.com>
2023-07-17 15:48:57 +0000

    Split the script to use `nix shell` to bring the node executable in scope.

--- .buildkite/bench-memory.sh
+++ .buildkite/bench-memory.sh
@@ -1,43 +1,49 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p coreutils gnugrep gawk time haskellPackages.hp2pretty buildkite-agent gnuplot
+#! nix-shell -i bash -p coreutils gnugrep gawk time haskellPackages.hp2pretty buildkite-agent
 
 set -euo pipefail
-
-artifact_name=memory
-
+echo "------------------------ Setup ------------------------------------------"
+TMPDIR="${TMPDIR:-/tmp}"
 export TMPDIR="/$TMPDIR/bench/memory"
 mkdir -p $TMPDIR
+echo "TMPDIR: $TMPDIR"
 
-echo "------------------------ Build ------------------------------------------"
-nix build -o bench-wallet
+artifact_name=memory
+echo "artifact_name: $artifact_name"
 
-echo "------------------------ Run --------------------------------------------"
-wallet_exe=`pwd`/bench-wallet/bin/cardano-wallet
-node_exe=`which cardano-node`
-node_db="`pwd`/lib/wallet-benchmarks/data/membench-snapshot.tgz"
-work_dir=`pwd`
-bench="cabal bench cardano-wallet-benchmarks:bench:memory \
-    --benchmark-option=--snapshot=$node_db \
-    --benchmark-option=--wallet=$wallet_exe \
-    --benchmark-option=--node=$node_exe \
-    --benchmark-option=--work-dir=$work_dir "
+export log="$artifact_name.log"
+echo "log: $log"
 
-$bench
+export error_log="$artifact_name.error.log"
+echo "error_log: $error_log"
+
+echo "------------------------ Setup done -------------------------------------"
+
+echo "------------------------ Nix call ---------------------------------------"
+nix shell \
+    '.#ci.benchmarks.memory' \
+    '.#cardano-node' \
+    '.#cardano-wallet' \
+     -c "scripts/bench-memory.sh"
+
+echo "------------------------ Nix call done ----------------------------------"
 
 echo "------------------------ Results ----------------------------------------"
-
 mv cardano-wallet.hp $artifact_name.hp
 hp2pretty $artifact_name.hp
 
 if [ -n "${BUILDKITE:-}" ]; then
   echo "--- Upload"
   buildkite-agent artifact upload $artifact_name.svg
+  buildkite-agent artifact upload $log
+  buildkite-agent artifact upload $error_log
 
   echo "+++ Heap profile"
   printf '\033]1338;url='"artifact://$artifact_name.svg"';alt='"Heap profile"'\a\n'
 
 fi
+echo "------------------------ Results done -----------------------------------"
 
 echo "------------------------ Cleanup ----------------------------------------"
-
 rm -rf $TMPDIR
+echo "------------------------ Cleanup done -----------------------------------"
