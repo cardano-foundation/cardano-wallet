c7c9342d190fef49c29a6af61c9f7be1e6c8803d
Yura Lazarev <Unisay@users.noreply.github.com>
2023-08-22 15:59:04 +0200

    E2E: fix timeout spec

--- lib/wallet-e2e/src/Cardano/Wallet/Spec/TimeoutSpec.hs
+++ lib/wallet-e2e/src/Cardano/Wallet/Spec/TimeoutSpec.hs
@@ -13,13 +13,11 @@ import Cardano.Wallet.Spec.Stories.Language
 import Control.Concurrent
     ( threadDelay )
 import Data.Time.TimeSpan
-    ( seconds )
+    ( milliseconds, seconds, toMicroseconds )
 import Effectful
     ( Eff, IOE, runEff )
 import Effectful.Fail
     ( Fail, runFail )
-import Effectful.State.Static.Local
-    ( State, evalState, get, modify )
 import Prelude hiding
     ( State, evalState, get, modify )
 import Test.Syd
@@ -28,29 +26,15 @@ import Test.Syd
 -- | This story will not timeout as it takes less than 1 second.
 noTimeout :: FxStory fxs '[FxTimeout, IOE] ()
 noTimeout = within (seconds 1) do
-    liftIO $ threadDelay $ 1000000 - 10
+    liftIO . threadDelay . round
+        $ toMicroseconds (seconds 1) - toMicroseconds (milliseconds 100)
 
 -- | This story will timeout as it takes longer than 1 second.
 timeoutIn10Milliseconds :: FxStory fxs '[FxTimeout, IOE] ()
 timeoutIn10Milliseconds = within (seconds 1) do
     liftIO $ threadDelay 1010000
 
--- | This story will timeout once, but then if retried will succeed.
-timeoutIn10MillisecondsOnce :: FxStory fxs '[State Int, FxTimeout, IOE] ()
-timeoutIn10MillisecondsOnce = do
-    modify @Int (+ 10)
-    delay <- get @Int
-    within (seconds 1) do liftIO $ threadDelay $ 1000 * (1010 - delay)
-
-type Story a =
-    Eff
-        [ FxTimeout
-        , FxAssert
-        , FxTrace
-        , Fail
-        , IOE
-        ]
-        a
+type Story a = Eff [FxTimeout, FxAssert, FxTrace, Fail, IOE] a
 
 run :: Story a -> IO (Either String (a, Seq Text))
 run =
@@ -71,8 +55,3 @@ timeoutSpec = describe "Timeouts" do
         run timeoutIn10Milliseconds >>= \case
             Left _ -> pass
             Right _ -> expectationFailure "should timeout"
-    it "timeout in 10 milliseconds, but retry" do
-        r <- run $ evalState (10 :: Int) timeoutIn10MillisecondsOnce
-        case r of
-            Left _ -> expectationFailure "should not timeout"
-            Right _ -> pass
