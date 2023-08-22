e6f3e0101660d0ce9a012eff20065cc650730e16
Jonathan Knowles <mail@jonathanknowles.net>
2023-08-22 06:47:45 +0000

    Add module `Cardano.CoinSelection.Size`.
    This module acts as a repository for types and functions that define the
    coin selection algorithm's abstract size model.

---
+++ lib/coin-selection/lib/Cardano/CoinSelection/Size.hs
@@ -0,0 +1,10 @@
+-- |
+-- Copyright: © 2023 Cardano Foundation
+-- License: Apache-2.0
+--
+-- Types and functions relating to the coin selection algorithm's abstract size
+-- model.
+--
+module Cardano.CoinSelection.Size
+    ()
+    where
