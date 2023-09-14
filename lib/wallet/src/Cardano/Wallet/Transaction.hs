d0f58dfa5aa7f19da5881b9ef8a856b1525ffe53
Johannes Lund <johannes.lund@iohk.io>
2023-09-14 15:36:37 +0200

    Apply suggestions from code review
    Co-authored-by: Jonathan Knowles <mail@jonathanknowles.net>

--- lib/wallet/src/Cardano/Wallet/Transaction.hs
+++ lib/wallet/src/Cardano/Wallet/Transaction.hs
@@ -96,6 +96,8 @@ import Cardano.Wallet.Primitive.Types.Tx.TxIn
     ( TxIn (..) )
 import Cardano.Wallet.Primitive.Types.Tx.TxOut
     ( TxOut (..) )
+import Cardano.Wallet.Write.Tx.SizeEstimation
+    ( TxWitnessTag )
 import Control.DeepSeq
     ( NFData (..) )
 import Data.List.NonEmpty
@@ -121,8 +123,6 @@ import qualified Cardano.Api as Cardano
 import qualified Cardano.Wallet.Primitive.Types.TokenMap as TokenMap
 import qualified Cardano.Wallet.Primitive.Types.Tx.TxOut as TxOut
 import qualified Cardano.Wallet.Write.Tx as Write
-import Cardano.Wallet.Write.Tx.SizeEstimation
-    ( TxWitnessTag )
 import qualified Data.Foldable as F
 import qualified Data.List as L
 import qualified Data.Map.Strict as Map
