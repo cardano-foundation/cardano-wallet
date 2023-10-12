dd3e7a480ff43419dfa98221387969a2615cd34a
Jonathan Knowles <mail@jonathanknowles.net>
2023-10-12 05:54:29 +0000

    Add comments explaining the reason for `SpecHook.hs` files.

--- lib/wallet/test/unit/SpecHook.hs
+++ lib/wallet/test/unit/SpecHook.hs
@@ -2,5 +2,9 @@ module SpecHook where
 
 import Test.Hspec
 
+-- Run all tests in parallel by default.
+--
+-- See: https://hspec.github.io/parallel-spec-execution.html
+--
 hook :: Spec -> Spec
 hook = parallel
