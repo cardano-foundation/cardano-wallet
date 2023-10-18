fe98a77082d5e7e6563ea06f3928bc71f003bf78
Jonathan Knowles <mail@jonathanknowles.net>
2023-10-18 05:46:15 +0000

    Export function `balanceTransaction` through public interface.

--- lib/balance-tx/lib/main/Cardano/Write/Tx.hs
+++ lib/balance-tx/lib/main/Cardano/Write/Tx.hs
@@ -2,4 +2,9 @@
 -- Copyright: © 2023 Cardano Foundation
 -- License: Apache-2.0
 --
-module Cardano.Write.Tx where
+module Cardano.Write.Tx
+    ( balanceTransaction
+    ) where
+
+import Internal.Cardano.Write.Tx.Balance
+    ( balanceTransaction )
