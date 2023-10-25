5a7853cd0288fec7b009a8eac8e384c38e38cce7
paolino <paolo.veronelli@gmail.com>
2023-10-25 15:46:28 +0000

    Add Block to HeaderHash era dependent extraction

---
+++ lib/read/lib/Cardano/Wallet/Read/Block/HeaderHash.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Cardano.Wallet.Read.Block.HeaderHash
+    ( getEraHeaderHash
+    , HeaderHash (..)
+    , HeaderHashT
+    ) where
+
+import Prelude
+
+import Cardano.Api
+    ( AllegraEra
+    , AlonzoEra
+    , BabbageEra
+    , ByronEra
+    , ConwayEra
+    , MaryEra
+    , ShelleyEra
+    )
+import Cardano.Ledger.Binary
+    ( EncCBOR
+    , EncCBORGroup
+    )
+import Cardano.Ledger.Crypto
+    ( StandardCrypto
+    )
+import Cardano.Ledger.Era
+    ( Era
+    , EraSegWits (..)
+    )
+import Cardano.Wallet.Read
+    ( Block (..)
+    )
+import Cardano.Wallet.Read.Eras.EraFun
+    ( EraFun (..)
+    )
+import Ouroboros.Consensus.Byron.Ledger
+    ( ByronHash
+    )
+import Ouroboros.Consensus.Shelley.Ledger
+    ( ShelleyHash
+    )
+import Ouroboros.Consensus.Shelley.Protocol.Abstract
+    ( ProtoCrypto
+    )
+
+import qualified Cardano.Ledger.Shelley.API as Shelley
+import qualified Ouroboros.Consensus.Shelley.Ledger.Block as O
+import qualified Ouroboros.Consensus.Shelley.Protocol.Abstract as Shelley
+import qualified Ouroboros.Network.Block as O
+
+type family HeaderHashT era where
+    HeaderHashT ByronEra = ByronHash
+    HeaderHashT ShelleyEra = ShelleyHash StandardCrypto
+    HeaderHashT AllegraEra = ShelleyHash StandardCrypto
+    HeaderHashT MaryEra = ShelleyHash StandardCrypto
+    HeaderHashT AlonzoEra = ShelleyHash StandardCrypto
+    HeaderHashT BabbageEra = ShelleyHash StandardCrypto
+    HeaderHashT ConwayEra = ShelleyHash StandardCrypto
+
+newtype HeaderHash era = HeaderHash (HeaderHashT era)
+
+getEraHeaderHash :: EraFun Block HeaderHash
+getEraHeaderHash =
+    EraFun
+        { byronFun = \(Block block) -> HeaderHash $ O.blockHash block
+        , shelleyFun = \(Block block) -> HeaderHash $ getHeaderHashShelley block
+        , allegraFun = \(Block block) -> HeaderHash $ getHeaderHashShelley block
+        , maryFun = \(Block block) -> HeaderHash $ getHeaderHashShelley block
+        , alonzoFun = \(Block block) -> HeaderHash $ getHeaderHashShelley block
+        , babbageFun = \(Block block) -> HeaderHash $ getHeaderHashShelley block
+        , conwayFun = \(Block block) -> HeaderHash $ getHeaderHashShelley block
+        }
+
+getHeaderHashShelley
+    :: ( ProtoCrypto (praos StandardCrypto) ~ StandardCrypto
+       , Shelley.ProtocolHeaderSupportsEnvelope (praos StandardCrypto)
+       , Era era
+       , EncCBORGroup (TxSeq era)
+       , EncCBOR (Shelley.ShelleyProtocolHeader (praos StandardCrypto))
+       )
+    => O.ShelleyBlock (praos StandardCrypto) era
+    -> ShelleyHash StandardCrypto
+getHeaderHashShelley
+    (O.ShelleyBlock (Shelley.Block header _) _) = Shelley.pHeaderHash header
