92372d5b42d8e0006e89f454c53a02652a151ca0
paolino <paolo.veronelli@gmail.com>
2023-10-26 10:19:54 +0000

    Add Block to PrevHeaderHash era dependent extraction

--- lib/read/lib/Cardano/Wallet/Read/Block/HeaderHash.hs
+++ lib/read/lib/Cardano/Wallet/Read/Block/HeaderHash.hs
@@ -5,7 +5,11 @@ module Cardano.Wallet.Read.Block.HeaderHash
     ( getEraHeaderHash
     , HeaderHash (..)
     , HeaderHashT
-    ) where
+    , PrevHeaderHash (..)
+    , PrevHeaderHashT
+    , getEraPrevHeaderHash
+    )
+where
 
 import Prelude
 
@@ -29,14 +33,21 @@ import Cardano.Ledger.Era
     ( Era
     , EraSegWits (..)
     )
+import Cardano.Protocol.TPraos.BHeader
+    ( PrevHash
+    )
 import Cardano.Wallet.Read
     ( Block (..)
     )
 import Cardano.Wallet.Read.Eras.EraFun
     ( EraFun (..)
     )
+import Ouroboros.Consensus.Block.Abstract
+    ( headerPrevHash
+    )
 import Ouroboros.Consensus.Byron.Ledger
-    ( ByronHash
+    ( ByronBlock
+    , ByronHash
     )
 import Ouroboros.Consensus.Shelley.Ledger
     ( ShelleyHash
@@ -84,3 +95,44 @@ getHeaderHashShelley
     -> ShelleyHash StandardCrypto
 getHeaderHashShelley
     (O.ShelleyBlock (Shelley.Block header _) _) = Shelley.pHeaderHash header
+
+type family PrevHeaderHashT era where
+    PrevHeaderHashT ByronEra = O.ChainHash ByronBlock
+    PrevHeaderHashT ShelleyEra = PrevHash StandardCrypto
+    PrevHeaderHashT AllegraEra = PrevHash StandardCrypto
+    PrevHeaderHashT MaryEra = PrevHash StandardCrypto
+    PrevHeaderHashT AlonzoEra = PrevHash StandardCrypto
+    PrevHeaderHashT BabbageEra = PrevHash StandardCrypto
+    PrevHeaderHashT ConwayEra = PrevHash StandardCrypto
+
+newtype PrevHeaderHash era = PrevHeaderHash (PrevHeaderHashT era)
+
+getPrevHeaderHashShelley
+    :: ( Era era
+       , EncCBORGroup (TxSeq era)
+       , EncCBOR (Shelley.ShelleyProtocolHeader proto)
+       , Shelley.ProtocolHeaderSupportsEnvelope proto
+       )
+    => O.ShelleyBlock proto era
+    -> PrevHash (ProtoCrypto proto)
+getPrevHeaderHashShelley (O.ShelleyBlock (Shelley.Block header _) _) =
+    Shelley.pHeaderPrevHash header
+
+getEraPrevHeaderHash :: EraFun Block PrevHeaderHash
+getEraPrevHeaderHash =
+    EraFun
+        { byronFun = \(Block block) ->
+            PrevHeaderHash $ headerPrevHash $ O.getHeader block
+        , shelleyFun = \(Block block) ->
+            PrevHeaderHash $ getPrevHeaderHashShelley block
+        , allegraFun = \(Block block) ->
+            PrevHeaderHash $ getPrevHeaderHashShelley block
+        , maryFun = \(Block block) ->
+            PrevHeaderHash $ getPrevHeaderHashShelley block
+        , alonzoFun = \(Block block) ->
+            PrevHeaderHash $ getPrevHeaderHashShelley block
+        , babbageFun = \(Block block) ->
+            PrevHeaderHash $ getPrevHeaderHashShelley block
+        , conwayFun = \(Block block) ->
+            PrevHeaderHash $ getPrevHeaderHashShelley block
+        }
