b4d983531ba9a07c24743ba021a64e5a4490bbdd
Yura Lazarev <Unisay@users.noreply.github.com>
2023-10-26 12:51:15 +0200

    Refactor latency benchmark script

--- .buildkite/bench-latency.sh
+++ .buildkite/bench-latency.sh
@@ -1,18 +1,7 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p coreutils buildkite-agent
+#! /usr/bin/env bash
 
 set -euo pipefail
 
-cd $(dirname $0)/..
+echo "+++ Build & run benchmark - shelley"
 
-echo "--- Build"
-
-nix build .#ci.benchmarks.latency -o bench-latency-shelley
-
-# Note: the tracing will not work if program output is piped
-# to another process (e.g. "tee").
-# It says "Error: Switchboard's queue full, dropping log items!"
-
-echo "+++ Run benchmark - shelley"
-
-./bench-latency-shelley/bin/latency
+nix run .#ci.benchmarks.latency
