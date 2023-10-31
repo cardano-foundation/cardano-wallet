82175c91a8e0c20c433c0fb77f7325947017c124
Pawel Jakubas <pawel.jakubas@iohk.io>
2023-10-31 13:42:54 +0100

    decryptPayload

--- lib/crypto-hash-extra/src/Crypto/Encryption/ChaChaPoly1305.hs
+++ lib/crypto-hash-extra/src/Crypto/Encryption/ChaChaPoly1305.hs
@@ -3,12 +3,17 @@
 
 module Crypto.Encryption.ChaChaPoly1305
     ( encryptPayload
+    , decryptPayload
     ) where
 
 import Prelude
 
+import Control.Monad
+    ( when
+    )
 import Crypto.Error
-    ( CryptoFailable (..)
+    ( CryptoError (..)
+    , CryptoFailable (..)
     )
 import Data.ByteString
     ( ByteString
@@ -18,32 +23,52 @@ import qualified Codec.CBOR.Encoding as CBOR
 import qualified Codec.CBOR.Write as CBOR
 import qualified Crypto.Cipher.ChaChaPoly1305 as Poly
 import qualified Data.ByteArray as BA
+import qualified Data.ByteString as BS
 
 
--- | ChaCha20/Poly1305 encrypting and signing the payload.
--- |
--- | The caller must ensure that the passphrase length is 32 bytes.
+-- | ChaCha20/Poly1305 encrypting the payload with passphrase and nonce.
+-- | The caller must ensure that the passphrase length is 32 bytes and
+-- | nonce is 12 bytes
 encryptPayload
     :: ByteString
        -- ^ Symmetric key / passphrase, 32-byte long
     -> ByteString
-       -- ^ Nonce
+       -- ^ Nonce, 12-byte long
     -> ByteString
         -- ^ Payload to be encrypted
     -> ByteString
         -- ^ Ciphertext with a 128-bit crypto-tag appended.
-encryptPayload passphrase nounce payload = unsafeSerialize $ do
-    nonce <- Poly.nonce12 nounce
-    st1 <- Poly.finalizeAAD <$> Poly.initialize passphrase nonce
+encryptPayload passphrase nonce payload = unsafeSerialize $ do
+    nonced <- Poly.nonce12 nonce
+    st1 <- Poly.finalizeAAD <$> Poly.initialize passphrase nonced
     let (out, st2) = Poly.encrypt payload st1
     return $ out <> BA.convert (Poly.finalize st2)
   where
     unsafeSerialize :: CryptoFailable ByteString -> ByteString
     unsafeSerialize =
         CBOR.toStrictByteString . CBOR.encodeBytes . useInvariant
-
     -- Encryption fails if the key is the wrong size, but that won't happen
     -- if the key was created with 'xxx'.
     useInvariant = \case
         CryptoPassed res -> res
         CryptoFailed err -> error $ "encryptPayload: " ++ show err
+
+-- | ChaCha20/Poly1305 decrypting with passphrase and nonce.
+-- | The caller must ensure that the passphrase length is 32 bytes and
+-- | nonce is 12 bytes
+decryptPayload
+    :: ByteString
+       -- ^ Symmetric key / passphrase, 32-byte long
+    -> ByteString
+       -- ^ Nonce, 12-byte long
+    -> ByteString
+        -- ^ Payload to be decrypted
+    -> CryptoFailable ByteString
+decryptPayload passphrase nonce bytes = do
+    let (payload, tag) = BS.splitAt (BS.length bytes - 16) bytes
+    nonced <- Poly.nonce12 nonce
+    st1 <- Poly.finalizeAAD <$> Poly.initialize passphrase nonced
+    let (out, st2) = Poly.decrypt payload st1
+    when (BA.convert (Poly.finalize st2) /= tag) $
+        CryptoFailed CryptoError_MacKeyInvalid
+    return out
