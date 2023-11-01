016d739f9a2b83725f6e8300edfe178de31cff21
Jonathan Knowles <mail@jonathanknowles.net>
2023-11-01 02:16:37 +0000

    Make `check-code-format.sh` fail on detection of consecutive blank lines.
    This is consistent with:
    
    https://github.com/input-output-hk/adrestia/blob/master/docs/code/Coding-Standards.md#use-only-a-single-blank-line-between-top-level-definitions

--- .buildkite/check-code-format.sh
+++ .buildkite/check-code-format.sh
@@ -2,8 +2,12 @@
 
 set -euo pipefail
 
-echo "+++ stylish-haskell"
+echo "+++ Check code format: imports"
 
 stylish-haskell -v --config .stylish-haskell.yaml --inplace `git ls-files -- '*.hs'`
+
+echo "+++ Check code format: blank lines"
+
+find . -type f -name '*.hs' -exec sed -i -r '/^$/N;/^\n$/D' {} +
 
 git diff --exit-code
