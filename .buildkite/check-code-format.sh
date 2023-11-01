7dae7af86e63af9a5c1f173fe77425dcc3ef4560
Jonathan Knowles <mail@jonathanknowles.net>
2023-11-01 03:42:01 +0000

    Check for blank lines at the starts of files.

--- .buildkite/check-code-format.sh
+++ .buildkite/check-code-format.sh
@@ -10,6 +10,10 @@ echo "+++ Check code format: no multiple consecutive blank lines"
 
 find . -type f -name '*.hs' -exec sed -i -r '/^$/N;/^\n$/D' {} +
 
+echo "+++ Check code format: no blank lines at the starts of files"
+
+find . -type f -name '*.hs' -exec sed -i -r '/./,$!d' {} +
+
 echo "+++ Check code format: no blank lines at the ends of files"
 
 find . -type f -name '*.hs' -exec sed -i -r -e :a -e '/^\n*$/{$d;N;ba' -e '}' {} +
