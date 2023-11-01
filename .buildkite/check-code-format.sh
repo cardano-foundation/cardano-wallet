feeafa3f36073bb5ea256cc141ef1c1476cf676a
Jonathan Knowles <mail@jonathanknowles.net>
2023-11-01 03:38:22 +0000

    Check for blank lines at the ends of files.

--- .buildkite/check-code-format.sh
+++ .buildkite/check-code-format.sh
@@ -6,8 +6,12 @@ echo "+++ Check code format: imports"
 
 stylish-haskell -v --config .stylish-haskell.yaml --inplace `git ls-files -- '*.hs'`
 
-echo "+++ Check code format: blank lines"
+echo "+++ Check code format: no multiple consecutive blank lines"
 
 find . -type f -name '*.hs' -exec sed -i -r '/^$/N;/^\n$/D' {} +
+
+echo "+++ Check code format: no blank lines at the ends of files"
+
+find . -type f -name '*.hs' -exec sed -i -r -e :a -e '/^\n*$/{$d;N;ba' -e '}' {} +
 
 git diff --exit-code
