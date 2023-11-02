49edb41ae229fccc97f6c629b44824efee6ea219
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2023-11-02 15:18:47 +0100

    Mock `UTxOHistory`

--- lib/customer-deposit-wallet/src/Cardano/Wallet/Deposit/Pure/UTxOHistory.hs
+++ lib/customer-deposit-wallet/src/Cardano/Wallet/Deposit/Pure/UTxOHistory.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeFamilies #-}
 module Cardano.Wallet.Deposit.Pure.UTxOHistory
     ( UTxOHistory
     , empty
@@ -6,4 +7,61 @@ module Cardano.Wallet.Deposit.Pure.UTxOHistory
     , getUTxO
     ) where
 
+{- Note [MockUTxOHistory]
+
+At the moment, this module implements a mock version of
+
 import Cardano.Wallet.DB.Store.UTxOHistory.Model
+
+in order to work with a 'UTxO' that is different from the
+'Cardano.Wallet.Primitive.*' types.
+
+Eventually, the implementation here should subsume the implementation
+in 'UTxOHistory.Model'.
+
+-}
+
+import Prelude
+
+import Cardano.Wallet.Deposit.Pure.UTxO
+    ( DeltaUTxO
+    , UTxO
+    )
+import Data.Delta
+    ( Delta (..)
+    )
+
+import qualified Cardano.Wallet.Deposit.Read as Read
+
+{-----------------------------------------------------------------------------
+    Type and Operations
+------------------------------------------------------------------------------}
+
+newtype UTxOHistory
+    = UTxOHistory {latest :: UTxO}
+    deriving (Eq, Show)
+
+-- | An empty UTxO history.
+empty :: UTxO -> UTxOHistory
+empty = UTxOHistory
+
+-- | Returns the most recent UTxO.
+getUTxO :: UTxOHistory -> UTxO
+getUTxO = latest
+
+{-----------------------------------------------------------------------------
+    Delta type
+------------------------------------------------------------------------------}
+
+-- | Changes to the UTxO history.
+data DeltaUTxOHistory
+    = -- | New slot tip, changes within that block.
+      AppendBlock Read.Slot DeltaUTxO
+
+instance Delta DeltaUTxOHistory where
+    type Base DeltaUTxOHistory = UTxOHistory
+    apply (AppendBlock newTip delta) = appendBlock newTip delta
+
+appendBlock :: Read.Slot -> DeltaUTxO -> UTxOHistory -> UTxOHistory
+appendBlock _newTip delta (UTxOHistory u)
+    = UTxOHistory (apply delta u)
