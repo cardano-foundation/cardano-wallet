b89d5b8cc305de3c518453696079b54e55299bfa
Yura Lazarev <Unisay@users.noreply.github.com>
2023-11-30 14:48:51 +0100

    Integration tests rely on the "recovery-phrase" command provided by the `cardano-address`, not `cardano-wallet`

--- lib/wallet/integration/src/Test/Integration/Framework/DSL.hs
+++ lib/wallet/integration/src/Test/Integration/Framework/DSL.hs
@@ -203,6 +203,7 @@ module Test.Integration.Framework.DSL
     , commandName
     , command
     , cardanoWalletCLI
+    , cardanoAddressCLI
     , generateMnemonicsViaCLI
     , createWalletViaCLI
     , createWalletFromPublicKeyViaCLI
@@ -3088,18 +3089,15 @@ commandName :: String
 commandName = "cardano-wallet"
 
 -- | Run a command using the 'cardano-wallet' executable.
-cardanoWalletCLI
-    :: forall r m. (CmdResult r, MonadIO m)
-    => [String]
-    -> m r
+cardanoWalletCLI :: (CmdResult r, MonadIO m) => [String] -> m r
 cardanoWalletCLI = liftIO . command [EchoStderr False] commandName
 
-generateMnemonicsViaCLI
-    :: forall r m. (CmdResult r, MonadIO m)
-    => [String]
-    -> m r
-generateMnemonicsViaCLI args = cardanoWalletCLI
-    (["recovery-phrase", "generate"] ++ args)
+cardanoAddressCLI :: (CmdResult r, MonadIO m) => [String] -> m r
+cardanoAddressCLI = liftIO . command [EchoStderr False] "cardano-address"
+
+generateMnemonicsViaCLI :: (CmdResult r, MonadIO m) => [String] -> m r
+generateMnemonicsViaCLI args =
+    cardanoAddressCLI (["recovery-phrase", "generate"] ++ args)
 
 createWalletViaCLI
     :: forall s m. (HasType (Port "wallet") s, MonadIO m)
