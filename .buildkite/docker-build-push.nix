59a2c46d5a73fee5802eea584a3b0342312057d0
paolino <paolo.veronelli@gmail.com>
2023-12-12 10:16:19 +0000

    Move docker push to a new tag controlled pipeline

--- .buildkite/docker-build-push.nix
+++ .buildkite/docker-build-push.nix
@@ -78,7 +78,7 @@ in
       tags+=( "${image.imageTag}" )
       tags+=( "latest" )
     elif [[ "$git_tag" = "rc-latest" ]]; then
-      tags+=( "${git_tag}" )
+      tags+=( "$git_tag" )
     else
       echo 'Not pushing docker image because this is neither a rc-latest nor a v20* tag build.'
     fi
