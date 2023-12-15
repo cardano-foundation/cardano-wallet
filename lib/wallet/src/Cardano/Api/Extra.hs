c0bf3d311036e7a976681274993e8d76d0440602
Jonathan Knowles <mail@jonathanknowles.net>
2023-12-15 05:35:07 +0000

    Delete unused code in `Cardano.Api.Extra`.

--- lib/wallet/src/Cardano/Api/Extra.hs
+++ lib/wallet/src/Cardano/Api/Extra.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE Rank2Types #-}
 
 -- |
@@ -9,36 +8,15 @@
 -- Module containing extra 'Cardano.Api' functionality needed by the wallet.
 module Cardano.Api.Extra
     ( inAnyCardanoEra
-    , unbundleLedgerShelleyBasedProtocolParams
     ) where
 
 import Cardano.Api
-    ( BundledProtocolParameters (..)
-    , InAnyCardanoEra (..)
+    ( InAnyCardanoEra (..)
     , IsCardanoEra (cardanoEra)
-    , ShelleyBasedEra (..)
     , Tx
     )
-import Cardano.Api.Shelley
-    ( ShelleyLedgerEra
-    )
-
-import qualified Cardano.Ledger.Core as Ledger
 
 -- | Helper function for more easily creating an existential
 -- @InAnyCardanoEra Tx@.
 inAnyCardanoEra :: IsCardanoEra era => Tx era -> InAnyCardanoEra Tx
 inAnyCardanoEra = InAnyCardanoEra cardanoEra
-
--- Not exposed by cardano-api
-unbundleLedgerShelleyBasedProtocolParams
-  :: ShelleyBasedEra era
-  -> BundledProtocolParameters era
-  -> Ledger.PParams (ShelleyLedgerEra era)
-unbundleLedgerShelleyBasedProtocolParams = \case
-  ShelleyBasedEraShelley -> \(BundleAsShelleyBasedProtocolParameters _ _ lpp) -> lpp
-  ShelleyBasedEraAllegra -> \(BundleAsShelleyBasedProtocolParameters _ _ lpp) -> lpp
-  ShelleyBasedEraMary -> \(BundleAsShelleyBasedProtocolParameters _ _ lpp) -> lpp
-  ShelleyBasedEraAlonzo -> \(BundleAsShelleyBasedProtocolParameters _ _ lpp) -> lpp
-  ShelleyBasedEraBabbage -> \(BundleAsShelleyBasedProtocolParameters _ _ lpp) -> lpp
-  ShelleyBasedEraConway -> \(BundleAsShelleyBasedProtocolParameters _ _ lpp) -> lpp
