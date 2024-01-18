3080252abc11288b806869a0f2797be1ad03157f
paolino <paolo.veronelli@gmail.com>
2024-01-18 08:34:24 +0000

    Bump hls to 2.5 for ghc 9.6.3

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -150,12 +150,7 @@ CHaP: haskell-nix: nixpkgs-recent: nodePkgs: haskell-nix.cabalProject' [
           (haskell-nix.tool "ghc8107" "stylish-haskell" "0.11.0.3")
           (haskell-nix.tool "ghc963" "haskell-language-server" ({pkgs, ...}: rec {
             # Use the github source of HLS that is tested with haskell.nix CI
-            src = pkgs.haskell-nix.sources."hls-2.0";
-            # `tool` normally ignores the `cabal.project` (if there is one in the hackage source).
-            # We need to use the github one (since it has settings to make hls build).
-            cabalProject = __readFile (src + "/cabal.project");
-            # sha256 for a `source-repository-package` in the `cabal.project` file.
-            sha256map."https://github.com/pepeiborra/ekg-json"."7a0af7a8fd38045fd15fb13445bdcc7085325460" = "sha256-fVwKxGgM0S4Kv/4egVAAiAjV7QB5PBqMVMCfsv7otIQ=";
+            src = pkgs.haskell-nix.sources."hls-2.5";
             }))
         ]);
         shellHook = "export LOCAL_CLUSTER_CONFIGS=${localClusterConfigs}";
