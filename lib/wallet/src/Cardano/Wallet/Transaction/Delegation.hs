80e86a3e4bb1671f94ecb9934316375b31d6a208
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-01-19 13:38:01 +0100

    Fix and remove `mkDelegationCertificates`

--- lib/wallet/src/Cardano/Wallet/Transaction/Delegation.hs
+++ lib/wallet/src/Cardano/Wallet/Transaction/Delegation.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+
 -- |
 -- Copyright: © 2024 Cardano Foundation
 -- License: Apache-2.0
@@ -5,9 +8,7 @@
 -- Tools for creating transactions that change the delegation status.
 --
 module Cardano.Wallet.Transaction.Delegation
-    ( toStakePoolDlgCert
-    , toStakeKeyRegCert
-    , toStakeKeyDeregCert
+    ( certificateFromDelegationAction
     ) where
 
 import Prelude
@@ -20,12 +21,17 @@ import Cardano.Address.Script
     ( KeyHash (..)
     , Script (..)
     )
+import Cardano.Crypto.Hash.Class
+    ( Hash (UnsafeHash)
+    )
 import Cardano.Wallet.Primitive.Ledger.Shelley
     ( toCardanoSimpleScript
     )
 import Cardano.Wallet.Primitive.Types.Pool
     ( PoolId (..)
-    , PoolOwner (..)
+    )
+import Cardano.Wallet.Transaction
+    ( DelegationAction (..)
     )
 import Crypto.Hash.Extra
     ( blake2b224
@@ -36,61 +42,79 @@ import Data.ByteString.Short
 
 import qualified Cardano.Api as Cardano
 import qualified Cardano.Api.Shelley as Cardano
+import qualified Cardano.Ledger.Keys as Ledger
+import qualified Internal.Cardano.Write.Tx as Write
 
 {-----------------------------------------------------------------------------
     Cardano.Certificate
 ------------------------------------------------------------------------------}
-
-toStakePoolDlgCert
-    :: Either XPub (Script KeyHash) -> PoolId -> Cardano.Certificate
-toStakePoolDlgCert cred (PoolId pid) = case cred of
-    Left xpub ->
-        Cardano.makeStakeAddressDelegationCertificate
-        (Cardano.StakeCredentialByKey $ Cardano.StakeKeyHash (toKeyHash xpub))
-        (Cardano.StakePoolKeyHash pool)
-    Right script ->
-        Cardano.makeStakeAddressDelegationCertificate
-        (Cardano.StakeCredentialByScript
-        . Cardano.hashScript
-        . Cardano.SimpleScript
-        $ toCardanoSimpleScript script)
-        (Cardano.StakePoolKeyHash pool)
+certificateFromDelegationAction
+    :: Write.RecentEra era
+        -- ^ Era in which we create the certificate
+    -> Either XPub (Script KeyHash)
+        -- ^ Our staking credential
+    -> DelegationAction
+        -- ^ Delegation action that we plan to take
+    -> [Cardano.Certificate (Write.CardanoApiEra era)]
+        -- ^ Certificates representing the action
+certificateFromDelegationAction Write.RecentEraBabbage cred = \case
+    Join poolId ->
+        [ Cardano.makeStakeAddressDelegationCertificate
+            $ Cardano.StakeDelegationRequirementsPreConway
+                babbageWitness
+                (toCardanoStakeCredential cred)
+                (toCardanoPoolId poolId)
+        ]
+    JoinRegisteringKey poolId ->
+        [ Cardano.makeStakeAddressRegistrationCertificate
+            $ Cardano.StakeAddrRegistrationPreConway
+                babbageWitness
+                (toCardanoStakeCredential cred)
+        , Cardano.makeStakeAddressDelegationCertificate
+            $ Cardano.StakeDelegationRequirementsPreConway
+                babbageWitness
+                (toCardanoStakeCredential cred)
+                (toCardanoPoolId poolId)
+        ]
+    Quit ->
+        [ Cardano.makeStakeAddressUnregistrationCertificate
+            $ Cardano.StakeAddrRegistrationPreConway
+                babbageWitness
+                (toCardanoStakeCredential cred)
+        ]
   where
-    toKeyHash = SL.KeyHash . UnsafeHash . toShort . blake2b224 . xpubPublicKey
-    pool = SL.KeyHash $ UnsafeHash $ toShort pid
+    babbageWitness = Cardano.ShelleyToBabbageEraBabbage
+certificateFromDelegationAction Write.RecentEraConway _cred =
+    error "certificateFromDelegationAction: not supported in Conway yet"
+  where
+    _conwayWitness = Cardano.ConwayEraOnwardsConway
 
-toStakeKeyRegCert :: Either XPub (Script KeyHash) -> Cardano.Certificate
-toStakeKeyRegCert cred = case cred of
+{-----------------------------------------------------------------------------
+    Cardano.StakeCredential
+------------------------------------------------------------------------------}
+
+toCardanoStakeCredential
+    :: Either XPub (Script KeyHash) -> Cardano.StakeCredential
+toCardanoStakeCredential = \case
     Left xpub ->
-        Cardano.makeStakeAddressRegistrationCertificate
-        . Cardano.StakeCredentialByKey
-        . Cardano.StakeKeyHash
-        . SL.KeyHash
-        . UnsafeHash
-        . toShort
-        . blake2b224
-        $ xpubPublicKey xpub
+        Cardano.StakeCredentialByKey
+        . toHashStakeKey
+        $ xpub
     Right script ->
-        Cardano.makeStakeAddressRegistrationCertificate
-        . Cardano.StakeCredentialByScript
+        Cardano.StakeCredentialByScript
         . Cardano.hashScript
         . Cardano.SimpleScript
         $ toCardanoSimpleScript script
 
-toStakeKeyDeregCert :: Either XPub (Script KeyHash) -> Cardano.Certificate
-toStakeKeyDeregCert = \case
-    Left xpub ->
-        Cardano.makeStakeAddressUnregistrationCertificate
-        . Cardano.StakeCredentialByKey
-        . Cardano.StakeKeyHash
-        . SL.KeyHash
-        . UnsafeHash
-        . toShort
-        . blake2b224
-        $ xpubPublicKey xpub
-    Right script ->
-        Cardano.makeStakeAddressUnregistrationCertificate
-        . Cardano.StakeCredentialByScript
-        . Cardano.hashScript
-        . Cardano.SimpleScript
-        $ toCardanoSimpleScript script
+toCardanoPoolId :: PoolId -> Cardano.PoolId
+toCardanoPoolId (PoolId pid) =
+    Cardano.StakePoolKeyHash . Ledger.KeyHash . UnsafeHash $ toShort pid
+
+toHashStakeKey :: XPub -> Cardano.Hash Cardano.StakeKey
+toHashStakeKey =
+    Cardano.StakeKeyHash
+    . Ledger.KeyHash
+    . UnsafeHash
+    . toShort
+    . blake2b224
+    . xpubPublicKey
