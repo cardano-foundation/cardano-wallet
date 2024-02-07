9b6118cc03c6ee428d38754156dd9af84de863b9
paolino <paolo.veronelli@gmail.com>
2024-02-07 14:35:47 +0000

    Bump hls to 2.6.0.0 with patched branch

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -1,7 +1,7 @@
 ############################################################################
 # Builds Haskell packages with Haskell.nix
 ############################################################################
-CHaP: haskell-nix: nixpkgs-recent: nodePkgs: haskell-nix.cabalProject' [
+hls: CHaP: haskell-nix: nixpkgs-recent: nodePkgs: haskell-nix.cabalProject' [
   ({ lib, pkgs, buildProject, ... }: {
     options = {
       gitrev = lib.mkOption {
@@ -23,6 +23,7 @@ CHaP: haskell-nix: nixpkgs-recent: nodePkgs: haskell-nix.cabalProject' [
         description = ''If false, prevent check results from being cached on `nix build`'';
         default = true;
       };
+
     };
   })
   ({ pkgs
@@ -98,6 +99,7 @@ CHaP: haskell-nix: nixpkgs-recent: nodePkgs: haskell-nix.cabalProject' [
       indexState = "2024-01-12T11:04:55Z";
 
       localClusterConfigs = config.src + /lib/local-cluster/test/data/cluster-configs;
+
     in {
       name = "cardano-wallet";
       compiler-nix-name = "ghc963";
@@ -151,7 +153,7 @@ CHaP: haskell-nix: nixpkgs-recent: nodePkgs: haskell-nix.cabalProject' [
 
           (haskell-nix.tool "ghc963" "haskell-language-server" ({pkgs, ...}: rec {
             # Use the github source of HLS that is tested with haskell.nix CI
-            src = pkgs.haskell-nix.sources."hls-2.5";
+            src = hls;
             }))
         ]);
         shellHook = "export LOCAL_CLUSTER_CONFIGS=${localClusterConfigs}";
