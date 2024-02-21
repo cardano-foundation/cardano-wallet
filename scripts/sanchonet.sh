7c2755bfd6d5ef926a6aa7257092c06b0dc87267
paolino <paolo.veronelli@gmail.com>
2024-02-21 16:19:27 +0000

    Add sanchonet starting script
    Co-authored-by: Heinrich Apfelmus <heinrich.apfelmus@iohk.io>

---
+++ scripts/sanchonet.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+export WALLET_TAG=rc-latest
+export NODE_TAG=8.7.3
+export WALLET_PORT=8090
+export NETWORK=sanchonet
+export WALLET_DB=/tmp/cardano-wallet-sanchonet.db
+export NODE_DB=/tmp/cardano-node-sanchonet.db
+
+docker-compose pull
+
+docker-compose up
