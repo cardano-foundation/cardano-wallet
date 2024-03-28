448090428f6b89225a8a8a724b8ba4c42ac00254
Pawel Jakubas <pawel.jakubas@iohk.io>
2024-03-28 17:48:36 +0100

    first golden passing

--- lib/unit/test/unit/Cryptography/Cipher/AES256CBCSpec.hs
+++ lib/unit/test/unit/Cryptography/Cipher/AES256CBCSpec.hs
@@ -15,13 +15,32 @@ import Cryptography.Cipher.AES256CBC
     , paddingPKCS7
     , unpaddingPKCS7
     )
+import Cryptography.Hash.Core
+    ( SHA256 (..)
+    )
+import Cryptography.KDF.PBKDF2
+    ( PBKDF2Config (..)
+    , generateKey
+    )
+import Data.ByteArray.Encoding
+    ( Base (..)
+    , convertFromBase
+    , convertToBase
+    )
 import Data.ByteString
     ( ByteString
+    )
+import Data.Either.Combinators
+    ( rightToMaybe
+    )
+import Data.Text
+    ( Text
     )
 import Test.Hspec
     ( Spec
     , describe
     , it
+    , shouldBe
     )
 import Test.QuickCheck
     ( Arbitrary (..)
@@ -34,6 +53,8 @@ import Test.QuickCheck
     )
 
 import qualified Data.ByteString as BS
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 
 spec :: Spec
 spec = do
@@ -74,6 +95,59 @@ spec = do
         \(CipherWrongSetup payload' key' iv') -> do
             decrypt WithoutPadding key' iv' payload' ===
                 Left WrongPayloadSize
+
+    describe "Golden Tests obtained by openssl" $ do
+        it "golden for key and iv with salt" $
+            tohex (saltKey, saltIv) `shouldBe`
+            ( "26A1A6F599173BAF863F97F2A18AF2FD8E99104E726D9EB682FCB7ED53517CF9"
+            , "131BAB9240C168CC60A9F6DFA8CA5A6D"
+            )
+
+        it "golden for key and iv with salt" $
+            tohex (noSaltKey, noSaltIv) `shouldBe`
+            ( "E11244295150E6713CD76E9A5112347093BDB6ACBF0C8021ABAE29881130B210"
+            , "6B7F0C406297F0D90E3BD65AD1FB94BA"
+            )
+
+         -- echo -n 0 | openssl enc -e -aes-256-cbc -pbkdf2 -iter 10000 -a -k "password" -nosalt -v
+         -- ZMGELuBkqtHVw5pUA353vQ==
+         -- bytes read   :        1
+         -- bytes written:       25
+        it "golden 1-byte payload" $
+            checkEncryption GoldenCase
+            { input = "0"
+            , key = noSaltKey
+            , iv = noSaltIv
+            } `shouldBe`
+            Right "ZMGELuBkqtHVw5pUA353vQ=="
+  where
+    --  We are using the following key and iv for goldens
+    -- $ openssl enc -aes-256-cbc -pbkdf2 -pass stdin -P -S 3030303030303030 -k "password" -iter 10000 -P
+    -- salt=3030303030303030
+    -- key=26A1A6F599173BAF863F97F2A18AF2FD8E99104E726D9EB682FCB7ED53517CF9
+    -- iv =131BAB9240C168CC60A9F6DFA8CA5A6D
+    (saltKey, saltIv) = generateKey (PBKDF2Config SHA256 10000 32 16) "password" (fromHex "3030303030303030")
+
+    -- $ openssl enc -aes-256-cbc -pbkdf2 -pass stdin -P -nosalt -k "password" -iter 10000 -P
+    -- key=E11244295150E6713CD76E9A5112347093BDB6ACBF0C8021ABAE29881130B210
+    -- iv =6B7F0C406297F0D90E3BD65AD1FB94BA
+    (noSaltKey, noSaltIv) = generateKey (PBKDF2Config SHA256 10000 32 16) "password" Nothing
+
+    fromHex = rightToMaybe . convertFromBase Base16 . T.encodeUtf8
+    tohex :: (ByteString, ByteString) -> (Text, Text)
+    tohex (a, b) =
+        let convert = T.toUpper . T.decodeUtf8 . convertToBase Base16
+        in (convert a, convert b)
+
+    checkEncryption GoldenCase {..} =
+        T.decodeUtf8 . convertToBase Base64
+        <$> encrypt WithPadding key iv input
+
+data GoldenCase = GoldenCase
+    { input :: ByteString
+    , key :: ByteString
+    , iv :: ByteString
+    } deriving (Show, Eq)
 
 newtype Payload = Payload
     { unPayload :: ByteString } deriving (Eq, Show)
