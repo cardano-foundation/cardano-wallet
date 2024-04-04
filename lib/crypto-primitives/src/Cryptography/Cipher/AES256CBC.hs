10444b6b3edf9ea97495203ea6b5bdc02825965c
Jonathan Knowles <mail@jonathanknowles.net>
2024-04-04 06:55:55 +0000

    Minor tweaks to documentation.

--- lib/crypto-primitives/src/Cryptography/Cipher/AES256CBC.hs
+++ lib/crypto-primitives/src/Cryptography/Cipher/AES256CBC.hs
@@ -1,5 +1,37 @@
 {-# LANGUAGE FlexibleContexts #-}
 
+-- | Provides support for AES 256.
+--
+-- AES is a subset of the family of block ciphers known as Rijndael ciphers.
+--
+-- That family includes no less than 15 variants, for three possible block
+-- sizes (128, 192 and 256 bits).
+--
+-- Three are supported by cryptonite: AES128, AES192 and AES256 (used here).
+--
+-- There are five possible key sizes (128, 160, 192, 224 and 256 bits).
+--
+-- In case of the cryptonite implementation we have the following constraints:
+-- (AES128 - 16 bytes, AES192 - 24 bytes, AES 256 - 32 bytes).
+--
+-- AES, as standardized by NIST, includes only three variants, all with 128-bit
+-- blocks, and with keys of 128, 192, or 256 bits. (The same as adopted in
+-- cryptonite.)
+--
+-- The block size of AES is always 128 bits, so a 256-bit initialisation vector
+-- (IV) is not possible for most modes of operation. These are the reasons we
+-- require the secret key to be 32 bytes and IV 16 bytes for AES256 used here.
+--
+-- Additionally, input length must be a multiple of block size, ie., 16 bytes.
+--
+-- In order to realize this PKCS#7 padding is made available as an option.
+-- This padding is preferred because it is unambiguous and widely used (for
+-- example in OpenSSL).
+--
+-- One can always read the last byte and remove that number of padding bytes,
+-- and then detect if the decryption was successful by checking that all of
+-- the bytes removed have the same value.
+--
 module Cryptography.Cipher.AES256CBC
     ( CipherMode (..)
     , CipherError (..)
@@ -42,28 +74,6 @@ import Data.Either.Extra
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as B8
 
--- | AES is a subset of the family of block ciphers known as Rijndael.
--- That family includes no less than 15 variants, for three possible block
--- sizes (128, 192 and 256 bits).
--- Three are supported by cryptonite, AES128, AES192 and AES256 (used here).
--- There are five possible key sizes (128, 160, 192, 224 and 256 bits).
--- In case of cryptonite implementation we have the following constraints:
--- (AES128 - 16 bytes, AES192 - 24 bytes, AES 256 - 32 bytes).
--- AES, as standardized by NIST, includes only three variants, all with 128-bit
--- blocks,
--- and with keys of 128, 192 or 256 bits.
--- The same as adopted in cryptonite.
--- The block size of AES is always 128 bits, so a 256 bit IV is not possible
--- for most modes of operation. These are the reasons we require the secret key
--- to be 32 bytes and iv 16 bytes for AES256 used here.
--- Additionally, input length must be a multiple of block size, ie., 16 bytes.
--- In order to realize this PKCS#7 padding is available as an option.
--- This padding is preferred because it is unambiguous and widely used (for
--- example in OpenSSL).
--- One can always read the last byte and remove that number of padding bytes,
--- and then detect if the decryption was successful by checking that all of
--- the bytes removed have the same value.
-
 data CipherMode =
     WithoutPadding | WithPadding
     deriving (Eq, Show)
@@ -74,7 +84,7 @@ data CipherError =
     | WrongPayloadSize
     deriving (Eq, Show)
 
--- | Initialize a block cipher
+-- | Initialise a block cipher.
 initCipher :: ByteString -> Either CryptoError AES256
 initCipher k = case cipherInit k of
     CryptoFailed e -> Left e
@@ -88,13 +98,12 @@ createIV =
 -- | Encrypt using AES256 using CBC mode
 encrypt
     :: CipherMode
-    -- ^ with or without padding
     -> ByteString
-    -- ^ secret key, needs to be 32 bytes
+    -- ^ Secret key: must be 32 bytes.
     -> ByteString
-    -- ^ initialisation vector (IV): must be 16 bytes
+    -- ^ Initialisation vector (IV): must be 16 bytes.
     -> ByteString
-    -- ^ payload, must be a multiple of a block size, ie., 16 bytes
+    -- ^ Payload: must be a multiple of a block size, ie., 16 bytes.
     -> Either CipherError ByteString
 encrypt mode key iv msg = do
    when (mode == WithoutPadding && BS.length msg `mod` 16 /= 0) $
@@ -106,16 +115,15 @@ encrypt mode key iv msg = do
    msg' <- maybeToRight EmptyPayload msgM
    mapBoth FromCryptonite (\c -> cbcEncrypt c initedIV msg') (initCipher key)
 
--- | Decrypt using AES256 using CBC mode
+-- | Decrypt using AES256 using CBC mode.
 decrypt
     :: CipherMode
-    -- ^ with or without padding
     -> ByteString
-    -- ^ secret key, needs to be 32 bytes
+    -- ^ Secret key: must be 32 bytes.
     -> ByteString
-    -- ^ iv, needs to be 16 bytes
+    -- ^ Initialisation vector (IV): must be 16 bytes.
     -> ByteString
-    -- ^ payload, must be a multiple of a block size, ie., 16 bytes
+    -- ^ Payload: must be a multiple of a block size, ie., 16 bytes.
     -> Either CipherError ByteString
 decrypt mode key iv msg = do
    when (mode == WithoutPadding && BS.length msg `mod` 16 /= 0) $
