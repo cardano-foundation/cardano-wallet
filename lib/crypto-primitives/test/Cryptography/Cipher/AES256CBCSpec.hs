2633f5102d155636109d62029600c7760a11739f
Pawel Jakubas <pawel.jakubas@iohk.io>
2024-04-04 17:13:05 +0200

    golden - part 2

--- lib/crypto-primitives/test/Cryptography/Cipher/AES256CBCSpec.hs
+++ lib/crypto-primitives/test/Cryptography/Cipher/AES256CBCSpec.hs
@@ -299,6 +299,60 @@ spec = do
             , salt = fromHex "3030303030303030"
             } `shouldBe`
             Right "U2FsdGVkX18wMDAwMDAwMPCoW2E+adl2BLopcz8iftA="
+
+        -- echo -n 0000000000000000 | openssl enc -e -aes-256-cbc -pbkdf2 -iter 10000 -a -k "password" -S 3030303030303030 -v
+        -- U2FsdGVkX18wMDAwMDAwMEmgP1lu1tbBnNx984qKn/KQzjySUk4bPGtysOLjtex2
+        -- bytes read   :       16
+        -- bytes written:       6
+        it "golden 16-byte payload salted" $
+            checkEncryption GoldenCase
+            { input = "0000000000000000"
+            , key = saltKey
+            , iv = saltIv
+            , salt = fromHex "3030303030303030"
+            } `shouldBe`
+            Right "U2FsdGVkX18wMDAwMDAwMEmgP1lu1tbBnNx984qKn/KQzjySUk4bPGtysOLjtex2"
+
+        -- echo -n 00000000000000000000 | openssl enc -e -aes-256-cbc -pbkdf2 -iter 10000 -a -k "password" -S 3030303030303030 -v
+        -- U2FsdGVkX18wMDAwMDAwMEmgP1lu1tbBnNx984qKn/LR9tAdmXupg7jaO09x75x+
+        -- bytes read   :       20
+        -- bytes written:       65
+        it "golden 20-byte payload salted" $
+            checkEncryption GoldenCase
+            { input = "00000000000000000000"
+            , key = saltKey
+            , iv = saltIv
+            , salt = fromHex "3030303030303030"
+            } `shouldBe`
+            Right "U2FsdGVkX18wMDAwMDAwMEmgP1lu1tbBnNx984qKn/LR9tAdmXupg7jaO09x75x+"
+
+        -- echo -n 0000000000000000000000000000000 | openssl enc -e -aes-256-cbc -pbkdf2 -iter 10000 -a -k "password" -S 3030303030303030 -v
+        -- U2FsdGVkX18wMDAwMDAwMEmgP1lu1tbBnNx984qKn/I6rOQXQsrifJ04fr1+IKEk
+        -- bytes read   :       31
+        -- bytes written:       65
+        it "golden 31-byte payload salted" $
+            checkEncryption GoldenCase
+            { input = "0000000000000000000000000000000"
+            , key = saltKey
+            , iv = saltIv
+            , salt = fromHex "3030303030303030"
+            } `shouldBe`
+            Right "U2FsdGVkX18wMDAwMDAwMEmgP1lu1tbBnNx984qKn/I6rOQXQsrifJ04fr1+IKEk"
+
+        -- echo -n 00000000000000000000000000000000 | openssl enc -e -aes-256-cbc -pbkdf2 -iter 10000 -a -k "password" -S 3030303030303030 -v
+        -- U2FsdGVkX18wMDAwMDAwMEmgP1lu1tbBnNx984qKn/K9sJJpdKMH4wS0xz8+OUa3
+        -- 8Hj/N4XOSY5x6a+7PCWwtg==
+        -- bytes read   :       32
+        -- bytes written:       90
+        it "golden 32-byte payload salted" $
+            checkEncryption GoldenCase
+            { input = "00000000000000000000000000000000"
+            , key = saltKey
+            , iv = saltIv
+            , salt = fromHex "3030303030303030"
+            } `shouldBe`
+            Right
+            "U2FsdGVkX18wMDAwMDAwMEmgP1lu1tbBnNx984qKn/K9sJJpdKMH4wS0xz8+OUa38Hj/N4XOSY5x6a+7PCWwtg=="
   where
     --  We are using the following key and iv for goldens
     -- $ openssl enc -aes-256-cbc -pbkdf2 -pass stdin -P -S 3030303030303030 -k "password" -iter 10000 -P
