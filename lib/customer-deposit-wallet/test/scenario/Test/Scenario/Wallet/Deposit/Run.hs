756a7fa7403d2e4fef34bd54f87b1927916b8953
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-04-09 13:04:40 +0200

    Add "Use case: Centralized Exchanges" to Deposit Wallet

--- lib/customer-deposit-wallet/test/scenario/Test/Scenario/Wallet/Deposit/Run.hs
+++ lib/customer-deposit-wallet/test/scenario/Test/Scenario/Wallet/Deposit/Run.hs
@@ -32,6 +32,7 @@ import Test.Scenario.Blockchain
     )
 
 import qualified Cardano.Wallet.Deposit.IO as Wallet
+import qualified Test.Scenario.Wallet.Deposit.Exchanges as Exchanges
 
 main :: IO ()
 main =
@@ -40,10 +41,25 @@ main =
 
 scenarios :: SpecWith ScenarioEnv
 scenarios = do
+    describe "Scenarios for centralized exchanges" $ do
+        it "Restore a wallet" $ \env ->
+            withWalletEnvMock env $
+                Exchanges.scenarioRestore xpub
+
+        it "Start a wallet" $ \env ->
+            withWalletEnvMock env $ \w -> do
+                Exchanges.scenarioRestore xpub w
+                Exchanges.scenarioStart w
+
+        it "Assign an address to a customer ID" $ \env -> do
+            withWalletEnvMock env $ \walletEnv ->
+                Wallet.withWalletInit walletEnv xpub 1
+                    Exchanges.scenarioCreateAddressList
+
     describe "Temporary tests" $ do
         it "Wallet receives funds that are sent to customer address" $ \env -> do
             withWalletEnvMock env $ \walletEnv ->
-                Wallet.withWalletInit xpub 1 walletEnv $
+                Wallet.withWalletInit walletEnv xpub 1 $
                     testBalance env
 
 xpub :: XPub
