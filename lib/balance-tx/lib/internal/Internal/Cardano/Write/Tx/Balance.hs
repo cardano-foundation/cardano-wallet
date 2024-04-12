cc87c6d62172b91c1d9b4de4b60501a6a279f402
Jonathan Knowles <mail@jonathanknowles.net>
2024-04-12 12:07:33 +0000

    Use named field puns when pattern matching on `UTxOIndex`.

--- lib/balance-tx/lib/internal/Internal/Cardano/Write/Tx/Balance.hs
+++ lib/balance-tx/lib/internal/Internal/Cardano/Write/Tx/Balance.hs
@@ -635,7 +635,7 @@ balanceTransactionWithSelectionStrategyAndNoZeroAdaAdjustment
     pp
     timeTranslation
     utxoAssumptions
-    (UTxOIndex availableUTxO availableUTxOIndex)
+    UTxOIndex {availableUTxO, availableUTxOIndex}
     genChange
     s
     selectionStrategy
