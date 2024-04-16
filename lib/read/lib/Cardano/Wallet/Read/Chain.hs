aa9f40ed42e73be27541b9b397ac3c7fca6422b4
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-04-16 14:15:12 +0200

    Add `ChainTip` type

--- lib/read/lib/Cardano/Wallet/Read/Chain.hs
+++ lib/read/lib/Cardano/Wallet/Read/Chain.hs
@@ -6,17 +6,26 @@ License: Apache-2.0
 Data types relating to the consensus about the blockchain.
 -}
 module Cardano.Wallet.Read.Chain
-    ( ChainPoint (GenesisPoint, BlockPoint)
+    ( -- * ChainPoint
+      ChainPoint (GenesisPoint, BlockPoint)
     , getChainPoint
     , prettyChainPoint
+    , chainPointFromChainTip
+
+    -- * ChainTip
+    , ChainTip (GenesisTip, BlockTip)
+    , getChainTip
+    , prettyChainTip
     ) where
 
 import Prelude
 
 import Cardano.Wallet.Read.Block
     ( Block
+    , BlockNo (..)
     , RawHeaderHash
     , SlotNo (..)
+    , getEraBlockNo
     , getEraHeaderHash
     , getEraSlotNo
     , getRawHeaderHash
@@ -62,3 +71,42 @@ prettyChainPoint (BlockPoint slot hash) =
   where
     hashF = T.take 8 . Hash.hashToTextAsHex
     slotF (SlotNo n) = T.pack (show n)
+
+chainPointFromChainTip :: ChainTip -> ChainPoint
+chainPointFromChainTip GenesisTip = GenesisPoint
+chainPointFromChainTip (BlockTip slot hash _) = BlockPoint slot hash
+
+{-----------------------------------------------------------------------------
+    Tip
+------------------------------------------------------------------------------}
+
+-- | Used in chain-sync protocol to advertise the tip of the server's chain.
+-- Records the 'ChainPoint' and the 'BlockNo' of the block.
+data ChainTip
+    = GenesisTip
+    | BlockTip !SlotNo !RawHeaderHash !BlockNo
+    deriving (Eq, Ord, Show, Generic)
+
+instance NoThunks ChainTip
+
+{-# INLINABLE getChainTip #-}
+getChainTip :: IsEra era => Block era -> ChainTip
+getChainTip block =
+    BlockTip
+        (getEraSlotNo block)
+        (getRawHeaderHash $ getEraHeaderHash block)
+        (getEraBlockNo block)
+
+-- | Short printed representation of a 'ChainPoint'.
+prettyChainTip :: ChainTip -> T.Text
+prettyChainTip GenesisTip =
+    "[tip genesis]"
+prettyChainTip (BlockTip slotNo hash blockNo) =
+    "[tip " <> hashF hash
+        <> " at slot " <> slotNoF slotNo
+        <> " at blockNo " <> blockNoF blockNo
+        <> "]"
+  where
+    hashF = T.take 8 . Hash.hashToTextAsHex
+    slotNoF (SlotNo n) = T.pack (show n)
+    blockNoF (BlockNo n) = T.pack (show n)
