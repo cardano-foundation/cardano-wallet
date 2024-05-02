ea089d4fb854248259434a9020518170fcbb539c
paolino <paolo.veronelli@gmail.com>
2024-05-02 12:55:28 +0000

    Add MaybeK type in the launcher lib

---
+++ lib/launcher/src/Data/MaybeK.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveFunctor #-}
+
+module Data.MaybeK
+    ( MaybeK (..)
+    , IsMaybe (..)
+    , maybeFromMaybeK
+    )
+where
+
+import Prelude
+
+-- | A type level tag to indicate whether a value is present or not.
+data IsMaybe = IsJust | IsNothing
+
+-- | A Maybe carring the type level tag.
+data MaybeK k a where
+    NothingK :: MaybeK IsNothing a
+    JustK :: a -> MaybeK IsJust a
+
+deriving instance Show a => Show (MaybeK k a)
+deriving instance Eq a => Eq (MaybeK k a)
+deriving instance Functor (MaybeK k)
+
+-- | Convert a MaybeK to a Maybe, dropping the type level tag.
+maybeFromMaybeK :: MaybeK k a -> Maybe a
+maybeFromMaybeK NothingK = Nothing
+maybeFromMaybeK (JustK x) = Just x
