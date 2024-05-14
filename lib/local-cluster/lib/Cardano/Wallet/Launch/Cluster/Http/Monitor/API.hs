6d806f2d1223ca2e3d3ba3566b43068f2d14d2f8
paolino <paolo.veronelli@gmail.com>
2024-05-14 16:07:12 +0000

    Report `RunningNode` in place of `RelayNode` on the `observe` api when the cluster is ready

--- lib/local-cluster/lib/Cardano/Wallet/Launch/Cluster/Http/Monitor/API.hs
+++ lib/local-cluster/lib/Cardano/Wallet/Launch/Cluster/Http/Monitor/API.hs
@@ -21,12 +21,23 @@ where
 
 import Prelude
 
+import Cardano.Launcher.Node
+    ( cardanoNodeConn
+    , nodeSocketFile
+    )
 import Cardano.Wallet.Launch.Cluster.Http.Monitor.OpenApi
     ( monitorStateSchema
     , observationSchema
     )
 import Cardano.Wallet.Launch.Cluster.Monitoring.Phase
     ( History (..)
+    , Phase (..)
+    )
+import Cardano.Wallet.Launch.Cluster.Node.RunningNode
+    ( RunningNode (..)
+    )
+import Control.Applicative
+    ( asum
     )
 import Control.Monitoring.Tracing
     ( MonitorState (..)
@@ -49,8 +60,17 @@ import Data.OpenApi
     ( NamedSchema (..)
     , ToSchema (..)
     )
+import Data.Text
+    ( Text
+    )
 import GHC.Generics
     ( Generic (..)
+    )
+import Ouroboros.Network.Magic
+    ( NetworkMagic (..)
+    )
+import Ouroboros.Network.NodeToClient
+    ( NodeToClientVersionData (..)
     )
 import Servant
     ( Post
@@ -62,6 +82,8 @@ import Servant.API
     , (:<|>)
     , (:>)
     )
+
+import qualified Data.Map as Map
 
 type ReadyAPI = "ready" :> Get '[JSON] Bool
 type StepAPI = "control" :> "step" :> PostNoContent
@@ -81,7 +103,52 @@ renderHistory History{history} = toJSON $ do
     pure
         $ object
             [ "time" .= time
-            , "phase" .= phase
+            , "phase" .= renderPhase phase
+            ]
+
+renderTagged :: Text -> Value
+renderTagged tagName =
+    object
+        [ "tag" .= tagName
+        ]
+
+renderTaggedWithContent :: Text -> (Maybe Value) -> Value
+renderTaggedWithContent tagName mContent =
+    object
+        $ [ "tag" .= tagName
+          ]
+            <> ["content" .= content | Just content <- [mContent]]
+
+renderPhase :: Phase -> Value
+renderPhase = \case
+    RetrievingFunds -> renderTagged "retrieving-funds"
+    Metadata -> renderTagged "metadata"
+    Genesis -> renderTagged "genesis"
+    Pool0 -> renderTagged "pool0"
+    Funding -> renderTagged "funding"
+    Pools -> renderTagged "pools"
+    Relay -> renderTagged "relay"
+    Cluster mNode -> renderTaggedWithContent "cluster" $ fmap renderNode mNode
+
+renderNode :: RunningNode -> Value
+renderNode
+    RunningNode
+        { runningNodeSocketPath
+        , runningNodeShelleyGenesis
+        , runningNodeVersionData =
+            NodeToClientVersionData
+                { networkMagic = NetworkMagic nm
+                , query
+                }
+        } =
+        object
+            [ "socket" .= nodeSocketFile runningNodeSocketPath
+            , "genesis" .= runningNodeShelleyGenesis
+            , "version"
+                .= object
+                    [ "magic" .= nm
+                    , "query" .= query
+                    ]
             ]
 
 parseHistory :: Value -> Parser History
@@ -91,8 +158,68 @@ parseHistory = withArray "History" $ \arr -> do
   where
     parsePhase = withObject "Phase" $ \o -> do
         time <- o .: "time"
-        phase <- o .: "phase"
+        phase <- o .: "phase" >>= parsePhase'
         pure (time, phase)
+
+type Tags a = [(Text, [Either a (Value -> Parser a)])]
+
+tag :: a -> b -> (a, [b])
+tag a b = (a, [b])
+
+tags :: a -> [b] -> (a, [b])
+tags a bs = (a, bs)
+
+parseTaggeds :: Tags a -> Value -> Parser a
+parseTaggeds ts = withObject "Tagged" $ \o -> do
+    t <- o .: "tag"
+    case Map.lookup t $ Map.fromList ts of
+        Just fs ->
+            let g :: Either a (Value -> Parser a) -> Parser a
+                g = \case
+                    Left a -> pure a
+                    Right f -> o .: "content" >>= f
+            in  asum $ g <$> fs
+        Nothing -> fail "Invalid tag"
+
+parsePhase' :: Value -> Parser Phase
+parsePhase' =
+    parseTaggeds
+        [ tag "retrieving-funds" $ Left RetrievingFunds
+        , tag "metadata" $ Left Metadata
+        , tag "genesis" $ Left Genesis
+        , tag "pool0" $ Left Pool0
+        , tag "funding" $ Left Funding
+        , tag "pools" $ Left Pools
+        , tag "relay" $ Left Relay
+        , tags "cluster" [Right parseNode, Left $ Cluster Nothing]
+        ]
+
+parseNode :: Value -> Parser Phase
+parseNode = withObject "RunningNode" $ \o -> do
+    socket <- o .: "socket"
+    genesis <- o .: "genesis"
+    version <- o .: "version" >>= parseVersionData
+    case cardanoNodeConn socket of
+        Left e -> fail e
+        Right nodeConn ->
+            pure
+                $ Cluster
+                $ Just
+                    RunningNode
+                        { runningNodeSocketPath = nodeConn
+                        , runningNodeShelleyGenesis = genesis
+                        , runningNodeVersionData = version
+                        }
+
+parseVersionData :: Value -> Parser NodeToClientVersionData
+parseVersionData = withObject "NodeToClientVersionData" $ \o -> do
+    nm <- o .: "magic"
+    query <- o .: "query"
+    pure
+        NodeToClientVersionData
+            { networkMagic = NetworkMagic nm
+            , query
+            }
 
 instance ToJSON (ApiT MonitorState) where
     toJSON = \case
--- lib/local-cluster/test/unit/Cardano/Wallet/Launch/Cluster/Http/Monitor/APISpec.hs
+++ lib/local-cluster/test/unit/Cardano/Wallet/Launch/Cluster/Http/Monitor/APISpec.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module Cardano.Wallet.Launch.Cluster.Http.Monitor.APISpec
     ( spec
@@ -9,13 +12,25 @@ where
 
 import Prelude
 
+import Cardano.Launcher.Node
+    ( CardanoNodeConn
+    , cardanoNodeConn
+    )
+import Cardano.Ledger.Shelley.Genesis
+    ( ShelleyGenesis
+    )
+import Cardano.Wallet.Launch.Cluster
+    ( RunningNode (..)
+    )
 import Cardano.Wallet.Launch.Cluster.Http.Monitor.API
     ( ApiT (..)
     )
 import Cardano.Wallet.Launch.Cluster.Monitoring.Phase
     ( History (..)
     , Phase (..)
-    , RelayNode (..)
+    )
+import Cardano.Wallet.Primitive.Ledger.Shelley
+    ( StandardCrypto
     )
 import Control.Monitoring.Tracing
     ( MonitorState (..)
@@ -24,7 +39,11 @@ import Data.Aeson
     ( FromJSON (..)
     , Result (..)
     , ToJSON (..)
+    , Value
     , fromJSON
+    )
+import Data.Aeson.QQ
+    ( aesonQQ
     )
 import Data.OpenApi
     ( ToSchema
@@ -34,6 +53,12 @@ import Data.Time
     ( Day (ModifiedJulianDay)
     , UTCTime (UTCTime)
     , secondsToDiffTime
+    )
+import Ouroboros.Network.Magic
+    ( NetworkMagic (..)
+    )
+import Ouroboros.Network.NodeToClient
+    ( NodeToClientVersionData (..)
     )
 import Test.Hspec
     ( Expectation
@@ -99,12 +124,92 @@ genPhase =
         , pure Funding
         , pure Pools
         , pure Relay
-        , Cluster
-            <$> oneof
-                [pure Nothing, Just <$> genRelayNodePath]
+        , Cluster <$> oneof [pure Nothing, Just <$> genRunningNode]
         ]
 
-genRelayNodePath :: Gen RelayNode
-genRelayNodePath =
-    RelayNode <$> do
-        oneof [pure "path1", pure "path1/path2", pure "/path3"]
+genCardanoNodeConn :: Gen CardanoNodeConn
+genCardanoNodeConn = do
+    mConn <-
+        cardanoNodeConn <$> do
+            oneof [pure "path1", pure "path1/path2", pure "/path3"]
+    case mConn of
+        Left e -> error $ "genCardanoNodeConn: " <> e
+        Right conn -> pure conn
+
+genRunningNode :: Gen RunningNode
+genRunningNode = do
+    socket <- genCardanoNodeConn
+    genesis <- genShelleyGenesis
+    version <- genNodeToClientVersionData
+    pure
+        $ RunningNode
+            { runningNodeSocketPath = socket
+            , runningNodeShelleyGenesis = genesis
+            , runningNodeVersionData = version
+            }
+
+genNodeToClientVersionData :: Gen NodeToClientVersionData
+genNodeToClientVersionData =
+    pure
+        $ NodeToClientVersionData
+            { networkMagic = NetworkMagic 42
+            , query = True
+            }
+
+genShelleyGenesis :: Gen (ShelleyGenesis StandardCrypto)
+genShelleyGenesis = pure $ case fromJSON shelleyGenesis of
+    Success genesis -> genesis
+    Error e -> error e
+
+shelleyGenesis :: Value
+shelleyGenesis =
+    [aesonQQ|
+{
+    "activeSlotsCoeff": 5.0e-2,
+    "epochLength": 21600,
+    "genDelegs": {
+        "8a1cebc0df78b69ef71099de3867a78d85b93b57513fc0508b27bee6": {
+            "delegate": "2103d8279e759d7163d1422467cfb88c19e584adc9506d4b8484a397",
+            "vrf": "8d8f6e4c0685ca835d7dbe4ec4c240f4d71d0ceb2e4fe1d7bd97b7b3d30f435d"
+        }
+    },
+    "initialFunds": {},
+    "maxKESEvolutions": 62,
+    "maxLovelaceSupply": 45000000000000000,
+    "networkId": "Testnet",
+    "networkMagic": 42,
+    "protocolParams": {
+        "a0": 0.3,
+        "decentralisationParam": 1.0,
+        "eMax": 18,
+        "extraEntropy": {
+            "tag": "NeutralNonce"
+        },
+        "keyDeposit": 2000000,
+        "maxBlockBodySize": 65536,
+        "maxBlockHeaderSize": 1100,
+        "maxTxSize": 16384,
+        "minFeeA": 44,
+        "minFeeB": 155381,
+        "minPoolCost": 340000000,
+        "minUTxOValue": 1000000,
+        "nOpt": 150,
+        "poolDeposit": 500000000,
+        "protocolVersion": {
+            "major": 6,
+            "minor": 0
+        },
+        "rho": 3.0e-3,
+        "tau": 0.2
+    },
+    "securityParam": 108,
+    "slotLength": 1,
+    "slotsPerKESPeriod": 129600,
+    "staking": {
+        "pools": {},
+        "stake": {}
+    },
+    "systemStart": "2024-03-25T10:34:26.544957596Z",
+    "updateQuorum": 1
+}
+|]
