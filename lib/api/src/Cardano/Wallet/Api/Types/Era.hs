2f515fb2ca4257c647c3eca6dfc8e9c1a757fafb
Jonathan Knowles <mail@jonathanknowles.net>
2024-05-23 02:39:36 +0000

    Add module `Cardano.Wallet.Api.Types.Era`.

---
+++ lib/api/src/Cardano/Wallet/Api/Types/Era.hs
@@ -0,0 +1,12 @@
+-- |
+-- Copyright:
+--    © 2018-2023 IOHK
+--    © 2023-2024 Cardano Foundation
+-- License:
+--    Apache-2.0
+--
+-- This module provides API types and functions relating to eras.
+--
+module Cardano.Wallet.Api.Types.Era
+    ()
+    where
