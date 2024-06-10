ce23514b498b09e610cbdbad29127fd9e5fd9d38
paolino <paolo.veronelli@gmail.com>
2024-06-10 10:16:57 +0000

    Add benchmarks reporting to read-blocks-bench

--- lib/benchmarks/exe/read-blocks.hs
+++ lib/benchmarks/exe/read-blocks.hs
@@ -2,6 +2,15 @@
 
 import Prelude
 
+import Cardano.BM.ToTextTracer
+    ( ToTextTracer (..)
+    , withToTextTracer
+    )
+import Cardano.Wallet.Benchmarks.Collect
+    ( newReporterFromEnv
+    , noSemantic
+    , runCriterionBenchmark
+    )
 import Cardano.Wallet.Primitive.Types
     ( GenesisParameters (..)
     , StartTime (..)
@@ -16,25 +25,35 @@ import Cardano.Wallet.Read.Block
 import Cardano.Wallet.Read.Block.Gen.Build
     ( exampleBlocks
     )
+import Control.Monad.Cont
+    ( evalContT
+    )
+import Control.Monad.IO.Class
+    ( MonadIO (..)
+    )
 import Criterion.Main
     ( bench
     , bgroup
-    , defaultMain
     , nf
     )
 import Data.Time.Clock.POSIX
     ( posixSecondsToUTCTime
+    )
+import System.IO
+    ( stdout
     )
 
 import qualified Cardano.Wallet.Primitive.Ledger.Read.Block as New
 import qualified Cardano.Wallet.Primitive.Types as W
 import qualified Data.ByteString.Char8 as B8
 
--- Our benchmark harness.
 main :: IO ()
-main =
-    defaultMain
-        [ bgroup
+main = evalContT $ do
+    ToTextTracer tr <- withToTextTracer (Left stdout) Nothing
+    reporter <- newReporterFromEnv tr noSemantic
+    liftIO
+        $ runCriterionBenchmark 60 tr reporter
+        $ bgroup
             "read blocks"
             [ bench "1 block" $ nf (run new) 1
             , bench "10 blocks" $ nf (run new) 10
@@ -42,7 +61,6 @@ main =
             , bench "1000 blocks" $ nf (run new) 1000
             , bench "10000 blocks" $ nf (run new) 10000
             ]
-        ]
 
 new :: GenesisParameters -> ConsensusBlock -> W.Block
 new gp = fst . New.fromCardanoBlock (getGenesisBlockHash gp)
