2d10dabc87076e207bf8af0fec8bb112c32a08dd
Github Action <gha@cardanofoundation.org>
2024-06-13 14:03:22 +0000

    Add a the "Add release commits" step

---
+++ scripts/buildkite/release/release-candidate.sh
@@ -0,0 +1,63 @@
+#! /usr/bin/env -S nix shell '.#cardano-node' 'nixpkgs#gnused' --command bash
+# shellcheck shell=bash
+
+set -euox pipefail
+
+# date from git tag
+# example v2023-04-04 -> 2023-04-04
+tag_date() {
+  echo "${1##v}"
+}
+# cabal version from git tag
+# example v2023-04-04 -> 2023.4.4
+tag_cabal_ver() {
+  tag_date "$1" | sed -e s/-0/-/g -e s/-/./g
+}
+
+git fetch --all
+
+BASE_COMMIT=$(git rev-parse HEAD)
+
+git checkout "$BASE_COMMIT"
+
+today=$(date +%Y-%m-%d)
+
+NEW_GIT_TAG=v$today
+
+NEW_CABAL_VERSION=$(tag_cabal_ver "$NEW_GIT_TAG")
+
+OLD_GIT_TAG=$( git tag -l "v2*-*-*" | sort | tail -n1)
+
+OLD_CABAL_VERSION=$(tag_cabal_ver "$OLD_GIT_TAG")
+
+CARDANO_NODE_TAG=$(cardano-node version | head -n1 | awk '{print $2}')
+
+RELEASE_CANDIDATE_BRANCH="release-candidate-new/$NEW_GIT_TAG"
+
+git config --global user.email "gha@cardanofoundation.org"
+git config --global user.name "Github Action"
+
+git branch -D "$RELEASE_CANDIDATE_BRANCH" || true
+git checkout -b "$RELEASE_CANDIDATE_BRANCH" || true
+
+sed -i "s|version: .*|version: $NEW_GIT_TAG|g" specifications/api/swagger.yaml
+git commit -m "Update wallet version in swagger.yaml" specifications/api/swagger.yaml
+
+git ls-files  '*.cabal' | xargs sed -i "s|$OLD_CABAL_VERSION|$NEW_CABAL_VERSION|g"
+git commit -am "Update cardano-wallet version in *.cabal files"
+
+sed -i "s|NODE_TAG=.*|NODE_TAG=$CARDANO_NODE_TAG|g" README.md
+sed -i "s|WALLET_TAG=.*|WALLET_TAG=$NEW_CABAL_VERSION|g" README.md
+sed -i "s|WALLET_VERSION=.*|WALLET_VERSION=$NEW_GIT_TAG|g" README.md
+git commit -am "Update cardano-wallet version in README.md"
+
+RELEASE_COMMIT=$(git rev-parse HEAD)
+
+git remote set-url origin "git@github.com:cardano-foundation/cardano-wallet.git"
+git remote get-url origin
+
+git push -f origin "$RELEASE_CANDIDATE_BRANCH"
+
+buildkite-agent meta-data set "release-version" "$NEW_GIT_TAG"
+buildkite-agent meta-data set "release-commit" "$RELEASE_COMMIT"
+buildkite-agent meta-data set "release-candidate-branch" "$RELEASE_CANDIDATE_BRANCH"
