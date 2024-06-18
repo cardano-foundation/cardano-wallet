97ab2626637318ea0df46325cb7fff3e65dd6692
Jonathan Knowles <mail@jonathanknowles.net>
2024-06-18 06:11:44 +0000

    Create definition for `flip toTextChunks []`.

--- lib/api/src/Cardano/Wallet/Api/Http/Shelley/Server.hs
+++ lib/api/src/Cardano/Wallet/Api/Http/Shelley/Server.hs
@@ -3241,7 +3241,7 @@ toMetadataEncrypted apiEncrypt payload saltM = do
                 toPair enc =
                     [ ( TxMetaText metaField
                       , TxMetaList
-                        ( map TxMetaText $ flip toTextChunks [] $
+                        ( map TxMetaText $ toTextChunks' $
                           toBase64Text enc
                         )
                       )
@@ -3254,13 +3254,16 @@ toMetadataEncrypted apiEncrypt payload saltM = do
     toBase64Text :: ByteString -> Text
     toBase64Text = T.decodeUtf8 . convertToBase Base64
 
-    toTextChunks :: Text -> [Text] -> [Text]
-    toTextChunks txt res =
-        if txt == T.empty then
-            reverse res
-        else
-            let (front, back) = T.splitAt 64 txt
-            in toTextChunks back (front:res)
+    toTextChunks' :: Text -> [Text]
+    toTextChunks' = flip toTextChunks []
+      where
+        toTextChunks :: Text -> [Text] -> [Text]
+        toTextChunks txt res =
+            if txt == T.empty then
+                reverse res
+            else
+                let (front, back) = T.splitAt 64 txt
+                in toTextChunks back (front:res)
 
     encryptingMsg
         :: (a, TxMetadataValue) -> Either ErrConstructTx (a, TxMetadataValue)
