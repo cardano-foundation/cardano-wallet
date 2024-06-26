2fd4565e282fd70c2caac7db499b3be5bc30a93a
Jonathan Knowles <mail@jonathanknowles.net>
2024-06-26 02:29:59 +0000

    Simplify definition of `toMetadataEncrypted`.

--- lib/api/src/Cardano/Wallet/Api/Http/Shelley/Server.hs
+++ lib/api/src/Cardano/Wallet/Api/Http/Shelley/Server.hs
@@ -3188,10 +3188,8 @@ toMetadataEncrypted
     -> TxMetadataWithSchema
     -> Maybe ByteString
     -> Either ErrConstructTx TxMetadata
-toMetadataEncrypted apiEncrypt payload saltM = do
-    msgValue <- extractMessage
-    msgValue' <- encryptMessage msgValue
-    pure $ updateTxMetadata msgValue'
+toMetadataEncrypted apiEncrypt payload saltM =
+    fmap updateTxMetadata . encryptMessage =<< extractMessage
   where
     pwd :: ByteString
     pwd = BA.convert $ unPassphrase $ getApiT $ apiEncrypt ^. #passphrase
