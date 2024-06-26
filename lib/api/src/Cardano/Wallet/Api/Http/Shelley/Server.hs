b9a66a096b18dd0fd620ebf120a75bcfa5310749
Jonathan Knowles <mail@jonathanknowles.net>
2024-06-26 02:23:33 +0000

    Flatten `where` hierarchy for function `encryptMessage`.

--- lib/api/src/Cardano/Wallet/Api/Http/Shelley/Server.hs
+++ lib/api/src/Cardano/Wallet/Api/Http/Shelley/Server.hs
@@ -3240,16 +3240,6 @@ toMetadataEncrypted apiEncrypt payload saltM = do
         encryptPairIfQualifies = \case
             (TxMetaText "msg", m) ->
                 bimap ErrConstructTxEncryptMetadata toPair (encryptValue m)
-              where
-                toPair encryptedMessage =
-                    [ (TxMetaText "msg", TxMetaList (toChunks encryptedMessage))
-                    , (TxMetaText "enc", TxMetaText "basic")
-                    ]
-                toChunks
-                    = fmap TxMetaText
-                    . T.chunksOf 64
-                    . T.decodeUtf8
-                    . convertToBase Base64
             pair ->
                 Right [pair]
 
@@ -3259,6 +3249,16 @@ toMetadataEncrypted apiEncrypt payload saltM = do
             . BL.toStrict
             . Aeson.encode
             . Cardano.metadataValueToJsonNoSchema
+
+        toPair encryptedMessage =
+            [ (TxMetaText "msg", TxMetaList (toChunks encryptedMessage))
+            , (TxMetaText "enc", TxMetaText "basic")
+            ]
+        toChunks
+            = fmap TxMetaText
+            . T.chunksOf 64
+            . T.decodeUtf8
+            . convertToBase Base64
 
     updateTxMetadata :: TxMetadataValue -> W.TxMetadata
     updateTxMetadata v = TxMetadata (Map.insert cip20MetadataKey v themap)
