67ee5cf1f31ea9baac3e69f95ed44025dc691a43
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-06-28 12:21:09 +0200

    Add E2E tests (macOS, arm64) to release pipeline

---
+++ scripts/buildkite/release/macos-silicon-e2e.sh
@@ -0,0 +1,52 @@
+#! /usr/bin/env bash
+# shellcheck shell=bash
+
+set -euox pipefail
+
+mkdir -p "$(pwd)/logs"
+
+TESTS_LOGDIR="$(pwd)/logs"
+export TESTS_LOGDIR
+
+CARDANO_NODE_CONFIGS="$(pwd)/configs/cardano"
+export CARDANO_NODE_CONFIGS
+
+VERSION=$(buildkite-agent meta-data get "release-version")
+echo "VERSION=$VERSION"
+
+buildkite-agent artifact \
+    download "result/macos-silicon/cardano-wallet-$VERSION-macos-silicon.tar.gz" "."
+
+tar xvzf "result/macos-silicon/cardano-wallet-$VERSION-macos-silicon.tar.gz"
+
+TESTS_E2E_BINDIR="$(pwd)/cardano-wallet-$VERSION-macos-silicon"
+export TESTS_E2E_BINDIR
+
+cd test/e2e
+
+TESTS_NODE_DB="$(pwd)/state/node_db"
+export TESTS_NODE_DB
+
+mkdir -p "$TESTS_NODE_DB"/preprod
+rsync -a --delete "$NODE_STATE_DIR/db/" "$TESTS_NODE_DB/preprod"
+
+tmpdir=$(mktemp -d /tmp/node-preprod.XXXXXX)
+
+CARDANO_NODE_SOCKET_PATH="$tmpdir/node.socket"
+export CARDANO_NODE_SOCKET_PATH
+
+TESTS_E2E_STATEDIR=$(pwd)/state
+export TESTS_E2E_STATEDIR
+
+TESTS_E2E_TOKEN_METADATA=https://metadata.world.dev.cardano.org/
+export TESTS_E2E_TOKEN_METADATA
+
+TESTS_E2E_FIXTURES="$FIXTURE_DECRYPTION_KEY"
+
+export TESTS_E2E_FIXTURES
+
+# We have to use the `nix develop` shell defined for x86_64-darwin
+# But we can still *test* the aarch64-darwin cardano-wallet executable
+nix develop --system x86_64-darwin -c rake "run_on[preprod,sync,true]"
+
+rm -Rv "$tmpdir"
