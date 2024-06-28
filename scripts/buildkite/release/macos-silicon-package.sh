8ffbac97b816fc302290d99da89ae67a92bc7493
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-06-28 11:59:53 +0200

    Add building macOS packages to `release.yml`

---
+++ scripts/buildkite/release/macos-silicon-package.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+# shellcheck shell=bash
+
+RELEASE_CANDIDATE_BRANCH=$(buildkite-agent meta-data get "release-candidate-branch")
+
+git checkout "$RELEASE_CANDIDATE_BRANCH"
+
+nix build -o result/macos-silicon .#packages.aarch64-darwin.ci.artifacts.macos-silicon.release
