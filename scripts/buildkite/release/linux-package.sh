bff9904d9f08a79d0a21123c97f04a9c8ab1bf50
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-07-02 13:38:06 +0200

    Fix building linux packages in `release.yml`

--- scripts/buildkite/release/linux-package.sh
+++ scripts/buildkite/release/linux-package.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env -S nix shell --command bash
 # shellcheck shell=bash
 
+set -euox pipefail
+
 RELEASE_CANDIDATE_BRANCH=$(buildkite-agent meta-data get "release-candidate-branch")
 
+git fetch --all
 git checkout "$RELEASE_CANDIDATE_BRANCH"
 
+rm -rf ./result/*
 nix build -o result/linux .#ci.artifacts.linux64.release
