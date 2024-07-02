b212473e2aaa48fed93c014e2918b094254c303f
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-07-02 11:28:21 +0200

    wip

--- scripts/buildkite/release/macos-intel-package.sh
+++ scripts/buildkite/release/macos-intel-package.sh
@@ -5,4 +5,5 @@ RELEASE_CANDIDATE_BRANCH=$(buildkite-agent meta-data get "release-candidate-bran
 
 git checkout "$RELEASE_CANDIDATE_BRANCH"
 
+rm -Rv result
 nix build -o result/macos-intel .#packages.x86_64-darwin.ci.artifacts.macos-intel.release
--- scripts/buildkite/release/macos-silicon-package.sh
+++ scripts/buildkite/release/macos-silicon-package.sh
@@ -5,4 +5,5 @@ RELEASE_CANDIDATE_BRANCH=$(buildkite-agent meta-data get "release-candidate-bran
 
 git checkout "$RELEASE_CANDIDATE_BRANCH"
 
+rm -Rv result
 nix build -o result/macos-silicon .#packages.aarch64-darwin.ci.artifacts.macos-silicon.release
