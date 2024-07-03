0d5eb21fde4ddd7c686122b4964a05c4fb661c8c
Github Action <gha@cardanofoundation.org>
2024-07-03 14:45:19 +0000

    Integrate all release steps to main buildkite pipeline

---
+++ scripts/buildkite/main/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = ''
+    Shell for the main pipeline
+    that builds release artifacts and runs E2E tests.
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:hamishmack/flake-utils/hkm/nested-hydraJobs";
+  };
+
+  outputs = inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+       ]; in
+    inputs.flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        # Imports
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in {
+        packages = { };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.curl
+            pkgs.jq
+            pkgs.rsync
+            pkgs.gnutar
+            pkgs.gnupg
+          ];
+          shellHook = ''
+            # use this hook to set up additional environment variables
+          '';
+        };
+      }
+    );
+}
