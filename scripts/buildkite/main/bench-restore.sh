0cdbe8017e452ad925e46bdb445c226e9da368c0
Github Action <gha@cardanofoundation.org>
2024-07-03 15:11:09 +0000

    Apply shellcheck lints

--- scripts/buildkite/main/bench-restore.sh
+++ scripts/buildkite/main/bench-restore.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p coreutils gnugrep gawk time haskellPackages.hp2pretty buildkite-agent gnuplot
+# shellcheck shell=bash
 
 set -euo pipefail
 
@@ -15,29 +16,29 @@ results=restore-$network.txt
 total_time=restore-time.txt
 
 export TMPDIR="/$TMPDIR/bench/restore"
-mkdir -p $TMPDIR
+mkdir -p "$TMPDIR"
 
 : "${node_db:=$HOME/node-db-$network}"
 
 echo "--- Build"
 nix build .#ci.benchmarks.restore -o bench-restore
 
-CARDANO_NODE_CONFIGS=`pwd`/configs/cardano
+CARDANO_NODE_CONFIGS=$(pwd)/configs/cardano
 
 bench="./bench-restore/bin/restore $network --node-db $node_db --cardano-node-configs $CARDANO_NODE_CONFIGS"
 
 echo "--- Run benchmarks - $network"
 
-command time -o $total_time -v $bench +RTS -N2 -qg -A1m -I0 -T -M16G -h -RTS 2>&1 | tee $log
+command time -o $total_time -v "$bench" +RTS -N2 -qg -A1m -I0 -T -M16G -h -RTS 2>&1 | tee $log
 
-grep -v INFO $log | awk '/All results/,EOF { print $0 }' >$results
+grep -v INFO $log | awk '/All results/,EOF { print $0 }' >"$results"
 
 echo "+++ Results - $network"
 
-cat $results
+cat "$results"
 
-mv restore.hp $artifact_name.hp
-hp2pretty $artifact_name.hp
+mv restore.hp "$artifact_name".hp
+hp2pretty "$artifact_name".hp
 
 GNUPLOT_PROGRAM=$(
   cat <<EOP
@@ -65,31 +66,32 @@ EOP
 
 if [ -n "${BUILDKITE:-}" ]; then
   echo "--- Upload"
-  buildkite-agent artifact upload $artifact_name.svg
-  buildkite-agent artifact upload $results
+  buildkite-agent artifact upload "$artifact_name".svg
+  buildkite-agent artifact upload "$results"
 
   for file in *.timelog; do
     # upload raw data
-    buildkite-agent artifact upload $file
+    buildkite-agent artifact upload "$file"
     # clean data (make time relative) for plot
-    cat $file | awk 'BEGIN {t0 = 0}{if (t0 == 0) {t0 = $1} else {print $1-t0,$2}}' >$file.dat
+    # shellcheck disable=SC2002
+    cat "$file" | awk 'BEGIN {t0 = 0}{if (t0 == 0) {t0 = $1} else {print $1-t0,$2}}' > "$file".dat
   done
 
   for file in *.json; do
-    buildkite-agent artifact upload $file
+    buildkite-agent artifact upload "$file"
   done
 
   # Plots all .log files in a single plot;
-  echo $GNUPLOT_PROGRAM | gnuplot
+  echo "$GNUPLOT_PROGRAM" | gnuplot
   buildkite-agent artifact upload plot.svg
 
   echo "+++ Heap profile"
-  printf '\033]1338;url='"artifact://$artifact_name.svg"';alt='"Heap profile"'\a\n'
+  printf '\033]1338;url='"artifact://%s"';alt='"Heap profile"'\a\n' "artifact_name.svg"
   echo "+++ Restore plot"
   printf '\033]1338;url='"artifact://plot.svg"';alt='"Restore plot"'\a\n'
 fi
 
-if [ -z "$(cat $results)" ]; then
+if [ -z "$(cat "$results")" ]; then
   echo "+++ Bad news"
   echo "FAILED - Missing results" >/dev/stderr
   exit 1
