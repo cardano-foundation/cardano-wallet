5a90b09c0865bd5de6e8e3cb70dab1140090cf9a
Github Action <gha@cardanofoundation.org>
2024-07-04 14:17:15 +0000

    Move windows trigger tag creation in the windows step group

--- scripts/buildkite/main/tag-release-candidate.sh
+++ scripts/buildkite/main/tag-release-candidate.sh
@@ -2,11 +2,15 @@
 
 set -euox pipefail
 
-TAG="release-candidate-tag"
-date=$(git show -s --format=%ci $TAG | awk '{print $1}')
-DTAG="$TAG-$date"
+git config --global user.email "gha@cardanofoundation.org"
+git config --global user.name "Github Action"
+git remote set-url origin "git@github.com:cardano-foundation/cardano-wallet.git"
 
-git remote set-url origin \
-  "https://$GITHUB_TOKEN@github.com/cardano-foundation/cardano-wallet.git"
+TAG="release-candidate-tag"
+date=$(git show -s --format=%ci | awk '{print $1}')
+DTAG="$TAG/$date"
+
+git tag --delete "$DTAG" || true
+git push origin --delete "$DTAG" || true
 git tag "$DTAG" "$BUILDKITE_COMMIT"
 git push origin "$DTAG"
