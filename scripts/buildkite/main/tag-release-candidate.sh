c87cd4717f7a37b177d3c7420a3edc96207f3ffd
paolino <paolo.veronelli@gmail.com>
2024-07-06 15:30:44 +0000

    Add `rc-latest` management to tag-release-candidate script

--- scripts/buildkite/main/tag-release-candidate.sh
+++ scripts/buildkite/main/tag-release-candidate.sh
@@ -8,9 +8,18 @@ git remote set-url origin "git@github.com:cardano-foundation/cardano-wallet.git"
 
 TAG="release-candidate-tag"
 date=$(git show -s --format=%ci | awk '{print $1}')
+
 DTAG="$TAG/$date"
+LATEST="rc-latest"
 
 git tag --delete "$DTAG" || true
+git tag --delete "$LATEST" || true
+
 git push origin --delete "$DTAG" || true
+git push origin --delete "$LATEST" || true
+
 git tag "$DTAG" "$BUILDKITE_COMMIT"
+git tag "$LATEST" "$BUILDKITE_COMMIT"
+
 git push origin "$DTAG"
+git push origin "$LATEST"
