8987fb5c628404ed4db890fe854fe20a89c700ac
paolino <paolo.veronelli@gmail.com>
2024-07-12 10:47:20 +0000

    Add private network configs

---
+++ configs/cardano/private/download.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -euo pipefail
+
+wget https://book.play.dev.cardano.org/environments/private/config.json
+wget https://book.play.dev.cardano.org/environments/private/topology.json
+wget https://book.play.dev.cardano.org/environments/private/byron-genesis.json
+wget https://book.play.dev.cardano.org/environments/private/shelley-genesis.json
+wget https://book.play.dev.cardano.org/environments/private/alonzo-genesis.json
+wget https://book.play.dev.cardano.org/environments/private/conway-genesis.json
