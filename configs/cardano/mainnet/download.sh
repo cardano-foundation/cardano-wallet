f869759cc99a38d44dbf481bd191b398f278f88a
paolino <paolo.veronelli@gmail.com>
2024-07-25 13:04:30 +0000

    Bump node configs

--- configs/cardano/mainnet/download.sh
+++ configs/cardano/mainnet/download.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 rm ./*.json
-curl https://book.play.dev.cardano.org/environments-pre/mainnet/config.json > config.json
-curl https://book.play.dev.cardano.org/environments-pre/mainnet/conway-genesis.json  > conway-genesis.json
-curl https://book.play.dev.cardano.org/environments-pre/mainnet/topology.json  > topology.json
-curl https://book.play.dev.cardano.org/environments-pre/mainnet/byron-genesis.json  > byron-genesis.json
-curl https://book.play.dev.cardano.org/environments-pre/mainnet/shelley-genesis.json  > shelley-genesis.json
-curl https://book.play.dev.cardano.org/environments-pre/mainnet/alonzo-genesis.json  > alonzo-genesis.json
+curl https://book.play.dev.cardano.org/environments/mainnet/config.json > config.json
+curl https://book.play.dev.cardano.org/environments/mainnet/conway-genesis.json  > conway-genesis.json
+curl https://book.play.dev.cardano.org/environments/mainnet/topology.json  > topology.json
+curl https://book.play.dev.cardano.org/environments/mainnet/byron-genesis.json  > byron-genesis.json
+curl https://book.play.dev.cardano.org/environments/mainnet/shelley-genesis.json  > shelley-genesis.json
+curl https://book.play.dev.cardano.org/environments/mainnet/alonzo-genesis.json  > alonzo-genesis.json
