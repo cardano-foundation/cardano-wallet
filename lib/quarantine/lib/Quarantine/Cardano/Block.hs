4db1f2cdbfae994ef5a293ba18344496a3f12c1c
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-08-09 14:08:01 +0200

    Add package that quarantines the compiler error

---
+++ lib/quarantine/lib/Quarantine/Cardano/Block.hs
@@ -0,0 +1,106 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+-- |
+-- Copyright: © 2022 IOHK
+-- License: Apache-2.0
+--
+-- The 'Block' type.
+--
+module Quarantine.Cardano.Block
+    ( ConsensusBlock
+    , Block (..)
+    -- , fromConsensusBlock
+    , toConsensusBlock
+    ) where
+
+import Prelude
+
+import Cardano.Ledger.Api
+    ( StandardCrypto
+    )
+import Quarantine.Cardano.Eras
+    ( Allegra
+    , Alonzo
+    , Babbage
+    , Byron
+    , Conway
+    , Era (..)
+    , IsEra (..)
+    , Mary
+    , Shelley
+    )
+{-
+import Cardano.Wallet.Read.Eras
+    ( EraValue (..)
+    , eraValue
+    )
+-}
+import Ouroboros.Consensus.Protocol.Praos
+    ( Praos
+    )
+import Ouroboros.Consensus.Protocol.TPraos
+    ( TPraos
+    )
+
+import qualified Ouroboros.Consensus.Byron.Ledger as O
+import qualified Ouroboros.Consensus.Cardano.Block as O
+import qualified Ouroboros.Consensus.Shelley.Ledger as O
+
+-- | Type synonym for 'CardanoBlock',
+-- using the same cryptographic functionalities as Mainnet.
+type ConsensusBlock = O.CardanoBlock O.StandardCrypto
+
+-- Family of era-specific block types
+-- TODO: ADP-3351 The results of this type family should be ledger types,
+-- not ouroboros-consensus types.
+type family BlockT era where
+    BlockT Byron =
+        O.ByronBlock
+    BlockT Shelley =
+        O.ShelleyBlock (TPraos StandardCrypto) (O.ShelleyEra StandardCrypto)
+    BlockT Allegra =
+        O.ShelleyBlock (TPraos StandardCrypto) (O.AllegraEra StandardCrypto)
+    BlockT Mary =
+        O.ShelleyBlock (TPraos StandardCrypto) (O.MaryEra StandardCrypto)
+    BlockT Alonzo =
+        O.ShelleyBlock (TPraos StandardCrypto) (O.AlonzoEra StandardCrypto)
+    BlockT Babbage =
+        O.ShelleyBlock (Praos StandardCrypto) (O.BabbageEra StandardCrypto)
+    BlockT Conway =
+        O.ShelleyBlock (Praos StandardCrypto) (O.ConwayEra StandardCrypto)
+
+newtype Block era = Block {unBlock :: BlockT era}
+
+deriving instance Show (BlockT era) => Show (Block era)
+deriving instance Eq (BlockT era) => Eq (Block era)
+
+{-
+-- | Convert block as received from cardano-node
+-- via Haskell library of mini-protocol.
+fromConsensusBlock :: ConsensusBlock -> EraValue Block
+fromConsensusBlock = \case
+    O.BlockByron b -> eraValue @Byron $ Block b
+    O.BlockShelley block -> eraValue @Shelley $ Block block
+    O.BlockAllegra block -> eraValue @Allegra $ Block block
+    O.BlockMary block -> eraValue @Mary $ Block block
+    O.BlockAlonzo block -> eraValue @Alonzo $ Block block
+    O.BlockBabbage block -> eraValue @Babbage $ Block block
+    O.BlockConway block -> eraValue @Conway $ Block block
+-}
+
+{-# INLINABLE toConsensusBlock #-}
+toConsensusBlock :: forall era . IsEra era => Block era -> ConsensusBlock
+toConsensusBlock = case theEra @era of
+    Byron -> O.BlockByron . unBlock
+    Shelley -> O.BlockShelley . unBlock
+    Allegra -> O.BlockAllegra . unBlock
+    Mary -> O.BlockMary . unBlock
+    Alonzo -> O.BlockAlonzo . unBlock
+    Babbage -> O.BlockBabbage . unBlock
+    Conway -> O.BlockConway . unBlock
---
+++ lib/quarantine/lib/Quarantine/Cardano/Tx.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+{- |
+Copyright: © 2022 IOHK
+License: Apache-2.0
+
+The 'Tx' type represents transactions as they are read from the mainnet ledger.
+It is compatible with the era-specific index types from @cardano-ledger@.
+-}
+module Quarantine.Cardano.Tx
+    ( -- * Transactions
+      Tx (..)
+    , TxT
+    ) where
+
+import Prelude
+
+import Cardano.Ledger.Alonzo.Tx
+    ( AlonzoTx
+    )
+import Cardano.Ledger.Api
+    ( StandardCrypto
+    )
+import Cardano.Ledger.Shelley.Tx
+    ( ShelleyTx
+    )
+import Quarantine.Cardano.Eras
+    ( Allegra
+    , Alonzo
+    , Babbage
+    , Byron
+    , Conway
+    , Mary
+    , Shelley
+    )
+
+import qualified Cardano.Chain.UTxO as Byron
+import qualified Cardano.Ledger.Api as Ledger
+
+-- | Closed type family returning the ledger 'Tx' type for each known @era@.
+type family TxT era where
+    TxT Byron = Byron.ATxAux ()
+    TxT Shelley = ShelleyTx (Ledger.ShelleyEra StandardCrypto)
+    TxT Allegra = ShelleyTx (Ledger.AllegraEra StandardCrypto)
+    TxT Mary = ShelleyTx (Ledger.MaryEra StandardCrypto)
+    TxT Alonzo = AlonzoTx (Ledger.AlonzoEra  StandardCrypto)
+    TxT Babbage = AlonzoTx (Ledger.BabbageEra StandardCrypto)
+    TxT Conway = AlonzoTx (Ledger.ConwayEra StandardCrypto)
+
+-- | A tx in any era
+newtype Tx era = Tx {unTx :: TxT era}
+
+deriving instance Show (TxT era) => Show (Tx era)
+deriving instance Eq (TxT era) => Eq (Tx era)
