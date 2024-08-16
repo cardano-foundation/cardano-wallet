49655bc08703d1f0082d1dc383ccdbd1f36ad72c
paolino <paolo.veronelli@gmail.com>
2024-08-16 14:18:03 +0000

    Fix image name in the docker-build.sh

--- scripts/buildkite/main/docker-build.sh
+++ scripts/buildkite/main/docker-build.sh
@@ -1,16 +1,20 @@
-#! /usr/bin/env -S nix shell --command bash
-# shellcheck shell=bash
+#! /bin/bash
 
 set -euox pipefail
 
 git fetch --all
 
 RELEASE_CANDIDATE_COMMIT=$(buildkite-agent meta-data get "release-candidate-commit")
+RELEASE_VERSION=$(buildkite-agent meta-data get "release-version")
 
 git checkout "$RELEASE_CANDIDATE_COMMIT"
 
 mkdir -p result
 
-nix build .#dockerImage -o result/docker-image
+mkdir -p artifacts
 
-docker load < result/docker-image
+TARGET="artifacts/cardano-wallet-$RELEASE_VERSION-docker-image.tgz"
+
+nix build .#dockerImage -o "$TARGET"
+
+docker load < "$TARGET"
