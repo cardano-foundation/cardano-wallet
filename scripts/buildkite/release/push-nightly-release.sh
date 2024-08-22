ffce9a7eacef62d7a895cf0b1dfa2b259ec5abd5
paolino <paolo.veronelli@gmail.com>
2024-08-22 06:42:34 +0000

    Add push nightly release script

---
+++ scripts/buildkite/release/push-nightly-release.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -euox pipefail
+
+NEW_GIT_TAG=$(buildkite-agent meta-data get release-version)
+
+unset GITHUB_TOKEN
+unset GH_TOKEN
+echo "$PUSH_ARTIFACTS_TOKEN" | gh auth login --with-token
+
+gh release delete nightly --yes || true
+gh release create \
+    -d \
+    -F "$RELEASE_SCRIPTS_DIR/release-template.md" \
+    -t "Nightly $NEW_GIT_TAG" \
+    "nightly"
