04b3ea5e660e8f6b282f9bb9ebae795a53dbd22d
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-08-30 16:09:51 +0200

    Use `Cardano.Wallet.Deposit.Pure.UTxO.UTxOHistory` directly

--- lib/customer-deposit-wallet/src/Cardano/Wallet/Deposit/Pure.hs
+++ lib/customer-deposit-wallet/src/Cardano/Wallet/Deposit/Pure.hs
@@ -53,7 +53,7 @@ import Cardano.Wallet.Address.BIP32
     ( BIP32Path (..)
     , DerivationType (..)
     )
-import Cardano.Wallet.Deposit.Pure.UTxOHistory
+import Cardano.Wallet.Deposit.Pure.UTxO.UTxOHistory
     ( UTxOHistory
     )
 import Cardano.Wallet.Deposit.Read
@@ -82,7 +82,7 @@ import qualified Cardano.Wallet.Deposit.Pure.Address as Address
 import qualified Cardano.Wallet.Deposit.Pure.Balance as Balance
 import qualified Cardano.Wallet.Deposit.Pure.Submissions as Sbm
 import qualified Cardano.Wallet.Deposit.Pure.UTxO as UTxO
-import qualified Cardano.Wallet.Deposit.Pure.UTxOHistory as UTxOHistory
+import qualified Cardano.Wallet.Deposit.Pure.UTxO.UTxOHistory as UTxOHistory
 import qualified Cardano.Wallet.Deposit.Read as Read
 import qualified Cardano.Wallet.Deposit.Write as Write
 import qualified Data.Delta as Delta
