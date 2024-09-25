19fd57971ce61679e0a88839a770cc232af94e4b
paolino <paolo.veronelli@gmail.com>
2024-09-25 14:21:53 +0000

    Add preview run hierarchy

--- configs/cardano/preview/download.sh
+++ configs/cardano/preview/download.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 rm ./*.json
-curl https://book.play.dev.cardano.org/environments-pre/preview/config.json > config.json
-curl https://book.play.dev.cardano.org/environments-pre/preview/conway-genesis.json  > conway-genesis.json
-curl https://book.play.dev.cardano.org/environments-pre/preview/topology.json  > topology.json
-curl https://book.play.dev.cardano.org/environments-pre/preview/byron-genesis.json  > byron-genesis.json
-curl https://book.play.dev.cardano.org/environments-pre/preview/shelley-genesis.json  > shelley-genesis.json
-curl https://book.play.dev.cardano.org/environments-pre/preview/alonzo-genesis.json  > alonzo-genesis.json
+curl https://book.play.dev.cardano.org/environments/preview/config.json > config.json
+curl https://book.play.dev.cardano.org/environments/preview/conway-genesis.json  > conway-genesis.json
+curl https://book.play.dev.cardano.org/environments/preview/topology.json  > topology.json
+curl https://book.play.dev.cardano.org/environments/preview/byron-genesis.json  > byron-genesis.json
+curl https://book.play.dev.cardano.org/environments/preview/shelley-genesis.json  > shelley-genesis.json
+curl https://book.play.dev.cardano.org/environments/preview/alonzo-genesis.json  > alonzo-genesis.json
