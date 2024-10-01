10c42013f527cdcd9f93db5d180b6753a595d1a1
paolino <paolo.veronelli@gmail.com>
2024-10-01 15:30:01 +0000

    Add transactions history in the addresses page
    Add hx-include to select customer widget
    
    Add slot to UTC translation to transactions UI page
    
    Move transactions view under the addresses page
    
    Remove transaction history page

---
+++ lib/ui/src/Cardano/Wallet/UI/Deposit/Handlers/Addresses/Transactions.hs
@@ -0,0 +1,143 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
+
+module Cardano.Wallet.UI.Deposit.Handlers.Addresses.Transactions
+where
+
+import Prelude
+
+import Cardano.Wallet.Deposit.IO.Network.Type
+    ( NetworkEnv
+    , slotsToUTCTimes
+    )
+import Cardano.Wallet.Deposit.Pure
+    ( Customer
+    , TxSummary (..)
+    , ValueTransfer (ValueTransfer)
+    )
+import Cardano.Wallet.Deposit.Read
+    ( Slot
+    )
+import Cardano.Wallet.Deposit.REST
+    ( WalletResource
+    , customerAddress
+    )
+import Cardano.Wallet.Read
+    ( SlotNo (..)
+    , slotFromChainPoint
+    , txIdFromHash
+    )
+import Cardano.Wallet.Read.Hash
+    ( hashFromStringAsHex
+    )
+import Cardano.Wallet.UI.Common.Layer
+    ( SessionLayer (..)
+    )
+import Cardano.Wallet.UI.Deposit.API
+    ( TransactionHistoryParams (TransactionHistoryParams, txHistoryCustomer)
+    )
+import Cardano.Wallet.UI.Deposit.Handlers.Lib
+    ( catchRunWalletResourceHtml
+    )
+import Control.Monad
+    ( replicateM
+    )
+import Control.Monad.IO.Class
+    ( MonadIO (..)
+    )
+import Data.Foldable
+    ( toList
+    )
+import Data.Function
+    ( on
+    )
+import Data.List
+    ( sortBy
+    )
+import Data.Map.Strict
+    ( Map
+    )
+import Data.Maybe
+    ( fromJust
+    )
+import Data.Time
+    ( UTCTime
+    )
+import Servant
+    ( Handler
+    )
+import System.Random.Stateful
+    ( UniformRange (..)
+    , mkStdGen
+    , runStateGen_
+    )
+
+import qualified Cardano.Wallet.Deposit.REST as REST
+import qualified Cardano.Wallet.Read as Read
+import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.Set as Set
+
+getCustomerHistory
+    :: NetworkEnv IO a
+    -> SessionLayer WalletResource
+    -> (Bool -> TransactionHistoryParams -> [TxSummary] -> Map Slot UTCTime -> html)
+    -> (BL.ByteString -> html)
+    -> TransactionHistoryParams
+    -> Handler html
+getCustomerHistory
+    network
+    layer
+    render
+    alert
+    params@TransactionHistoryParams{..} = do
+        catchRunWalletResourceHtml layer alert id $ do
+            h <- REST.getCustomerHistory txHistoryCustomer
+            r <- customerAddress txHistoryCustomer
+            case r of
+                Nothing -> pure $ alert "Address not discovered"
+                Just _ -> do
+                    let (b, ss) = fakeData txHistoryCustomer . toList $ h
+                        slots = Set.fromList $ slotFromChainPoint . txChainPoint <$> ss
+                    times <- liftIO $ slotsToUTCTimes network slots
+                    pure $ render b params ss times
+
+-- fake data generation until DB is implemented
+
+fakeData :: Customer -> [TxSummary] -> (Bool, [TxSummary])
+fakeData c [] =
+    (True,)
+        $ sortBy
+            (on compare $ \(TxSummaryC _ cp _) -> cp)
+        $ txSummaryG (fromIntegral c)
+fakeData _c xs = (False, xs)
+
+unsafeMkTxId :: String -> Read.TxId
+unsafeMkTxId = txIdFromHash . fromJust . hashFromStringAsHex
+
+hexOfInt :: Int -> Char
+hexOfInt n = "0123456789abcdef" !! (n `mod` 16)
+
+txSummaryG :: Int -> [TxSummary]
+txSummaryG c = runStateGen_ pureGen $ \g -> do
+    ns <- uniformRM (1, 10) g
+    replicateM ns $ do
+        txId <- txIdR g
+        cp <- chainPointR g
+        spent <- Read.ValueC <$> uniformRM (0, 1000) g <*> pure mempty
+        received <- Read.ValueC <$> uniformRM (0, 1000) g <*> pure mempty
+        pure $ TxSummaryC txId cp $ ValueTransfer spent received
+  where
+    pureGen = mkStdGen c
+    txIdR g = do
+        ls <- replicateM 64 $ hexOfInt <$> uniformRM (0, 15) g
+        pure $ unsafeMkTxId ls
+    headerHash g = replicateM 64 $ hexOfInt <$> uniformRM (0, 15) g
+    chainPointR g = do
+        slot <- uniformRM (0, 100) g
+        case slot of
+            0 -> pure Read.GenesisPoint
+            _ -> do
+                r <- hashFromStringAsHex <$> headerHash g
+                case r of
+                    Just h -> pure $ Read.BlockPoint (SlotNo slot) h
+                    Nothing -> error "chainPointR: invalid hash"
--- lib/ui/src/Cardano/Wallet/UI/Deposit/Html/Pages/Addresses.hs
+++ lib/ui/src/Cardano/Wallet/UI/Deposit/Html/Pages/Addresses.hs
@@ -13,13 +13,8 @@ import Cardano.Wallet.Deposit.IO
 import Cardano.Wallet.Deposit.Read
     ( Address
     )
-import Cardano.Wallet.UI.Common.Html.Htmx
-    ( hxPost_
-    , hxTarget_
-    , hxTrigger_
-    )
 import Cardano.Wallet.UI.Common.Html.Lib
-    ( linkText
+    ( AlertH
     , truncatableText
     )
 import Cardano.Wallet.UI.Common.Html.Pages.Lib
@@ -41,9 +36,6 @@ import Cardano.Wallet.UI.Lib.Address
 import Cardano.Wallet.UI.Type
     ( WHtml
     )
-import Data.Text.Class
-    ( ToText (..)
-    )
 import Lucid
     ( Html
     , HtmlT
@@ -51,20 +43,14 @@ import Lucid
     , class_
     , div_
     , id_
-    , input_
-    , min_
-    , name_
-    , style_
-    , type_
-    , value_
-    )
-import Lucid.Html5
-    ( max_
-    , size_
-    , step_
     )
 
-import qualified Data.ByteString.Lazy.Char8 as BL
+import Cardano.Wallet.UI.Deposit.Html.Lib
+    ( selectCustomerH
+    )
+import Cardano.Wallet.UI.Deposit.Html.Pages.Addresses.Transactions
+    ( transactionsElementH
+    )
 
 addressesH :: WHtml ()
 addressesH = do
@@ -75,29 +61,20 @@ customerAddressH addr = truncatableText "address-text" $ toHtml encodedAddr
   where
     encodedAddr = encodeMainnetAddress addr
 
-addressElementH :: (BL.ByteString -> Html ()) -> WalletPresent -> Html ()
+addressElementH :: AlertH -> WalletPresent -> Html ()
 addressElementH = onWalletPresentH $ \case
-    (WalletPublicIdentity _xpub customers) -> do
-        div_ [class_ "row mt-5"] $ do
-            div_ [class_ "col"] $ record (Just 11) $ do
-                simpleField "Customer Number"
-                    $ div_ [class_ "d-flex justify-content-end"]
-                    $ input_
-                        [ type_ "number"
-                        , hxTarget_ "#customer-address"
-                        , class_ "form-control"
-                        , hxTrigger_ "load, change"
-                        , hxPost_ $ linkText customerAddressLink
-                        , min_ "0"
-                        , max_ $ toText $ customers - 1
-                        , step_ "1"
-                        , name_ "customer"
-                        , value_ "0"
-                        , size_ "5"
-                        , style_ "width: 7em"
-                        ]
-                simpleField "Address"
-                    $ div_
-                        [ id_ "customer-address"
-                        ]
-                        mempty
+    WalletPublicIdentity _xpub customers ->
+        div_ [id_ "view-control"] $ do
+            div_ [class_ "row"] $ do
+                div_ [class_ "col"] $ record (Just 11) $ do
+                    selectCustomerH
+                        "#customer-address"
+                        (Just "#view-control")
+                        customerAddressLink
+                        $ fromIntegral customers
+                    simpleField "Address"
+                        $ div_
+                            [ id_ "customer-address"
+                            ]
+                            mempty
+            transactionsElementH
