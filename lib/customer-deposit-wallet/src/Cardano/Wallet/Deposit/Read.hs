2f2d09e679ce83a995b837ff8efb01a1521a7dfa
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-10-07 13:48:32 +0200

    Adapt to latest cardano-wallet-agda

--- lib/customer-deposit-wallet/src/Cardano/Wallet/Deposit/Read.hs
+++ lib/customer-deposit-wallet/src/Cardano/Wallet/Deposit/Read.hs
@@ -12,7 +12,6 @@ module Cardano.Wallet.Deposit.Read
     , Read.EraValue (..)
     , Read.Conway
 
-    , Network (..)
     , Read.SlotNo
     , Read.ChainPoint (..)
     , Slot
@@ -50,6 +49,9 @@ module Cardano.Wallet.Deposit.Read
     , GenesisData
     , GenesisHash
 
+    , Read.NetworkId (Read.Mainnet, Read.Testnet)
+    , Read.getNetworkId
+
     -- * Dummy Values useful for testing
     , dummyAddress
     ) where
@@ -83,7 +85,6 @@ import Data.Word
     ( Word8
     )
 
-import qualified Cardano.Chain.Genesis as Byron
 import qualified Cardano.Wallet.Read as Read
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as B8
@@ -93,7 +94,6 @@ import qualified Data.ByteString.Short as SBS
     Type definitions
     with dummies
 ------------------------------------------------------------------------------}
-data Network = Testnet | Mainnet
 
 -- | Synonym for readability.
 -- The ledger specifications define @Addr@.
@@ -143,11 +143,3 @@ mockNextBlock old txs =
     slotNumber = case old of
         Read.GenesisPoint -> Read.SlotNo 0
         Read.BlockPoint{slotNo = n} -> succ n
-
-{-----------------------------------------------------------------------------
-    Genesis
-------------------------------------------------------------------------------}
-
--- GenesisData is not part of the ledger specification proper
-type GenesisData = Byron.GenesisData
-type GenesisHash = Byron.GenesisHash
