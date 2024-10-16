aab2a182627103bacd3ed3bfb076c16d06a7ec30
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-10-16 16:20:39 +0200

    Add `fromAnyCardanoEra` to convert to `Read`

---
+++ lib/primitive/lib/Cardano/Wallet/Primitive/Ledger/Read/Eras.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE GADTs #-}
+-- |
+-- Copyright: © 2024 Cardano Foundation
+-- License: Apache-2.0
+--
+-- Convert from Cardano.Api to Cardano.Wallet.Read.
+--
+module Cardano.Wallet.Primitive.Ledger.Read.Eras
+    ( fromAnyCardanoEra
+    )
+    where
+
+import Cardano.Api
+    ( AnyCardanoEra (..)
+    , CardanoEra (..)
+    )
+
+import qualified Cardano.Wallet.Read as Read
+
+-- | Convert an era from 'Cardano.Api' to 'Read'.
+fromAnyCardanoEra :: AnyCardanoEra -> Read.EraValue Read.Era
+fromAnyCardanoEra (AnyCardanoEra era) =
+    case era of
+        ByronEra -> Read.EraValue Read.Byron
+        ShelleyEra -> Read.EraValue Read.Shelley
+        AllegraEra -> Read.EraValue Read.Allegra
+        MaryEra -> Read.EraValue Read.Mary
+        AlonzoEra -> Read.EraValue Read.Alonzo
+        BabbageEra -> Read.EraValue Read.Babbage
+        ConwayEra -> Read.EraValue Read.Conway
