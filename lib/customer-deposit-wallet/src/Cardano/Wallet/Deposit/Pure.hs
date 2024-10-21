905e18ef2007f66a619a235916ce90795f387cfe
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-10-21 18:18:48 +0200

    Implement `createPayment`, pure

--- lib/customer-deposit-wallet/src/Cardano/Wallet/Deposit/Pure.hs
+++ lib/customer-deposit-wallet/src/Cardano/Wallet/Deposit/Pure.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Cardano.Wallet.Deposit.Pure
@@ -38,7 +39,9 @@ module Cardano.Wallet.Deposit.Pure
     , getTxHistoryByTime
 
       -- ** Writing to the blockchain
+    , ErrCreatePayment (..)
     , createPayment
+
     , BIP32Path (..)
     , DerivationType (..)
     , getBIP32PathsForOwnedInputs
@@ -93,6 +96,15 @@ import Cardano.Wallet.Deposit.Read
     , TxId
     , WithOrigin
     )
+import Control.Monad.Trans.Except
+    ( runExceptT
+    )
+import Data.Bifunctor
+    ( first
+    )
+import Data.Digest.CRC32
+    ( crc32
+    )
 import Data.Foldable
     ( fold
     , foldl'
@@ -127,6 +139,8 @@ import qualified Cardano.Wallet.Deposit.Pure.UTxO as UTxO
 import qualified Cardano.Wallet.Deposit.Pure.UTxO.UTxOHistory as UTxOHistory
 import qualified Cardano.Wallet.Deposit.Read as Read
 import qualified Cardano.Wallet.Deposit.Write as Write
+import qualified Cardano.Wallet.Read.Hash as Hash
+import qualified Control.Monad.Random.Strict as Random
 import qualified Data.Delta as Delta
 import qualified Data.List as L
 import qualified Data.Map.Strict as Map
@@ -139,11 +153,18 @@ type Customer = Address.Customer
 
 data WalletState = WalletState
     { walletTip :: Read.ChainPoint
+    -- ^ The wallet includes information from all blocks until
+    -- and including this one.
     , addresses :: !Address.AddressState
+    -- ^ Addresses and public keys known to this wallet.
     , utxoHistory :: !UTxOHistory.UTxOHistory
+    -- ^ UTxO of this wallet, with support for rollbacks.
     , txHistory :: !TxHistory
+    -- ^ (Summarized) transaction history of this wallet.
     , submissions :: Sbm.TxSubmissions
+    -- ^ Queue of pending transactions.
     , rootXSignKey :: Maybe XPrv
+    -- ^ Maybe a private key for signing transactions.
     -- , info :: !WalletInfo
     }
 
@@ -342,15 +363,99 @@ wonders interval =
 
 {-----------------------------------------------------------------------------
     Operations
-    Writing to blockchain
+    Constructing transactions
 ------------------------------------------------------------------------------}
+data ErrCreatePayment
+    = ErrCreatePaymentNotRecentEra (Read.EraValue Read.Era)
+    | ErrCreatePaymentBalanceTx (Write.ErrBalanceTx Write.Conway)
+    deriving (Eq, Show)
 
+-- | Create a payment to a list of destinations.
 createPayment
-    :: [(Address, Write.Value)] -> WalletState -> Maybe Write.TxBody
-createPayment = undefined
+    :: Read.EraValue Read.PParams
+    -> Write.TimeTranslation
+    -> [(Address, Write.Value)]
+    -> WalletState
+    -> Either ErrCreatePayment Write.Tx
+createPayment (Read.EraValue (Read.PParams pparams :: Read.PParams era)) a b w =
+    case Read.theEra :: Read.Era era of
+        Read.Conway ->
+            first ErrCreatePaymentBalanceTx
+                $ createPaymentConway pparams a b w
+        era' -> Left $ ErrCreatePaymentNotRecentEra (Read.EraValue era')
 
--- needs balanceTx
--- needs to sign the transaction
+-- | In the Conway era: Create a payment to a list of destinations.
+createPaymentConway
+    :: Write.PParams Write.Conway
+    -> Write.TimeTranslation
+    -> [(Address, Write.Value)]
+    -> WalletState
+    -> Either (Write.ErrBalanceTx Write.Conway) Write.Tx
+createPaymentConway pparams timeTranslation destinations w =
+    fmap (Read.Tx . fst)
+    . flip Random.evalRand (pilferRandomGen w)
+    . runExceptT
+    . balance
+        (availableUTxO w)
+        (addresses w)
+    . mkPartialTx
+    $ paymentTxBody
+  where
+    paymentTxBody :: Write.TxBody
+    paymentTxBody = Write.TxBody
+        { spendInputs = mempty
+        , collInputs = mempty
+        , txouts =
+            Map.fromList $ zip [(toEnum 0)..] $ map (uncurry Write.mkTxOut) destinations
+        , collRet = Nothing
+        }
+
+    mkPartialTx :: Write.TxBody -> Write.PartialTx Write.Conway
+    mkPartialTx txbody = Write.PartialTx
+        { tx = Read.unTx $ Write.mkTx txbody
+        , extraUTxO = mempty :: Write.UTxO Write.Conway
+        , redeemers = mempty
+        , stakeKeyDeposits = Write.StakeKeyDepositMap mempty
+        , timelockKeyWitnessCounts = Write.TimelockKeyWitnessCounts mempty
+        }
+
+    balance utxo addressState =
+        Write.balanceTx
+            pparams
+            timeTranslation
+            Write.AllKeyPaymentCredentials
+            (Write.constructUTxOIndex $ Write.toConwayUTxO utxo)
+            (changeAddressGen addressState)
+            ()
+
+    changeAddressGen s = Write.ChangeAddressGen
+        { Write.genChangeAddress =
+            first Read.decompactAddr . Address.newChangeAddress s
+        , Write.maxLengthChangeAddress =
+            Read.decompactAddr $ Address.mockMaxLengthChangeAddress s
+        }
+
+-- | Use entropy contained in the current 'WalletState'
+-- to construct a pseudorandom seed.
+-- (NOT a viable source of cryptographic randomness.)
+--
+-- Possible downsides of this approach:
+--
+-- 1. security/privacy
+-- 2. concurrency
+-- 3. retries for different coin selections
+pilferRandomGen :: WalletState -> Random.StdGen
+pilferRandomGen =
+    Random.mkStdGen . fromEnum . fromChainPoint . walletTip
+  where
+    fromChainPoint (Read.GenesisPoint) = 0
+    fromChainPoint (Read.BlockPoint _ headerHash) =
+        crc32 $ Hash.hashToBytes headerHash
+
+{-----------------------------------------------------------------------------
+    Operations
+    Signing transactions
+------------------------------------------------------------------------------}
 
 getBIP32PathsForOwnedInputs :: Write.Tx -> WalletState -> [BIP32Path]
 getBIP32PathsForOwnedInputs tx w =
@@ -373,6 +478,11 @@ getBIP32Paths w =
 
 signTx :: Write.Tx -> WalletState -> Maybe Write.Tx
 signTx _tx _w = undefined
+
+{-----------------------------------------------------------------------------
+    Operations
+    Pending transactions
+------------------------------------------------------------------------------}
 
 addTxSubmission :: Write.Tx -> WalletState -> WalletState
 addTxSubmission _tx _w = undefined
