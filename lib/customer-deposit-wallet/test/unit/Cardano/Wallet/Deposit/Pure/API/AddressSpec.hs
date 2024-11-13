df1723e605abec1c34903193102013863cfcebb7
Johannes Lund <anviking@me.com>
2024-11-13 20:56:59 +0100

    Test `decodeAddress text = Right addr ==> encodeAddress addr == text`

--- lib/customer-deposit-wallet/test/unit/Cardano/Wallet/Deposit/Pure/API/AddressSpec.hs
+++ lib/customer-deposit-wallet/test/unit/Cardano/Wallet/Deposit/Pure/API/AddressSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE QuasiQuotes #-}
 module Cardano.Wallet.Deposit.Pure.API.AddressSpec
     ( spec
     )
@@ -10,8 +11,25 @@ import Cardano.Wallet.Deposit.Pure.API.Address
     , decodeAddress
     , encodeAddress
     )
+import Cardano.Wallet.Read.Address
+    ( toShortByteString
+    )
 import Control.Monad
     ( forM_
+    )
+import Data.ByteString.Base58
+    ( bitcoinAlphabet
+    , encodeBase58
+    )
+import Data.Either
+    ( isLeft
+    , isRight
+    )
+import Data.Function
+    ( (&)
+    )
+import Data.Text
+    ( Text
     )
 import Test.Cardano.Ledger.Core.Arbitrary
     ()
@@ -23,9 +41,21 @@ import Test.Hspec
     )
 import Test.QuickCheck
     ( Arbitrary (..)
+    , Gen
+    , checkCoverage
+    , cover
+    , elements
     , forAll
+    , label
+    , oneof
+    , property
     , (===)
     )
+
+import qualified Codec.Binary.Bech32 as Bech32
+import qualified Codec.Binary.Bech32.TH as Bech32
+import qualified Data.ByteString.Short as B8
+import qualified Data.Text.Encoding as T
 
 spec :: Spec
 spec = do
@@ -33,6 +63,25 @@ spec = do
         it "rountrips correctly on random addresses" $ forAll arbitrary $ \x ->
             decodeAddress (encodeAddress x)
                 === Right x
+
+        it "decodeAddress text = Right addr ==> encodeAddress addr == text"
+            $ checkCoverage $ forAll genArbitrarilyEncodedAddress $ \text -> do
+                let getErrorLabel e = case e of
+                        InvalidBech32Encoding _e -> "invalid bech32 encoding"
+                        InvalidBase58Encoding -> "invalid base58 encoding"
+                        InvalidHumanReadablePart _hrp -> "invalid hrp"
+                        InvalidDataPart _ -> "invalid data part"
+                        AddressFlavorMismatch -> "flavor mismatch"
+                        AddressDecodingError _ -> "decoding error"
+                        AddressNetworkMismatch -> "network mismatch"
+
+                let res = decodeAddress text
+                case res of
+                    Right addr -> label "success" $ encodeAddress addr === text
+                    Left e -> label (getErrorLabel e) $ property True
+                    & cover 0.2 (isLeft res) "failure"
+                    & cover 0.2 (isRight res) "success"
+
         it "roundtrips correctly on some addresses from online examples"
             $ do
                 let testCases =
@@ -48,3 +97,27 @@ spec = do
             let secretlyMainnetAddr = "addr_test1z92l7rnra7sxjn5qv5fzc4fwsrrm29mgkleqj9a0y46j5lyjz4gwd3njhyqwntdkcm8rrgapudajydteywgtuvl6etjshn59kk"
             decodeAddress secretlyMainnetAddr
                 `shouldBe` Left AddressNetworkMismatch
+
+-- | Generate 'Text' heavily biased towards values of incorrectly encoded
+-- addresses
+genArbitrarilyEncodedAddress :: Gen Text
+genArbitrarilyEncodedAddress = oneof
+    [ encodeAddrBech32 <$> genAddrHrp <*> arbitrary
+    , encodeAddrBase58 <$> arbitrary
+    ]
+  where
+    encodeAddrBech32 hrp addr = Bech32.encodeLenient hrp dataPart
+      where
+        bytes = B8.fromShort $ toShortByteString addr
+        dataPart = Bech32.dataPartFromBytes bytes
+
+    genAddrHrp = elements
+        [ [Bech32.humanReadablePart|addr|]
+        , [Bech32.humanReadablePart|addr_test|]
+        , [Bech32.humanReadablePart|notaddr|]
+        ]
+
+    encodeAddrBase58 = T.decodeUtf8
+        . encodeBase58 bitcoinAlphabet
+        . B8.fromShort
+        . toShortByteString
