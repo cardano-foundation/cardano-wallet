821722fc0b7d1dc1f4b803ed00778a922b591b1b
Pawel Jakubas <pawel.jakubas@iohk.io>
2024-11-27 19:21:33 +0100

    polish helper of command

--- lib/signing/src/Command/Deriving.hs
+++ lib/signing/src/Command/Deriving.hs
@@ -48,6 +48,9 @@ import Options.Applicative
     )
 import Options.Applicative.Help.Pretty
     ( Doc
+    , annotate
+    , bold
+    , indent
     , pretty
     , vsep
     )
@@ -58,6 +61,7 @@ import System.IO
 import System.IO.Extra
     ( hGetBytes
     , hPutString
+    , progName
     )
 import Text.Read
     ( readEither
@@ -74,11 +78,18 @@ mod :: (Cmd -> parent) -> Mod CommandFields parent
 mod liftCmd = command "derive" $
     info (helper <*> fmap liftCmd parser) $ mempty
         <> progDesc "Derive payment public/signing keys of account public key for a given index"
-        <> header "Deliver payment credential keys: public key with and without chain code and private key extended and signing"
+        <> header "Deliver payment credential keys: public key, with and without chain code, respective public key hash and corresponding extended and signing private keys"
         <> footerDoc (Just $ vsep
             [ prettyText "The extended account private key is read from stdin and credential index is taken as an argument."
             , prettyText ""
             , prettyText "Example:"
+            , indent 2 $ annotate bold $ prettyText "cat acct.xsk"
+            , indent 2 $ prettyText "acct_xsk1mqqrxjztq35xkjzvx8lw730xnxf8lyrgd8fmtrk588fetal8wfvc37fnv7udef5uuktx7lxw3c3msnjmma20k823vy4z9qmclj4352uzq8r72xw2d77l9jafnzr4zjpl9jk7xkeewwq90nweh4q95rd5cud5269p"
+            , indent 2 $ annotate bold $ pretty $ progName <>" derive --index 0 <<< $(cat acct.xsk)"
+            , indent 2 $ prettyText "xprv: 600d9fc48ef9c6249a6d8190762bdad8439a2c2d13cdd1661878023a06e8725993e19505261ca08fa403d91c975d6e842164fc0614fabbeb998bcb1f71001e07ff273a153a3905108dbb490112d4e3740ec5a3d206423188523bee1c0e8a2c17"
+            , indent 2 $ prettyText "prv: 600d9fc48ef9c6249a6d8190762bdad8439a2c2d13cdd1661878023a06e8725993e19505261ca08fa403d91c975d6e842164fc0614fabbeb998bcb1f71001e07"
+            , indent 2 $ prettyText "xpub: cc1c6c947f4245d297a7fe9f34bffe08fb6f25322057cc3eb87556fdbca5eaa9ff273a153a3905108dbb490112d4e3740ec5a3d206423188523bee1c0e8a2c17"
+            , indent 2 $ prettyText "pub: cc1c6c947f4245d297a7fe9f34bffe08fb6f25322057cc3eb87556fdbca5eaa9"
             ])
   where
     parser = Deriving
@@ -109,4 +120,4 @@ run Deriving{derivationIndex} = do
         "xprv: " <> T.unpack (toHex extendedPrivate) <> "\n" <>
         "prv: " <> T.unpack (toHex private) <> "\n" <>
         "xpub: " <> T.unpack (toHex extendedPublic) <> "\n" <>
-        "pub: " <> T.unpack (toHex public) <> "\n"
+        "pub: " <> T.unpack (toHex public)
