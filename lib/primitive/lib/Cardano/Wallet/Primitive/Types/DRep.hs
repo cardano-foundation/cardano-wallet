667512caf9e116bfa6eb792908907cb856c8d130
Pawel Jakubas <pawel.jakubas@iohk.io>
2024-12-10 16:28:10 +0100

    Primitive.Types.DRep adjusted

--- lib/primitive/lib/Cardano/Wallet/Primitive/Types/DRep.hs
+++ lib/primitive/lib/Cardano/Wallet/Primitive/Types/DRep.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BinaryLiterals #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
@@ -9,10 +10,8 @@ module Cardano.Wallet.Primitive.Types.DRep
     , DRepKeyHash (..)
     , DRepScriptHash (..)
     , DRep (..)
-    , encodeDRepKeyHashBech32
-    , decodeDRepKeyHashBech32
-    , encodeDRepScriptHashBech32
-    , decodeDRepScriptHashBech32
+    , encodeDRepIDBech32
+    , decodeDRepIDBech32
     )
 where
 
@@ -20,6 +19,9 @@ import Prelude
 
 import Control.DeepSeq
     ( NFData
+    )
+import Data.Bifunctor
+    ( first
     )
 import Data.ByteString
     ( ByteString
@@ -32,6 +34,9 @@ import Data.Text.Class
     , TextDecodingError (TextDecodingError)
     , ToText (..)
     )
+import Data.Word
+    ( Word8
+    )
 import Fmt
     ( Buildable (..)
     )
@@ -41,12 +46,15 @@ import GHC.Generics
 
 import qualified Codec.Binary.Bech32 as Bech32
 import qualified Codec.Binary.Bech32.TH as Bech32
+import qualified Data.ByteString as BS
 
+-- | This is raw key hash credential
 newtype DRepKeyHash = DRepKeyHash { getDRepKeyHash :: ByteString }
     deriving (Generic, Eq, Ord, Show)
 
 instance NFData DRepKeyHash
 
+-- | This is raw script hash credential
 newtype DRepScriptHash = DRepScriptHash { getDRepScriptHash :: ByteString }
     deriving (Generic, Eq, Ord, Show)
 
@@ -57,58 +65,57 @@ data DRepID =
     deriving (Eq, Generic, Show, Ord)
     deriving anyclass NFData
 
--- | Encode 'DRepKeyHash' as Bech32 with "drep" hrp.
-encodeDRepKeyHashBech32 :: DRepKeyHash -> Text
-encodeDRepKeyHashBech32 =
+-- | Encode 'DRepID' as Bech32 with "drep" hrp.
+encodeDRepIDBech32 :: DRepID -> Text
+encodeDRepIDBech32 drepid =
     Bech32.encodeLenient hrp
         . Bech32.dataPartFromBytes
-        . getDRepKeyHash
+        $ appendCip0129BytePrefix
   where
     hrp = [Bech32.humanReadablePart|drep|]
+    appendCip0129BytePrefix = case drepid of
+        DRepFromKeyHash (DRepKeyHash payload) ->
+            -- according to CIP-0129 (https://github.com/cardano-foundation/CIPs/tree/master/CIP-0129)
+            -- drep    0010....
+            -- keyhash ....0010
+            let fstByte = 0b00100010 :: Word8
+            in BS.cons fstByte payload
+        DRepFromScriptHash (DRepScriptHash payload) ->
+            -- according to CIP-0129 (https://github.com/cardano-foundation/CIPs/tree/master/CIP-0129)
+            -- drep       0010....
+            -- scripthash ....0011
+            let fstByte = 0b00100011 :: Word8
+            in BS.cons fstByte payload
 
--- | Decode a Bech32 encoded 'DRepKeyHash'.
-decodeDRepKeyHashBech32 :: Text -> Either TextDecodingError DRepKeyHash
-decodeDRepKeyHashBech32 t =
+-- | Decode a Bech32 encoded 'DRepID'.
+decodeDRepIDBech32 :: Text -> Either TextDecodingError DRepID
+decodeDRepIDBech32 t =
     case fmap Bech32.dataPartToBytes <$> Bech32.decodeLenient t of
         Left _ -> Left textDecodingError
         Right (hrp', Just bytes)
-            | hrp' == hrp -> Right $ DRepKeyHash bytes
+            | hrp' == hrp ->
+              let (fstByte, payload) = first BS.head $ BS.splitAt 1 bytes
+              in case fstByte of
+                  0b00100010 ->
+                      Right $ DRepFromKeyHash (DRepKeyHash payload)
+                  0b00100011 ->
+                      Right $ DRepFromScriptHash (DRepScriptHash payload)
+                  _ ->
+                      Left textFirstByteError
         Right _ -> Left textDecodingError
       where
         textDecodingError = TextDecodingError $ unwords
             [ "Invalid DRep key hash: expecting a Bech32 encoded value"
             , "with human readable part of 'drep'."
             ]
+        textFirstByteError = TextDecodingError $ unwords
+            [ "Invalid DRep metadata: expecting a byte '00100010' value for key hash or"
+            , "a byte '0b00100011' value for script hash."
+            ]
         hrp = [Bech32.humanReadablePart|drep|]
 
--- | Encode 'DRepScriptHash' as Bech32 with "drep_script" hrp.
-encodeDRepScriptHashBech32 :: DRepScriptHash -> Text
-encodeDRepScriptHashBech32 =
-    Bech32.encodeLenient hrp
-        . Bech32.dataPartFromBytes
-        . getDRepScriptHash
-  where
-    hrp = [Bech32.humanReadablePart|drep_script|]
-
--- | Decode a Bech32 encoded 'DRepScriptHash'.
-decodeDRepScriptHashBech32 :: Text -> Either TextDecodingError DRepScriptHash
-decodeDRepScriptHashBech32 t =
-    case fmap Bech32.dataPartToBytes <$> Bech32.decodeLenient t of
-        Left _ -> Left textDecodingError
-        Right (hrp', Just bytes)
-            | hrp' == hrp -> Right $ DRepScriptHash bytes
-        Right _ -> Left textDecodingError
-      where
-        textDecodingError = TextDecodingError $ unwords
-            [ "Invalid DRep Script hash: expecting a Bech32 encoded value"
-            , "with human readable part of 'drep_script'."
-            ]
-        hrp = [Bech32.humanReadablePart|drep_script|]
-
 instance Buildable DRepID where
-    build = \case
-        DRepFromKeyHash keyhash -> build $ encodeDRepKeyHashBech32 keyhash
-        DRepFromScriptHash scripthash -> build $ encodeDRepScriptHashBech32 scripthash
+    build = build . encodeDRepIDBech32
 
 -- | A decentralized representation ('DRep') will
 -- vote on behalf of the stake delegated to it.
@@ -122,26 +129,16 @@ data DRep
 instance ToText DRep where
     toText Abstain = "abstain"
     toText NoConfidence = "no_confidence"
-    toText (FromDRepID (DRepFromKeyHash keyhash)) =
-        encodeDRepKeyHashBech32 keyhash
-    toText (FromDRepID (DRepFromScriptHash scripthash)) =
-        encodeDRepScriptHashBech32 scripthash
+    toText (FromDRepID drepid) = encodeDRepIDBech32 drepid
 
 instance FromText DRep where
     fromText txt = case txt of
         "abstain" -> Right Abstain
         "no_confidence" -> Right NoConfidence
-        _ -> case decodeDRepKeyHashBech32 txt of
-                Right keyhash ->
-                     Right $ FromDRepID $ DRepFromKeyHash keyhash
-                Left _ -> case decodeDRepScriptHashBech32 txt of
-                    Right scripthash ->
-                        Right $ FromDRepID $ DRepFromScriptHash scripthash
-                    Left _ -> Left $ TextDecodingError $ unwords
-                        [ "I couldn't parse the given decentralized representative (DRep)."
-                        , "I am expecting either 'abstain', 'no_confidence'"
-                        , "or bech32 encoded drep having prefixes: 'drep'"
-                        , "or 'drep_script'."]
+        _ -> case decodeDRepIDBech32 txt of
+                Right drepid ->
+                     Right $ FromDRepID drepid
+                Left err -> Left err
 
 instance Buildable DRep where
     build = \case
