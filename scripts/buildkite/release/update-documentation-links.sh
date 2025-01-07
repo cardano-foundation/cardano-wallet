0ab00451831b7bbbddde89ae001813ad44d48796
paolino <paolo.veronelli@gmail.com>
2025-01-07 10:05:04 +0000

    Add update-documentation-links.sh script to fix the release links in gh-pages branch

---
+++ scripts/buildkite/release/update-documentation-links.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+set -euox pipefail
+
+RELEASE_VERSION=$(buildkite-agent meta-data get release-version)
+GH_PAGES_BRANCH="gh-pages"
+
+git checkout "${GH_PAGES_BRANCH}"
+git pull origin "${GH_PAGES_BRANCH}"
+cd releases
+./make_redirects.sh "${RELEASE_VERSION}"
+git remote set-url origin "https://$GITHUB_TOKEN@github.com/cardano-foundation/cardano-wallet.git"
+git push origin "${GH_PAGES_BRANCH}"
