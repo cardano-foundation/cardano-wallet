c1a71faa66a76c7baf5f7cc8c4ee2c89013f0bc7
paolino <paolo.veronelli@gmail.com>
2025-01-13 11:59:55 +0000

    Add 'latest' tag push in case of release

--- scripts/buildkite/release/push-to-dockerhub.sh
+++ scripts/buildkite/release/push-to-dockerhub.sh
@@ -5,6 +5,7 @@ set -euox pipefail
 TRIGGERED_BY=$(buildkite-agent meta-data get base-build)
 NEW_GIT_TAG=$(buildkite-agent meta-data get release-version)
 TEST_RC=$(buildkite-agent meta-data get test-rc)
+CABAL_VERSION=$(buildkite-agent meta-data get release-cabal-version)
 
 if [ "$RELEASE" == "false" ]; then
     if [ "$TEST_RC" == "TRUE" ]; then
@@ -23,6 +24,8 @@ main_build=$(curl -H "Authorization: Bearer $BUILDKITE_API_TOKEN" \
 
 mkdir -p artifacts
 
+repo="cardano-foundation/cardano-wallet"
+
 artifact() {
     local artifact_name=$1
     # shellcheck disable=SC2155
@@ -33,10 +36,19 @@ artifact() {
     curl -H "Authorization: Bearer $BUILDKITE_API_TOKEN" -L \
         -o "artifacts/$artifact_name" \
         "$artifact_value"
-    local image_name="cardanofoundation/cardano-wallet:$TAG"
     docker login -u cfhal -p "$DOCKER_HUB_TOKEN"
     docker load -i "artifacts/$artifact_name"
-    docker push "$image_name"
+    local image_name="$repo:$TAG"
+    if [ "$RELEASE" == "false" ]; then
+        local loaded_image_name="$repo:$CABAL_VERSION"
+        docker tag "$loaded_image_name" "$image_name"
+        docker push "$image_name"
+    else
+        local latest_image_name="$repo:latest"
+        docker push "$image_name"
+        docker tag "$image_name" "$latest_image_name"
+        docker push "$latest_image_name"
+    fi
 }
 
 artifact "cardano-wallet-$NEW_GIT_TAG-docker-image.tgz"
