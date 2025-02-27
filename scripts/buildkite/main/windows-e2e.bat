e409fa2c1602d889e35244abd5473b81098247c2
Johannes Lund <anviking@me.com>
2025-02-27 16:09:38 +0100

    Use buildkite-agent to download the testing bundle

--- scripts/buildkite/main/windows-e2e.bat
+++
@@ -1,7 +0,0 @@
-set PATH=%PATH%;C:\Users\hal\AppData\Local\Microsoft\WinGet\Links
-
-cd test\e2e
-call bundle install
-call bundle exec rake get_latest_windows_tests[%BUILDKITE_BRANCH%,bins,any,latest]
-bins\cardano-wallet-integration-test-e2e.exe
-exit /b %ERRORLEVEL%
