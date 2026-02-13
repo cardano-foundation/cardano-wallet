c6892571abe8434e99db6d327da234236077f1e4
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-02-13 10:26:21 +0000

    refactor: move scripts/buildkite/ to scripts/ci/

--- scripts/release/release-candidate.sh
+++ scripts/release/release-candidate.sh
@@ -80,7 +80,7 @@ sed -i "s|WALLET_TAG=.*|WALLET_TAG=$NEW_CABAL_VERSION|g" README.md
 sed -i "s|WALLET_VERSION=.*|WALLET_VERSION=$NEW_GIT_TAG|g" README.md
 git commit -am "Update cardano-wallet version in README.md"
 
-sed -i "s|$OLD_GIT_TAG|$NEW_GIT_TAG|g" scripts/buildkite/main/ruby-e2e.sh
+sed -i "s|$OLD_GIT_TAG|$NEW_GIT_TAG|g" scripts/ci/ruby-e2e.sh
 git commit -am "Update cardano-wallet version in ruby-e2e.sh"
 
 sed -i "s|RELEASE_WALLET_TAG=.*|RELEASE_WALLET_TAG=$NEW_CABAL_VERSION|g" run/common/docker/run.sh
