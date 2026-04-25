83f701c2adfac593a61b9028d83ba7993d9b52cb
Github Action <gha@cardanofoundation.org>
2026-04-25 05:59:23 +0200

    Update cardano-wallet version in run/common/docker/run.sh

--- run/common/docker/run.sh
+++ run/common/docker/run.sh
@@ -24,7 +24,7 @@ fi
 source .env
 
 # Define and export wallet and node version tags
-RELEASE_WALLET_TAG=2026.4.17
+RELEASE_WALLET_TAG=2026.4.25
 
 WALLET_TAG=${WALLET_TAG:=$RELEASE_WALLET_TAG}
 export WALLET_TAG
