6922f3cf7b0281540c25cd09ec45695871cd8194
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-07-16 12:03:50 +0100

    test: deduplicate applyNM property assertions
    Extract the shared iterate and unit assertion bodies into
    prop_applyNM_iterate_with and prop_applyNM_unit_with over a plain
    Kleisli function; the generic and bounded-list variants become
    one-line adapters.

--- lib/unit/test/unit/Control/Monad/UtilSpec.hs
+++ lib/unit/test/unit/Control/Monad/UtilSpec.hs
@@ -83,10 +83,8 @@ prop_applyNM_iterate
     -> Fun a (m a)
     -> a
     -> Property
-prop_applyNM_iterate (getNonNegative -> n) (applyFun -> f) a =
-    applyNM n f a === applyNM_iterate n f a
-  where
-    applyNM_iterate n' f' = (!! n') . iterate (f' =<<) . pure
+prop_applyNM_iterate (getNonNegative -> n) (applyFun -> f) =
+    prop_applyNM_iterate_with n f
 
 prop_applyNM_iterate_list
     :: Fun Int (Maybe (Either Int (Int, Int)))
@@ -94,25 +92,27 @@ prop_applyNM_iterate_list
     -> Property
 prop_applyNM_iterate_list (applyFun -> boundedF) a =
     forAll (chooseInt (0, 5)) $ \n ->
-        applyNM n f a === applyNM_iterate n f a
+        prop_applyNM_iterate_with n (boundedList . boundedF) a
+
+prop_applyNM_iterate_with
+    :: (Monad m, Eq (m a), Show (m a)) => Int -> (a -> m a) -> a -> Property
+prop_applyNM_iterate_with n f a =
+    applyNM n f a === applyNM_iterate n f a
   where
-    f = boundedList . boundedF
     applyNM_iterate n' f' = (!! n') . iterate (f' =<<) . pure
 
 prop_applyNM_unit
     :: (Monad m, Eq (m a), Show (m a)) => Fun a (m a) -> a -> Property
-prop_applyNM_unit (applyFun -> f) a =
-    conjoin
-        [ applyNM 0 f a === pure a
-        , applyNM 1 f a === f a
-        , applyNM 2 f a === (f <=< f) a
-        , applyNM 3 f a === (f <=< f <=< f) a
-        , applyNM 4 f a === (f <=< f <=< f <=< f) a
-        ]
+prop_applyNM_unit (applyFun -> f) = prop_applyNM_unit_with f
 
 prop_applyNM_unit_list
     :: Fun Int (Maybe (Either Int (Int, Int))) -> Int -> Property
-prop_applyNM_unit_list (applyFun -> boundedF) a =
+prop_applyNM_unit_list (applyFun -> boundedF) =
+    prop_applyNM_unit_with (boundedList . boundedF)
+
+prop_applyNM_unit_with
+    :: (Monad m, Eq (m a), Show (m a)) => (a -> m a) -> a -> Property
+prop_applyNM_unit_with f a =
     conjoin
         [ applyNM 0 f a === pure a
         , applyNM 1 f a === f a
@@ -120,8 +120,6 @@ prop_applyNM_unit_list (applyFun -> boundedF) a =
         , applyNM 3 f a === (f <=< f <=< f) a
         , applyNM 4 f a === (f <=< f <=< f <=< f) a
         ]
-  where
-    f = boundedList . boundedF
 
 boundedList :: Maybe (Either a (a, a)) -> [a]
 boundedList Nothing = []
